99b13c0ae01b487ed6017a6be997bc38ca11c064
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2002-08-08 08:41:20 +0000

    Moved location of ACCESSION and RELEASES dirs

--- trunk/Rfam/CVSimport/Modules/Rfam.pm
+++ trunk/Rfam/CVSimport/Modules/Rfam.pm
@@ -7,19 +7,54 @@ package Rfam;
 
 use strict;
 
-use vars qw( $root_dir
+use vars qw( @ISA
+	     @EXPORT
+	     $root_dir
 	     $current_dir
 	     $accession_dir
 	     $releases_dir
+	     $rcs_master_dir
+	     $rcs_attic_dir
 	     $scripts_dir
-	     $acclog_file );
+	     $acclog_file 
+	     @view_file_set
+             @align_file_set
+             @model_file_set
+	     @ann_file_set
+             @rcs_file_set 
+	     @optional_file_set );
 
-$root_dir      = "/nfs/farm/Rfam";
-$current_dir   = "$root_dir/CURRENT";
-$accession_dir = "$root_dir/ACCESSION";
-$releases_dir  = "$root_dir/RELEASES";
-$scripts_dir   = "/pfam/db/Rfam/scripts";
-$acclog_file   = "$accession_dir/acclog";
+@ISA    = qw( Exporter );
+@EXPORT = qw( $root_dir
+	      $current_dir
+	      $accession_dir 
+	      $releases_dir 
+	      $rcs_master_dir 
+	      $rcs_attic_dir 
+	      $scripts_dir    
+	      $acclog_file
+	      @view_file_set
+	      @align_file_set
+	      @ann_file_set
+	      @model_file_set
+	      @rcs_file_set 
+              @optional_file_set );
+
+$root_dir       = "/nfs/farm/Rfam";
+$current_dir    = "$root_dir/CURRENT";
+$accession_dir  = "/pfam/db/Rfam/ACCESSION";
+$releases_dir   = "/pfam/db/Rfam/RELEASES";
+$rcs_master_dir = "$root_dir/RCS_MASTER";
+$rcs_attic_dir  = "$root_dir/RCS_ATTIC";
+$scripts_dir    = "/pfam/db/Rfam/scripts";
+$acclog_file    = "$accession_dir/acclog";
+
+@align_file_set    = ( "SEED", "ALIGN" );
+@view_file_set     = ( "SEED.ann", "ALIGN.ann" ); # must be in same order as @align_file_set
+@ann_file_set      = ( "DESC" );
+@model_file_set    = ( "CM" );
+@rcs_file_set      = ( @align_file_set, @ann_file_set, @model_file_set );
+@optional_file_set = ();
 
 
 sub get_allaccs {
