7f71e350c2305df9ce4149b9a5b5358b4cdb30ec
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2002-08-08 16:00:04 +0000

    Fixed wrt new set of cvs commits

--- trunk/Rfam/CVSimport/release/redoview.pl
+++ trunk/Rfam/CVSimport/release/redoview.pl
@@ -4,25 +4,16 @@ use strict;
 use lib '/pfam/db/Rfam/scripts/Modules';
 use Rfam;
 
-my $file = shift;
 my @accs;
 
-if( $file ) {
-    open( F, $file ) or die;
-    while(<F>) {
-	if( /^(\S+)\s*/ ) {
-	    push( @accs, $1 );
-	}
-    }
+if( @ARGV ) {
+    @accs = @ARGV;
 }
 else {
     @accs = &Rfam::get_allaccs();
 }
 
-chdir "$Rfam::current_dir" or die;
-
 foreach my $acc ( @accs ) {
-    my $id = Rfam::acc2id( $acc );
-    print "Doing $id ....\n";
-    system "$Rfam::scripts_dir/rfamrcs/makerfamview.pl $id" and die;
+    print "Doing $acc ....\n";
+    system "$view_maker $acc" and die;
 }
