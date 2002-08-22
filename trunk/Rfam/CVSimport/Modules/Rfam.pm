e8d10e1339f735d22f1b6d78857b2f2e34558f4f
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2002-08-22 10:44:12 +0000

    Added scores file to optional file list

--- trunk/Rfam/CVSimport/Modules/Rfam.pm
+++ trunk/Rfam/CVSimport/Modules/Rfam.pm
@@ -59,7 +59,7 @@ $acclog_file    = "$accession_dir/acclog";
 @output_file_set   = ( "OUTPUT" );
 @model_file_set    = ( "CM" );
 @rcs_file_set      = ( @align_file_set, @ann_file_set, @model_file_set, @output_file_set );
-@optional_file_set = ();
+@optional_file_set = ( "scores" );
 
 $view_maker = "$scripts_dir/rfamrcs/makerfamview.pl";
 
