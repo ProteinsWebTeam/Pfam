c40b77c4cfe2c7dc628c5547e6fead7b81083ace
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2002-11-12 17:43:54 +0000

    greatly simplified by removal of accession lines

--- trunk/Rfam/CVSimport/rfamrcs/makerfamview.pl
+++ trunk/Rfam/CVSimport/rfamrcs/makerfamview.pl
@@ -1,11 +1,12 @@
 #!/usr/local/bin/perl -w
 
 use strict;
-use lib '/nfs/disk100/pubseq/Pfam/bioperl';
-use Bio::SimpleAlign;
 use lib '/pfam/db/Rfam/scripts/Modules';
+use lib '/pfam/db/bioperl';
 use Rfam;
+use Bio::Rfam::RfamAlign;
 
+#my $current_dir = ".";
 my $acc = shift;
 chdir "$current_dir/$acc" or die;
 
@@ -19,33 +20,33 @@ while( <DESC> ) {
 close DESC;
 
 foreach my $file ( @align_file_set ) {
-    my $aln = new Bio::SimpleAlign;
+    my $aln = new Bio::Rfam::RfamAlign;
     open( ALN, $file ) or die;
-    $aln -> read_selex( \*ALN );
+    $aln -> read_stockholm( \*ALN );
     close ALN;
     my $numseq = scalar ( $aln -> eachSeq() );
 
-    my %accessions;
-    open( TEMP, ">temp.$$" ) or die;
-    foreach my $seq ( $aln -> eachSeq() ) {
-	my $id = $seq->id();
-	print TEMP "emblrelease:$id\n";
-    }
-    close ALN;
-    close TEMP;
+#    my %accessions;
+#    open( TEMP, ">temp.$$" ) or die;
+#    foreach my $seq ( $aln -> eachSeq() ) {
+#	my $id = $seq->id();
+#	print TEMP "emblrelease:$id\n";
+#    }
+#    close ALN;
+#    close TEMP;
 
-    my $id;
-    open( GETZ, "getz -f id -f acc \@temp.$$ |" ) or die;
-    while(<GETZ>) {
-	if( /^ID\s+(\S+)\s+/ ) {
-	    $id = $1;
-	}
-	if( /^AC\s+(\S+)\s*\;\s*/ ) {
-	    $accessions{$id} = $1 unless $accessions{$id};
-	}
-    }
-    close GETZ or die;
-    unlink "temp.$$" or die "can't remove temp.$$";
+#    my $id;
+#    open( GETZ, "getz -f id -f acc \@temp.$$ |" ) or die;
+#    while(<GETZ>) {
+#	if( /^ID\s+(\S+)\s+/ ) {
+#	    $id = $1;
+#	}
+#	if( /^AC\s+(\S+)\s*\;\s*/ ) {
+#	    $accessions{$id} = $1 unless $accessions{$id};
+#	}
+#    }
+#    close GETZ or die;
+#    unlink "temp.$$" or die "can't remove temp.$$";
 
     open( ALNOUT, ">$file.ann" ) or die;
 
@@ -63,40 +64,26 @@ foreach my $file ( @align_file_set ) {
 	    print ALNOUT "#=GF SQ   $numseq\n\n";
 	    $seen = 1;
 	}
-	if( /^((?:\#=GR )?(\S+)\/\d+-\d+\s+)(.*)\s*$/ ) {
-	    my( $nse, $id, $seq ) = ( $1, $2, $3 );
-	    my $length = length $nse;
-	    die "$id not found in your sequence database" if( not $id );
-	    $nse =~ s/$id/$accessions{$id}/g;
-	    $nse =~ s/\s+$//;
-	    my $format = "\%-".$length."s%s\n";
-	    printf ALNOUT ( "$format", $nse, $seq );
-	}
-	elsif( /^(\#=GS (\S+)\/\d+-\d+)\s+(.*)\s*$/ ) {
-	    my( $nse, $id, $rest ) = ( $1, $2, $3 );
-	    die "$id not found in your sequence database" if( not $id );
-	    $nse =~ s/$id/$accessions{$id}/g;
-	    printf ALNOUT ( "%-34s%s\n", $nse, $rest );
-	}
-	else {
+#	if( /^((?:\#=GR )?(\S+)\/\d+-\d+\s+)(.*)\s*$/ ) {
+#	    my( $nse, $id, $seq ) = ( $1, $2, $3 );
+#	    my $length = length $nse;
+#	    die "$id not found in your sequence database" if( not $id );
+#	    $nse =~ s/$id/$accessions{$id}/g;
+#	    $nse =~ s/\s+$//;
+#	    my $format = "\%-".$length."s%s\n";
+#	    printf ALNOUT ( "$format", $nse, $seq );
+#	}
+#	elsif( /^(\#=GS (\S+)\/\d+-\d+)\s+(.*)\s*$/ ) {
+#	    my( $nse, $id, $rest ) = ( $1, $2, $3 );
+#	    die "$id not found in your sequence database" if( not $id );
+#	    $nse =~ s/$id/$accessions{$id}/g;
+#	    printf ALNOUT ( "%-34s%s\n", $nse, $rest );
+#	}
+#	else {
 	    print ALNOUT;
-	}
+#	}
     }
     close REF or die;
     close ALNOUT;
 }
 
-#temporary hack
-
-open( SEEDOUT, ">SEED.$$" ) or die;
-open( SEED, "SEED.ann" ) or die;
-while(<SEED>) {
-    if( /SS_cons/ ) {
-	tr/\>\</\<\>/;
-    }
-    print SEEDOUT;
-}
-close SEED;
-close SEEDOUT;
-
-rename( "SEED.$$", "SEED.ann" ) or die;
