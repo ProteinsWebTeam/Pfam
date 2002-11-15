97f0587320b7842ec865fa87fac5678427e1180e
mm1 <mm1@98333f76-0502-11df-81af-936bbf3a1e26>
2002-11-15 14:33:15 +0000

    Updated

--- trunk/Rfam/CVSimport/rfamrdb/upload_embl_file.pl
+++ trunk/Rfam/CVSimport/rfamrdb/upload_embl_file.pl
@@ -11,10 +11,23 @@ require "/nfs/team71/pfam/mm1/rfam/scripts/Modules/Bio/Rfam.pm";
 
 use strict;
 
-my  $rdb = Bio::Rfam->rdb_update();
+my $database = shift;
+die "Need database to upload dopey!!! \n" if(!$database);
+
+my $rdb;
+
+if ($database =~ /rfam2/) {
+ # print "rfam2 \n";
+  $rdb = Bio::Rfam->switchover_rdb_update();
+} else {
+  print "rfam \n";
+  $rdb = Bio::Rfam->rdb_update();
+}
+
+#exit(0);
 print "RDB: $rdb \n";
 
 
-$rdb->add_rfamseq(  "/nfs/team71/pfam/mm1/scripts/Rfam/embl.dat");
+$rdb->add_rfamseq(  "/nfs/team71/pfam/mm1/RESTORES/Rfam/embl.dat");
 
 print "DONE \n";
