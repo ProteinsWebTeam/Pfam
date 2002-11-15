95e1dbd4f6589241f893ca517e9181ac99e03767
mm1 <mm1@98333f76-0502-11df-81af-936bbf3a1e26>
2002-11-15 14:32:20 +0000

    Updated!

--- trunk/Rfam/CVSimport/rfamrdb/load_rfam_into_rdb.pl
+++ trunk/Rfam/CVSimport/rfamrdb/load_rfam_into_rdb.pl
@@ -5,17 +5,29 @@ use lib '/nfs/WWWdev/SANGER_docs/cgi-bin/Rfam';
 use RfamWWWConfig;
 use EntryWWW;
 
-use lib '/nfs/disk56/mm1/rfam/scripts/Modules';
+use lib '/nfs/team71/pfam//mm1/rfam/scripts/Modules';
 
-require "/nfs/disk56/mm1/rfam/scripts/Modules/UpdateRDB.pm";
-require "/nfs/disk56/mm1/rfam/scripts/Modules/Bio/Rfam.pm";
+require "/nfs/team71/pfam/mm1/rfam/scripts/Modules/UpdateRDB.pm";
+require "/nfs/team71/pfam/mm1//rfam/scripts/Modules/Bio/Rfam.pm";
 
 #use UpdateRDB;
 #use Rfam;
 
 use strict;
 
-my @arr = qw(RF00001 RF00002 RF00003 RF00004 RF00005 RF00006 RF00007 RF00008 RF00009 RF00010 RF00011 RF00012 RF00013 RF00014 RF00015 RF00016 RF00017 RF00018 RF00019 RF00020 RF00021 RF00022 RF00023 RF00024 RF00025);
+my $database = shift;
+ my  $rdb;
+die "need database to upload dopey!! \n" if(!$database);
+if ($database =~ /rfam2/) {
+  $rdb  = Bio::Rfam->switchover_rdb_update();
+} else {
+  $rdb = Bio::Rfam->rdb_update();
+}
+
+print "RDB: $rdb \n\n";
+#exit(0);
+
+my @arr = qw(RF00001 RF00002 RF00003 RF00004 RF00005 RF00006 RF00007 RF00008 RF00009 RF00010 RF00011 RF00012 RF00013 RF00014 RF00015 RF00016 RF00017 RF00018 RF00019 RF00020 RF00021 RF00022 RF00023 RF00024 RF00025 RF00026 RF00027 RF00028 RF00029 RF00030);
 
 
 foreach (@arr) {
@@ -38,9 +50,10 @@ foreach (@arr) {
   
   
   
-  my  $rdb = Bio::Rfam->rdb_update();
+ 
+
   
-  print "RDB: $rdb \n\n";
+
 
 $rdb->check_in_Entry( $en );
 
