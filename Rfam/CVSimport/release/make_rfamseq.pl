9062fa52c044f0c3fb189897d1d854a823aca228
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2002-12-17 13:08:49 +0000

    Added -v option, some comments, and blast index making stuff

--- Rfam/CVSimport/release/make_rfamseq.pl
+++ Rfam/CVSimport/release/make_rfamseq.pl
@@ -12,19 +12,39 @@ BEGIN {
 use lib $bioperl_dir;
 
 use strict;
+use Getopt::Long;
 use Bio::SeqIO;
+
+my $verbose;
+&GetOptions( "v" => \$verbose );
 
 my $dir = shift;
 $dir = "." if not $dir;
 chdir "$dir" or die "can't cd to $dir";
 my @filelist = glob( "*.dat" );
 
+# convert embl format to fasta
+# we want to be keyed off accession rather than id so change disply_id
+
 foreach my $file ( @filelist ) {
     my( $root ) = $file =~ /^(\S+)\.dat/;
     my $in  = Bio::SeqIO->new( -file => $file, '-format' => 'EMBL' );
     my $out = Bio::SeqIO->new( -file => ">$root.fa", '-format' => 'Fasta' );
     while ( my $seq = $in->next_seq() ) {
+	print STDERR $seq->accession, "\n" if $verbose;
 	$seq -> display_id( $seq -> accession );
 	$out -> write_seq( $seq );
     }
 }
+
+# build blast indices
+# do this in a new loop to avoid race condition
+
+foreach my $file ( @filelist ) {
+    my( $root ) = $file =~ /^(\S+)\.dat/;
+    system "formatdb -i $root.fa -p F" and die;
+}
+
+# make fasta and embl bioperl indices
+# not implemented yet
+
