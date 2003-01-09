d054c1a4be867d1066e83d039c53d829589588bb
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-01-09 11:42:47 +0000

    Added local/global info to thr file so cmblast and web search can use properly

--- trunk/Rfam/CVSimport/release/make_thr.pl
+++ trunk/Rfam/CVSimport/release/make_thr.pl
@@ -5,7 +5,7 @@
 
 use strict;
 
-my( $acc, $id, $ga, $wi );
+my( $acc, $id, $ga, $wi, $loc );
 
 while(<>) {
     /^\#=GF\s+AC\s+(\S+)/ and do {
@@ -22,9 +22,21 @@ while(<>) {
     };
     /^\#=GF\s+BM\s+cmsearch.*-W\s+(\d+)\s+/ and do { 
         $wi = $1;
+	if( /\-local/ ) {
+	    $loc = 1;
+	}
+	else {
+	    $loc = 0;
+	}
 	next;
     };
     /^\/\// and do {
-	print "$acc\t$id\t$ga\t$wi\n";
+	print "$acc\t$id\t$ga\t$wi\t";
+	if( $loc ) {
+	    print "local\n";
+	}
+	else {
+	    print "global\n";
+	}
     };
 }
