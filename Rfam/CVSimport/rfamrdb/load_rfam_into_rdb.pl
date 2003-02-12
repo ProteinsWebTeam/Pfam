638d60aca9b4fd28215e08033f847f2b6059b287
mm1 <mm1@98333f76-0502-11df-81af-936bbf3a1e26>
2003-02-12 18:19:03 +0000

    Updated to take an Rfam accession number

--- Rfam/CVSimport/rfamrdb/load_rfam_into_rdb.pl
+++ Rfam/CVSimport/rfamrdb/load_rfam_into_rdb.pl
@@ -1,61 +1,31 @@
 #!/usr/local/bin/perl 
 
-use lib '/nfs/WWWdev/SANGER_docs/cgi-bin/Rfam';
 
-use RfamWWWConfig;
-use EntryWWW;
+use lib '/nfs/team71/pfam/mm1/rfam_cvs/scripts/Modules';
+use lib '/pfam/db/bioperl';
 
-use lib '/nfs/team71/pfam//mm1/rfam/scripts/Modules';
+use UpdateRDB;
+use Rfam;
 
-require "/nfs/team71/pfam/mm1/rfam/scripts/Modules/UpdateRDB.pm";
-require "/nfs/team71/pfam/mm1//rfam/scripts/Modules/Bio/Rfam.pm";
 
-#use UpdateRDB;
-#use Rfam;
+
+
 
 use strict;
-
-my $database = shift;
+#my $database = shift;
  my  $rdb;
-die "need database to upload dopey!! \n" if(!$database);
-if ($database =~ /rfam2/) {
-  $rdb  = Bio::Rfam->switchover_rdb_update();
-} else {
-  $rdb = Bio::Rfam->rdb_update();
-}
+my $family = shift;
 
-print "RDB: $rdb \n\n";
-#exit(0);
+die "need Rfam to upload dopey!! \n" if(!$family);
+#if ($database =~ /rfam2/) {
+  $rdb  = Rfam->switchover_rdb_update();
+#} else {
+#  $rdb = Rfam->rdb_update();
+#}
 
-my @arr = qw(RF00001 RF00002 RF00003 RF00004 RF00005 RF00006 RF00007 RF00008 RF00009 RF00010 RF00011 RF00012 RF00013 RF00014 RF00015 RF00016 RF00017 RF00018 RF00019 RF00020 RF00021 RF00022 RF00023 RF00024 RF00025 RF00026 RF00027 RF00028 RF00029 RF00030);
+my $db = Rfam::default_db();
+my $en = $db->get_Entry_by_acc($family );
+my $id =  $en->author();
 
+ $rdb->check_in_Entry( $en );
 
-foreach (@arr) {
-  
-  my $en;
-  
-  my $qacc = $_;
-  
-  my ($db, $id, $author, $description, $model_length, $seed_source      , $alignment_method , $gathering_cutoff, $trusted_cutoff  , $noise_cutoff    , $comment         , $previous_id      , $cmbuild , $cmcalibrate  , $num_seed , $num_full );
-  
-  eval {
-    
-    $db = &RfamWWWConfig::get_database();
-    
-    $en = $db->get_Entry_by_acc( $qacc );
-    
-    $id = $en->id();
-    
-  };
-  
-  
-  
- 
-
-  
-
-
-$rdb->check_in_Entry( $en );
-
-
-}
