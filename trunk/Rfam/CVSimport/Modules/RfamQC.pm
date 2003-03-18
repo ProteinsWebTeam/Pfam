42abca1cd300c18f945d73409df0bb5038373a56
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-03-18 16:15:54 +0000

    QC methods that can be called from anywhere

---
+++ trunk/Rfam/CVSimport/Modules/RfamQC.pm
@@ -0,0 +1,73 @@
+
+#
+# Quality control routines for Rfam
+#
+
+package RfamQC;
+
+use strict;
+use Rfam;
+use Rfam::RfamAlign;
+use Bio::Index::Fasta;
+use Bio::Index::Swissprot;
+
+
+sub valid_sequences {
+    my $family = shift;
+    my $inx = Bio::Index::Fasta -> new( '-filename' => $Rfam::rfamseq_new_inx );
+    my $error;
+
+    my @aligns = qw( SEED ALIGN );
+    foreach my $alnfile ( @aligns ) {
+	my $aln = new Rfam::RfamAlign;
+	open( ALN, "$family/$alnfile" ) or die "can't open $family/$alnfile";
+	$aln -> read_stockholm( \*ALN );
+	foreach my $seq ( $aln -> eachSeq ) {
+	    my $rfamseq = $inx -> fetch( $seq->id );
+	    if( not $rfamseq ) {
+		print "$family/$alnfile: cannot find ".$seq->id." in rfamseq database\n";
+		$error = 1;
+		next;
+	    }           
+	    my $str_ali = $seq -> seq();
+	    $str_ali =~ s/[.-]//g;
+	    $str_ali = uc( $str_ali );
+	    my( $start, $end ) = ( $seq->start, $seq->end );
+	    if( $end > $rfamseq->length or $start > $rfamseq->length ) {
+		print "$family/$alnfile: The sequence of ".$seq->id." does not match the rfamseq database\n";
+		$error = 1;
+		next;
+	    }
+	    if( $seq->end < $seq->start ) {
+		( $end, $start ) = ( $start, $end );
+	    }
+	    my $rfamseqtrunc = $rfamseq -> trunc( $start, $end );
+	    my $str_rfamseq;
+	    if( $seq->end < $seq->start ) {
+		$str_rfamseq = $rfamseqtrunc -> revcom() -> seq();
+	    }
+	    else {
+		$str_rfamseq = $rfamseqtrunc -> seq();
+	    }
+	    
+	    $str_ali     =~ tr/T/U/;
+	    $str_rfamseq =~ tr/T/U/;
+	    
+	    if( $str_ali ne $str_rfamseq ) {
+		print "$family/$alnfile: The sequence of ".$seq->id." does not match the rfamseq database\n";
+		$error = 1;
+		next;
+	    }
+	}
+    }
+    if( $error ) {
+	return 0;
+    }
+    else {
+	return 1;
+    }    
+}
+
+
+
+1;
