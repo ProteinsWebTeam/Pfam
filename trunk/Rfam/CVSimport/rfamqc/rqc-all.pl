a06dcba0f764ec0ad726664a454f1db06f71d0d7
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-04-14 15:01:55 +0000

    added rqc-check check

--- trunk/Rfam/CVSimport/rfamqc/rqc-all.pl
+++ trunk/Rfam/CVSimport/rfamqc/rqc-all.pl
@@ -7,12 +7,12 @@ use strict;
 foreach my $family (@ARGV){
     print STDERR "\nSPELL CHECK\n\n";
     system ("pqc-spell $family");
-#    print STDERR "\nSEQUENCE CHECK\n\n";
-#    system ("rqc-seqs $family");
     print STDERR "\nOVERLAP CHECK - ignoring $family\n\n";
     system ("rqc-overlap $family -i $family");
     print STDERR "\nFORMAT CHECK\n\n";
     system ("rqc-format $family");
+    print STDERR "\nMISSING CHECK\n\n";
+    system ("rqc-check $family");
     print STDERR "\nSEQUENCE CHECK\n\n";
     system ("rqc-seqs $family");
 }
