adfe7bcbc71fbe544a179640b8f109721f28ea22
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-05-19 12:27:17 +0000

    Work on current or flatfile

--- trunk/Rfam/CVSimport/release/make_ss_pics.pl
+++ trunk/Rfam/CVSimport/release/make_ss_pics.pl
@@ -10,9 +10,42 @@ use lib $rfam_mod_dir;
 use strict;
 use Rfam;
 
-my $db = Rfam::default_db();
-foreach my $acc ( $db->get_allacc() ) {
-    system "$Rfam::scripts_dir/make/aln2ps.pl $Rfam::current_dir/$acc/SEED > $acc.ps" and die;
+my $flatfile = shift;
+my @accs;
+if( $flatfile ) {
+    my $acc;
+    open( F, $flatfile ) or die;
+    $/ = "//\n";
+    while(<F>) {
+	if( /\#=GF AC   (RF\d+)/ ) {
+	    $acc = $1;
+	}
+	open( A, ">$acc.seed" ) or die;
+	print A "$_";
+	close A;
+	push( @accs, $acc );
+    }
+    close F;
+}
+else {
+    my $db = Rfam::default_db();
+    @accs = $db->get_allacc();
+}
+
+foreach my $acc ( @accs ) {
+    my $file;
+    if( $flatfile ) {
+	$file = "$acc.seed";
+    }
+    else {
+	$file = "$Rfam::current_dir/$acc/SEED";
+    }
+    system "$Rfam::scripts_dir/make/aln2ps.pl $file > $acc.ps" and die;
     system "pstopnm --portrait --stdout $acc.ps | pnmtojpeg --quality 90 > $acc.jpg" and die;
     system "pstopnm --portrait --stdout --xsize 200 $acc.ps | pnmtojpeg --quality 90 > tn_$acc.jpg" and die;
+    unlink( "$acc.ps" ) or die;
+    if( $flatfile ) {
+	unlink( "$acc.seed" ) or die;
+    }
 }
+
