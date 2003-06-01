af2a497c75215651646bc092f20cf4ddabfb0304
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-06-01 10:46:00 +0000

    make a connect file from an alignment

---
+++ trunk/Rfam/CVSimport/make/aln2connect.pl
@@ -0,0 +1,12 @@
+#!/usr/local/bin/perl -w
+
+use lib '/pfam/db/bioperl';
+use lib "$ENV{HOME}/rfam/scripts/Modules";
+use Rfam::RfamAlign;
+
+my $file = shift;
+my $id   = shift;
+my $aln = Rfam::RfamAlign->new();
+open( F, $file ) or die;
+$aln -> read_stockholm( \*F );
+$aln -> write_connect( \*STDOUT, $id );
