c607b0a20b62ac91dd00f8628380776d4040ee84
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-08-24 15:12:57 +0000

    rewrite to use StandAloneBlast, parse blast reports as we go, and to report a list of name start ends - rfsearch.pl can use this to reduce the size of outfiles from the blast step

--- trunk/Rfam/CVSimport/make/rfamseq_blast.pl
+++ trunk/Rfam/CVSimport/make/rfamseq_blast.pl
@@ -18,13 +18,17 @@ use Rfam;
 use Bio::Tools::BPlite;
 use Bio::Index::Fasta;
 use Bio::SeqIO;
+use Bio::Tools::Run::StandAloneBlast;
+use Bio::SearchIO;
+use Bio::SearchIO::Writer::TextResultWriter;
 
-my( $evalue, $division, $minidb, $help, $length );
+my( $evalue, $division, $minidb, $help, $length, $listhits );
 &GetOptions( "e=s"    => \$evalue,
 	     "d=s"    => \$division,
 	     "h"      => \$help,
 	     "w=s"    => \$length,
-	     "minidb" => \$minidb );
+	     "minidb" => \$minidb,
+	     "l"      => \$listhits );
 
 $evalue = 10 if( not $evalue );
 
@@ -67,74 +71,74 @@ else {
     $glob = "*.fa";
 }
 
+
+my %hitlist;
 foreach my $db ( glob( "$Rfam::rfamseq_current_dir/$glob" ) ) {
+    my $factory = Bio::Tools::Run::StandAloneBlast->new( 'program'  => 'blastn',
+							 'database' => $db,
+							 'F'        => 'F',
+							 'W'        => 7,
+							 'b'        => 100000,
+							 'v'        => 100000,
+							 'e'        => $evalue );
+
     print STDERR "searching $db\n";
-    if( $minidb ) {
-	system "blastall -v 100000 -b 100000 -p blastn -i $fafile -e $evalue -W 7 -d $db > $$.blast" and die;
-	my %seqlist = %{ &parse_blast( "$$.blast" ) };
-	foreach my $seqid ( keys %seqlist ) {
-	    foreach my $reg ( @{ $seqlist{ $seqid } } ) {
-		my $seq = &get_seq( $seqid, $reg->{'start'}, $reg->{'end'} );
-		next if not $seq;
-		my $seqstr = $seq->seq();
-		$seqstr =~ s/(.{1,60})/$1\n/g;
-		print ">", $seq->id(), "\n$seqstr";
+
+    my $seqio = Bio::SeqIO -> new( '-file' => $fafile, '-format' => 'Fasta' );
+    while( my $seq = $seqio -> next_seq() ) {
+	my $report = $factory->blastall( $seq );
+	while( my $result = $report->next_result ) {
+	    unless( $listhits or $minidb ) {
+		my $writer = new Bio::SearchIO::Writer::TextResultWriter();
+		my $out = new Bio::SearchIO( -writer => $writer );
+		$out->write_result( $result );
+	    }
+	    while( my $hit = $result->next_hit() ) {
+		while( my $hsp = $hit->next_hsp() ) {
+		    push( @{ $hitlist{$hit->name} }, $hsp );
+		}
 	    }
 	}
     }
-    else {
-	system "blastall -v 100000 -b 100000 -p blastn -i $fafile -e $evalue -W 7 -d $db" and die;
+
+    foreach my $id ( keys %hitlist ) {
+	my @se;
+	my @hsplist = sort { $a->start('hit') <=> $b->start('hit') } @{ $hitlist{$id} };
+	while( my $hsp = shift @hsplist ) {
+	    my( $start, $end ) = ( $hsp->start('hit'), $hsp->end('hit') );
+	    $start = $start - $length;
+	    $end   = $end   + $length;
+	    $start = 1 if( $start < 1 );
+
+	    # Merge overlapping regions - because hsps are sorted by start 
+	    # we only need to check if it overlaps with the last one
+	    if( scalar(@se) and 
+		$start <= $se[ scalar(@se)-1 ]->{'end'} and 
+		$end >= $se[ scalar(@se)-1 ]->{'end'} ) {
+		
+		$se[ scalar(@se)-1 ]->{'end'} = $end;
+	    }
+	    else {
+		push( @se, { 'start' => $start, 'end' => $end } );
+	    }
+	}
+
+	foreach my $se ( @se ) {
+	    if( $listhits ) {
+		print "$id ", $se->{'start'}, " ", $se->{'end'}, "\n";
+	    }
+	    elsif( $minidb ) {
+		my $faout = Bio::SeqIO -> new( '-format' => 'Fasta' );
+		foreach my $se ( @se ) {
+		    my $seq = &get_seq( $id, $se->{'start'}, $se->{'end'} );
+		    $faout -> write_seq( $seq );
+		}
+	    }
+	}
     }
 }
 
 #########
-
-sub parse_blast {
-    my $blastfile = shift;
-    open( BL, $blastfile ) or die;
-    my $report = new Bio::Tools::BPlite( -fh => \*BL );
-    my %list;
-    {
-        while( my $sbjct = $report -> nextSbjct ) {
-            my $name = $sbjct -> name();
-            $name =~ /^(\S+)\s+/;
-            $name = $1;
-            while( my $hsp = $sbjct->nextHSP ) {
-                my( $start, $end ) = ( $hsp->subject->start, $hsp->subject->end );
-                # add window length onto each end
-                $start = $start - $length;
-                $end   = $end   + $length;
-                $start = 1 if( $start < 1 );
-
-                # avoid having multiple copies of one region in minidb
-                my $already;
-                if( exists $list{ $name } ) {
-                    foreach my $se ( sort @{ $list{ $name } } ) {
-                        if( $se->{'start'} >= $start and $se->{'start'} <= $end ) {
-                            $se->{'start'} = $start;
-                            $already = 1;
-                        }
-                        if( $se->{'end'} >= $start and $se->{'end'} <= $end ) {
-                            $se->{'end'} = $end;
-                            $already = 1;
-                        }
-                        if( $se->{'start'} <= $start and $se->{'end'} >= $end ) {
-                            $already = 1;
-                        }
-                    }
-                }
-
-                unless( $already ) {
-                    push( @{ $list{ $name } }, { 'start' => $start,
-                                                 'end'   => $end } );
-                }
-            }
-        }
-        last if ( $report -> _parseHeader == -1 );
-        redo;
-    }
-    return \%list;
-}
 
 sub get_seq {
     # fixes start < 1 and end > length
