330255da4d7935b05cdab82664dfe67d5fc81204
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-10-02 15:49:07 +0000

    index the rfamseq fasta files

---
+++ Rfam/CVSimport/release/index_rfamseq.pl
@@ -0,0 +1,19 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use Bio::Index::Fasta;
+
+my $inxfile = shift;
+my $inx = Bio::Index::Fasta->new( $inxfile, 'WRITE' );
+
+$inx -> id_parser( \&sv_parser );
+$inx -> make_index( @ARGV );
+
+# enables us to add keys as accession AND accession.version - cool!
+sub sv_parser {
+    if( $_[0] =~ /^>\s*(\S+)\.(\d+)/ ) {
+        return $1, "$1.$2";
+    } else {
+        return;
+    }
+}
