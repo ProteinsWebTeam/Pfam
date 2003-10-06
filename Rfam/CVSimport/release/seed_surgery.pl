c018e56db5ebf30ce22ae05c868b804516ab1b5f
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-10-06 08:33:46 +0000

    Complete rewrite - more understandable, uses SV style accessions.  Missing blast search functionality to deal with slight changes in sequence, and search to deal with complete accession changes.  Also contains some cruft subroutines from previous version.

--- Rfam/CVSimport/release/seed_surgery.pl
+++ Rfam/CVSimport/release/seed_surgery.pl
@@ -28,116 +28,217 @@ my( $noaction,
     $dir,
     $noclean,
     $rename,
-    $queue );
+    $queue,
+    $verbose );
 
 &GetOptions( "noaction" => \$noaction,
 	     "dir"      => \$dir,
 	     "noclean"  => \$noclean,
 	     "rename"   => \$rename,
-	     "q=s"      => \$queue );
+	     "q=s"      => \$queue,
+	     "v"        => \$verbose );
 
 if( $dir ) {
     chdir $dir or die "can't chdir to $dir\n";
 }
-my $newinx = Bio::Index::Fasta->new( $Rfam::rfamseq_new_inx );
+
+my $inx = Bio::Index::Fasta->new( $Rfam::rfamseq_current_inx );
+
+END { undef $inx; }  # stop bizarre seq faults;
+
 my %cache;
 my @list;
 my $db = Rfam::default_db();
 @list = @ARGV or @list = $db->get_allacc();
 
-
-#open( LOG, ">log" ) or die "can't write to $dir/log\n";
-
 foreach my $acc ( @list ) {
-    my $changed;
+    print STDERR "Family $acc:\n";
+    my( $seq_change, $cosmetic_change );
     my $aln = new Rfam::RfamAlign;
     open( SEED, "$acc/SEED" ) or die;
     $aln -> read_stockholm( \*SEED );
-    my $newaln = Rfam::RfamAlign->new();
-    $newaln->ss_cons( $aln->ss_cons );
 
-    foreach my $seq ( $aln -> each_seq() ) {
-	my $addseq = Bio::LocatableSeq -> new( -id    => $seq->id,
-					       -start => $seq->start,
-					       -end   => $seq->end,
-					       -seq   => $seq->seq );
-	my $curstr = $seq -> seq();
-	if( $curstr =~ tr/Tt/Uu/ ) {    # "It's RNA dammit" (SRE)
-	    printf( "%s   %-10s%10d%10d   ", $acc, $seq->id,$seq->start,$seq->end );
-	    print "T_TO_U\n";
-	    $addseq -> seq( $curstr );
-	    $changed = 1;
-	}
-	$curstr =~ s/[\.\-]//g;
-	$curstr = uc( $curstr );
-
-	if( !$rename and my $newseq = &return_seq( $newinx, $seq->id, $seq->start, $seq->end ) ) {
-	    my $newstr = $newseq -> seq;
-	    $newstr =~ tr/Ttu/UUU/; 
-	    $newstr = uc( $newstr );
-	    if( $newstr ne $curstr ) {
-		print "\n$curstr\n$newstr\n";
-		printf( "%s   %-10s%10d%10d   %-10s%10d%10d   ", $acc, $seq->id, $seq->start, $seq->end, $seq->id, $seq->start, $seq->end );
-		my $fixed = &find_match( $newinx, $addseq );
-		if( $fixed == -1 ) {
-		    print "RENUMBER\tFIXED\n";
-		    $changed = 1;
-		}
-		elsif( $fixed ) {
-		    print $fixed, "_MISMATCH\tFIXED\n";
-		    $changed = 1;
-		}
-		else {
-		    print "MISMATCH\tNOTFIXED\n";
-		    $changed = 1;
-		}
-	    }
-	}
-	else {
-	    if( $rename ) {
-		printf( "%s   %-10s%10d%10d     ", $acc, $seq->id, $seq->start, $seq->end );
-		my $fixed = &find_match( $newinx, $addseq, 1 );
-		if( $fixed == -1 ) {
-		    print "RENUMBER\tFIXED\n";
-		    $changed = 1;
-		}
-		elsif( $fixed ) {
-		    print $fixed, "_MISMATCH\tFIXED\n";
-		    $changed = 1;
-		}
-		else {
-		    print "MISMATCH\tNOTFIXED\n";
-		    $changed = 1;
-		    next;
-		}		
-	    }
-	    else {
-		printf( "%s   %-10s%10d%10d     ", $acc, $seq->id, $seq->start, $seq->end );
-#		$aln -> remove_seq( $seq );
-		print "DELETE\tFIXED\n";
-		$changed = 1;
-		next;
-	    }
-	}
-	$newaln -> add_seq( $addseq );
+    my $newaln = Rfam::RfamAlign -> new();
+    if( $aln->ss_cons ) {
+	$newaln->ss_cons( $aln->ss_cons );
+    }
+    if( $aln->match_states ) {
+	$newaln->match_states( $aln->match_states );
     }
 
-#    if( $newaln -> allgaps_columns_removed ) {
-#	printf( "%s   GAPS_REMOVED\n", $acc );
-#	$changed = 1;
-#    }
+    foreach my $oldseq ( $aln -> each_seq() ) {
+	my( $oldstart, $oldend ) = ( $oldseq->start, $oldseq->end );
 
-    if( $changed and not $noaction ) {
+	# $seq is the sequence we will add to the new aln object -
+	# we can't simply manipulate the existing sequences because
+	# everything goes very bad when we change the id of a sequence
+	# object that is in an alignment (Que?)
+	my $seq = Bio::LocatableSeq -> new( '-id'    => $oldseq->id(),
+					    '-start' => $oldseq->start(),
+					    '-end'   => $oldseq->end(),
+					    '-seq'   => $oldseq->seq() );
+
+	if( $seq->id() =~ /^(\S+)\.(\d+)$/ ) {
+	    $seq->accession_number($1);
+	    $seq->version($2);
+	}
+	else {
+	    $seq->accession_number( $seq->id() );
+	}
+	
+	if( my $change = &t_to_u( $seq ) ) {
+	    print STDERR $seq->id, " T_TO_U\n";
+	    $cosmetic_change = 1;
+	}
+
+	if( my $newsv = &new_sv( $seq ) ) {         # accession exists in database
+	    print STDERR $seq->id, " ACCESSION_OK\n" if $verbose;
+	    my( $newstart, $newend ) = &subseq_is_unchanged( $seq );
+	    if( $newend ) { 
+		if( $newstart != $seq->start or $newend != $seq->end ) {
+		    print STDERR $seq->id, " START_END_CHANGE\n" if $verbose;		    
+		    $seq->start( $newstart );
+		    $seq->end( $newend );
+		    $cosmetic_change = 1;
+		}
+		else {
+		    print STDERR $seq->id, " SEQ_OK\n" if $verbose;
+		}
+
+		if( $seq->version() and $seq->version() == $newsv ) {   
+		    # leave well alone
+		}
+		else {                              # sv is different
+		    print STDERR $seq->id, " NEW_VERSION $newsv\n" if $verbose;
+		    $seq->version( $newsv );
+		    $seq->id( $seq->accession_number.".".$newsv );
+		    $cosmetic_change = 1;
+		}
+	    }
+	    else {   # the sequence has changed (hopefully only slightly)
+		print STDERR $seq->id, " SEQ_CHANGE\n" if $verbose;
+	    }
+	}
+	else {       # we've got to map to a new accession
+	    print STDERR $seq->id, " DELETE\n";
+	    print STDERR "\n" if $verbose;
+	    $seq_change = 1;
+	    next;
+	}
+
+	print STDERR sprintf( "%-28s%-28s\n", $oldseq->id."/".$oldseq->start."-".$oldseq->end, $seq->id."/".$seq->start."-".$seq->end ) if $verbose;
+	print STDERR "\n" if $verbose;
+	$newaln -> add_seq( $seq );
+
+    }
+
+    if( !$noaction and ( $cosmetic_change or $seq_change ) ) {
 	open( NEW, ">$acc/SEEDNEW" ) or die "can't write to $acc/SEEDNEW";
 	$newaln -> write_stockholm( \*NEW );
 	close NEW;
 
 	rename( "$acc/SEED", "$acc/SEEDOLD" ) or die "can't rename SEED to SEEDOLD";
 	rename( "$acc/SEEDNEW", "$acc/SEED" ) or die "can't rename SEEDNEW to SEED";
+
+	if( $seq_change ) {
+	    print STDERR "Family $acc: The alignment has changed - you must rebuild the family\n\n";
+	}
+	else {
+	    print STDERR "Family $acc: Cosmetic changes - no rebuild needed\n\n";
+	}
     }
 }
 
-undef $newinx;
+
+###############
+
+sub t_to_u {
+    my $seq = shift;
+    my $curstr = $seq -> seq();
+    my $change;
+    if( $curstr =~ tr/Tt/Uu/ ) {    # "It's RNA dammit" (SRE)
+	$seq->seq( $curstr );
+	$change = 1;
+    }
+    return $change;
+}
+
+sub get_seq {
+    my $acc = shift;
+    eval {
+	$cache{$acc} = $inx->fetch( $acc ) unless exists $cache{$acc};
+    };
+    if( $@ or not $cache{$acc} ) {
+#	warn "$acc not found in your database: $@\n";
+	return undef;
+    }
+    return $cache{$acc};
+}
+
+sub new_sv {
+    my $seq    = shift;
+    my $newseq = &get_seq( $seq->accession_number() );
+    if( $newseq ) {
+	my( $version ) = $newseq->id() =~ /^\S+\.(\d+)/;
+	return $version if( $version );
+    }
+    return undef;
+}
+
+sub subseq_is_unchanged {
+    my $seq    = shift;
+    my $newseq = &get_seq( $seq->accession_number() );
+
+    my $reverse;
+    my( $oldstr, $oldstr2 );
+
+    $seq->alphabet( 'rna' );
+    my $revseq = $seq->revcom();
+
+    if( $seq->start > $seq->end ) {   # we did have a reverse strand hit
+	$oldstr  = $revseq->seq();    # so we'll try that first
+	$oldstr2 = $seq->seq();
+	$reverse = 1;
+    }
+    else {                            # try forward strand hit first
+	$oldstr  = $seq->seq();
+	$oldstr2 = $revseq->seq();
+    }
+
+    $oldstr =~ s/[\-\.]//g;
+    $oldstr =~ tr/Uu/Tt/;
+
+    $oldstr2 =~ s/[\-\.]//g;
+    $oldstr2 =~ tr/Uu/Tt/;
+
+    if( $newseq->seq() =~ /^(\w*)$oldstr(\w*)$/i ) {
+	my $start = length($1)+1;
+	my $end   = length($1)+length($oldstr);
+#	print STDERR $seq->id, " HIT_SAME_STRAND\n";
+	if( $reverse ) {
+	    return $end, $start;
+	}
+	else {
+	    return $start, $end;
+	}
+    }
+    elsif( $newseq->seq() =~ /^(\w*)$oldstr2(\w*)$/ ) {  # now try the opposite strand
+	my $start = length($1)+length($oldstr2);
+	my $end   = length($1)+1;
+#	print STDERR $seq->id, " HIT_OPPOSITE_STRAND\n";
+	if( $reverse ) {
+	    return $end, $start;
+	}
+	else {
+	    return $start, $end;
+	}
+    }
+    else {
+	return undef;
+    }
+}
+
+##############
 
 
 sub return_seq {
