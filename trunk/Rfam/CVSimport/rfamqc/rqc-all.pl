90c3a26ed0e8f02646b2cb72c288c3f9d02da81e
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-10-08 14:11:18 +0000

    use rqc-overlap-rdb

--- trunk/Rfam/CVSimport/rfamqc/rqc-all.pl
+++ trunk/Rfam/CVSimport/rfamqc/rqc-all.pl
@@ -8,7 +8,7 @@ foreach my $family (@ARGV){
     print STDERR "\nSPELL CHECK\n\n";
     system ("pqc-spell $family");
     print STDERR "\nOVERLAP CHECK - ignoring $family\n\n";
-    system ("rqc-overlap $family -i $family");
+    system ("rqc-overlap-rdb $family -i $family");
     print STDERR "\nFORMAT CHECK\n\n";
     system ("rqc-format $family");
     print STDERR "\nMISSING CHECK\n\n";
