1da14fdd004976db324880b952b9e45c9c6f2cd3
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2003-12-16 17:58:36 +0000

    need BEGIN magic to distinguidh between pfam and rfam modules!

--- Rfam/CVSimport/make/rfamseq_blast.pl
+++ Rfam/CVSimport/make/rfamseq_blast.pl
@@ -1,5 +1,18 @@
 #!/usr/local/bin/perl -w
 
+BEGIN {
+    $rfam_mod_dir = 
+        (defined $ENV{'RFAM_MODULES_DIR'})
+            ?$ENV{'RFAM_MODULES_DIR'}:"/pfam/db/Rfam/scripts/Modules";
+    $bioperl_dir =
+        (defined $ENV{'BIOPERL_DIR'})
+            ?$ENV{'BIOPERL_DIR'}:"/pfam/db/bioperl";
+
+}
+
+use lib $bioperl_dir;
+use lib $rfam_mod_dir;
+
 use strict;
 use Getopt::Long;
 use Rfam;
