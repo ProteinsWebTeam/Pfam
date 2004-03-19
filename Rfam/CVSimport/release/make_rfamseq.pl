fa4896005a29ebead7b3a0064f72aea274d26213
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2004-03-19 10:22:14 +0000

    do the indexing here and make embl_sv.txt

--- Rfam/CVSimport/release/make_rfamseq.pl
+++ Rfam/CVSimport/release/make_rfamseq.pl
@@ -11,19 +11,29 @@
 use strict;
 use Getopt::Long;
 use Bio::SeqIO;
+use Bio::Index::Fasta;
+use Bio::Index::EMBL;
+
 
 my( $verbose, @filelist );
 &GetOptions( "v"    => \$verbose,
 	     "f=s@" => \@filelist );
 
 my $dir = shift;
-$dir = "." if not $dir;
+$dir = `pwd` if not $dir;
+chomp $dir;
+
+if( $dir !~ /\// ) {
+    die "Must give absolute path to directory [$dir]!";
+}
+
 chdir "$dir" or die "can't cd to $dir";
 @filelist = glob( "*.dat" ) unless( @filelist );
 
 # convert embl format to fasta
 # we want to be keyed off accession rather than id so change disply_id
 
+my @sv;
 foreach my $file ( @filelist ) {
     my( $root ) = $file =~ /^(\S+)\.dat/;
     my $in  = Bio::SeqIO->new( -file => $file, '-format' => 'EMBL' );
@@ -32,17 +42,28 @@ foreach my $file ( @filelist ) {
 	print STDERR $seq->accession, "\n" if $verbose;
 	$seq -> display_id( $seq->accession.".".$seq->seq_version );
 	$out -> write_seq( $seq );
+	push( @sv,  $seq->accession.".".$seq->seq_version );
     }
 }
 
 # build blast indices
 # do this in a new loop to avoid race condition
 
+my( @falist, @datlist );
 foreach my $file ( @filelist ) {
     my( $root ) = $file =~ /^(\S+)\.dat/;
     system "formatdb -i $root.fa -p F" and die;
+    system "xdformat -I -n $root.fa" and die;
 }
 
-# make fasta and embl bioperl indices
-# not implemented yet
+# make fasta bioperl index
+
+system "/pfam/db/Rfam/scripts/release/index_rfamseq.pl rfamseq.fa.bpi";
+
+# make embl_sv.txt
+open( F, ">embl_sv.txt" ) or die;
+foreach my $sv ( sort @sv ) {
+    print F "$sv\n";
+}
+close F;
 
