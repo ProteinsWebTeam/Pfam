0241d1ae61a7e3d689becdb581f19a569c3d0491
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2004-05-21 08:57:08 +0000

    add help and more options

--- Rfam/CVSimport/make/stk2ps.pl
+++ Rfam/CVSimport/make/stk2ps.pl
@@ -4,10 +4,40 @@ use strict;
 use Getopt::Long;
 use Rfam::RfamAlign;
 
-my $fontsize = 10;
-GetOptions( "f=s" => \$fontsize );
+my( $fontsize, 
+    $lines, 
+    $fitpage,
+    $help );
 
-open( F, shift ) or die;
+GetOptions( "s=s" => \$fontsize,
+	    "l=s" => \$lines,
+	    "fit" => \$fitpage,
+	    "h"   => \$help );
+
+my $infile = shift;
+
+if( !$infile or $help ) {
+    print <<EOF;
+Usage: $0 <options> <stkfile>
+
+Take a stockholm alignment with secondary structure markup and
+render a postscript page for printing.
+
+Options:   -h         show this help
+           -s <n>     set fontsize to <n> points (default 10)
+           -l <n>     set number of lines on the page
+           --fit      attempt to break pages at block boundaries
+
+EOF
+    exit(0);
+}
+
+my @args = ( '-fh' => \*STDOUT );
+push( @args, '-fontsize' => $fontsize ) if $fontsize;	     
+push( @args, '-fitpage' => 1 )          if $fitpage;	     
+push( @args, '-lines' => $lines )       if $lines;	     
+
+open( F, $infile ) or die "can't open [$infile]\n";
 my $aln = Rfam::RfamAlign->new();
 $aln->read_stockholm( \*F );
-$aln->write_coloured_ps( \*STDOUT, $fontsize );
+$aln->write_coloured_ps( @args );
