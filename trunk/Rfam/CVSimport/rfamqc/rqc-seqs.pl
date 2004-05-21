0071aab0e17999a7a154f7d03fcfda11865e527d
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2004-05-21 12:59:29 +0000

    remove annoying BEGIN block

--- trunk/Rfam/CVSimport/rfamqc/rqc-seqs.pl
+++ trunk/Rfam/CVSimport/rfamqc/rqc-seqs.pl
@@ -1,17 +1,5 @@
 #!/usr/local/bin/perl -w
 
-BEGIN {
-    $rfam_mod_dir = 
-        (defined $ENV{'RFAM_MODULES_DIR'})
-            ?$ENV{'RFAM_MODULES_DIR'}:"/pfam/db/Rfam/scripts/Modules";
-    $bioperl_dir =
-        (defined $ENV{'BIOPERL_DIR'})
-            ?$ENV{'BIOPERL_DIR'}:"/pfam/db/bioperl";
-}
-
-use lib $rfam_mod_dir;
-use lib $bioperl_dir;
-
 use strict;
 use RfamQC;
 
