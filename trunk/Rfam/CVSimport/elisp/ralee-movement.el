4187d3800e1a03417e37af8773d36e149231d71a
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2004-05-26 21:08:40 +0000

    seperated more stuff out

--- trunk/Rfam/CVSimport/elisp/ralee-movement.el
+++ trunk/Rfam/CVSimport/elisp/ralee-movement.el
@@ -24,6 +24,37 @@
     (forward-line ralee-jump-num)
     (move-to-column column)))
 
+(defun ralee-jump-to-pair ()
+  "jump to the pairing base"
+  (interactive)
+  (let (paired-column)
+    (setq paired-column (ralee-paired-column (current-column)))
+    (if paired-column
+	(progn
+	  (message "column %s pairs with column %s" (current-column) paired-column)
+	  (move-to-column paired-column))
+      (message "No pair!"))))
+
+
+(defun ralee-jump-to-pair-in-other-window ()
+  "jump the cursor to the pairing base in other window
+- make the other window if necessary"
+  (interactive)
+  (let (paired-column
+	line)
+    (setq line (current-line))
+    (setq paired-column (ralee-paired-column (current-column)))
+    (if paired-column
+	(progn
+	  (if (one-window-p)
+	      (split-window))  ; make another window if there isn't already one
+	  (message "column %s pairs with column %s" (current-column) paired-column)
+	  (select-window (next-window))
+	  (goto-line line)
+	  (move-to-column paired-column)
+	  (recenter))
+      (message "No pair!"))))
+
 
 
 
