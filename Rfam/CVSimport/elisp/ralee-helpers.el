73bb6fa4f543bdf833a01de2123e999e29d3fc71
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2004-05-31 18:41:13 +0000

    add copyright stuff

--- Rfam/CVSimport/elisp/ralee-helpers.el
+++ Rfam/CVSimport/elisp/ralee-helpers.el
@@ -1,5 +1,26 @@
 ;;; ralee-helpers
 
+; Copyright (c) 2004 Sam Griffiths-Jones
+;
+; This is part of RALEE -- see
+; http://www.sanger.ac.uk/Users/sgj/code/ralee/ and the README file
+; that should accompany this file.
+;
+; RALEE is free software; you can redistribute it and/or modify it
+; under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+;
+; RALEE is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with RALEE; if not, write to the Free Software Foundation,
+; Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+
 (defun show-structure-ps ()
   "take the current sequence and RNAfold it"
   (interactive)
--- Rfam/CVSimport/elisp/ralee-movement.el
+++ Rfam/CVSimport/elisp/ralee-movement.el
@@ -1,30 +1,53 @@
 ;;; ralee-movement
 
-(defun ralee-jump-right ()
+; Copyright (c) 2004 Sam Griffiths-Jones
+;
+; This is part of RALEE -- see
+; http://www.sanger.ac.uk/Users/sgj/code/ralee/ and the README file
+; that should accompany this file.
+;
+; RALEE is free software; you can redistribute it and/or modify it
+; under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+;
+; RALEE is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with RALEE; if not, write to the Free Software Foundation,
+; Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+
+
+
+(defun jump-right ()
   "move the pointer jump-num characters to the right"
   (interactive)
   (forward-char ralee-jump-num))
 
-(defun ralee-jump-left ()
+(defun jump-left ()
   "move the pointer jump-num characters to the left"
   (interactive)
   (backward-char ralee-jump-num))
 
-(defun ralee-jump-up ()
+(defun jump-up ()
   "move the pointer jump-num lines up"
   (interactive)
   (let ((column (current-column)))
     (forward-line (- 0 ralee-jump-num))
     (move-to-column column)))
 
-(defun ralee-jump-down ()
+(defun jump-down ()
   "move the pointer jump-num lines down"
   (interactive)
   (let ((column (current-column)))
     (forward-line ralee-jump-num)
     (move-to-column column)))
 
-(defun ralee-jump-to-pair ()
+(defun jump-to-pair ()
   "jump to the pairing base"
   (interactive)
   (let (paired-column)
@@ -36,7 +59,7 @@
       (message "No pair!"))))
 
 
-(defun ralee-jump-to-pair-in-other-window ()
+(defun jump-to-pair-in-other-window ()
   "jump the cursor to the pairing base in other window
 - make the other window if necessary"
   (interactive)
