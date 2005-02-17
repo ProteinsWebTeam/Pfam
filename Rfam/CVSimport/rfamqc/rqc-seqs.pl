7c527af6a965dee9799df8ca94467ecbeb7c70aa
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2005-02-17 13:52:56 +0000

    make log files for tracking

--- Rfam/CVSimport/rfamqc/rqc-seqs.pl
+++ Rfam/CVSimport/rfamqc/rqc-seqs.pl
@@ -5,11 +5,14 @@ use RfamQC;
 
 my $family = shift;
 
+open( LOG, ">$family/sequences" ) or die;
+
 if( &RfamQC::valid_sequences( $family ) ) {
     print "$family passes sequence checks\n";
     exit(0);
 }    
 else {
     print "$family contains errors.  You should rebuild this family.\n";
+    print LOG "$family contains errors.  You should rebuild this family.\n";
     exit(1);
 }
