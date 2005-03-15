71f9df5acf60d0336953f96b2a080ebe8c6bc14b
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2005-03-15 09:58:20 +0000

    fix getz for order_by_embl_taxonomy

--- Rfam/CVSimport/Modules/Rfam/RfamAlign.pm
+++ Rfam/CVSimport/Modules/Rfam/RfamAlign.pm
@@ -117,6 +117,161 @@ sub allgaps_columns_removed {
 }
 
 
+sub merge_alignment {
+    # merge alignments based on rf lines
+    my $self = shift;
+
+    foreach my $aln ( @_ ) {
+	my @newrf = split( //, $self->match_states() );
+        my @oldrf = split( //, $aln->match_states() );
+	
+        my( $new_lastm, $new_nextm ); # save the position of the next and last match 
+        my( $old_lastm, $old_nextm ); # state so we can decide which wat to justify
+
+        my $insdir;   # 1 for right, 0 for left, undef for check
+
+        for( my $i=0; $i<@newrf; $i++ ) {
+            # look ahead for the next match states
+            for( my $j=$i; $j<@oldrf; $j++ ) {
+                if( $oldrf[$j] ne '.' ) {
+                    $old_nextm = $j;
+                    last;
+                }
+            }
+            for( my $j=$i; $j<@newrf; $j++ ) {
+                if( $newrf[$j] ne '.' ) {
+                    $new_nextm = $j;
+                    last;
+                }
+            }
+
+#           print "$i $newrf[$i] $oldrf[$i] $new_lastm $new_nextm $old_lastm $old_nextm ";
+
+            if( $newrf[$i] eq $oldrf[$i] ) {
+                undef $insdir;
+#               print "\n";
+            }
+            else {
+                # we need to insert a column in one or other alignment
+                # or deal with a completely missed match state :(
+
+                if( $oldrf[$i] eq '.' ) { 
+                    # we need an insert in newaln
+                    # but do we insert at position i to left justify or after the 
+                    # last match state to right justify? I think this comes down 
+                    # to whether the position after the last match state or before
+                    # the next match state has the most non-gap chars in the 
+                    # alignment!
+
+                    my $inspos;
+                    if( not defined $insdir ) {
+                        my( $count1, $count2 ) = (0,0);
+                        foreach my $seq ( $aln -> each_seq() ) {
+                            my @ary = split( //, $seq->seq );
+                            $count1 ++ if( $ary[$old_nextm-1] =~ /\w/ );
+                            $count2 ++ if( $ary[$old_lastm+1] =~ /\w/ );
+                        }
+                        if( $count1 > $count2 ) {
+                            $insdir = 1;  # right justify
+                        }
+                        else {
+                            $insdir = 0;  # left justify
+                        }
+#                       print "1=$count1 2=$count2 ";
+                    }
+                    
+                    if( $insdir ) {
+                        $inspos = $new_lastm+1;
+                    }
+                    else {
+                        $inspos = $i;
+                    }
+                    # how many gaps to insert?
+                    my @ins;
+                    until( $oldrf[$i] ne '.' ) {
+                        $i++;
+                        push( @ins, '.' );
+                    }
+                    $i--;
+
+#                   print "INSERT1 $inspos ",scalar(@ins),"\n";
+                    splice( @newrf, $inspos, 0, @ins );
+                    $self->insert_column( $inspos, join('',@ins) );
+                }
+                elsif( $newrf[$i] eq '.' ) { 
+                    # we need an insert in aln
+                    # same decision as above!
+                    my $inspos;
+                    if( not defined $insdir ) {
+                        my( $count1, $count2 ) = (0,0);
+                        foreach my $seq ( $self -> each_seq() ) {
+                            my @ary = split( //, $seq->seq );
+                            $count1 ++ if( $ary[$new_nextm-1] =~ /\w/ );
+                            $count2 ++ if( $ary[$new_lastm+1] =~ /\w/ );
+                        }
+                        if( $count1 > $count2 ) {
+                            $insdir = 1;  # right justify
+                        }
+                        else {
+                            $insdir = 0;  # left justify
+                        }
+#                       print "1=$count1 2=$count2 ";
+                    }
+                    
+                    if( $insdir ) {
+                        $inspos = $old_lastm+1;
+                    }
+                    else {
+                        $inspos = $i;
+                    }
+
+                    my @ins;
+                    # how many gaps to insert?
+                    until( $newrf[$i] ne '.' ) {
+                        $i++;
+                        push( @ins, '.' );
+                    }
+                    $i--;
+
+#                   print "INSERT2 $inspos ",scalar(@ins), "\n";
+                    splice( @oldrf, $inspos, 0, @ins );
+                    $aln->insert_column( $inspos, join('',@ins) );
+                }
+                else {
+                    # we have a skipped match state
+                    # look ahead for the match
+                    for( my $j=1; $j<5; $j++ ) {
+                        if( $j==4 ) {
+                            die "I can't match up your RF lines\n";
+                        }
+                        if( $newrf[$i+$j] eq $oldrf[$i] ) {
+                            splice( @oldrf, $i, 0, '-' );
+                            $aln->insert_column( $i, "-" );
+                            last;
+                        }
+                        elsif( $newrf[$i] eq $oldrf[$i+$j] ) {
+                            splice( @newrf, $i, 0, '-' );
+                            $self->insert_column( $i, "-" );
+                            last;
+                        }
+                    }
+                }
+            }
+            $new_lastm = $i if( $newrf[$i] =~ /\w/ );
+            $old_lastm = $i if( $oldrf[$i] =~ /\w/ );
+        }
+	foreach my $seq ( $aln->each_seq() ) {
+	    unless( $self->each_seq_with_id( $seq->id ) ) {
+		$self -> add_seq( $seq );
+	    }
+	}
+    }
+
+    return $self;
+}
+
+
+
 sub order_by_embl_taxonomy {
     my $self = shift;
     my $newaln = Rfam::RfamAlign->new();
@@ -136,7 +291,7 @@ sub order_by_embl_taxonomy {
 
 	my( $ocstring, $acc );
 	my $fh = IO::File->new;
-	$fh -> open( "getz -f acc -f tax -f sl '[EMBL-acc:$idstr]' |" );
+	$fh -> open( "getz -f acc -f tax -f id '[EMBL-acc:$idstr]' |" );
 	while(<$fh>) {
 #	    print;
 	    if( /^AC\s+(\S+)\;/ ) {
@@ -145,7 +300,7 @@ sub order_by_embl_taxonomy {
 	    if( /^OC\s+(.*)/ ) {
 		$ocstring .= "$1 ";
 	    }
-	    if( /^SQ\s+/ ) {
+	    if( /^ID\s+/ ) {
 #		print "$acc      $ocstring\n";
 		$tax{$acc} = $ocstring;
 		$ocstring = $acc = undef;
