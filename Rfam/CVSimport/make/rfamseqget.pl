929573ab820a10ab66dc2cb24d8fbf2d399d6006
sgj <sgj@98333f76-0502-11df-81af-936bbf3a1e26>
2005-04-11 09:29:41 +0000

    Get sequences from rfamseq

---
+++ Rfam/CVSimport/make/rfamseqget.pl
@@ -0,0 +1,38 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+use Getopt::Long;
+
+my( $a, $b );
+GetOptions( "a=s" => \$a,
+	    "b=s" => \$b );
+
+my $rfamseq = "/pfam/db/rfamseq/CURRENT/rfamseq.fa";
+
+my $name = shift;
+
+if( $name =~ /(\S+)\/(\d+)-(\d+)/ ) {
+    $name = $1;
+    $a = $2;
+    $b = $3;
+}
+
+my $options = "-n";
+my( $st, $en ) = ( $a, $b );
+
+if( $a and $b and $a > $b ) {
+    $options .= " -r";
+    ( $st, $en ) = ( $b, $a );
+}
+
+$options .= " -a $st" if( $a );
+$options .= " -b $en" if( $b );
+
+open( XD, "xdget $options $rfamseq $name |" ) or die;
+while(<XD>) {
+    if( /^\>(\S+)\s+(.*)\s+\{.*\}/ ) {
+	print ">$1/$a-$b  $2\n";
+	next;
+    }
+    print;
+}
