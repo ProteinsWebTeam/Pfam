af9990b24864d5cc20c6eb0020d24804e11c54e8
sm3 <sm3@98333f76-0502-11df-81af-936bbf3a1e26>
2005-05-31 13:42:53 +0000

    Script to deal with file copying + bsubbing to linux machines

---
+++ trunk/Rfam/CVSimport/make/pfold_wrapper.pl
@@ -0,0 +1,34 @@
+#!/usr/local/bin/perl -w
+use Getopt::Long;
+my ($outfile, $robust);
+&GetOptions( "outfile=s" => \$outfile,
+	     "robust=s"  => \$robust,
+	     "h"         => \$help );
+sub help {
+    print STDERR <<EOF;
+ 
+pfold_wrapper.pl - Script to run pfold without having to copy things to /pfam/db manually
+ 
+Usage:   pfold_wrapper.pl <FILE> -outfile <OUTPUTFILE> -robust <VALUE> (optional)
+Options:       -h              show this help
+               -outfile        name of output file to be written to
+	       -robust         "robust" value for pfold - default is 0.05
+EOF
+}
+
+my $input = shift;
+my $dir = `pwd`;
+if (!$robust){
+$robust = 0.05;
+}
+if (!$outfile){
+print STDERR "No output specified - Exiting!\n";
+}
+chomp ($input, $dir, $outfile, $robust);
+system "cp $input /pfam/db/Rfam/tmp/pfolds/in.$$";
+chdir "/pfam/db/Rfam/tmp/pfolds/";
+system "bsub -q pfam_fast -Rlinux -I \"pfold.pl -robust $robust in.$$ > $outfile.$$";
+system "cp $outfile.$$ $dir/$outfile";
+unlink "in.$$";
+unlink "$outfile.$$";
+
