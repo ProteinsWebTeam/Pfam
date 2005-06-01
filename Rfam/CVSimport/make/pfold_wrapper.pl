832295fdb9cebbc477a952444e4e173bf80ddfe9
sm3 <sm3@98333f76-0502-11df-81af-936bbf3a1e26>
2005-06-01 10:50:50 +0000

    Added error catching

--- Rfam/CVSimport/make/pfold_wrapper.pl
+++ Rfam/CVSimport/make/pfold_wrapper.pl
@@ -14,15 +14,23 @@ Options:       -h              show this help
                -outfile        name of output file to be written to
 	       -robust         "robust" value for pfold - default is 0.05
 EOF
+exit;
 }
-
+if ($help){
+&help;
+}
 my $input = shift;
+if (!$input){
+print STDERR "No input specified - Exiting!\n";
+&help;
+}
 my $dir = `pwd`;
 if (!$robust){
 $robust = 0.05;
 }
 if (!$outfile){
 print STDERR "No output specified - Exiting!\n";
+&help;
 }
 chomp ($input, $dir, $outfile, $robust);
 system "cp $input /pfam/db/Rfam/tmp/pfolds/in.$$";
