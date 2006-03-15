1735e2c995c9fc64d8763ddeaf2251932f82ae5f
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-03-15 11:17:56 +0000

    *** empty log message ***

---
+++ PfamWeb/lib/PfamWeb/Controller/Clans.pm
@@ -0,0 +1,98 @@
+
+package PfamWeb::Controller::Clans;
+
+use strict;
+use warnings;
+use base "Catalyst::Controller";
+
+
+#pick up a URL like http://localhost:3000/clans/index
+
+sub index  : Local  {
+    
+    
+}
+
+#pick up a URL like http://localhost:3000/clans/summary/CL0001
+#Relative to the class
+sub summary : LocalRegex('^summary\/(CL\d{4})$') { 
+  my ($this, $c) = @_;
+  my $clan_acc = $c->req->snippets->[0]; #get what was captured in the regex
+  $c->stash->{clan} = PfamWeb::Model::Clans->find( {clan_acc=>$clan_acc} ); 
+  if ($c->stash->{clan}){
+     $c->stash->{template} = "pages/clanSum.tt";
+  }else{ 
+     $c->stash->{template} = "pages/error.tt";
+  }
+  $c->log->info( "clan summary: Clan object: |", $c->stash->{clan}, "|" );
+}
+
+
+
+
+
+
+# pick up URLs like
+#   http://localhost:3000/summary?acc=PF00067
+# or
+#   http://localhost:3000/summary?id=p450
+
+sub default : Private {
+  my( $this, $c ) = @_;
+
+  if( defined $c->req->param("acc") ) {
+
+	$c->req->param("acc") =~ m/^(PF\d{5})$/;
+	$c->log->info( "found accession |$1|" );
+
+	$c->stash->{pfam} = PfamWeb::Model::Pfam->find( { pfamA_acc => $1 } )
+	  if defined $1;
+
+  } elsif( defined $c->req->param("id") ) {
+
+	$c->req->param("id") =~ m/(^\w+$)/;
+	$c->log->info( "found ID |$1|" );
+
+	$c->stash->{pfam} = PfamWeb::Model::Pfam->find( { pfamA_id => $1 } )
+	  if defined $1;
+
+  }
+
+  $c->stash->{template} = "pages/error.tt"
+	unless defined $c->stash->{pfam};
+
+  $c->log->info( "default: Pfam object: |", $c->stash->{pfam}, "|" );
+
+}
+
+
+# hand off to TT to render the page. Default to "view.tt", which is
+# the standard summary page right now. If actions have had problems,
+# they'll have switched to point to "error.tt"
+
+sub end : Private {
+  my( $this, $c ) = @_;
+
+  # see if a view was specified
+  my $view;
+  if( $c->req->param( "view" ) ) {
+	( $view = $c->req->param( "view" ) ) =~ s/^([A-Za-z_]+).*$/$1/;
+
+	# set the view
+	$c->stash->{template} ||= "pages/" . $this->{views}->{$view}
+  }
+
+  # pick a tab
+  if( $c->req->param( "tab" ) ) {
+	$c->req->param( "tab" ) =~ /^([A-Za-z]+).*$/;
+	$c->stash->{selectedTab} = $1;
+  }
+
+  # make sure there's a template defined ultimately
+  $c->stash->{template} ||= "pages/" . $this->{views}->{default};
+
+  # and use it
+  $c->forward( "PfamWeb::View::TT" );
+}
+
+1;
