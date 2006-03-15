7e3722e25eb6e4e78c730d17e5634ae90fa879d2
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-03-15 11:38:24 +0000

    Ok... changing the database server port too now...

--- PfamWeb/lib/PfamWeb/Model/BaseModel.pm
+++ PfamWeb/lib/PfamWeb/Model/BaseModel.pm
@@ -7,15 +7,17 @@ use warnings;
 use base "DBIx::Class";
 use Sys::Hostname;
 
-my $dbHost;
+my( $dbHost, $dbPort );
 if( hostname eq "gromit" ) {
   $dbHost = "127.0.0.1";
+  $dbPort = "3336";
 } else {
   $dbHost = "pfam";
+  $dbPort = "3306";
 }
 
 __PACKAGE__->load_components( qw/DB/ );
-__PACKAGE__->connection( "dbi:mysql:pfam_19_0:$dbHost:3306",
+__PACKAGE__->connection( "dbi:mysql:pfam_19_0:$dbHost:$dbPort",
 						 "pfam",
                          "password" );
 
