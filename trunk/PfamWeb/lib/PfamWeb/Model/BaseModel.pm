75a03deca51deac896494646adbd48d4d5a9992f
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-03-15 11:35:49 +0000

    Try to detect hostname and set the database host machine name appropriately.

--- trunk/PfamWeb/lib/PfamWeb/Model/BaseModel.pm
+++ trunk/PfamWeb/lib/PfamWeb/Model/BaseModel.pm
@@ -5,9 +5,17 @@ use strict;
 use warnings;
 
 use base "DBIx::Class";
+use Sys::Hostname;
+
+my $dbHost;
+if( hostname eq "gromit" ) {
+  $dbHost = "127.0.0.1";
+} else {
+  $dbHost = "pfam";
+}
 
 __PACKAGE__->load_components( qw/DB/ );
-__PACKAGE__->connection( "dbi:mysql:pfam_19_0:pfam:3306",
+__PACKAGE__->connection( "dbi:mysql:pfam_19_0:$dbHost:3306",
 						 "pfam",
                          "password" );
 
