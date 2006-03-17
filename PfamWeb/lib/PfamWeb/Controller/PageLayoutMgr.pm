1e1b76106860df37e03f04332c3fa3814796ec46
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-03-17 13:02:28 +0000

    Comment out the logging call that dumps the layout to the log.

--- PfamWeb/lib/PfamWeb/Controller/PageLayoutMgr.pm
+++ PfamWeb/lib/PfamWeb/Controller/PageLayoutMgr.pm
@@ -1,4 +1,14 @@
 
+# PageLayoutMgr.pm
+# jt6 20060317 WTSI
+#
+# manage the storage (and maybe later access) of page layouts
+#
+# Right now this is very simplistic. It'll need to get a bit cleverer
+# before going live...
+#
+# $Id: PageLayoutMgr.pm,v 1.2 2006-03-17 13:02:28 jt6 Exp $
+
 package PfamWeb::Controller::PageLayoutMgr;
 
 use strict;
@@ -11,10 +21,10 @@ sub store : Global {
   my( $this, $c ) = @_;
 
   # just have a quick look to see if there was a layout set already
-  if( defined $c->session->{layout} ) {
-	my $layout = $c->session->{layout};
-	$c->log->debug( "goBlock started: ", $layout->{right}->{goBlock} ? "open" : "closed" );
-  }
+  #   if( defined $c->session->{layout} ) {
+  # 	my $layout = $c->session->{layout};
+  # 	$c->log->debug( "goBlock started: ", $layout->{right}->{goBlock} ? "open" : "closed" );
+  #   }
 
   # retrieve the new layout from our input parameters, defaulting to
   # the empty string if there's no input
@@ -27,7 +37,7 @@ sub store : Global {
 
   # pretty-print the JSON object to the log
   my $js = $json->objToJson($layout, {pretty => 1, indent => 2});
-  $c->log->info( "received a layout string: |$js|" );
+  #$c->log->info( "received a layout string: |$js|" );
 
   # stash the layout object (a JSON object) in the session object
   $c->session->{layout} = $layout;
