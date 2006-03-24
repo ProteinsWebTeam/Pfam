19e89fe9cc6cb9bff114426ccaed41a3b52d5e07
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-03-24 11:45:13 +0000

    Change the syntax for "for" loops that walk over the contents of an array, so that it now works correctly in safari.

--- trunk/PfamWeb/root/static/javascripts/blockConfig.js
+++ trunk/PfamWeb/root/static/javascripts/blockConfig.js
@@ -4,12 +4,12 @@
 //
 // set up collapsing blocks
 //
-// $Id: blockConfig.js,v 1.1 2006-03-20 14:44:40 jt6 Exp $
+// $Id: blockConfig.js,v 1.2 2006-03-24 11:45:13 jt6 Exp $
 
 // initialise everything
 function initialiseBlocks() {
   var divs = YAHOO.util.Dom.get( "content" ).getElementsByTagName( "div" );
-  for( var i in divs ) {
+  for( var i = 0; i < divs.length; i++ ) {
 	var div = divs[i];
 	if( ( ' ' + div.className + ' ' ).indexOf("block") != -1 ) {
 	  new Block( divs[i].id );
