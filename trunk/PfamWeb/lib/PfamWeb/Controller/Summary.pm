7fa8e291ab9cdc8e9e66fa3faf8108370f283c1f
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-04-10 10:58:35 +0000

    Speed ups for the database access.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Summary.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Summary.pm
@@ -5,12 +5,13 @@
 # Controller to build the main Pfam family page. Still a test-bed of
 # sorts.
 #
-# $Id: Summary.pm,v 1.7 2006-03-29 13:33:15 jt6 Exp $
+# $Id: Summary.pm,v 1.8 2006-04-10 10:58:35 rdf Exp $
 
 package PfamWeb::Controller::Summary;
 
 use strict;
 use warnings;
+use Data::Dumper;
 use base "Catalyst::Controller";
 
 
@@ -98,10 +99,9 @@ sub end : Private {
   my $auto_pfam = $c->stash->{pfam}->auto_pfamA; 
   my $no_archs;
   foreach my $arch ( PfamWeb::Model::PfamA_architecture->search( {'auto_pfamA' => $auto_pfam },
-																 { join       => [qw/ arch /],
-																   order_by   =>"arch.no_seqs DESC" }
-															   )
-				   ) {
+	  							 { join        => [qw/ arch /],
+								   prefetch    => [qw/ arch /],
+							     	   order_by    => "arch.no_seqs DESC"})) {
       $no_archs++;
       push @seqs_acc, $arch->pfamseq_acc;
   }
@@ -134,9 +134,10 @@ sub end : Private {
   #----------------------------------------------------------------------
   # get the PDB details
 
-  my @maps = PfamWeb::Model::PdbMap->search( { auto_pfam => $auto_pfam,
-															pfam_region => 1 },
-														  { join => [qw/ pdb / ] }
+  my @maps = PfamWeb::Model::PdbMap->search( { auto_pfam   => $auto_pfam,
+					       pfam_region => 1 },
+					     { join        => [qw/ pdb / ],
+					       prefetch    => [qw/ pdb /]}
 														);
   $c->stash->{pfamMaps} = \@maps;
 
@@ -156,9 +157,11 @@ sub end : Private {
   $summaryData{numStructures} = scalar(keys %pdb_unique);
 
   #Number of species
-  my @species = PfamWeb::Model::PfamA_reg_full->search({auto_pfamA => $auto_pfam,
-							in_full => 1},
-						       {join => [ qw/pfamseq/]});
+  my @species = PfamWeb::Model::PfamA_reg_full->search({ auto_pfamA => $auto_pfam,
+						         in_full    => 1 },
+						       { join       => [ qw/pfamseq/ ],
+						         prefetch   => [ qw/pfamseq/ ]});
+  
 
   my %species_unique = map{$_->species => 1}@species;
   $summaryData{numSpecies} = scalar(keys %species_unique);
@@ -168,6 +171,103 @@ sub end : Private {
 
   $c->stash->{summaryData} = \%summaryData;
 
+  #----------------------------------------------------------------------
+  #Build up the set of database cross references 
+  #There are many different ways of getting the DBXREFS.
+
+# +-----------------+
+# | db_id           | Notes      
+# +-----------------+
+# | PRC             | Internal
+# | IPFAM           | Internal
+# | SCOP            | Structure 
+# | HOMSTRAD        | Structure
+# | PROSITE         | Motif
+# | MIM             | Mutation
+# | TC              | Motif
+# | PRINTS          | Motif
+# | SMART           | Motif
+# | CAZY            | Motif
+# | URL             | ?
+# | PROSITE_PROFILE | Motif
+# | LOAD            | Motif
+# | MEROPS          | Motif
+# | PFAMB           | Internal
+# | COGS            | Motif
+# | PUBMED          | Lit
+# | PDB             | Structure
+# | INTERPRO        | Motif/Lit
+# | GO              | Function
+# +-----------------+
+
+  #Lets do the ones we already have data pulled out for......
+  my (%atobPRODOM, %Xrefs); 
+  #Interpro  
+  if ($c->stash->{pfam}->interpro_id){
+      push(@{$Xrefs{interpro}}, $c->stash->{pfam}->interpro_id);
+  }
+   $c->log->debug(Dumper(%Xrefs));
+  #pdb
+  my @pdb_ids = keys (%pdb_unique); 
+  if(scalar(@pdb_ids )){
+      $Xrefs{pdb} = \@pdb_ids;
+  }
+
+  
+  foreach my $xref ($c->stash->{pfam}->pfamA_database_links){
+      if($xref->db_id eq "PFAMB"){
+	  $atobPRODOM{$xref->db_link} = $xref;
+      }else{
+	  push(@{$Xrefs{$xref->db_link}}, $xref);
+      }
+  }  
+
+  #Get PfamA to PfamA links based on PRC
+  my @atoaPRC = PfamWeb::Model::PfamA2pfamA_PRC_results->search( {"pfamA1.pfamA_acc" => $acc,
+							          "evalue" => { "<=", "0.01"}  },
+								 {join               => [qw/pfamA1 pfamA2/],
+								  select             => [ "pfamA1.pfamA_id", "pfamA2.pfamA_id" ],
+								  as                 => [qw/l_pfamA_id r_pfamA_id /],
+								  prefetch           => [qw/pfamA2 pfamA1/]});
+     
+  if(scalar(@atoaPRC)){
+      $Xrefs{atobPRC} = \@atoaPRC;
+  }
+
+  #Get PfamB to PfamA links based on PRC
+  my @atobPRC = PfamWeb::Model::PfamB2pfamA_PRC_results->search( {pfamA_acc => $acc},
+								 {join      => [qw/pfamA pfamB/],
+								  prefetch  => [qw/pfamA pfamB/]});
+  
+  #Now go through the various operations to find the union between PRC and PRODOM pfamB links
+  my %atobPRC = map{$_->pfamB_acc => $_ }@atobPRC;
+  my( %atobBOTH);
+  foreach ( keys %atobPRC, keys %atobPRODOM ) {
+      $atobBOTH{$_} = $atobPRC{$_} if( exists( $atobPRC{$_} ) and exists( $atobPRODOM{$_} ) );
+  }
+
+  # and then prune out those accessions that are in both lists
+  foreach ( keys %atobPRC ) {
+      delete $atobPRC{$_} if exists $atobBOTH{$_};
+  }
+  
+  foreach ( keys %atobPRODOM ) {
+      delete $atobPRODOM{$_} if exists $atobBOTH{$_};
+  }
+  
+  # Now populate the hash of Xrefs;
+  my @atobPRC_pruned = values %atobPRC;
+  $Xrefs{atobPRC} = \@atobPRC_pruned if(scalar(@atobPRC_pruned));
+  
+  my @atobPRODOM = values %atobPRODOM;
+  $Xrefs{atobPRODOM} = \@atobPRODOM if(scalar(@atobPRODOM));
+  
+  my @atobBOTH = values %atobBOTH;
+  $Xrefs{atobBOTH} = \@atobBOTH if(scalar(@atobBOTH));
+  
+  $c->stash->{xrefs} = \%Xrefs;
+
+  
   #----------------------------------------------------------------------
   # make sure there's a template defined ultimately
   $c->stash->{template} ||= "pages/" . $this->{views}->{default};
