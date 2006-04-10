66109d4831e72227267b0ab89c2f2faf6299dbd7
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-04-10 11:00:43 +0000

    New tables to be modelled.

---
+++ PfamWeb/lib/PfamWeb/Model/PfamB2pfamA_PRC_results.pm
@@ -0,0 +1,26 @@
+package PfamWeb::Model::PfamB2pfamA_PRC_results;
+
+use strict;
+use warnings;
+
+use base "PfamWeb::Model::BaseModel";
+
+
+__PACKAGE__->load_components( qw/Core/); #Do we want to add DB
+__PACKAGE__->table("pfamB2pfamA_PRC_results"); # This is how we define the table
+__PACKAGE__->add_columns( qw/auto_pfamB model_start1 model_end1 length1 align1 auto_pfamA model_start2 model_end2 length2 align2 evalue/); # The columns that we want to have access to
+
+#Set up the primary keys
+__PACKAGE__->set_primary_key( "auto_pfamB", "auto_pfamA");
+
+#Set up relationships
+#1 to many relationship
+__PACKAGE__->has_one( "pfamA" => "PfamWeb::Model::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA"},
+		      {proxy => [qw/pfamA_id pfamA_acc/]});
+
+__PACKAGE__->has_one( "pfamB" => "PfamWeb::Model::PfamB",
+		      {"foreign.auto_pfamB"  => "self.auto_pfamB"},
+		      {proxy => [qw/pfamB_id pfamB_acc/]});
+
+1;
