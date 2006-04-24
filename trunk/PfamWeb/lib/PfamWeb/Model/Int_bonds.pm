98e74d9301e245bdead405bbccec57dddd818266
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-04-24 14:40:36 +0000

    First cut of the models for the interaction tables.

---
+++ trunk/PfamWeb/lib/PfamWeb/Model/Int_bonds.pm
@@ -0,0 +1,22 @@
+package PfamWeb::Model::Int_bonds;
+
+use strict;
+use warnings;
+
+use base "PfamWeb::Model::BaseModel";
+
+
+__PACKAGE__->load_components( qw/Core/); #Do we want to add DB
+__PACKAGE__->table("int_bonds"); # This is how we define the table
+__PACKAGE__->add_columns( qw/auto_int_bonds bond_name/); # The columns that we want to have access to
+
+#Set up the primary keys
+__PACKAGE__->set_primary_key( "auto_int_bonds", "bond_name" );
+
+#Set up relationships
+#1 to many relationship
+
+__PACKAGE__->has_many( "int_atoms" => "PfamWeb::Model::Int_atoms",
+		      {"foreign.auto_int_bonds"  => "self.auto_int_bonds"});
+
+1;
---
+++ trunk/PfamWeb/lib/PfamWeb/Model/Int_ext_links.pm
@@ -0,0 +1,34 @@
+package PfamWeb::Model::Int_ext_links;
+
+use strict;
+use warnings;
+
+use base "PfamWeb::Model::BaseModel";
+
+
+__PACKAGE__->load_components( qw/Core/); #Do we want to add DB
+__PACKAGE__->table("int_ext_links"); # This is how we define the table
+__PACKAGE__->add_columns( qw/auto_int_ext_links auto_pfamaseq_A pfamseq_acc_A auto_pfamA_A pfamA_id_A auto_pfamseq_B pfamseq_acc_B auto_pfamA_B pfamA_id_B reference experiment_ref auto_int_pfamAs dataset/); # The columns that we want to have access to
+
+#Set up the primary keys
+__PACKAGE__->set_primary_key("auto_int_ext_links", "auto_pfamaseq_A", "pfamseq_acc_A", "auto_pfamA_A", "auto_pfamseq_B", "pfamseq_acc_B",  "auto_pfamA_B", "auto_int_pfamAs" );
+
+#Set up relationships
+#1 to many relationship
+
+__PACKAGE__->has_one( "pfamA_A" => "PfamWeb::Model::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA_A"});
+
+__PACKAGE__->has_one( "pfamA_B" => "PfamWeb::Model::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA_B"});
+
+__PACKAGE__->has_one( "pfamseq_A" => "PfamWeb::Model::Pfamseq",
+		      {"foreign.auto_pfamseq"  => "self.auto_pfamseq_A"});
+
+__PACKAGE__->has_one( "pfamseq_B" => "PfamWeb::Model::Pfam",
+		      {"foreign.auto_pfameq"  => "self.auto_pfamseq_B"});
+
+__PACKAGE__->has_one( "int_pfamAs" => "PfamWeb::Model::IntPfamAs",
+		      {"foreign.auto_int_pfamAs" => "self.auto_int_pfamAs"});
+
+1;
