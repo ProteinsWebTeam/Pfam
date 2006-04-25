8865acfdd3a88f95d66205d00d3618ec6270d60a
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-04-25 16:46:16 +0000

    Override the begin method from the application class, to get a clan object from the model, rather than pfam data.

--- PfamWeb/lib/PfamWeb/Controller/Clan.pm
+++ PfamWeb/lib/PfamWeb/Controller/Clan.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build the main Pfam clans page.
 #
-# $Id: Clan.pm,v 1.1 2006-04-20 16:32:53 jt6 Exp $
+# $Id: Clan.pm,v 1.2 2006-04-25 16:46:16 jt6 Exp $
 
 package PfamWeb::Controller::Clan;
 
@@ -14,6 +14,33 @@ use warnings;
 use Data::Dumper;
 
 use base "Catalyst::Controller";
+
+sub begin : Private {
+  my( $this, $c ) = @_;
+
+  if( defined $c->req->param( "acc" ) ) {
+
+	$c->req->param( "acc" ) =~ m/^(CL\d{4})$/;
+	$c->log->info( "$this: found accession |$1|" );
+
+	$c->stash->{clan} = PfamWeb::Model::Clans->find( { clan_acc => $1 } )
+	  if defined $1;
+
+  } elsif( defined $c->req->param( "id" ) ) {
+
+	$c->req->param( "id" ) =~ m/^(\w+)$/;
+	$c->log->info( "$this: found ID |$1|" );
+	
+	$c->stash->{clan} = PfamWeb::Model::Clans->find( { clan_id => $1 } )
+	  if defined $1;
+
+  }
+
+  # we're done here unless there's an entry specified
+  $c->log->warn( "$this: no ID or accession" ) and return
+	unless defined $c->stash->{clan};
+
+}
 
 #-------------------------------------------------------------------------------
 # pick up http://localhost:3000/clan
