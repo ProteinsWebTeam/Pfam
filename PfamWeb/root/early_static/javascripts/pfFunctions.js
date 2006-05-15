5f75eb2c6dd7b6b2227e83cb88edc6d7930b9546
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-05-15 11:01:41 +0000

    Switch to using prototype library shortcuts and ajax calls everyhere.

--- PfamWeb/root/early_static/javascripts/pfFunctions.js
+++ PfamWeb/root/early_static/javascripts/pfFunctions.js
@@ -2,77 +2,117 @@
 // pfFunctions.js
 // jt6 20060412 WTSI
 //
-// $Id: pfFunctions.js,v 1.1 2006-04-12 16:20:57 jt6 Exp $
+// javascript glue for the site. Requires the prototype library.
+//
+// $Id: pfFunctions.js,v 1.2 2006-05-15 11:01:41 jt6 Exp $
 
 // show the specified tab in the page body
 function show( id ) {
-  var divs = YAHOO.util.Dom.get( "content" ).getElementsByTagName( "div" );
-  for( var i = 0; i < divs.length; i++ ) {
-    var div = divs[i];
-    var style = div.style;
-	if( div.className == "block" ) {
-      if( id == div.id ) {
-        style.display = "block";
-      } else {
-        style.display = "none";
-      }
-    }
-  }
 
-  var lis = YAHOO.util.Dom.get( "sidebar" ).getElementsByTagName( "li" );
-  for( var i = 0; i < lis.length; i++ ) {
-    var li = lis[i];
-    var label = id + "Selector";
-    if( li.id == label ) {
-	  li.className = "selected";
-    } else {
-      li.className = "";
-    }
-  }
+  // show/hide the blocks themselves
+  $$("#content div.block").each( function( block ) {
+								   if( id == block.id ) {
+									 block.style.display = "block";
+								   } else {
+									 Element.hide( block );
+								   }
+								 } );
 
+  // set the appropriate selector in the sidebar
+  $$("#sidebar li").each( function( item ) {
+							if( id+"Selector" == item.id ) {
+							  Element.addClassName( item, "selected" );
+							} else {
+							  Element.removeClassName( item, "selected" );
+							}
+						  } );
 }
 
-// set up the callbacks for the domain graphics loading
-var graphicsResponseSuccess = function( oResponse ) {
-  var divs = YAHOO.util.Dom.get( "domainsBlock" ).getElementsByTagName( "div" );
-  var parent;
-  for( var i = 0; i< divs.length; i++ ) {
-    if( ( ' ' + divs[i].className + ' ' ).indexOf("blockContent") != -1 ) {
-      parent = divs[i];
-      break;
-    }
+
+// show/hide the DAS sources selection panel
+showSources = true;
+function revealDASSources() {
+  if( showSources ) {
+	Effect.BlindDown('checkboxes');
+	showSources = false;
+	Element.update( $("revealControl"), "Hide" );
+  } else {
+	Effect.BlindUp('checkboxes');
+	showSources = true;
+	Element.update( $("revealControl"), "Show" );
   }
-  parent.innerHTML = oResponse.responseText;
 }
 
-var graphicsResponseFailure = function( oResponse ) {
-  var message = "<p>Loading failed.</p>";
-  var divs = YAHOO.util.Dom.get( "domainsBlock" ).getElementsByTagName( "div" );
-  var parent;
-  for( var i = 0; i < divs.length; i++ ) {
-    if( ( ' ' + divs[i].className + ' ' ).indexOf("blockContent") != -1 ) {
-      parent = divs[i];
-      break;
-    }
-  }
-  parent.innerHTML = message;
+
+// callbacks for the domain graphics generation call
+function dgSuccess( oResponse ) {
+  Element.update( $("dgph").parentNode, oResponse.responseText );
+}
+function dgFailure() {
+  Element.update( $("dgph"), "Domain graphics loading failed." );
 }
 
-var graphicsCallback = { success:graphicsResponseSuccess,
-                         failure:graphicsResponseFailure };
 
-// and for the species tree load
-var treeResponseSuccess = function( oResponse ) {
+// callbacks for the species tree generation call
+function stSuccess( oResponse ) {
   var tree = new YAHOO.widget.TreeView("treeDiv");
   var root = tree.getRoot();
   eval( oResponse.responseText );
   tree.draw();
 }
-
-var treeResponseFailure = function( oResponse ) {
-  var treeDiv = YAHOO.util.Dom.get( "treeDiv" );
-  treeDiv.innerHTML = "Failed to load species tree.";
+function stFailure() {
+  Element.update( $("stph"), "Tree loading failed." );
 }
 
-var treeCallback = { success:treeResponseSuccess,
-                     failure:treeResponseFailure };
+
+// callbacks for the alignment tree generation call
+function atSuccess( oResponse ) {
+  Element.update( $("alignmentTree"), oResponse.responseText );
+}
+function atFailure() {
+  var p = $("atph");
+
+  // if a previous update succeeds, the "atph" should have
+  // disappeared. We need to re-create it before trying to update it
+  // with an error message...
+  if( ! p ) {
+	p = document.createElement( "p" );
+	p.id = "atph";
+	var parent = $("alignmentTree");
+	parent.insertBefore( p, parent.firstChild );
+  }
+  Element.update( $("atph"), "Alignment tree loading failed." );
+}
+
+
+// re-load the alignment tree
+//function loadAlignmentTree( 
+
+
+// set-up to defer loading of the heavier components of the page
+Event.observe( window, "load", postLoad, false );
+
+// individual blocks will populate this...
+var loadOptions = {};
+
+// and this will make the ajax calls
+function postLoad() {
+  new Ajax.Request( loadOptions.dg.uri,
+					{ method:     "get", 
+					  parameters: loadOptions.dg.params,
+					  onComplete: dgSuccess,
+					  onFailure:  dgFailure
+					} );
+  new Ajax.Request( loadOptions.st.uri,
+	                { method:     "get", 
+					  parameters: loadOptions.st.params,
+                      onComplete: stSuccess,
+                      onFailure:  stFailure
+					} );
+  new Ajax.Request( loadOptions.at.uri,
+					{ method:     "get", 
+					  parameters: loadOptions.at.params,
+					  onComplete: atSuccess,
+					  onFailure:  atFailure
+					} );
+}
