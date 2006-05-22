a1d6326c74faaf29172b125fa19e99b38a89d6fa
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-05-22 12:38:52 +0000

    New modules for drawing das tracks.

---
+++ trunk/PfamLib/Bio/Pfam/Drawing/Layout/Config/GenericDasSourceConfig.pm
@@ -0,0 +1,87 @@
+package Bio::Pfam::Drawing::Layout::Config::GenericDasSourceConfig;
+use strict;
+use Data::Dumper;
+use vars qw($AUTOLOAD @ISA $VERSION);
+
+
+
+sub new {
+  my $caller = shift;
+  my $self = bless {}, ref($caller) || $caller;
+  return $self;
+}
+
+#Configure the DAS source.
+
+sub configureSource {
+    my($self, $features) = @_;
+    $self->_setDrawingType($features);
+    $self->_setDrawingStyles($features);
+
+}
+
+
+sub _setDrawingType{
+    my($self, $feature) = @_;
+    #Note, feature is an array ref....
+    for(my $i = 0; $i < scalar(@$feature); $i++){
+	print STDERR "Start : |$feature|".$feature->[$i]->{'end'}."\n";
+	if($feature->[$i]->{'end'} && $feature->[$i]->{'start'}){
+	    if($feature->[$i]->{'end'} == $feature->[$i]->{'start'}){
+		$feature->[$i]->{'drawingType'} = "Markup";
+	    }else{
+		$feature->[$i]->{'drawingType'} = "Region";
+	    }
+	}else{
+	    $feature->[$i]->{'hidden'} = 1 ;
+	}
+    }
+}
+
+sub _setDrawingStyles{
+    my ($self,$features) = @_;
+    
+    for(my $i = 0; $i < scalar(@$features); $i++){
+	if($features->[$i]->{'drawingType'} eq "Region"){
+	    
+	    #Now construct the URL
+	    #$self->_construct_URL($region);
+	    
+	    #Now contruct the label
+	    #$self->_construct_label($region);
+	    $self->_setRegionColours($features->[$i]);
+	}elsif($features->[$i]->{'drawingType'} eq "Markup"){
+	    #_headColour
+	    #_headStyle
+	    #$self->_lineColour($markup);
+	    #$self->_lineStyle($markup);
+	    #v_align
+	} 
+    }
+}
+
+
+sub _setRegionColours{
+    my($self, $feature, $colour1, $colour2) = @_;
+    if($colour1){
+	$feature->{'colour1'} = $colour1;
+    }else{
+	$feature->{'colour1'} = "cccccc";
+    }
+    if($colour2){
+	$feature->{'colour2'} = $colour2;	
+    }else{
+	$feature->{'colour2'} = "a2a2a2";
+    }
+}
+
+sub _setHeadColour{
+
+}
+
+
+sub resolveOverlaps{
+
+}
+
+1;
---
+++ trunk/PfamLib/Bio/Pfam/Drawing/Layout/DasLayoutManager.pm
@@ -0,0 +1,146 @@
+package Bio::Pfam::Drawing::Layout::DasLayoutManager;
+
+use strict;
+use vars qw($AUTLOAD @ISA @EXPORT $VERSION);
+use Exporter;
+use Bio::Pfam::Drawing::Layout::LayoutManager;
+use Bio::Pfam::Drawing::Layout::Config::GenericDasSourceConfig;
+use Data::Dumper;
+@ISA = qw(Bio::Pfam::Drawing::Layout::LayoutManager);
+
+
+
+sub layout_DAS_sequences_and_features {
+    my ($self, $sequence, $features) = @_;
+    
+    #Okay, the way das works is going to be slightly different.
+    #For each source, we want to display the sequence at the top,
+    #the draw all features below the sequence.
+
+    my $uid = 1;
+    foreach my $source (keys %$features){
+	#first get the sequence object.
+	my $sourceId;
+	if($source =~ /(pfam|smart|superfamily|cath_sptr)/i){
+	    my $id = lc($1);
+	    $sourceId = $id."Das";
+	}else{
+	    $sourceId = "genericDas";
+	}
+	#What we do with das is slightly different.  We display all features (of a type that are accepted)
+	my $config = $self->getSourceConfigurator($sourceId);
+	#print Dumper($features);
+	$config->configureSource($features->{$source});
+	#
+	my $featureSetsRef = $self->resolveOverlaps($features->{$source}, \$uid);
+	print "Got ".scalar(@$featureSetsRef)."\n";
+	foreach my $featureSet (@$featureSetsRef){
+	    #print Dumper($featureSet);
+	    my $l_seq = Bio::Pfam::Drawing::Layout::Sequence->new();
+	    $l_seq->hidden(1);
+	    $l_seq->convertDasSeqAndFeatures($sequence, $sourceId, $featureSet );
+	    $self->add_seq($l_seq);
+	}
+    }
+    #$self->_set_graphics_styles;
+}
+
+
+
+
+sub scale_x {
+  my ($self, $scale_x) = @_;
+  print STDERR "In scale_x\n";
+  if ($scale_x){
+      print STDERR "Setting scale $scale_x\n";
+      $self->{'scale_x'} = $scale_x;
+  }
+  if(!$self->{'scale_x'}){
+    $self->{'scale_x'} = 1.0;
+  }
+  return $self->{'scale_x'};
+}
+
+sub scale_y {
+  my ($self, $scale_y) = @_;
+  if ($scale_y){
+    $self->{'scale_y'} = $scale_y;
+  }
+  if(!$self->{'scale_y'}){
+    $self->{'scale_y'} = 0.5;
+  }
+  return $self->{'scale_y'};
+}
+
+
+
+sub getSourceConfigurator {
+    my ($self, $source) = @_;
+    
+    if(!$self->{'config'}->{$source}){
+	
+	#See if we can find a config object
+	my $sourceConf = ucfirst $source;
+	$sourceConf = "Bio::Pfam::Drawing::Layout::Config::".$sourceConf."Config"; #Check
+	my $config;
+	warn ("The requested Config class, $sourceConf is not available:$@\n") unless (eval "require $sourceConf");
+	
+	eval{
+	    $config = $sourceConf->new();
+	};
+	if($@){
+	    #looks like we can not find a config for thisregion
+	    warn "\n**Using default config for $source (Could not find  $sourceConf) :$@**\n\n";
+	    $config = Bio::Pfam::Drawing::Layout::Config::GenericDasSourceConfig->new();
+	}
+	$self->{'config'}->{$source} = $config; 
+    }
+    return $self->{'config'}->{$source};
+
+}
+
+
+sub resolveOverlaps{
+    my ($self, $features, $uidRef) = @_;
+    my $featureSets = [];
+  FEATURE:
+    for(my $i = 0; $i < scalar(@$features); $i++){
+	next if ($features->[$i]->{'hidden'});
+	$features->[$i]->{'uid'} = $$uidRef;
+	$$uidRef++;
+	my $assignedToSet = 0;
+      SET:
+	for (my $j = 0; $j < scalar(@$featureSets); $j++){
+	    my $overlapWithSet = 0;
+	    foreach my $feature (@{$featureSets->[$j]}){
+		next SET if($features->[$i]->{'drawingType'} ne $feature->{'drawingType'});
+		print "$features->[$i]->{'start'}, $features->[$i]->{'end'}, $feature->{'start'}, $feature->{'end'}\n";
+		if(($features->[$i]->{'start'} <= $feature->{'end'} &&  
+		    $features->[$i]->{'start'} >= $feature->{'start'}) 
+		   || ($features->[$i]->{'end'} <= $feature->{'end'} &&  
+		       $features->[$i]->{'end'} >= $feature->{'start'})){
+		    $overlapWithSet = 1;
+		    print "Failed\n";
+		    last;
+		}
+	    }
+	   
+	    if(!$overlapWithSet){
+		push(@{$featureSets->[$j]}, $features->[$i]);
+		print "Adding to existing set\n";
+		$assignedToSet = 1;
+		next FEATURE;;
+	    }
+	    #If we
+	}
+	if(!$assignedToSet){
+	    $featureSets->[scalar(@$featureSets)] =  [$features->[$i]];
+	    next FEATURE;
+	}
+	warn "error resolving overlaps, I should never get here\n";
+    }
+    return($featureSets);
+}
+
+
+1;
