c0d82d12b7e123f681d164416ab7409bdb3bd0db
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-05-24 16:03:59 +0000

    Add our library copy of DasLite to the front of @INC, so that we can use a version with a recent bugfix. Change the column names, to keep up with changes in the database table.

--- PfamWeb/lib/PfamWeb/Model/Das_sources.pm
+++ PfamWeb/lib/PfamWeb/Model/Das_sources.pm
@@ -4,14 +4,12 @@
 #
 # Model for the das_sources table.
 #
-# $Id: Das_sources.pm,v 1.1 2006-05-15 12:16:03 jt6 Exp $
+# $Id: Das_sources.pm,v 1.2 2006-05-24 16:03:59 jt6 Exp $
 
 package PfamWeb::Model::Das_sources;
 
 use strict;
 use warnings;
-
-use Bio::DasLite;
 
 use base "PfamWeb::Model::BaseModel";
 
@@ -21,7 +19,7 @@ __PACKAGE__->load_components( qw/Core/ );
 __PACKAGE__->table( "das_sources" );
 
 # get the columns that we want to keep
-__PACKAGE__->add_columns( qw/ server_id name url system helperurl defaultServer /);
+__PACKAGE__->add_columns( qw/ server_id name url system helper_url default_server /);
 
 # set up the primary keys/contraints
 __PACKAGE__->set_primary_key( "server_id" );
@@ -29,6 +27,8 @@ __PACKAGE__->set_primary_key( "server_id" );
 # no relationships
 
 BEGIN {
+  unshift @INC, "/nfs/WWWdev/SANGER_docs/catalyst/PfamLib";
+  use Bio::DasLite;
   __PACKAGE__->{dasLite} = Bio::DasLite->new( { dsn     => PfamWeb->config->{dasDsn},
 												timeout => PfamWeb->config->{dasTo},
 												proxy   => PfamWeb->config->{dasProxy}
