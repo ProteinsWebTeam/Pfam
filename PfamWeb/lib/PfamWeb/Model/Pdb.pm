2b546f4c7edfd5ab63df8eb98223522346615a3b
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-07 15:08:06 +0000

    Merged in stuff form PdbNew, which is obsolete.

--- PfamWeb/lib/PfamWeb/Model/Pdb.pm
+++ PfamWeb/lib/PfamWeb/Model/Pdb.pm
@@ -7,16 +7,22 @@ use warnings;
 use base "PfamWeb::Model::BaseModel";
 
 __PACKAGE__->load_components( qw/Core/ );
-__PACKAGE__->table( "pdb" );
-__PACKAGE__->add_columns( qw/auto_pdb pdb_id header title/ );
+__PACKAGE__->table( "pdb_new" );
+__PACKAGE__->add_columns( qw/auto_pdb pdb_id header title date resolution experiment_short experiment_long pubmed_id/ );
 __PACKAGE__->set_primary_key( "auto_pdb" );
 
+__PACKAGE__->has_many( "pdbAuthor" => "PfamWeb::Model::PdbAuthor",
+		       { "foreign.auto_pdb" => "self.auto_pdb" });
+
+__PACKAGE__->has_many( "pdbResidueData" => "PfamWeb::Model::PdbResidueData",
+		       { "foreign.auto_pdb" => "self.auto_pdb" });
+
 __PACKAGE__->has_many(    "pdbMap" => "PfamWeb::Model::PdbMap",
-						 { "foreign.auto_pdb" => "self.auto_pdb" } );
+			  { "foreign.auto_pdb" => "self.auto_pdb" } );
 
 __PACKAGE__->might_have( "image"  => "PfamWeb::Model::PdbImage",
-						 { "foreign.auto_pdb" => "self.auto_pdb" },
-						 { proxy => [ qw/pdb_image/ ] } );
+			 { "foreign.auto_pdb" => "self.auto_pdb" },
+			 { proxy => [ qw/pdb_image/ ] } );
 
 1;
 
