67e95db2a75668727c5fdf9dd656c260d78b74ab
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-10 12:46:03 +0000

    Model for the pdb_residue table.

---
+++ PfamWeb/lib/PfamWeb/Model/Pdb_residue.pm
@@ -0,0 +1,41 @@
+
+package PfamWeb::Model::Pdb_residue;
+
+use strict;
+use warnings;
+
+use base "PfamWeb::Model::BaseModel";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+__PACKAGE__->table( "pdb_residue_data" );
+__PACKAGE__->add_columns( qw/ auto_pdb
+							  msd_chain
+							  chain
+							  serial
+							  pdb_res
+							  pdb_seq_number
+							  dssp_code
+							  auto_pfamseq
+							  pfamseq_res
+							  pfamseq_seq_number / );
+
+__PACKAGE__->set_primary_key( "auto_pdb" );
+
+__PACKAGE__->has_one( "pdb" => "PfamWeb::Model::Pdb",
+					  { "foreign.auto_pdb" => "self.auto_pdb" },
+					  { proxy => [ qw/ pdb / ] }
+					);
+
+__PACKAGE__->has_one( "pfamseq" => "PfamWeb::Model::Pfamseq",
+					  { "foreign.auto_pfam" => "self.auto_pfam" },
+					  { proxy => [ qw/ pfamseq_id pfamseq_acc / ] }
+					);
+
+__PACKAGE__->has_one( "pfam_anseq" => "PfamWeb::Model::Pfam_annseq",
+					  { "foreign.auto_pfam" => "self.auto_pfam" },
+					  { proxy => [ qw/ pfamseq_storable / ] }
+					);
+
+1;
+
