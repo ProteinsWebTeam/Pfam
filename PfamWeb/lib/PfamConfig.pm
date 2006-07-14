927fa9e01615546b44130b2c6af413c823a769b9
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-14 13:14:41 +0000

    Give the full path to the .ini configuration file.

--- PfamWeb/lib/PfamConfig.pm
+++ PfamWeb/lib/PfamConfig.pm
@@ -9,7 +9,7 @@
 # external libraries from a configuration file, rather than having to
 # hard-code them into "use lib" lines in each source file
 
-# $Id: PfamConfig.pm,v 1.1 2006-03-16 17:32:01 jt6 Exp $
+# $Id: PfamConfig.pm,v 1.2 2006-07-14 13:14:41 jt6 Exp $
 
 package PfamConfig;
 
@@ -22,8 +22,9 @@ sub import {
 
   # we expect to find the configuration file in the root directory of
   # the catalyst application
+  
   $Module::PortablePath::CONFIGS = {
-	 default => "pfamweb.ini",
+	 default => "/nfs/WWWdev/SANGER_docs/catalyst/PfamWeb/pfamweb.ini",
   };
 
   Module::PortablePath::import(@_);
