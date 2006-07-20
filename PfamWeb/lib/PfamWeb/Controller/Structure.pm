cf2a8ad48e177c11a40aa2868abd106d29ae1f93
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-20 09:00:20 +0000

    Fix the patterns for capturing the URL. Add the beginnings of error catching. Fix the redirection when given "entry=blah".

--- PfamWeb/lib/PfamWeb/Controller/Structure.pm
+++ PfamWeb/lib/PfamWeb/Controller/Structure.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build the structure page.
 #
-# $Id: Structure.pm,v 1.1 2006-07-14 13:08:06 jt6 Exp $
+# $Id: Structure.pm,v 1.2 2006-07-20 09:00:20 jt6 Exp $
 
 package PfamWeb::Controller::Structure;
 
@@ -26,14 +26,21 @@ sub begin : Private {
 
   my $pdb;
   if( defined $c->req->param("id") ) {
-	$c->req->param("id") =~ m/(^\d\w{3}$)/;
+
+	$c->req->param("id") =~ m/^([0-9][A-Z0-9]{3})$/i;
 	$pdb = PfamWeb::Model::Pdb->find( { pdb_id => $1 } ) if defined $1;
 
-	$c->log->info( "$this: found ID |$1|" );
+	$c->log->info( "Structure::begin: found ID |$1|" );
+
+  } elsif( defined $c->req->param("entry") ) {
+
+	$c->req->param("entry") =~ m/^([0-9][A-Z0-9]{3})$/i;
+	$c->log->debug( "Structure::begin: looks like an ID ($1); redirecting" );
+	$c->res->redirect( $c->uri_for( "/structure", { id => $1 } ) );
   }
 
   # we're done here unless there's an entry specified
-  $c->log->warn( "$this: no valid PDB ID supplied" ) and return
+  $c->log->warn( "Structure::begin: no valid PDB ID supplied" ) and return
 	unless defined $pdb;
 
   # stash the PDB object
@@ -80,12 +87,24 @@ sub default : Path {
 sub end : Private {
   my( $this, $c ) = @_;
 
-  # set up the TT view
-  $c->stash->{pageType} = "structure";
-  $c->stash->{template} = "pages/layout.tt";
+  # don't try to render a page unless there's a Pdb object in the stash
+  return 0 unless defined $c->stash->{pdb};
 
-  # and use it
+  # set up the TT view
+  # check for errors
+  if ( scalar @{ $c->error } ) {
+	$c->stash->{errors}   = $c->error;
+	$c->stash->{template} = "components/blocks/structure/errors.tt";
+  } else {
+	$c->stash->{pageType} = "structure";
+	$c->stash->{template} = "pages/layout.tt";
+  }
+
+  # and render the page
   $c->forward( "PfamWeb::View::TT" );
+
+  # clear any errors
+  $c->error(0);
 
 }
 
