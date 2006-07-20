9b711d48c85015b35903ab5c6b88faaa631fa8c3
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-20 08:46:16 +0000

    Add statistics for the summary icons. Make a start on the error checking catch. Fix a problem with the redirection when given "entry=blah".

--- PfamWeb/lib/PfamWeb/Controller/Clan.pm
+++ PfamWeb/lib/PfamWeb/Controller/Clan.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build the main Pfam clans page.
 #
-# $Id: Clan.pm,v 1.2 2006-04-25 16:46:16 jt6 Exp $
+# $Id: Clan.pm,v 1.3 2006-07-20 08:46:16 jt6 Exp $
 
 package PfamWeb::Controller::Clan;
 
@@ -12,6 +12,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use Storable qw( thaw );
 
 use base "Catalyst::Controller";
 
@@ -20,8 +21,8 @@ sub begin : Private {
 
   if( defined $c->req->param( "acc" ) ) {
 
-	$c->req->param( "acc" ) =~ m/^(CL\d{4})$/;
-	$c->log->info( "$this: found accession |$1|" );
+	$c->req->param( "acc" ) =~ m/^(CL\d{4})$/i;
+	$c->log->info( "Clan::begin: found accession |$1|" );
 
 	$c->stash->{clan} = PfamWeb::Model::Clans->find( { clan_acc => $1 } )
 	  if defined $1;
@@ -29,16 +30,105 @@ sub begin : Private {
   } elsif( defined $c->req->param( "id" ) ) {
 
 	$c->req->param( "id" ) =~ m/^(\w+)$/;
-	$c->log->info( "$this: found ID |$1|" );
+	$c->log->info( "Clan::begin: found ID |$1|" );
 	
 	$c->stash->{clan} = PfamWeb::Model::Clans->find( { clan_id => $1 } )
 	  if defined $1;
 
+  } elsif( defined $c->req->param( "entry" ) ) {
+
+	# see if this is really an accession...
+	if( $c->req->param( "entry" ) =~ /^(CL\d{4})$/i ) {
+
+	  $c->log->debug( "Clan::begin: looks like a clan accession ($1); redirecting" );
+	  $c->res->redirect( $c->uri_for( "/clan", { acc => $1 } ) );
+	  return 1;
+
+	} else {
+
+	  # no; assume it's an ID and see what happens...
+	  $c->log->debug( "Clan::begin: doesn't look like a clan accession ($1); redirecting with an ID" );
+	  $c->res->redirect( $c->uri_for( "/clan", { id => $c->req->param( "entry" ) } ) );
+	  return 1;
+
+	}
+
   }
 
   # we're done here unless there's an entry specified
-  $c->log->warn( "$this: no ID or accession" ) and return
-	unless defined $c->stash->{clan};
+  unless( defined $c->stash->{clan} ) {
+	$c->log->warn( "Clan::begin: no ID or accession" );
+	return 1;
+  }
+
+  #----------------------------------------
+  # get the data items for the overview bar
+
+  my %summaryData;
+
+  # get the auto number - should be quicker to use
+  my $autoClan = $c->stash->{clan}->auto_clan;
+
+  # number of sequences
+  my $rs = PfamWeb::Model::Clan_membership->find(
+    { auto_clan => $autoClan },
+    {
+      select => [ { sum => "pfam.num_full" } ],
+      as => [ 'total_in_clan' ],
+	 join => [qw/pfam/]
+    }
+  );
+  $summaryData{numSequences} = $rs->get_column( "total_in_clan" );
+
+  # count the number of architectures
+  $rs = PfamWeb::Model::ClanArchitecture->find(
+    { auto_clan => $autoClan },
+    {
+      select => [ { count => "auto_clan" } ],
+      as => [ 'count' ]
+    }
+  );
+  $summaryData{numArchitectures} = $rs->get_column( "count" );
+
+  # number of interactions
+  $rs = PfamWeb::Model::Clan_membership->find(
+    { auto_clan => $autoClan },
+	{ select => [ { count => "auto_pfamA_A" } ],
+	  as     => [ qw/NumInts/ ],
+	  join   => [ qw/pfamAInts/ ]
+    }
+  );
+  $summaryData{numIpfam} = $rs->get_column( "NumInts" );
+
+ # get the PDB details
+  my @maps = PfamWeb::Model::Clan_membership->search(
+    { auto_clan            => $autoClan,
+	  "pdbmap.pfam_region" => 1 },
+	{ select => [ qw/pdb.pdb_id/ ],
+	  as     => [ qw/pdb_id/ ],
+	  join   => { pdbmap => "pdb" }
+    }
+  );
+
+  # number of structures known for the domain
+  my %pdb_unique = map {$_->get_column("pdb_id") => 1} @maps;
+  $summaryData{numStructures} = scalar(keys %pdb_unique);
+  $c->stash->{pdbUnique} = \%pdb_unique;
+
+  # number of species
+  my @species = PfamWeb::Model::Clan_membership->search(
+    { auto_clan              => $autoClan,
+ 	  "pfamARegFull.in_full" => 1 },
+    { select => [ qw/pfamseq.species/ ],
+	  as     => [ qw/species/ ],
+	   join   => { pfamARegFull => "pfamseq" }
+    }
+  );
+
+  my %species_unique = map {$_->get_column("species") => 1} @species;
+  $summaryData{numSpecies} = scalar(keys %species_unique);
+
+  $c->stash->{summaryData} = \%summaryData;
 
 }
 
@@ -48,12 +138,32 @@ sub begin : Private {
 sub generateSummary : Path {
   my( $this, $c ) = @_;
 
-  # set up the TT view
-  $c->stash->{pageType} = "clan";
-  $c->stash->{template} = "pages/layout.tt";
+  # empty; just here to capture the URL
+}
+
+#-------------------------------------------------------------------------------
+# default end
+
+sub end : Private {
+  my( $this, $c ) = @_;
+
+  # don't try to render unless there's a Clan object in the stash
+  return 0 unless defined $c->stash->{clan};
+
+  # check for errors
+  if ( scalar @{ $c->error } ) {
+	$c->stash->{errors}   = $c->error;
+	$c->stash->{template} = "components/blocks/clan/errors.tt";
+  } else {
+	$c->stash->{pageType} = "clan";
+	$c->stash->{template} = "pages/layout.tt";
+  }
 
   # and use it
   $c->forward( "PfamWeb::View::TT" );
+
+  # clear any errors
+  $c->error(0);
 
 }
 
@@ -76,6 +186,3 @@ sub generateSummary : Path {
 #   }
 #   $c->log->info( "clan summary: Clan object: |", $c->stash->{clan}, "|" );
 # }
-
-
-
