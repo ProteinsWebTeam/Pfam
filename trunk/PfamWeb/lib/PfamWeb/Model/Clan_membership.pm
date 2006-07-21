8a2230e810ccdfa51125f3be07a3dd9407fd0f2f
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-21 14:56:35 +0000

    Add a couple of extra columns to the proxies.

--- trunk/PfamWeb/lib/PfamWeb/Model/Clan_membership.pm
+++ trunk/PfamWeb/lib/PfamWeb/Model/Clan_membership.pm
@@ -21,9 +21,9 @@ __PACKAGE__->set_primary_key( "auto_pfamA" );
 #1 to 1 releationship
 __PACKAGE__->has_one( "pfam" => "PfamWeb::Model::Pfam",
 		      {"foreign.auto_pfamA"  => "self.auto_pfamA"},
-		        {proxy => [qw/pfamA_id pfamA_acc num_full/]});
+		        {proxy => [qw/ pfamA_id pfamA_acc num_seed num_full model_length /]});
 
-#Not sure about this one.... there will be many of the same auto_clan in this table, 
+#Not sure about this one.... there will be many of the same auto_clan in this table,
 #but only one in the clans table, another one for jt6
 __PACKAGE__->has_one( "clans" => "PfamWeb::Model::Clans",
 		      {"foreign.auto_clan" => "self.auto_clan"},
