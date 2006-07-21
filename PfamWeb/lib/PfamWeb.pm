994d9544c726eb0ebe9da253993330a077dfb2b4
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-07-21 14:46:24 +0000

    Add an "auto" method to catch the "showTab" CGI parameters and pass it through to the stash for the templates to deal with.

--- PfamWeb/lib/PfamWeb.pm
+++ PfamWeb/lib/PfamWeb.pm
@@ -6,7 +6,7 @@
 # application. Configuration is all done through the pfamweb.yml
 # config file and there's (currently) not much else in here.
 #
-# $Id: PfamWeb.pm,v 1.9 2006-07-20 10:05:11 jt6 Exp $
+# $Id: PfamWeb.pm,v 1.10 2006-07-21 14:46:24 jt6 Exp $
 
 package PfamWeb;
 
@@ -35,43 +35,23 @@ use PfamConfig qw( pfamlib );
 
 our $VERSION = '0.01';
 
-
-#
-# Configure the application
-#
-
-#__PACKAGE__->config( file => "../PfamConfig/pfamweb.yml" );
-
-# use the default location for the config file, i.e. the root of the
-# catalyst application
-
-#
-# Start the application
-#
+# configure the application
 __PACKAGE__->setup;
 
-=head1 NAME
+sub auto : Private {
+  my ( $self, $c ) = @_;
 
-PfamWeb - Catalyst based application
+  my $tab;
+  ( $tab ) = $c->req->param( "tab" ) =~ /^(\w+)$/
+	if defined $c->req->param( "tab" );
 
-=head1 SYNOPSIS
+  $c->stash->{showTab} = $1 if defined $tab;
 
-    script/pfamweb_server.pl
+  return 1;
+}
 
-=head1 DESCRIPTION
 
-Catalyst based application.
-
-=head1 METHODS
-
-=cut
-
-#
-# Output a friendly welcome message
-#
-=head2 default
-
-=cut
+# catch-all method that displays the main site index page
 
 sub default : Private {
   my ( $self, $c ) = @_;
@@ -88,30 +68,5 @@ sub default : Private {
 	$c->forward( "PfamWeb::View::TT" );
   }
 }
-
-#
-# Uncomment and modify this end action after adding a View component
-#
-#=head2 end
-#
-#=cut
-#
-#sub end : Private {
-#    my ( $self, $c ) = @_;
-#
-#    # Forward to View unless response body is already defined
-#    $c->forward( $c->view('') ) unless $c->response->body;
-#}
-
-=head1 AUTHOR
-
-John Tate,,,,
-
-=head1 LICENSE
-
-This library is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
