6cfc2f45c289ec11cde30ae343839c0106f5267c
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-08-14 15:33:34 +0000

    Stuff to deal with drawing graph.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Protein/Simap.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Protein/Simap.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build a set of graphics for a given UniProt entry.
 #
-# $Id: Simap.pm,v 1.3 2006-08-14 10:44:47 jt6 Exp $
+# $Id: Simap.pm,v 1.4 2006-08-14 15:33:34 rdf Exp $
 
 package PfamWeb::Controller::Protein::Simap;
 
@@ -14,6 +14,7 @@ use Data::Dumper;
 use Storable qw(thaw);
 use Time::HiRes qw( gettimeofday );
 
+use Bio::Pfam::AlignPfam;
 use Bio::Pfam::WebServices::Client::Simap;
 use Bio::Pfam::Drawing::Layout::PfamLayoutManager;
 
@@ -38,19 +39,19 @@ sub getSimapData : Path('/getsimapdata') {
   my @seqs;
   push(@seqs, thaw($seqStorable->annseq_storable));
   my $layoutPfam = Bio::Pfam::Drawing::Layout::PfamLayoutManager->new;
-
+  $layoutPfam->scale_x(1);
   my %regionsAndFeatures = ( "PfamA"      => 1,
                              "PfamB"      => 1,
                              "noFeatures" => 1 );
   $layoutPfam->layout_sequences_with_regions_and_features(\@seqs, \%regionsAndFeatures);
   my $drawingXML = $layoutPfam->layout_to_XMLDOM;
 
-  $c->log->debug( "proxy is set to: |".$this->{simapProxy}."|" )
-	if defined $this->{simapProxy};
+  $c->log->debug( "\n\n\n proxy is set to: |".$this->{simapProxy}."| \n\n\n\n" );
+#	if defined $this->{simapProxy};
 
   my $simap = Bio::Pfam::WebServices::Client::Simap->new(
-                             '-proxy'      => $this->{simapProxy},
-                             '-md5'        => $c->stash->{pfamseq}->md5,
+							 '-proxy'      => $this->{simapProxy},
+							 '-md5'        => $c->stash->{pfamseq}->md5,
 							 '-maxHits'    => 50,
 							 '-minSWscore' => 1,
 							 '-maxEvalues' => 0.001,
@@ -59,7 +60,86 @@ sub getSimapData : Path('/getsimapdata') {
 							 '-showAli'    => 0);
   $simap->queryService;
   $simap->processResponse4Website($drawingXML, $c->stash->{pfamseq});
+  my $ali = $simap->hits2Ali;
+ 
+  
+  #Now Parse the alignment
+  my $pfamaln = new Bio::Pfam::AlignPfam->new;
+  $pfamaln->read_msf($ali);
+  #open(ALIGN, "fa.sto") || die "Could not open ALIGN:[$1]\n";
+  #$pfamaln->read_stockholm(\*ALIGN);
 
+
+  #Remove any gaps in the alignment
+  my $pos=0;
+  foreach my $seq ( $pfamaln->each_seq() ){
+    $pos++;
+    last if($seqAcc eq $seq->id);
+  }
+  $pfamaln->splice_by_seq_pos($pos);
+
+  die "Length of alignment duff\n" unless ($pfamaln->length == $c->stash->{pfamseq}->length);
+
+
+  #Calulate the average ID per column.
+  my ($idPerCol, $averageId, $residuesPerCol) = $pfamaln->average_percentage_identity_per_column;
+  my $imageNode = $drawingXML->documentElement;
+  my $graph = $drawingXML->createElement( "graph" );
+  $imageNode->appendChild($graph);
+  $graph->setAttribute("length",  $c->stash->{pfamseq}->length);
+  $graph->setAttribute("height",  150);
+  $graph->setAttribute("graphType","mixed");
+  $graph->setAttribute("showLegend", "1");
+  $graph->setAttribute("title", "% Identity of homologs");
+  #Set up the y-axis
+  my $axisY = $drawingXML->createElement( "axisY" );
+  $graph->appendChild($axisY);
+  $axisY->setAttribute("label", "% identity");
+  #$axisY->setAttribute("colour", "offblack1");
+
+  #Set up the x-axis 
+  my $axisX = $drawingXML->createElement( "axisX" );
+  $graph->appendChild($axisX);
+  $axisX->setAttribute("label", "residue");
+  #$axisX->setAttribute("colour", "offblack1");
+  
+  #Set up the data
+  my $graphData = $drawingXML->createElement( "graphData" );
+  $graph->appendChild($graphData);
+  my $dataSeries1 = $drawingXML->createElement( "dataSeries" );
+  $graphData->appendChild($dataSeries1);
+  $dataSeries1->setAttribute("label", "% Residue Identity");
+  $dataSeries1->setAttribute("colour", "green");
+  $dataSeries1->setAttribute("type", "area");
+  
+  my $dataSeries2 = $drawingXML->createElement( "dataSeries" );
+  $graphData->appendChild($dataSeries2);
+  $dataSeries2->setAttribute("label", "Average % Residue Identity");
+  $dataSeries2->setAttribute("colour", "blue");
+  $dataSeries2->setAttribute("type", "lines");
+ 
+  my $dataSeries3 = $drawingXML->createElement( "dataSeries" );
+  $graphData->appendChild($dataSeries3);
+  $dataSeries3->setAttribute("label", "% Residue Aligned");
+  $dataSeries3->setAttribute("colour", "red");
+  $dataSeries3->setAttribute("type", "lines");
+ 
+  
+  for( my $r = 0; $r < @$idPerCol; $r++){
+    my $d1 = $drawingXML->createElement( "data" );
+    $dataSeries1->appendChild($d1);
+    $d1->setAttribute("x",$r+1);
+    $d1->setAttribute("y", $$idPerCol[$r]);
+    my $d2 = $drawingXML->createElement( "data" );
+    $dataSeries2->appendChild($d2);
+    $d2->setAttribute("x",$r+1);
+    $d2->setAttribute("y", $averageId);
+    my $d3 = $drawingXML->createElement( "data" );
+    $dataSeries3->appendChild($d3);
+    $d3->setAttribute("x",$r+1);
+    $d3->setAttribute("y", $$residuesPerCol[$r]);
+  }
+  
   my $pfamImageset = Bio::Pfam::Drawing::Image::ImageSet->new;
   $pfamImageset->create_images($drawingXML);
   $c->stash->{imageset} = $pfamImageset;
