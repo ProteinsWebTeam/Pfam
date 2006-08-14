f508d2c851c9037860955d9bac61e3357f5080a3
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-08-14 10:53:23 +0000

    Added a post-load call.

--- trunk/PfamWeb/root/static/javascripts/clans.js
+++ trunk/PfamWeb/root/static/javascripts/clans.js
@@ -4,31 +4,27 @@
 //
 // javascript glue for the clans section
 //
-// $Id: clans.js,v 1.1 2006-07-21 14:48:40 jt6 Exp $
+// $Id: clans.js,v 1.2 2006-08-14 10:53:23 jt6 Exp $
 
 // load ajax components for the clans page
 
 function clanPostLoad() {
-  console.debug( "in clanPostLoad... uri = |" + loadOptions.cg.uri + "|" );
-   new Ajax.Request( loadOptions.cg.uri,
+  new Ajax.Request( loadOptions.cg.uri,
  					{ method: "get",
  					  parameters: loadOptions.cg.params,
  					  onComplete: cgSuccess,
  					  onFailure:  cgFailure
  					} );
-  console.debug( "in clanPostLoad; submitted request" );
 }
 
 //------------------------------------------------------------
 // callbacks for the domain graphics generation call
 
 function cgSuccess( oResponse ) {
-  console.debug( "cgSuccess !" );
   Element.update( $("clanGraphicsHolder"), oResponse.responseText );
 }
 
 function cgFailure() {
-  console.debug( "cgFailure !" );
   Element.update( $("cgph"), "Graphics loading failed." );
 }
 
