1679ee908b5afc8f756c3812395d3413bd305f5b
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-08-14 10:46:12 +0000

    Moved into the Structures subdirectory, plus switched to use the Schema model.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Structure/GetImage.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Structure/GetImage.pm
@@ -1,28 +1,89 @@
 
-package PfamWeb::Controller::GetImage;
+# GetImage.pm
+# jt6 20060314 WTSI
+#
+# $Id: GetImage.pm,v 1.5 2006-08-14 10:46:12 jt6 Exp $
+
+=head1 NAME
+
+PfamWeb::Controller::Structure::GetImage - server an image of the
+structure
+
+=cut
+
+package PfamWeb::Controller::Structure::GetImage;
+
+=head1 DESCRIPTION
+
+Retrieves an image of the specified PDB entry from the DB and serves
+it up with the appropriate MIME type (image/gif). Relies on
+L<Structure::begin|PfamWeb::Controller::Structure/"begin : Private">
+to extract the PDB ID from the URL.
+
+Generates an B<image file>, MIME type C<image/gif>.
+
+$Id: GetImage.pm,v 1.5 2006-08-14 10:46:12 jt6 Exp $
+
+=cut
 
 use strict;
 use warnings;
-use base 'Catalyst::Controller';
 
-sub default : Private {
+use base "PfamWeb::Controller::Structure";
+
+#-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+=head2 default : Path
+
+Pick up http://localhost:3000/structure/getimage?id=1abc and serves
+the image for that entry. Returns a blank image if no image is found
+for this entry.
+
+=cut
+
+sub default : Path {
   my( $this, $c ) = @_;
 
-  $c->req->param("pdbid") =~ m/^(\d\w{3})$/;
-  $c->stash->{pdb} = PfamWeb::Model::Pdb->search( { pdb_id => $1 } )->next;
+  return unless defined $c->stash->{pdb};
 }
+
+#-------------------------------------------------------------------------------
+
+=head2 end : Private
+
+Push the file to the response
+
+=cut
 
 sub end : Private {
   my( $this, $c ) = @_;
 
-  if( defined $c->stash->{pdb} ) {
-	# we retrieved a Pdb object, which we can use to get the image
-	# itself
+  return unless defined $c->stash->{pdb};
+
+  if( defined $c->stash->{pdb}->pdb_image ) {
 	$c->res->content_type( "image/gif" );
 	$c->res->write( $c->stash->{pdb}->pdb_image );
   } else {
-	$c->res->redirect( "static/images/blank.gif" );
+	$c->res->redirect( "/images/blank.gif" );
   }
+
 }
+
+#-------------------------------------------------------------------------------
+
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+
+Rob Finn, C<rdf@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
