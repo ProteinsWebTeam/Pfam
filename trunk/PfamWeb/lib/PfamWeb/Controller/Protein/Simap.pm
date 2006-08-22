00231e937eea398079ff5bde768e1d34c68adc08
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-08-22 13:36:43 +0000

    first cut for producing graph of simap data

--- trunk/PfamWeb/lib/PfamWeb/Controller/Protein/Simap.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Protein/Simap.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build a set of graphics for a given UniProt entry.
 #
-# $Id: Simap.pm,v 1.4 2006-08-14 15:33:34 rdf Exp $
+# $Id: Simap.pm,v 1.5 2006-08-22 13:36:43 rdf Exp $
 
 package PfamWeb::Controller::Protein::Simap;
 
@@ -34,7 +34,7 @@ sub getSimapData : Path('/getsimapdata') {
   $c->log->debug( "Protein::Simap::getSimapData listing parameters:" );
 
   my $seqAcc = $c->stash->{pfamseq}->pfamseq_acc;
-
+  my $length = $c->stash->{pfamseq}->length;
   my $seqStorable = $c->stash->{pfamseq}->annseq;
   my @seqs;
   push(@seqs, thaw($seqStorable->annseq_storable));
@@ -42,7 +42,7 @@ sub getSimapData : Path('/getsimapdata') {
   $layoutPfam->scale_x(1);
   my %regionsAndFeatures = ( "PfamA"      => 1,
                              "PfamB"      => 1,
-                             "noFeatures" => 1 );
+                             "noFeatures" => 0 );
   $layoutPfam->layout_sequences_with_regions_and_features(\@seqs, \%regionsAndFeatures);
   my $drawingXML = $layoutPfam->layout_to_XMLDOM;
 
@@ -60,12 +60,12 @@ sub getSimapData : Path('/getsimapdata') {
 							 '-showAli'    => 0);
   $simap->queryService;
   $simap->processResponse4Website($drawingXML, $c->stash->{pfamseq});
-  my $ali = $simap->hits2Ali;
+  my $pfamaln = $simap->hits2Ali;
  
   
   #Now Parse the alignment
-  my $pfamaln = new Bio::Pfam::AlignPfam->new;
-  $pfamaln->read_msf($ali);
+  #my $pfamaln = new Bio::Pfam::AlignPfam->new;
+  #$pfamaln->read_msf($ali);
   #open(ALIGN, "fa.sto") || die "Could not open ALIGN:[$1]\n";
   #$pfamaln->read_stockholm(\*ALIGN);
 
@@ -89,7 +89,11 @@ sub getSimapData : Path('/getsimapdata') {
   $graph->setAttribute("length",  $c->stash->{pfamseq}->length);
   $graph->setAttribute("height",  150);
   $graph->setAttribute("graphType","mixed");
-  $graph->setAttribute("showLegend", "1");
+  if($length < 100){
+    $graph->setAttribute("showLegend", "0");
+  }else{
+    $graph->setAttribute("showLegend", "1");
+  }
   $graph->setAttribute("title", "% Identity of homologs");
   #Set up the y-axis
   my $axisY = $drawingXML->createElement( "axisY" );
@@ -125,21 +129,37 @@ sub getSimapData : Path('/getsimapdata') {
   $dataSeries3->setAttribute("type", "lines");
  
   
-  for( my $r = 0; $r < @$idPerCol; $r++){
+  for( my $r = 1; $r < @$idPerCol; $r++){
     my $d1 = $drawingXML->createElement( "data" );
     $dataSeries1->appendChild($d1);
-    $d1->setAttribute("x",$r+1);
-    $d1->setAttribute("y", $$idPerCol[$r]);
+    $d1->setAttribute("x",$r);
+    #Smooth this data over a window of four aa
+    my @smooth = @$idPerCol[($r-2)..($r+2)];
+    my $smoothTotal = 0;
+    my $n = 0;
+    foreach (@smooth){
+      if($_){
+	$smoothTotal += $_;
+	$n++;
+      }
+    }
+
+    my $smoothedId = 0;
+    if($n > 0){
+      $smoothedId = $smoothTotal/$n;
+    }
+
+    $d1->setAttribute("y", $smoothedId);
+
     my $d2 = $drawingXML->createElement( "data" );
     $dataSeries2->appendChild($d2);
-    $d2->setAttribute("x",$r+1);
+    $d2->setAttribute("x",$r);
     $d2->setAttribute("y", $averageId);
     my $d3 = $drawingXML->createElement( "data" );
     $dataSeries3->appendChild($d3);
-    $d3->setAttribute("x",$r+1);
-    $d3->setAttribute("y", $$residuesPerCol[$r]);
+    $d3->setAttribute("x",$r);
+    $d3->setAttribute("y", $$residuesPerCol[($r-1)]);
   }
-  
   my $pfamImageset = Bio::Pfam::Drawing::Image::ImageSet->new;
   $pfamImageset->create_images($drawingXML);
   $c->stash->{imageset} = $pfamImageset;
