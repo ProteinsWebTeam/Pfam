67a227391db3b7d06d9bce88a44a2e170edba0d4
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-09-13 08:31:54 +0000

    Add a first cut of a global error-catching method.

--- trunk/PfamWeb/lib/PfamWeb.pm
+++ trunk/PfamWeb/lib/PfamWeb.pm
@@ -2,18 +2,29 @@
 # PfamWeb.pm
 # jt 20060316 WTSI
 #
-# This is the main class for the Pfam website catalyst
-# application. Configuration is all done through the pfamweb.yml
-# config file and there's (currently) not much else in here.
-#
-# $Id: PfamWeb.pm,v 1.11 2006-08-14 10:23:50 jt6 Exp $
+# $Id: PfamWeb.pm,v 1.12 2006-09-13 08:31:54 jt6 Exp $
+
+=head1 NAME
+
+PfamWeb - main class for the Pfam website application
+
+=cut
 
 package PfamWeb;
+
+=head1 DESCRIPTION
+
+This is the main class for the Pfam website catalyst
+application. Configuration is all done through the C<pfamweb.yml>
+config file.
+
+$Id: PfamWeb.pm,v 1.12 2006-09-13 08:31:54 jt6 Exp $
+
+=cut
 
 use strict;
 use warnings;
 
-#
 # Set flags and add plugins for the application
 #
 #         -Debug: activates the debug mode for very useful log messages
@@ -34,33 +45,40 @@ use Catalyst qw/ -Debug
 #                Session::Store::FastMmap
 #                Session::State::Cookie
 
+# configure library loading. This module reads the pfamweb.ini file in
+# the root of the application and gets the path to the specified
+# "library module," which it then adds to the include path
+
 use PfamConfig qw( pfamlib );
 
 our $VERSION = '0.01';
 
-# configure the application
+#-------------------------------------------------------------------------------
+
+=head1 CONFIGURATION
+
+Configuration is done through a single YAML file, C<pfamweb.yml>,
+which contains all of the catalyst-specific settings, and an INI file,
+C<pfamweb.ini>, which points C<Module::PortablePath> at associated
+library modules in other filesystem trees.
+
+=cut
+
 __PACKAGE__->setup;
 
+#-------------------------------------------------------------------------------
 
-sub auto : Private {
-  my( $self, $c ) = @_;
+=head2 default : Private
 
-  my $tab;
-  ( $tab ) = $c->req->param( "tab" ) =~ /^(\w+)$/
-	if defined $c->req->param( "tab" );
+Re-directs straight to the index page for the whole site. This is the
+default page for any errors, broken (internal) links, etc.
 
-  $c->stash->{showTab} = $1 if defined $tab;
-
-  return 1;
-}
-
-
-# catch-all method that displays the main site index page
+=cut
 
 sub default : Private {
-  my( $self, $c ) = @_;
+  my( $this, $c ) = @_;
 
-  # Hello World
+  # Hello World - the default catalyst welcome page
   #$c->response->body( $c->welcome_message );
 
   # catch the root of the application. This could be moved into a
@@ -73,6 +91,84 @@ sub default : Private {
   }
 }
 
+#-------------------------------------------------------------------------------
 
+=head1 METHODS
+
+=head2 auto : Private
+
+Checks the request parameters for a "tab" parameter and sets the
+appropriate tab name in the stash. That will be picked up by the
+tab_layout.tt view, which will use it to figure out which tab to show
+by default.
+
+=cut
+
+sub auto : Private {
+  my( $this, $c ) = @_;
+
+  my $tab;
+  ( $tab ) = $c->req->param( "tab" ) =~ /^(\w+)$/
+	if defined $c->req->param( "tab" );
+
+  $c->stash->{showTab} = $1 if defined $tab;
+
+  return 1;
+}
+
+=head2 default : Private
+
+A catch-all method that displays the main site index page.
+
+=cut
+
+#-------------------------------------------------------------------------------
+
+=head2 reportError : Private
+
+Records site errors in the database.
+
+=cut
+
+sub reportError : Private {
+  my( $this, $c ) = @_;
+
+  foreach my $e ( @{$c->error} ) {
+
+	$c->log->debug( "PfamWeb::reportError: reporting a site error: |$e|" );
+
+	my $rs = $c->model( "WebUser::ErrorLog" )->find( { message => $e } );
+
+	if( $rs ) {
+	  $c->log->debug( "PfamWeb::reportError: seen that error before; update" );
+	  $rs->update( { num => $rs->num + 1 } );
+	} else {
+	  $c->log->debug( "PfamWeb::reportError: new error message; creating" );
+	  $rs->create( { message => $e,
+					 num     => 1,
+					 first   => [ "CURRENT_TIMESTAMP" ] } );
+	}
+	
+  }
+
+  $c->log->debug( "PfamWeb::reportError: there are now "
+				  . $c->model( "WebUser::ErrorLog" )->count . " errors in the table" );
+
+}
+
+#-------------------------------------------------------------------------------
+
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+
+Rob Finn, C<rdf@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
