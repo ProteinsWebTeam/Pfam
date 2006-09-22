2fdd76dc7250654c85f36ac06574f59d31076176
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-09-22 10:47:02 +0000

    Added POD and changes to error handling.

--- PfamWeb/lib/PfamWeb/Controller/Clan/Browse.pm
+++ PfamWeb/lib/PfamWeb/Controller/Clan/Browse.pm
@@ -2,9 +2,13 @@
 # Browse.pm
 # jt6 20060821 WTSI
 #
-# Controller for the "browse" pages for clans.
-#
-# $Id: Browse.pm,v 1.1 2006-08-22 15:12:51 jt6 Exp $
+# $Id: Browse.pm,v 1.2 2006-09-22 10:47:02 jt6 Exp $
+
+=head1 NAME
+
+PfamWeb::Controller::Structure - controller for the clan "browse"
+
+=cut
 
 package PfamWeb::Controller::Clan::Browse;
 
@@ -13,9 +17,29 @@ use warnings;
 
 use base "Catalyst::Controller";
 
-sub begin : Private {}
 
-# pick up a URL like http://localhost:3000/clan/browse/
+=head1 DESCRIPTION
+
+Just a simple wrapper around a template that displays a "browse" page
+for clans. Since there are relatively few clans we can put them all on
+one page, rather than splitting them up like the families.
+
+Generates a B<full page>.
+
+$Id: Browse.pm,v 1.2 2006-09-22 10:47:02 jt6 Exp $
+
+=cut
+
+#-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+=head2 browse : Path
+
+Picks up a URL like C<http://localhost:3000/clan/browse/>. Retrieves a
+list of all clan IDs and hands off to the template.
+
+=cut
 
 sub browse : Path {
   my( $this, $c ) = @_;
@@ -29,6 +53,13 @@ sub browse : Path {
 
 }
 
+#-------------------------------------------------------------------------------
+
+=head2 end : Private
+
+Renders the clan browse pages.
+
+=cut
 
 # render the page
 
@@ -37,20 +68,33 @@ sub end : Private {
 
   # check for errors
   if ( scalar @{ $c->error } ) {
-	$c->stash->{errors}   = $c->error;
-	$c->stash->{template} = "components/errors.tt";
+	$c->stash->{template} = "pages/error.tt";
   } else {
 	$c->stash->{pageType} = "clan";
 	$c->stash->{template} = "pages/browseClans.tt";
   }
 
   # and use it
-  $c->stash->{fullPage} = 1;
   $c->forward( "PfamWeb::View::TT" );
 
   # clear any errors
-  $c->error(0);
+  $c->clear_errors;
 
 }
+
+#-------------------------------------------------------------------------------
+
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+
+Rob Finn, C<rdf@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+This program is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
