30256ecf3a3fe82471807833c35dc900184e745f
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-09-28 09:40:09 +0000

    Starting to build the data structures need for the domain page.

--- PfamWeb/lib/PfamWeb/Controller/Int/Domain.pm
+++ PfamWeb/lib/PfamWeb/Controller/Int/Domain.pm
@@ -3,7 +3,7 @@
 #
 # Controller to build the main Domain page.
 #
-# $Id: Domain.pm,v 1.1 2006-08-22 13:22:51 rdf Exp $
+# $Id: Domain.pm,v 1.2 2006-09-28 09:40:09 rdf Exp $
 
 package PfamWeb::Controller::Int::Domain;
 
@@ -24,12 +24,43 @@ sub generateDomainIntSum : Path {
 
   if( defined $c->req->param("acc") ) {
     $c->req->param("acc") =~ m/^(PF\d{5})$/i;
-    #$c->stash->{domain} = $c->model("PfamDB::Interactions")->find( { pfamA_acc_A => $1 } );
-    $c->log->debug("Int::Domain::generateDomainIntSum: Got domain data for:");
+    $c->stash->{pfam} = $c->model("PfamDB::Pfam")->find( { pfamA_acc => $1 } );
   }elsif(defined $c->req->param("id") ) {
 
   }elsif(defined $c->req->param("entry") ) {
 
+  }
+  if($c->stash->{pfam}->pfamA_id){
+    # Now that we have a pfam domain
+    my @rs = $c->model("PfamDB::Int_pfamAs")->search({ auto_pfamA_A => $c->stash->{pfam}->auto_pfamA },
+						     { join     => [qw/pfamA_B/],
+						       prefetch => [qw/pfamA_B/]});
+    if(scalar(@rs)){
+      $c->stash->{domainInts} = \@rs;
+      my $noSeqs = $c->model("PfamDB::Interactions")->find({ auto_pfamA_A => $c->stash->{pfam}->auto_pfamA },
+							   { select => { count => {distinct => "auto_pfamseq_A" }},
+							     as     => [qw/noSeqs/] });
+      $c->stash->{noSeqs} = $noSeqs->get_column("noSeqs");
+      my $noStruc = $c->model("PfamDB::Interactions")->find({ auto_pfamA_A => $c->stash->{pfam}->auto_pfamA },
+								{ select => { count => {distinct => "auto_pdb" }},
+								  as     => [qw/noStrucs/] });
+      
+      my %summaryData;
+      $summaryData{numSpecies} = 0;
+      $summaryData{numArchitectures} = 0;
+      $summaryData{numStructures}  = $noSeqs->get_column("noSeqs");
+      $summaryData{numSequences}   = $noStruc->get_column("noStrucs");
+      $summaryData{numInt}         = scalar(@rs);
+      $c->stash->{summaryData} = \%summaryData;
+      
+      $c->log->debug("Get ".$noSeqs->get_column("noSeqs")." sequences");
+      $c->log->debug("Found ".scalar(@{$c->stash->{domainInts}})." domain interactions");
+      $c->log->debug("Int::Domain::generateDomainIntSum: Got domain data for:");
+    }else{
+      $c->stash->{noiPfam} = 1;
+    }
+  }else{
+    $c->stash->{noPfam} = 1;
   }
 
 }
