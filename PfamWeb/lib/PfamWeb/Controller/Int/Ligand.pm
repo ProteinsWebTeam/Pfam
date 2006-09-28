72670ac85c232f53cd5ae65543f520273ab66a1f
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-09-28 13:27:10 +0000

    Use the default end method from the parent.

--- PfamWeb/lib/PfamWeb/Controller/Int/Ligand.pm
+++ PfamWeb/lib/PfamWeb/Controller/Int/Ligand.pm
@@ -3,7 +3,7 @@
 #
 # Controller to build the main Ligand page.
 #
-# $Id: Ligand.pm,v 1.1 2006-08-22 13:22:51 rdf Exp $
+# $Id: Ligand.pm,v 1.2 2006-09-28 13:26:55 jt6 Exp $
 
 package PfamWeb::Controller::Int::Ligand;
 
@@ -13,48 +13,24 @@ use Data::Dumper;
 
 use base "PfamWeb::Controller::Int";
 
+__PACKAGE__->config( SECTION => "ligand" );
+
 #-------------------------------------------------------------------------------
 # pick up http://localhost:3000/int
 
 sub generateLigandIntSum : Path {
   my( $this, $c ) = @_;
-  
+
   $c->log->debug("Int::Ligand::generateLigandIntSum: Hello");
 
-  
   if( defined $c->req->param("code") ) {
     $c->req->param("code") =~ m/^(\w{3})$/i;
     $c->stash->{ligand} = $c->model("PfamDB::Ligands")->find( { three_letter_code => $1 } );
     $c->log->debug("Int::Ligand::generateLigandIntSum: Got ligand data for:".$c->stash->{ligand}->name.":");
   }
 
-
-
 }
 
 #-------------------------------------------------------------------------------
-sub end : Private {
-  my( $this, $c ) = @_;
-
-  # don't try to render a page unless there's a Pfam object in the stash
-  return 0 unless defined $c->stash->{ligand};
-
-  # check for errors
-  if ( scalar @{ $c->error } ) {
-        $c->stash->{errors}   = $c->error;
-        $c->stash->{template} = "components/blocks/ipfam/errors.tt";
-  } else {
-        $c->stash->{pageType} = "iLigand";
-        $c->stash->{template} ||= "pages/layout.tt";
-  }
-
-  # and render the page
-  $c->forward( "PfamWeb::View::TT" );
-
-  # clear any errors
-  $c->error(0);
-
-}
- 
 
 1;
