3ac65fe104f874dd1b2508b1f58b2a956031c892
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-10-27 08:53:53 +0000

    Altered to get unique domain graphics for a Pfam entry (based on the Pfam-A entries) and domain graphics for a given archiecture as with the Pfam-A entries.

--- PfamWeb/lib/PfamWeb/Controller/PfamB/DomainGraphics.pm
+++ PfamWeb/lib/PfamWeb/Controller/PfamB/DomainGraphics.pm
@@ -4,56 +4,93 @@
 #
 # Controller to build a set of domain graphics for a given PfamB.
 #
-# $Id: DomainGraphics.pm,v 1.1 2006-10-23 12:23:06 jt6 Exp $
+# $Id: DomainGraphics.pm,v 1.2 2006-10-27 08:53:53 rdf Exp $
 
 package PfamWeb::Controller::PfamB::DomainGraphics;
 
 use strict;
 use warnings;
-
+use Data::Dump qw( dump );
 use Storable qw(thaw);
 
 use base "PfamWeb::Controller::PfamB";
 
 
-# pick up a URL like http://localhost:3000/family/domaingraphics?acc=PF00067
+# pick up a URL like http://localhost:3000/pfamB/domaingraphics?acc=PB000067
 
 sub getData : Path {
   my( $this, $c ) = @_;
+  my $auto = $c->stash->{pfam}->auto_pfamB;
 
-#   my $auto = $c->stash->{pfam}->auto_pfamB;
 
-#   my @architectures =
-# 	$c->model("PfamDB::PfamB_reg")->search( { auto_pfamB => $auto },
-# 											{ join      => [ qw/ pfamseq_architecture  /],
-# 											  select    => [ { count => "auto_architecture" }, x ],
-# 											  as        => [ qw/ count x / ],
-# 											  group_by  => [ qw/ auto_architecture / ],
-# 											  order_by  => "count DESC" }
-# 										  );
-#   $c->log->debug( "found " . scalar @architectures . " architectures" );
+  #Get a list of all the sequneces and unique archiectures for this family;
+  my( $arch ) = $c->req->param( "arch" ) =~ /^(\d+|nopfama)$/i;
 
-#   my @seqs;
-#   foreach my $arch ( @architectures ) {
-# 	push @seqs, thaw( $arch->annseq_storable );
-#   }
-#   $c->log->debug( "found " . scalar @seqs . " storables" );
 
-#   my $layout = Bio::Pfam::Drawing::Layout::PfamLayoutManager->new;
-#   #$layout->scale_x( $this->{scale_x} ); #0.33
-#   #$layout->scale_y( $this->{scale_y} ); #0.45
+  my (@seqs, %seenArch, %example , @architectures);
+  if($arch){
+    $arch = lc($arch);
+    if($arch eq "nopfama"){
+      ###hmmm we are going to have to get everything and throw away stuff with no auto_architecture.....
+      @architectures =
+	$c->model("PfamDB::PfamB_reg")->search( { auto_pfamB => $auto },
+						{join      => [ qw/ pfamseq_architecture annseq pfamseq/],
+						 prefetch  => [ qw/ pfamseq_architecture annseq pfamseq/]
+						});
+      foreach my $arch ( @architectures ) {
+	if($arch->auto_architecture !~ /\d+/){
+	  push @seqs, thaw( $arch->annseq_storable );
+	}
+      }
+    }else{
+      
+      @architectures =
+	$c->model("PfamDB::PfamB_reg")->search( { auto_pfamB => $auto,
+						  auto_archiecture => $arch },
+						{join      => [ qw/ pfamseq_architecture annseq pfamseq/],
+						 prefetch  => [ qw/ pfamseq_architecture annseq pfamseq/]
+						});
+      #query has already done the selection
+      foreach my $arch ( @architectures ) {
+	push @seqs, thaw( $arch->annseq_storable );
+      }
+    }#End if/else
+  }else{
+    @architectures =
+      $c->model("PfamDB::PfamB_reg")->search( { auto_pfamB => $auto },
+					      {join      => [ qw/ pfamseq_architecture annseq pfamseq/],
+					       prefetch  => [ qw/ pfamseq_architecture annseq pfamseq/]
+					      });
 
-#   my %regionsAndFeatures = ( "PfamA"      => 1,
-# 							 "noFeatures" => 1 );
-#   $layout->layout_sequences_with_regions_and_features( \@seqs, \%regionsAndFeatures );
+    $c->log->debug( "found " . scalar @architectures . " architectures" );
+    foreach my $arch ( @architectures ) {
+      if(!$seenArch{$arch->auto_architecture}){
+	push @seqs, thaw( $arch->annseq_storable );
+	$example{$arch->pfamseq_acc} = $arch->auto_architecture;
+      }
+      $seenArch{$arch->auto_architecture}++;
+    }
+  }
+  $c->log->debug( "found " . scalar @seqs . " storables" );
 
-#   my $imageset = Bio::Pfam::Drawing::Image::ImageSet->new;
-#   $imageset->create_images( $layout->layout_to_XMLDOM );
+  my $layout = Bio::Pfam::Drawing::Layout::PfamLayoutManager->new;
+  my %regionsAndFeatures = ( "PfamA"      => 1,
+			     "PfamB"      => 1,
+			     "noFeatures" => 1 );
+  $layout->layout_sequences_with_regions_and_features( \@seqs, \%regionsAndFeatures );
+  my $imageset = Bio::Pfam::Drawing::Image::ImageSet->new;
+  $imageset->create_images( $layout->layout_to_XMLDOM );
 
-#   $c->stash->{images} = $imageset;
 
-#   # set up the view and rely on "end" from the parent class to render it
-#   $c->stash->{template} = "components/blocks/family/domainSummary.tt";
+  $c->log->debug(dump(\%seenArch));
+
+  $c->stash->{images} = $imageset;
+  $c->stash->{archExamples} = \%example;
+  $c->stash->{counts} = \%seenArch;
+  #   # set up the view and rely on "end" from the parent class to render it
+  $c->stash->{template} = "components/blocks/family/domainSummary.tt";
+
+
 
 }
 
