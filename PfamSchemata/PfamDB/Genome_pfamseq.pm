8f884cb0efe46b8cc2977acf36a0a6dc3b0b54b3
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-10-31 13:48:30 +0000

    First pass at genome tables

---
+++ PfamSchemata/PfamDB/Genome_pfamseq.pm
@@ -0,0 +1,34 @@
+package PfamDB::Genome_pfamseq;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+#Set up the table
+__PACKAGE__->table( "genome_pfamseq" );
+
+#Get the columns that we want to keep
+__PACKAGE__->add_columns( qw/auto_pfamseq ncbi_code/);
+
+#Set the the keys
+__PACKAGE__->set_primary_key( "auto_pfamseq", "ncbi_code" );
+
+
+#Now Set up the relationships
+
+
+#Do all of the annotated regions
+
+##pfamseq
+__PACKAGE__->has_one("pfamseq",  => "PfamDB::Pfamseq",
+		     {"foreign.auto_pfamseq" => "self.auto_pfamseq"} );
+
+##genome_species
+__PACKAGE__->has_one( "genome_sepcies",  => "PfamDB::Genome_species",
+		      {"foreign.ncbi_code" => "self.ncbi_code"} );
+
+
+1;
---
+++ PfamSchemata/PfamDB/Genome_species.pm
@@ -0,0 +1,29 @@
+package PfamDB::Genome_species;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+#Set up the table
+__PACKAGE__->table( "genome_species" );
+
+#Get the columns that we want to keep
+__PACKAGE__->add_columns( qw/ncbi_code species grouping num_distinct_regions num_total_regions num_proteins sequence_coverage residue_coverage total_genome_proteins total_aa_length/);
+
+#Set the the keys
+__PACKAGE__->set_primary_key( "ncbi_code" );
+
+
+#Now Set up the relationships
+
+
+#Do all of the annotated regions
+
+##genome pfamseq
+__PACKAGE__->has_many("genomePfamSeq",  => "PfamDB::Genome_pfamseq",
+		     {"foreign.ncbi_code" => "self.ncbi_code"} );
+
+1;
