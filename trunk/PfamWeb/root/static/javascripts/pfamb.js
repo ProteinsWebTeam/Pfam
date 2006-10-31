4d292470111004279454fde88e0450e78e810ec5
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-10-31 15:08:57 +0000

    Add post-load hooks for new data.

--- trunk/PfamWeb/root/static/javascripts/pfamb.js
+++ trunk/PfamWeb/root/static/javascripts/pfamb.js
@@ -4,17 +4,27 @@
 //
 // javascript glue for the PfamB section
 //
-// $Id: pfamb.js,v 1.1 2006-10-23 12:09:25 jt6 Exp $
+// $Id: pfamb.js,v 1.2 2006-10-31 15:08:57 jt6 Exp $
 
 // this will make the ajax calls for the family page components
 
 function pfambPostLoad() {
-  new Ajax.Request( loadOptions.st.uri,
- 	                { method:     "get", 
- 					  parameters: loadOptions.st.params,
-                      onComplete: stSuccess,
-                      onFailure:  stFailure
- 					} );
+  if( typeof( loadOptions.st.uri ) != "undefined" ) {
+	new Ajax.Request( loadOptions.st.uri,
+					  { method:     "get", 
+						parameters: loadOptions.st.params,
+						onComplete: stSuccess,
+						onFailure:  stFailure
+					  } );
+  }
+  if( typeof( loadOptions.dg.uri ) != "undefined" ) {
+	new Ajax.Request( loadOptions.dg.uri,
+					  { method:     'get', 
+						parameters: loadOptions.dg.params,
+						onComplete: dgSuccess,
+						onFailure:  dgFailure
+					  } );
+  }
 }
 
 //------------------------------------------------------------
