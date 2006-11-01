60d77886c693f035712404b669f3295cdfc6eb60
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-11-01 13:12:22 +0000

    Started getting the Proteome data out of the database.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Proteome.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Proteome.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build the main Pfam Proteome page.
 #
-# $Id: Proteome.pm,v 1.1 2006-08-22 13:24:05 rdf Exp $
+# $Id: Proteome.pm,v 1.2 2006-11-01 13:12:22 rdf Exp $
 
 package PfamWeb::Controller::Proteome;
 
@@ -12,55 +12,25 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use Data::Dumper;
 
-use base "Catalyst::Controller";
+use base "PfamWeb::Controller::Section";
+
+# set the name of the section
+__PACKAGE__->config( SECTION => "proteome" );
 
 sub begin : Private {
   my( $this, $c ) = @_;
 
   if( defined $c->req->param( "ncbiCode" ) ) {
 
-    $c->req->param( "ncbiCode" ) =~ m/^(\d+)$/i;
-    $c->log->info( "Proteome::begin: found ncbiCode |$1|" );
-    
-    #$c->stash->{clan} = $c->model("PfamDB::Clans")->find( { clan_acc => $1 } )
-    #if defined $1;
-    
+    my ($ncbiCode) = $c->req->param( "ncbiCode" ) =~ m/^(\d+)$/i;
+    $c->log->info( "Proteome::begin: found ncbiCode |$ncbiCode|" );
+
+    $c->stash->{proteomeSpecies} = $c->model("PfamDB::Proteome_species")->find( { ncbi_code => $ncbiCode});
   }
 }
 
-#-------------------------------------------------------------------------------
-sub generateSummary : Path {
-  my( $this, $c ) = @_;
-
-  # empty; just here to capture the URL
-}
-
-#-------------------------------------------------------------------------------
-# default end
-
-sub end : Private {
-  my( $this, $c ) = @_;
-
-  # don't try to render unless there's a Clan object in the stash
-  #return 0 unless defined $c->stash->{clan};
-
-  # check for errors
-  if ( scalar @{ $c->error } ) {
-	$c->stash->{errors}   = $c->error;
-	$c->stash->{template} = "components/blocks/proteome/errors.tt";
-  } else {
-	$c->stash->{pageType} = "proteome";
-	$c->stash->{template} = "pages/layout.tt";
-  }
-
-  # and use it
-  $c->forward( "PfamWeb::View::TT" );
-
-  # clear any errors
-  $c->error(0);
-
-}
 
 #-------------------------------------------------------------------------------
 
