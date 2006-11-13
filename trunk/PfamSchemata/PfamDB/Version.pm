688277f7d689250fdbc5a3f54ea9feb235699476
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-11-13 11:47:00 +0000

    Added release date column.

--- trunk/PfamSchemata/PfamDB/Version.pm
+++ trunk/PfamSchemata/PfamDB/Version.pm
@@ -11,7 +11,11 @@ __PACKAGE__->load_components( qw/Core/ );
 __PACKAGE__->table( "VERSION" );
 
 #Get the columns that we want to keep
-__PACKAGE__->add_columns( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);
+__PACKAGE__->add_columns( qw/pfam_release
+							 pfam_release_date
+							 swiss_prot_version
+							 trembl_version
+							 hmmer_version/);
 
 __PACKAGE__->set_primary_key( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);
 
