71bc2f04a28a00b8013e88b8700f96560bca9ee4
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2006-11-15 11:13:07 +0000

    Model for new table contating PfamA relationships based on SCOOP analysis

---
+++ PfamSchemata/PfamDB/PfamA2pfamA_scoop_results.pm
@@ -0,0 +1,28 @@
+package PfamDB::PfamA2pfamA_scoop_results;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+
+__PACKAGE__->load_components( qw/Core/); #Do we want to add DB
+__PACKAGE__->table("pfamA2pfamA_scoop_results"); # This is how we define the table
+__PACKAGE__->add_columns( qw/ auto_pfamA1 auto_pfamA2 score /); # The columns that we want to have access to
+
+#Set up the primary keys
+__PACKAGE__->set_primary_key( "auto_pfamA1", "auto_pfamA2");
+
+#Set up relationships
+#1 to many relationship
+__PACKAGE__->has_one( "pfamA1" => "PfamDB::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA1"},);
+		      #{proxy => [qw/pfamA_id pfamA_acc/]});
+
+__PACKAGE__->has_one( "pfamA2" => "PfamDB::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA2"},);
+		      #{proxy => [qw/pfamA_id pfamA_acc/]});
+
+
+
+1;
