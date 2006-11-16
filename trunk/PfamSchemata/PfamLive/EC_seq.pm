e0afdca33b9b57495949158efc5b24291a091fd9
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-11-16 11:10:43 +0000

    Initial check-in of table definitions for the pfamlive database.

---
+++ trunk/PfamSchemata/PfamLive/EC_seq.pm
@@ -0,0 +1,35 @@
+
+package PfamLive::EC_seq;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+__PACKAGE__->table( "ec_seq" );
+
+__PACKAGE__->add_columns( qw/ index_pfamA_ec
+							  auto_pfamseq / );
+
+__PACKAGE__->set_primary_key( "index_pfamA_ec" );
+
+__PACKAGE__->has_one( EC_info => "PfamLive::EC_info",
+					  { "foreign.index_pfamA_ec" => "self.index_pfamA_ec" },
+					  { proxy                    => [ qw/ index_pfamA_ec
+														  auto_pfamA
+														  ec_number
+														  reaction
+														  number_seq
+														  in_family / ] } );
+
+__PACKAGE__->might_have( pfamseq_markup => "PfamLive::Pfamseq_markup",
+						 { "foreign.auto_pfamseq" => "self.auto_pfamseq" },
+						 {  proxy                 => [ qw/ auto_markup / ] });
+
+__PACKAGE__->might_have( pfamseq => "PfamLive::Pfamseq_markup",
+						 { "foreign.auto_pfamseq" => "self.auto_pfamseq" },
+						 {  proxy                 => [ qw/ pfamseq_acc pfamseq_id / ] });
+
+1;
---
+++ trunk/PfamSchemata/PfamLive/Pfamseq_markup.pm
@@ -0,0 +1,33 @@
+package PfamLive::Pfamseq_markup;
+
+use strict;
+use warnings;
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+#Set up the table
+__PACKAGE__->table( "pfamseq_markup" );
+
+#Get the columns that we want to keep
+__PACKAGE__->add_columns(qw/auto_pfamseq auto_markup residue annotation/);
+
+__PACKAGE__->set_primary_key("auto_pfamseq");
+
+__PACKAGE__->has_one("pfamseq" => "PfamLive::Pfamseq",
+		     {"foreign.auto_pfamseq" => "self.auto_pfamseq"},
+		     {proxy => [qw/pfamseq_id pfamseq_acc/]});
+
+# renamed this relationship from "auto_markup" because it was blocking
+# access to the column of that name.
+# jt6 20060801 WTSI
+__PACKAGE__->has_one("autoMarkup" => "PfamLive::Markup_key",
+		     {"foreign.auto_markup" => "self.auto_markup"},
+		     {proxy => [qw/label/]});
+
+__PACKAGE__->might_have("pdbResidue" => "PfamLive::Pdb_residue",
+						{"foreign.auto_pfamseq" => "self.auto_pfamseq",
+						 "foreign.pfamseq_seq_number" => "self.residue"},
+						{proxy => [qw/chain pdb_seq_number pdb_res auto_pdb pfamseq_seq_number/]});
+
+1;
