21f5f1055e88658a1bb65db8b434bdca13b9ad14
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2006-11-16 11:10:43 +0000

    Initial check-in of table definitions for the pfamlive database.

---
+++ PfamSchemata/PfamLive/Interpro.pm
@@ -0,0 +1,18 @@
+
+package PfamLive::Interpro;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+__PACKAGE__->table( "interpro" );
+__PACKAGE__->add_columns( qw/auto_pfamA interpro_id abstract/ );
+__PACKAGE__->set_primary_key( "auto_pfamA" );
+
+__PACKAGE__->has_one( "pfamA" => "PfamLive::Pfam",
+		      {"foreign.auto_pfamA"  => "self.auto_pfamA" },
+		      { proxy => [ qw/ pfamA_acc pfamA_id description / ] } );
+1;
+
