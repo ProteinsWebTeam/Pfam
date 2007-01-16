00bca9fa246645e76cfe37ea3b40bf9f70842103
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-01-16 08:57:41 +0000

    Altered to cope with lots of new columns in the DB.

--- trunk/PfamSchemata/PfamDB/Version.pm
+++ trunk/PfamSchemata/PfamDB/Version.pm
@@ -12,10 +12,15 @@ __PACKAGE__->table( "VERSION" );
 
 #Get the columns that we want to keep
 __PACKAGE__->add_columns( qw/pfam_release
-							 pfam_release_date
-							 swiss_prot_version
-							 trembl_version
-							 hmmer_version/);
+														 pfam_release_date
+														 swiss_prot_version
+														 trembl_version
+														 hmmer_version
+														 number_families
+														 pfamA_coverage
+														 pfamB_additional_coverage
+														 pfamA_residue_coverage
+														 pfamB_additional_residue_coverage/);
 
 __PACKAGE__->set_primary_key( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);
 
