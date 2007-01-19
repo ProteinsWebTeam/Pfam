59a9588173ac7bce9732c575f46889e15b2e5daa
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-01-19 16:41:31 +0000

    Add the names of required classes to the "load_classes" call, to avoid problems with insecure dependencies when running under taint mode.

--- trunk/PfamSchemata/WebUser.pm
+++ trunk/PfamSchemata/WebUser.pm
@@ -4,7 +4,7 @@
 
 # The base class for the web_user tables
 
-# $Id: WebUser.pm,v 1.2 2006-09-29 15:23:22 jt6 Exp $
+# $Id: WebUser.pm,v 1.3 2007-01-19 16:41:31 jt6 Exp $
 
 package WebUser;
 
@@ -13,7 +13,7 @@ use warnings;
 
 use base "DBIx::Class::Schema";
 
-__PACKAGE__->load_classes();
+__PACKAGE__->load_classes( qw/ Das_sources  ErrorLog  Family_count  News  Users / );
 
 1;
 
