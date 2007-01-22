ec9e5509c67f81f533ab3a01043df1edd3c23f1d
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-01-22 15:01:21 +0000

    Various changes, some unfinished.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
@@ -2,7 +2,7 @@
 # Root.pm
 # jt 20061003 WTSI
 #
-# $Id: Root.pm,v 1.6 2007-01-15 14:40:03 jt6 Exp $
+# $Id: Root.pm,v 1.7 2007-01-22 15:01:21 jt6 Exp $
 
 =head1 NAME
 
@@ -15,12 +15,10 @@ package PfamWeb::Controller::Root;
 =head1 DESCRIPTION
 
 This is the root class for the Pfam website catalyst application. It
-installs global actions for the main site index page, a catch for
-errors from within the site, an C<auto> action that handles tab
-selection for the whole site, and a default C<end> that renders the
-index page.
+installs global actions for the main site index page and other top-level
+functions.
 
-$Id: Root.pm,v 1.6 2007-01-15 14:40:03 jt6 Exp $
+$Id: Root.pm,v 1.7 2007-01-22 15:01:21 jt6 Exp $
 
 =cut
 
@@ -32,70 +30,43 @@ use base "Catalyst::Controller";
 
 # sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config->{namespace} = '';
 
 #-------------------------------------------------------------------------------
 
 =head1 METHODS
 
-=head2 index : Private
-
-Drops straight to the site index page.
-
-=cut
-
-#sub index : Private {
-#	my( $this, $c ) = @_;
-#
-#}
-
-#-------------------------------------------------------------------------------
-
 =head2 default : Private
 
-Catch unexpected redirects, etc. This action just drops straight to
-the site index page but it records the redirect in the error database
-before that.
+Generates the main site index page. Also traps 404s and redirects to a 404 page.
 
 =cut
 
 sub default : Private {
   my( $this, $c ) = @_;
 
+  $c->log->debug( "Root.pm: action: " . $c->req->action );
+  $c->log->debug( "Root.pm: URI:    " . $c->req->uri );
+  $c->log->debug( "Root.pm: this:   " . $c->uri_for( "/" ) );
+
+  # before we go too far, make sure this isn't a broken URL
+  $c->res->redirect( $c->uri_for( "/404" ), 404 )
+   unless $c->req->uri eq $c->uri_for( "/" );
+
+  # now, knowing that this is a request for the site index page...
+
   # retrieve the news items from the feed
   my @entries = $c->model("WebUser::News")
-	->search( {}, { order_by => "pubDate DESC" } );
+                	->search( {}, { order_by => "pubDate DESC" } );
   $c->stash->{newsItems} = \@entries;
-  $c->log->debug( "PfamWeb::index: found " . scalar @entries . " news items " );
 
   # get the top-ten families
   my @topTen = $c->model("WebUser::Family_count")
-	->search( {},
-			  { order_by => "view_count DESC" } );
+                 ->search( {},
+                           { order_by => "view_count DESC" } );
   $c->stash->{topTen} = \@topTen;
 
-  $c->log->debug("PfamWeb::index: generating site index");
-
-  # record an error message, because we shouldn't error arrive here
-  # unless via a broken link, missing page, etc.
-
-  # first, figure out where the broken link was, internal or external
-  #	my $ref = ( defined $c->req->referer ) ? $c->req->referer : "";
-  #	my $where = ( $ref =~ /sanger/ ) ? "internal" : "external";
-  #
-  #	# record the error
-  #	$c->error(   "Found a broken $where link: \""
-  #						 . $c->req->uri
-  #						 . "\", referer: "
-  #						 . ( $ref eq "" ? "unknown" : "\"" . $ref . "\"" ) );
-  #
-  #	# report it
-  #	$c->forward("/reportError");
-  #
-  #	# and clear the errors before we render the page
-  #	$c->clear_errors;
-
+  $c->log->debug("PfamWeb::default: generating site index");
 }
 
 #-------------------------------------------------------------------------------
@@ -107,30 +78,38 @@ appropriate tab name in the stash. That will be picked up by the
 tab_layout.tt view, which will use it to figure out which tab to show
 by default.
 
-Also adds the Pfam version and release date to the stash, so that it's
-generally accessible throughout the app.
+Also adds the Pfam version data to the stash, so that it's generally 
+accessible throughout the app.
 
 =cut
 
 sub auto : Private {
   my( $this, $c ) = @_;
 
-  #	return 1 if $c->controller eq $c->controller("Auth");
-  #
-  #  unless( $c->user_exists ) {
-  #    $c->log->debug( "Root::auto: unknown user; redirecting to login" );
-  #    $c->res->redirect( $c->uri_for( "/login" ) );
-  #		return 0;
-  #  }
-  #  $c->log->debug( "Root::auto: user authenticated" );
+  #----------------------------------------
+  # authentication/authorisation stuff...
+  
+#  return 1 if $c->controller eq $c->controller("Auth");
+# 
+#  unless( $c->user_exists ) {
+#    $c->log->debug( "Root::auto: unknown user; redirecting to login" );
+#    $c->res->redirect( $c->uri_for( "/login" ) );
+#    return 0;
+#  }
+#  $c->log->debug( "Root::auto: user authenticated" );
 
+  #----------------------------------------
+  # tack on a trailing slash, if required
+  
   # if the URL doesn't end in a slash, tack one on
-  #	if( $c->req->path and $c->req->path !~ /\/$/ and not $c->req->param ) {
-  #		$c->log->debug( "Root::auto: appending a trailing slash to the URL: "
-  #										 . $c->req->path );
-  #		$c->res->redirect( "/" . $c->req->path . "/", 301);
-  #		return 0;
-  #	}
+  #if( $c->req->path and $c->req->path !~ /\/$/ and not $c->req->param ) {
+  #	$c->log->debug( "Root::auto: appending a trailing slash to the URL: "
+  #									 . $c->req->path );
+  #	$c->res->redirect( "/" . $c->req->path . "/", 301);
+  #	return 0;
+  #}
+
+  #----------------------------------------
 
   # pick a tab
   my $tab;
@@ -167,13 +146,13 @@ sub graph : Local {
 
 #-------------------------------------------------------------------------------
 
-=head2 about : Global
+=head2 about : Local
 
 Show an "about Pfam" page.
 
 =cut
 
-sub about : Global {
+sub about : Local {
   my( $this, $c ) = @_;
 
   $c->stash->{template} = "components/tools/about.tt";
@@ -181,13 +160,13 @@ sub about : Global {
 
 #-------------------------------------------------------------------------------
 
-=head2 annotate : Global
+=head2 annotate : Local
 
 Show an annotation upload form.
 
 =cut
 
-sub annotate : Global {
+sub annotate : Local {
   my( $this, $c ) = @_;
 
   $c->stash->{template} = "pages/annotation.tt";
@@ -195,22 +174,51 @@ sub annotate : Global {
 
 #-------------------------------------------------------------------------------
 
-=head2 end : Private
+=head2 fourOhFour : Path( "/404" )
 
-Renders the index page for the site, ignoring any errors that were
-encountered up to this point.
+Generates a "404" page.
 
 =cut
 
-sub end : Private {
+sub fourOhFour : Path( "/404" ){
   my( $this, $c ) = @_;
 
-  unless( $c->response->body ) {
-	$c->stash->{fullPage} = 1;
-	$c->stash->{template} ||= "pages/index.tt";
-	$c->forward( "PfamWeb::View::TT" );
-  }
+  $c->log->debug( "Root.pm: 404 !" );
+  $c->stash->{template} = "pages/404.tt";
+}
 
+  # record an error message, because we shouldn't error arrive here
+  # unless via a broken link, missing page, etc.
+
+  # first, figure out where the broken link was, internal or external
+  #	my $ref = ( defined $c->req->referer ) ? $c->req->referer : "";
+  #	my $where = ( $ref =~ /sanger/ ) ? "internal" : "external";
+  #
+  #	# record the error
+  #	$c->error(   "Found a broken $where link: \""
+  #						 . $c->req->uri
+  #						 . "\", referer: "
+  #						 . ( $ref eq "" ? "unknown" : "\"" . $ref . "\"" ) );
+  #
+  #	# report it
+  #	$c->forward("/reportError");
+  #
+  #	# and clear the errors before we render the page
+  #	$c->clear_errors;
+
+
+#-------------------------------------------------------------------------------
+
+=head2 end : Private
+
+Renders the index page for the site by default, but the default template can be 
+overridden by setting it in an action (eg for a 404 template).
+
+=cut
+
+sub end : ActionClass( "RenderView" ) {
+  my( $this, $c ) = @_;
+  $c->stash->{template} ||= "pages/index.tt";
 }
 
 #-------------------------------------------------------------------------------
@@ -228,4 +236,4 @@ it under the same terms as Perl itself.
 
 =cut
 
-  1;
+1;
