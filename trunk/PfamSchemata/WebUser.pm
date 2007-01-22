7a7630c3f1f9edca09627663de2854adbd9b1a8a
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-01-22 09:13:54 +0000

    Remove the reference to User.pm, because that's not currently checked in.

--- trunk/PfamSchemata/WebUser.pm
+++ trunk/PfamSchemata/WebUser.pm
@@ -4,7 +4,7 @@
 
 # The base class for the web_user tables
 
-# $Id: WebUser.pm,v 1.3 2007-01-19 16:41:31 jt6 Exp $
+# $Id: WebUser.pm,v 1.4 2007-01-22 09:13:54 jt6 Exp $
 
 package WebUser;
 
@@ -13,7 +13,7 @@ use warnings;
 
 use base "DBIx::Class::Schema";
 
-__PACKAGE__->load_classes( qw/ Das_sources  ErrorLog  Family_count  News  Users / );
+__PACKAGE__->load_classes( qw/ Das_sources  ErrorLog  Family_count  News  / );
 
 1;
 
