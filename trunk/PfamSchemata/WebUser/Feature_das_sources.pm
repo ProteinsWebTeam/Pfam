d13826f3b5a22d8276095b2dc26878ec9f980172
aj5 <aj5@98333f76-0502-11df-81af-936bbf3a1e26>
2007-02-07 18:15:28 +0000

    New tables

---
+++ trunk/PfamSchemata/WebUser/Feature_das_sources.pm
@@ -0,0 +1,36 @@
+
+# Feature_das_sources.pm
+# aj5
+#
+# Model for the das_sources table.
+#
+
+package WebUser::Feature_das_sources;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+# set up the table
+__PACKAGE__->table( "feature_das_sources" );
+
+# get the columns that we want to keep
+__PACKAGE__->add_columns( qw/ server_id name url system sequence_type helper_url default_server /);
+
+# set up the primary keys/contraints
+__PACKAGE__->set_primary_key( qw/ server_id system sequence_type / );
+
+# relationships
+
+__PACKAGE__->might_have( alignment_sources_from => "WebUser::Alignment_das_sources",
+						{	"foreign.from_system"	=> "self.system",
+						 	"foreign.from_type"		=> "self.sequence_type"	} );
+
+__PACKAGE__->might_have( alignment_sources_to => "WebUser::Alignment_das_sources",
+						{	"foreign.to_system"	=> "self.system",
+						 	"foreign.to_type"	=> "self.sequence_type"	} );
+
+1;
