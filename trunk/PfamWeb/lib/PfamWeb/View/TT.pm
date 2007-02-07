f1fd280904a9b3b85f88468e94c554a8ea39a9f2
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-02-07 11:06:29 +0000

    Add "use warnings" to keep webpublish happy.

--- trunk/PfamWeb/lib/PfamWeb/View/TT.pm
+++ trunk/PfamWeb/lib/PfamWeb/View/TT.pm
@@ -5,11 +5,13 @@
 # An empty view class. Everything clever is handled by the controllers
 # or the templates themselves.
 #
-# $Id: TT.pm,v 1.2 2006-03-16 17:45:06 jt6 Exp $
+# $Id: TT.pm,v 1.3 2007-02-07 11:06:29 jt6 Exp $
 
 package PfamWeb::View::TT;
 
 use strict;
+use warnings;
+
 use base 'Catalyst::View::TT';
 
 =head1 NAME
