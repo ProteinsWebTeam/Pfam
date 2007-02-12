70a516e2bf8da2778ab714edb0bf3452b5b701e7
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2007-02-12 14:29:35 +0000

    Adding tables to Pfamlive schema from PfamDB.  they may still need some tweaking.

---
+++ PfamSchemata/PfamLive/Clan_database_links.pm
@@ -0,0 +1,23 @@
+package PfamLive::Clan_database_links;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+
+__PACKAGE__->load_components( qw/Core/); #Do we want to add DB
+__PACKAGE__->table("clan_database_links"); # This is how we define the table
+__PACKAGE__->add_columns( qw/auto_clan db_id comment db_link other_params/); # The columns that we want to have access to
+__PACKAGE__->set_primary_key( "auto_clan");
+
+#Set up relationships
+
+#1 to 1 relationship that must be there.....
+__PACKAGE__->has_one( "clans" => "PfamLive::Clans",
+		       {"foreign.auto_clan"  => "self.auto_clan"});
+
+
+
+
+1;
