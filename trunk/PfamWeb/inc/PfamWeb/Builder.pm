7944dcc9fe3a2d6913ff5f1402f558aaaf013a9b
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-02-28 14:43:19 +0000

    Checking in so I can move things around in the repository again. I love doing that...

--- trunk/PfamWeb/inc/PfamWeb/Builder.pm
+++ trunk/PfamWeb/inc/PfamWeb/Builder.pm
@@ -5,7 +5,7 @@
 # A custom Module::Builder subclass to handle installation of the
 # PfamWeb application
 #
-# $Id: Builder.pm,v 1.4 2007-02-27 17:06:34 jt6 Exp $
+# $Id: Builder.pm,v 1.5 2007-02-28 14:43:00 jt6 Exp $
 
 package PfamWeb::Builder;
 
@@ -14,68 +14,52 @@ use warnings;
 
 use base "Module::Build";
 
+use File::Basename;
 use File::Path;
-use File::Copy;
-use File::Find;
+use File::Spec::Functions;
 use Config::General;
 
 use Data::Dump qw(dump);
-
-# a "global" so that we have somewhere to keep track of the MB object
-# and similarly the config hash that we built
-my( $build, $config );
 
 #-------------------------------------------------------------------------------
 #- new elements ----------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-# copied directly from Module::Build::Base::_find_file_by_type
-
-sub local_find_file_by_type  {
-  my( $this, $type, $dir ) = @_;
-  return { map {$_, $_}
-    map $this->localize_file_path( $_ ),
-    grep !/\.\#/, @{ $this->rscan_dir( $dir, qr{\.$type$} ) } };
-}
-
-sub find_conf_files {
-  return shift->local_find_file_by_type( "conf", "conf" );
-}
-
-sub find_data_files {
-  return shift->local_find_file_by_type( "tt", "data" );
-}
+# copy the contents of htdocs (CSS, images, javascripts, jars) across
 
 sub process_htdocs_files {
   my $this = shift;
 
-  print "***** processing htdocs files...\n";
-        
   # shortcuts...
   my $defRoot = $this->notes( "defaultAppRoot" );
   my $appRoot = $this->config_data( "configHash" )->{View}->{TT}->{CONSTANTS}->{root};
-  
-  my $htdocsDir = File::Spec->catdir( $this->blib, "htdocs" );
-  File::Path::mkpath( $htdocsDir );
+
+  # make a directory for all of this...  
+  mkpath( catdir( $this->blib, "htdocs" ) );
   
   my $files = $this->find_htdocs_files;
-  print "--------------------------------------------------\n";
-  print dump $files;
-  print "--------------------------------------------------\n";
 
   while( my( $from, $to ) = each %$files ) {
-    print "***** checking |$from| -> |$to|\n";
     next if $this->up_to_date( $from, $to );
-    print "***** |$from| -> |$to| needs update\n";
     
-    if( m/\.(css|js)$/ and $defRoot ne $appRoot ) {
+    print "$from -> $to\n";
+    
+    # only need to edit CSS and JS files, and only if the default application 
+    # root has been changed by the user
+    if( $from =~ /\.(css|js)$/ and $defRoot ne $appRoot ) {
         
-    	print "processing $from\n" if $build->args( "verbose_log" );
-    
     	open( IN, $from )
     	  or warn "(WW) WARNING: couldn't open file \"$from\": $!" and return;    
-    	open( OUT, ">to" )
-    	  or warn "(WW) WARNING: couldn't write file \"$to\": $!"  and return;
+    	  
+  	  # make sure the output directory exists...
+      eval {
+    	  mkpath( dirname( $to ), 0, 0777 ) unless -d $to;    	  
+    	  open( OUT, ">$to" );
+      };
+      if( $@ ) {
+    	  warn "(WW) WARNING: couldn't write file \"$to\": $!";
+    	  return;
+      }
     
     	# get the default value and the new value of the URL root from the
     	# notes and build a hideous regex to replace one with the other
@@ -88,107 +72,54 @@ sub process_htdocs_files {
     	close OUT;
   	
     } else {
-
-    	# not a file that we need to firkle with; just copy it into blib
-    	print "copying $from -> $to\n" if $this->args( "verbose_log" );
-    
       $this->copy_if_modified( from => $from, to => $to );
     }
 
-    $this->add_to_cleanup( $to );
   }
 
 }
 
-sub find_htdocs_files {
+sub find_core_files {
   my $this = shift;
-  
-return { map {$_, File::Spec->catdir( $this->blib, $_ ) }
-         map $this->localize_file_path($_),
-         grep !/\.\#/,
-         @{ $this->rscan_dir( "htdocs", sub { ! -d and $File::Find::dir !~ /CVS$/ } ) } };
+  return $this->local_find_file_by_type( "pm", $this->notes( "pfamCoreDir" ) ); 
+}
+
+sub find_model_files {
+  my $this = shift;
+  return $this->local_find_file_by_type( "pm", $this->notes( "pfamModelDir" ) ); 
 }
 
 #-------------------------------------------------------------------------------
-# copy the contents of htdocs (CSS, images, javascripts, jars) across
+# We've implicitly added process_conf, process_data and process_htdocs methods
+# by adding the new build elements in the mail Build.PL script, and these 
+# "find" methods are used by MB to allow the various process_* methods to work.
 
-sub old_process_htdocs_files {
-  my $this = shift;
-
-  # get a copy of the MB object for use in the callback
-  $build = $this unless defined $build;
-
-  # retrieve the configuration hash that we build from user input
-  $config = $this->config_data( "configHash" ) unless defined $config;
-
-  print "(ii) copying static files\n";
-  mkpath "blib/htdocs";
-
-  # walk the tree from "data" downwards and copy any template file
-  # that we find into the blib tree
-  finddepth( { wanted     => \&forEachStaticFile,
-      			   no_chdir   => 1 },
-        			 "htdocs" );
-
+# this one is copied directly from Module::Build::Base::_find_file_by_type
+sub local_find_file_by_type  {
+  my( $this, $type, $dir ) = @_;
+  return { map {$_, $_}
+    map $this->localize_file_path( $_ ),
+    grep !/\.\#/, @{ $this->rscan_dir( $dir, qr{\.$type$} ) } };
 }
 
-#----------------------------------------
-# callback for handling CSS and JS files
+# and using that...
+sub find_conf_files {
+  return shift->local_find_file_by_type( "conf", "conf" );
+}
 
-sub forEachStaticFile {
+sub find_data_files {
+  return shift->local_find_file_by_type( "tt", "data" );
+}
 
-  print "\$File::Find::dir:  |$File::Find::dir|\n"  if $build->args( "verbose_log" ) > 1;
-  print "\$_:                |$_|\n"                if $build->args( "verbose_log" ) > 1;
-  print "\$File::Find::name: |$File::Find::name|\n" if $build->args( "verbose_log" ) > 1;
-
-  # skip CVS directories entirely
-  return if $File::Find::dir =~ /CVS$/ or -d;
-
-  # see if the derived file is up to date
-  return if $build->up_to_date( "$File::Find::name", "blib/$File::Find::name" );
-
-  # make the directory before we try to copy stuff into it...
-  mkpath "blib/$File::Find::dir"
-	or warn "(WW) WARNING: couldn't create directory \"blib/$File::Find::dir\": $!"
-	  unless -d "blib/$File::Find::dir";
-
-  # handle the CSS/JS files. See if the user has changed the default
-  # setting for the application root URL: if it's changed, we need to
-  # edit the files; if not, copy them.
-
-  if( m/\.(css|js)$/ and 
-      $build->notes( "defaultAppRoot" ) ne $config->{View}->{TT}->{CONSTANTS}->{root} ) {
-
-  	print "processing $File::Find::name -> blib/$File::Find::dir\n"
-  	  if $build->args( "verbose_log" );
+# a bit more specialised: find any file in the htdocs directory, but ignore
+# CVS directories
+sub find_htdocs_files {
+  my $this = shift;
   
-  	open( IN, $File::Find::name )
-  	  or warn "(WW) WARNING: couldn't open ".uc($1)." file \"$File::Find::name\": $!"
-  		and return;
-  
-  	open( OUT, ">blib/$File::Find::name" )
-  	  or warn "(WW) WARNING: couldn't write ".uc($1)." file \"blib/$File::Find::name\": $!"
-  		and return;
-  
-  	# get the default value and the new value of the URL root from the
-  	# notes and build a hideous regex to replace one with the other
-  	while( <IN> ) {
-  	  s/(??{$build->notes("defaultAppRoot")})/$config->{View}->{TT}->{CONSTANTS}->{root}/eog;
-  	  print OUT $_;
-  	}
-  
-  	close IN;
-  	close OUT;
-	
-  } else {
-
-  	# not a file that we need to firkle with; just copy it into blib
-  	print "copying $File::Find::name -> blib/$File::Find::name\n"
-  	  if $build->args( "verbose_log" );
-  
-  	copy( "$File::Find::name", "blib/$File::Find::dir" )
-  	  or warn "(WW) WARNING: couldn't copy static file \"$_\": $!";
-  }
+return { map {$_, catdir( $this->blib, $_ ) }
+         map $this->localize_file_path($_),
+         grep !/\.\#/,
+         @{ $this->rscan_dir( "htdocs", sub { ! -d and $File::Find::dir !~ /CVS$/ } ) } };
 }
 
 #-------------------------------------------------------------------------------
@@ -213,26 +144,33 @@ sub forEachStaticFile {
 #  ExtUtils::Install::install($self->install_map, !$self->quiet, 1, $self->{args}{uninst}||0);
 #}
 
-
 # override the "install" action entirely
 
 sub ACTION_fakeinstall {
   my $this = shift;
+
+  foreach my $type ( $this->install_types ) {
+    my $dest = $this->install_destination( $type );
+    print "|$type| ---> |$dest|\n"; 
+  }
+
+  print "install_base_relpaths:\n" . ( dump $this->install_base_relpaths ) . "\n";
+  print "install_path:\n" . ( dump $this->install_path ) . "\n";
+  print "install_map:\n" . ( dump $this->install_map ) . "\n";
  
-  $config = $this->config_data( "configHash" ) unless defined $config;
-  
- my $map = {
-    "blib/lib"        => $config->{installRoot} . "/lib",
-    "blib/htdocs"     => $config->{installRoot} . "/htdocs",
-    "blib/data"       => $config->{installRoot} . "/data",
-    "blib/conf"       => $config->{installRoot} . "/conf",
-    "blib/pfam-core"  => $this->notes( "pfamCoreDir" ),
-    "blib/pfam-model" => $this->notes( "pfamModelDir" ),
-    "read"            => "",
-    "write"           => ""
-  };
+#  my $map = {
+#    "blib/lib"        => $this->config_data( "configHash" )->{installRoot} . "/lib",
+#    "blib/htdocs"     => $this->config_data( "configHash" )->{installRoot} . "/htdocs",
+#    "blib/data"       => $this->config_data( "configHash" )->{installRoot} . "/data",
+#    "blib/conf"       => $this->config_data( "configHash" )->{installRoot} . "/conf",
+#    "blib/pfam-core"  => $this->notes( "pfamCoreDir" ),
+#    "blib/pfam-model" => $this->notes( "pfamModelDir" ),
+#    "read"            => "",
+#    "write"           => ""
+#  };
+
   require ExtUtils::Install;
-  ExtUtils::Install::install( $map, 0, 1, 0 );
+  #ExtUtils::Install::install( $this->install_map, 0, 1, 0 );
  
 #my $map = $this->install_map;
 
