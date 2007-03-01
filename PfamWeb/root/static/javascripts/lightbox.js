972a4aee4b6d6e1cd7181a1b5317b06b3b41e302
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-03-01 11:48:38 +0000

    Change "/catalyst/PfamWeb" to "/catalyst/pfam".

--- PfamWeb/root/static/javascripts/lightbox.js
+++ PfamWeb/root/static/javascripts/lightbox.js
@@ -35,8 +35,8 @@
 //
 
 // If you would like to use a custom loading image or close button reference them in the next two lines.
-var loadingImage = '/catalyst/PfamWeb/images/lb_loading.gif';		
-var closeButton = '/catalyst/PfamWeb/images/lb_close.gif';		
+var loadingImage = '/catalyst/pfam/images/lb_loading.gif';		
+var closeButton = '/catalyst/pfam/images/lb_close.gif';		
 
 
 
