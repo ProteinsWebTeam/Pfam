f1b49491edd999192dfe0016517a8029bf38d8fc
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-03-15 14:35:16 +0000

    Add POD and the boilerplate license.

--- trunk/PfamWeb/inc/PfamWeb/Builder.pm
+++ trunk/PfamWeb/inc/PfamWeb/Builder.pm
@@ -2,12 +2,27 @@
 # PfamBuilder.pm
 # jt 20070208 WTSI.
 #
-# A custom Module::Builder subclass to handle installation of the
-# PfamWeb application
-#
-# $Id: Builder.pm,v 1.9 2007-03-08 14:13:11 jt6 Exp $
+# $Id: Builder.pm,v 1.10 2007-03-15 14:35:16 jt6 Exp $
+
+=head1 NAME
+
+PfamWeb::Builder - Module::Build subclass for installing PfamWeb
+
+= cut
 
 package PfamWeb::Builder;
+
+=head1 DESCRIPTION
+
+A custom Module::Builder subclass to handle installation of the
+PfamWeb application. Used by the Build.PL installation script, this
+module provides the extra methods that L<Module::Build> requires to
+install the various extra file types and directories in the PfamWeb
+distribution, such as the static HTML content in htdocs.
+
+$Id: Builder.pm,v 1.10 2007-03-15 14:35:16 jt6 Exp $
+
+=cut
 
 use strict;
 use warnings;
@@ -22,10 +37,27 @@ use ExtUtils::Install;
 use Data::Dump qw(dump);
 
 #-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+L<Module::Build> allows a package author to add extra elements to the
+installation process. We're using that to allow L<Module::Build> to
+install extra file types and to edit them along the way. These methods
+are called automatically by the L<Module::Build> calls, as used by the
+Build.PL installation script.
+
+=cut
+
+#-------------------------------------------------------------------------------
 #- new elements ----------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-# copy the contents of htdocs (CSS, images, javascripts, jars) across
+=head2 process_htdocs_files
+
+Copies the contents of htdocs (CSS, images, javascripts, jars) into
+the build library directory (blib).
+
+=cut
 
 sub process_htdocs_files {
   my $this = shift;
@@ -34,43 +66,43 @@ sub process_htdocs_files {
   my $defRoot = $this->notes( "defaultAppRoot" );
   my $appRoot = $this->notes( "appRoot" );
 
-  # make a directory for all of this...  
+  # make a directory for all of this...
   mkpath( catdir( $this->blib, "htdocs" ) );
-  
+
   my $files = $this->find_htdocs_files;
 
   while( my( $from, $to ) = each %$files ) {
     next if $this->up_to_date( $from, $to );
-    
-    # only need to edit CSS and JS files, and only if the default application 
+
+    # only need to edit CSS and JS files, and only if the default application
     # root has been changed by the user
     if( $from =~ /\.(css|js)$/ and $defRoot ne $appRoot ) {
-        
+
       print "processing $from -> $to\n";
-    
-    	open( IN, $from )
-    	  or warn "(WW) WARNING: couldn't open file \"$from\": $!" and return;    
-    	  
-  	  # make sure the output directory exists...
+
+        open( IN, $from )
+          or warn "(WW) WARNING: couldn't open file \"$from\": $!" and return;
+
+      # make sure the output directory exists...
       eval {
-    	  mkpath( dirname( $to ), 0, 0777 ) unless -d $to;    	  
-    	  open( OUT, ">$to" );
+          mkpath( dirname( $to ), 0, 0777 ) unless -d $to;
+          open( OUT, ">$to" );
       };
       if( $@ ) {
-    	  warn "(WW) WARNING: couldn't write file \"$to\": $!";
-    	  return;
+          warn "(WW) WARNING: couldn't write file \"$to\": $!";
+          return;
       }
-    
-    	# get the default value and the new value of the URL root from the
-    	# notes and build a hideous regex to replace one with the other
-    	while( <IN> ) {
-    	  s/(??{$defRoot})/$appRoot/eog;
-    	  print OUT $_;
-    	}
-    
-    	close IN;
-    	close OUT;
-  	
+
+	  # get the default value and the new value of the URL root from the
+	  # notes and build a hideous regex to replace one with the other
+	  while( <IN> ) {
+		s/(??{$defRoot})/$appRoot/eog;
+		print OUT $_;
+	  }
+
+	  close IN;
+	  close OUT;
+
     } else {
       $this->copy_if_modified( from => $from, to => $to );
     }
@@ -80,32 +112,52 @@ sub process_htdocs_files {
 }
 
 #-------------------------------------------------------------------------------
-# We've implicitly added process_conf, process_data and process_htdocs methods
-# by adding the new build elements in the mail Build.PL script, and these 
-# "find" methods are used by MB to allow the various process_* methods to work.
 
-# this one is copied directly from Module::Build::Base::_find_file_by_type
+=pod
+
+We've implicitly added L<process_conf>, L<process_data> and
+L<process_htdocs> methods by adding the new build elements in the mail
+Build.PL script, and these "find" methods are used by MB to allow the
+various process_* methods to work.
+
+=head2 local_find_file_by_type
+
+This method is copied directly from L<Module::Build::Base::_find_file_by_type>.
+
+=cut
+
 sub local_find_file_by_type  {
   my( $this, $type, $dir ) = @_;
   return { map {$_, $_}
     map $this->localize_file_path( $_ ),
-    grep !/\.\#/, @{ $this->rscan_dir( $dir, qr{\.$type$} ) } };
+    grep !/\.\# /, @{ $this->rscan_dir( $dir, qr{\.$type$} ) } };
 }
 
-# this is the same idea, but a bit more specialised: find any file in the htdocs
-# directory, but ignore CVS directories
+=head2 local_find_file_by_type
+
+This is the same idea, but a bit more specialised: find any file in
+the htdocs directory, but ignore CVS directories.
+
+=cut
+
 sub find_htdocs_files {
   my $this = shift;
-  
-return { map {$_, catdir( $this->blib, $_ ) }
-         map $this->localize_file_path($_),
-         grep !/\.\#/,
-         @{ $this->rscan_dir( "htdocs", sub { ! -d and $File::Find::dir !~ /CVS$/ } ) } };
+
+  return { map {$_, catdir( $this->blib, $_ ) }
+           map $this->localize_file_path($_),
+           grep !/\.\#/,
+           @{ $this->rscan_dir( "htdocs", sub { ! -d and $File::Find::dir !~ /CVS$/ } ) } };
 }
 
 #--------------------------------------
 
-# and using that...
+=head2 find_TYPE_files
+
+These methods use L<local_find_file_by_type> to find specified file
+types in the specified directories.
+
+=cut
+
 sub find_conf_files {
   return shift->local_find_file_by_type( "conf", "conf" );
 }
@@ -128,14 +180,31 @@ sub find_pfamModel_files {
 #- actions ---------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
+=head1 ACTIONS
+
+=head2 ACTION_install
+
+As per L<Module::Build>, this method uses
+L<ExtUtils::Install::install> to install files from the blib directory
+into the directory structure determined by L<install_map>.
+
+=cut
+
 sub ACTION_install {
   my $this = shift;
-  
+
   $this->depends_on( "build" );
-  ExtUtils::Install::install( $this->install_map, 0, 0, 0 ); 
+  ExtUtils::Install::install( $this->install_map, 0, 0, 0 );
 }
 
 #--------------------------------------
+
+=head2 ACTION_fakeinstall
+
+Does the same as L<install> but doesn't actually copy any files. Just
+describes what would be done if you ran L<install>.
+
+=cut
 
 sub ACTION_fakeinstall {
   my $this = shift;
@@ -154,29 +223,34 @@ sub ACTION_fakeinstall {
 }
 
 #-------------------------------------------------------------------------------
-# a pretty dumb check that the configuration file is in good
-# shape. Config::General seems happy to process any old junk, so in
-# the end this is not much more than a cursory look for unclosed
-# blocks, egregiously broken syntax, etc.
+
+=head2 ACTION_check_config
+
+A pretty dumb check that the configuration file is in good
+shape. Config::General seems happy to process any old junk, so in the
+end this is not much more than a cursory look for unclosed blocks,
+egregiously broken syntax, etc.
+
+=cut
 
 sub ACTION_check_config {
   my $this = shift;
-  
+
   my $configFile = $this->args( "config_file" );
 
   unless( $configFile ) {
-    print STDERR <<EOFccUsage;    
+    print STDERR <<EOFccUsage;
 (EE) ERROR: no configuration file specified.
 
 Usage: Build check_config --config_file=<config_file>
 EOFccUsage
     exit 1;
-  }      
+  }
   unless( -f $configFile ) {
     print STDERR "(EE) ERROR: no configuration file found at \"$configFile\": $1\n";
     exit 1;
   }
-  
+
   print "(ii) test processing config file \"$configFile\"...\n";
 
   my $conf = new Config::General( $configFile );
@@ -188,5 +262,32 @@ EOFccUsage
 }
 
 #-------------------------------------------------------------------------------
+
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+
+=cut
 
 1;
