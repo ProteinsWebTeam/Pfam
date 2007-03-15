5bb8ac413617c6ddb7a528781ed21d1945411b07
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-03-15 15:03:56 +0000

    Add license boilerplate.

--- trunk/PfamWeb/root/static/javascripts/treeTweak.js
+++ trunk/PfamWeb/root/static/javascripts/treeTweak.js
@@ -7,7 +7,26 @@
 // TextNodes, although we're primarily interested in the TaskNodes,
 // which have a check method from the outset
 //
-// $Id: treeTweak.js,v 1.2 2007-03-13 10:35:09 jt6 Exp $
+// $Id: treeTweak.js,v 1.3 2007-03-15 15:03:52 jt6 Exp $
+
+// Copyright (c) 2007: Genome Research Ltd.
+// 
+// Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+// 
+// This is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
 
 YAHOO.widget.TextNode.prototype.check = function( state ) { 
   for( var i = 0; i < this.children.length; i++ ) {
