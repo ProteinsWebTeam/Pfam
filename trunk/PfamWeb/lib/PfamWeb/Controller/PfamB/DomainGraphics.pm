cca488545087868784131758c5e39e09d97494ba
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-05-17 08:31:27 +0000

    Whitespace changes, plus a couple of extra log messages

--- trunk/PfamWeb/lib/PfamWeb/Controller/PfamB/DomainGraphics.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/PfamB/DomainGraphics.pm
@@ -2,7 +2,7 @@
 # DomainGraphics.pm
 # jt6 20061023 WTSI
 #
-# $Id: DomainGraphics.pm,v 1.4 2007-03-15 14:06:10 jt6 Exp $
+# $Id: DomainGraphics.pm,v 1.5 2007-05-17 08:31:27 jt6 Exp $
 
 =head1 NAME
 
@@ -46,12 +46,12 @@ sub default : Path {
   # get a list of all the sequences and unique architectures for this
   # family
   ( $c->stash->{auto_arch} ) = $c->req->param( "arch" ) =~ /^(\d+|nopfama)$/i
-	if $c->req->param( "arch" );
+    if $c->req->param( "arch" );
 
   my( @seqs, %seqInfo, %seenArch, @architectures, %archStore );
 
   if( $c->stash->{auto_arch} ){
-	# we want to see all of the sequences with a given architecture
+    # we want to see all of the sequences with a given architecture
 
     if( $c->stash->{auto_arch} =~ /^nopfama$/i ) {
 
@@ -59,82 +59,91 @@ sub default : Path {
       # stuff with no auto_architecture.....
 
       @architectures = $c->model("PfamDB::PfamB_reg")
-		->search( { auto_pfamB => $auto },
-				  { join      => [ qw/ pfamseq_architecture annseq pfamseq /],
-					prefetch  => [ qw/ pfamseq_architecture annseq pfamseq /] } );
+                         ->search( { auto_pfamB => $auto },
+                                   { join      => [ qw/ pfamseq_architecture annseq pfamseq / ],
+                                     prefetch  => [ qw/ pfamseq_architecture annseq pfamseq / ] } );
       foreach my $arch ( @architectures ) {
-		push @seqs, thaw( $arch->annseq_storable )
-		  unless $arch->auto_architecture =~ /\d+/;
-	  }
+        push @seqs, thaw( $arch->annseq_storable )
+        unless $arch->auto_architecture =~ /\d+/;
+      }
 
     } else {
 
       @architectures = $c->model("PfamDB::PfamB_reg")
-		->search( { auto_pfamB => $auto,
-					auto_architecture => $c->stash->{auto_arch} },
-				  { join      => [ qw/ pfamseq_architecture annseq pfamseq /],
-					prefetch  => [ qw/ pfamseq_architecture annseq pfamseq /] } );
+                         ->search( { auto_pfamB => $auto,
+                                     "pfamseq_architecture.auto_architecture" => $c->stash->{auto_arch} },
+                                   { join      => [ qw/ pfamseq_architecture annseq pfamseq / ],
+                                     prefetch  => [ qw/ pfamseq_architecture annseq pfamseq / ] } );
+
       # query has already done the selection
       foreach my $arch ( @architectures ) {
-		push @seqs, thaw( $arch->annseq_storable );
+        push @seqs, thaw( $arch->annseq_storable );
       }
     }
 
   } else {
-	# we want to see the unique architectures containing this domain
+    # we want to see the unique architectures containing this domain
 
     @architectures = $c->model("PfamDB::PfamB_reg")
-	  ->search( { auto_pfamB => $auto },
-				{ join      => [ qw/ pfamseq_architecture annseq pfamseq /],
-				  prefetch  => [ qw/ pfamseq_architecture annseq pfamseq /] } );
+                       ->search( { auto_pfamB => $auto },
+                                 { join      => [ qw/ pfamseq_architecture annseq pfamseq / ],
+                                   prefetch  => [ qw/ pfamseq_architecture annseq pfamseq / ] } );
+    $c->log->debug( "DomainGraphics::default: found |" .scalar @architectures . "| architectures" );
 
-	# grab only the unique architectures as they fly by
+    # grab only the unique architectures as they fly by
     foreach my $arch ( @architectures ) {
-      unless( $seenArch{ $arch->auto_architecture } ) {		
-		push @seqs, thaw( $arch->annseq_storable );
-		$archStore{ $arch->pfamseq_id } = $arch;
-
+      unless( $seenArch{ $arch->auto_architecture } ) {    
+        push @seqs, thaw( $arch->annseq_storable );
+        $archStore{ $arch->pfamseq_id } = $arch;
       }
       $seenArch{ $arch->auto_architecture }++;
     }
+    $c->log->debug( "DomainGraphics::default: thawed |" .scalar @seqs . "| sequences" );
 
-	# generate the extra mapping for the architectures
-	foreach my $seq ( @seqs ) {
-	  if( $archStore{$seq->id}->auto_architecture =~ /^(\d+)$/ ) {
-		my $rs = $c->model("PfamDB::Architecture")->find( auto_architecture => $1);
-		my @domains = split /\~/, $rs->architecture;
-		$seqInfo{$seq->id}{arch} = \@domains;
-		$seqInfo{$seq->id}{auto_arch} = $1;
-		$seqInfo{$seq->id}{num} = $seenArch{ $1 } ;
-	  } else {
-		$seqInfo{$seq->id}{arch} = "no Pfam A domains";
-		$seqInfo{$seq->id}{auto_arch} = "nopfama";
-		$seqInfo{$seq->id}{num} =  $seenArch{ $archStore{$seq->id}->auto_architecture };
-	  }
-	}
-
-	# and finally sort the sequences according to the number of
-	# sequences that have a given architecture
-	@seqs = sort { $seqInfo{$b->id}{num} <=> $seqInfo{$a->id}{num} } @seqs;
+    # generate the extra mapping for the architectures
+    foreach my $seq ( @seqs ) {
+      my $aa = $archStore{$seq->id}->auto_architecture || "";
+      $c->log->debug( "DomainGraphics::default: checking | " . $seq->id . 
+                      "|, architecture |" . $aa . "|" );
+      if( $aa =~ /^(\d+)$/ ) {
+        $c->log->debug( "DomainGraphics::default: found architecture |$1|" );
+        my $rs = $c->model("PfamDB::Architecture")
+                   ->find( { auto_architecture => $1 } );
+        my @domains = split /\~/, $rs->architecture;
+        $seqInfo{$seq->id}{arch} = \@domains;
+        $seqInfo{$seq->id}{auto_arch} = $1;
+        $seqInfo{$seq->id}{num} = $seenArch{ $1 } ;
+      } else {
+        $c->log->debug( "DomainGraphics::default: no PfamA domains" );
+        $seqInfo{$seq->id}{arch} = "no Pfam A domains";
+        $seqInfo{$seq->id}{auto_arch} = "nopfama";
+        $seqInfo{$seq->id}{num} = $seenArch{ $aa };
+      }
+    }
+  
+    # and finally sort the sequences according to the number of
+    # sequences that have a given architecture
+    @seqs = sort { $seqInfo{$b->id}{num} <=> $seqInfo{$a->id}{num} } @seqs;
   }
 
-  $c->log->debug( "found " . scalar @seqs . " storables" );
+  $c->log->debug( "DomainGraphics::default: ended up with |" . scalar @seqs . "| storables" );
 
   # draw them...
   my $layout = Bio::Pfam::Drawing::Layout::PfamLayoutManager->new;
   my %regionsAndFeatures = ( "PfamA"      => 1,
-							 "PfamB"      => 1,
-							 "noFeatures" => 1 );
+                             "PfamB"      => 1,
+                             "noFeatures" => 1 );
 
   $layout->layout_sequences_with_regions_and_features( \@seqs,
-													   { PfamA      => 1,
-														 PfamB      => 1,
-														 noFeatures => 1 } );
+                                                       { PfamA      => 1,
+                                                         PfamB      => 1,
+                                                         noFeatures => 1 } );
 
   my $imageset = Bio::Pfam::Drawing::Image::ImageSet->new;
   $imageset->create_images( $layout->layout_to_XMLDOM );
 
   # dump the images and the mapping into the stash
+  $c->stash->{numRows} = scalar @architectures;
   $c->stash->{images} = $imageset;
   $c->stash->{seqInfo}  = \%seqInfo;
 
