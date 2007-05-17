390f57e9c09b3e07af1f907f5fb2b7f19236e771
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-05-17 08:35:11 +0000

    Mostly whitespace changes, with a bit of cosmetic rearrangment too.

--- PfamWeb/lib/PfamWeb/Controller/PfamB.pm
+++ PfamWeb/lib/PfamWeb/Controller/PfamB.pm
@@ -4,7 +4,7 @@
 #
 # Controller to build a PfamB  page.
 #
-# $Id: PfamB.pm,v 1.5 2007-03-15 14:06:12 jt6 Exp $
+# $Id: PfamB.pm,v 1.6 2007-05-17 08:35:11 jt6 Exp $
 
 =head1 NAME
 
@@ -20,7 +20,7 @@ A C<Controller> to handle pages for Pfam B entries.
 
 Generates a B<full page>.
 
-$Id: PfamB.pm,v 1.5 2007-03-15 14:06:12 jt6 Exp $
+$Id: PfamB.pm,v 1.6 2007-05-17 08:35:11 jt6 Exp $
 
 =cut
 
@@ -48,22 +48,20 @@ sub begin : Private {
 
   if( defined $c->req->param("acc") ) {
 
-	$c->req->param("acc") =~ m/^(PB\d{6})$/i;
-	$c->log->info( "PfamB::begin: found a PfamB, accession |$1|" );
+  $c->req->param("acc") =~ m/^(PB\d{6})$/i;
+  $c->log->info( "PfamB::begin: found a PfamB, accession |$1|" );
 
-	  if (defined $1){
-		$c->stash->{pfam} = $c->model("PfamDB::PfamB")->find( { pfamB_acc => $1 } );
-		$c->stash->{prodom} = $c->model("PfamDB::PfamB_database_links")
-		  ->find( { auto_pfamB => $c->stash->{pfam}->auto_pfamB,
-					db_id => "PRODOM"} );
-	  }
+    if (defined $1){
+      $c->stash->{pfam}   = $c->model("PfamDB::PfamB")
+                              ->find( { pfamB_acc => $1 } );
+    }
   }
 
   # we're done here unless there's an entry specified
   unless( defined $c->stash->{pfam} ) {
-	$c->log->warn( "PfamB::begin: no ID or accession" );
-	$c->error( "No valid Pfam family accession or ID" );
-	return;
+    $c->log->warn( "PfamB::begin: no ID or accession" );
+    $c->error( "No valid Pfam family accession or ID" );
+    return;
   }
 
   # flag this as a PfamB
@@ -72,9 +70,15 @@ sub begin : Private {
 }
 
 #-------------------------------------------------------------------------------
-# pick up http://localhost:3000/pfamb?acc=PB000001
 
-sub generateSummary : Path {
+=head2 default : Path
+
+Just stuffs the hash with extra information, such as summary data and 
+database cross-references.
+
+=cut
+
+sub default : Path {
   my( $this, $c ) = @_;
 
   # the accession should have been dropped into the stash by the begin
@@ -101,38 +105,32 @@ sub _getSummaryData : Private {
   my $auto_pfam = $c->stash->{pfam}->auto_pfamB;
 
   # get the PDB details
-  my @maps = $c->model("PfamDB::PdbMap")->search(
-    { auto_pfam   => $auto_pfam,
-	  pfam_region => 0 },
-	{ join        => [ qw/ pdb / ],
-	  prefetch    => [ qw/ pdb / ] } );
+  my @maps = $c->model("PfamDB::PdbMap")
+               ->search( { auto_pfam   => $auto_pfam,
+                           pfam_region => 0 },
+                        { join        => [ qw/ pdb / ],
+                          prefetch    => [ qw/ pdb / ] } );
   $c->stash->{pfamMaps} = \@maps;
 
   # count the number of architectures
-  my $rs = $c->model("PfamDB::PfamB_reg")->find(
-    { auto_pfamB => $auto_pfam },
-    {
-      select => [
-        { count => { distinct => "auto_architecture" } }
-      ],
-      as => [ 'count' ],
-	 join => [ qw/ pfamseq_architecture / ]
-    }
-  );
+  my $rs = $c->model("PfamDB::PfamB_reg")
+             ->find( { auto_pfamB => $auto_pfam },
+                     { select => [
+                                   { count => { distinct => "auto_architecture" } }
+                                 ],
+                       as     => [ 'count' ],
+                       join   => [ qw/ pfamseq_architecture / ] } );
 
   # number of architectures....
   $summaryData{numArchitectures} = $rs->get_column( "count" );
 
   # count number of sequences in full alignment
-  $rs = $c->model("PfamDB::PfamB_reg")->find(
-    { auto_pfamB => $auto_pfam },
-    {
-      select => [
-        { count => "auto_pfamB" }
-      ],
-      as => [ 'count' ]
-    }
-  );
+  $rs = $c->model("PfamDB::PfamB_reg")
+          ->find( { auto_pfamB => $auto_pfam },
+                  { select => [
+                                { count => "auto_pfamB" }
+                              ],
+                    as => [ 'count' ] } );
 
   # number of sequences in full alignment
   $summaryData{numSequences} = $rs->get_column( "count" );
@@ -143,20 +141,20 @@ sub _getSummaryData : Private {
   $c->stash->{pdbUnique} = \%pdb_unique;
 
   # number of species
-  my @species = $c->model("PfamDB::PfamB_reg")->search(
-    { auto_pfamB => $auto_pfam },
-    { join       => [ qw/pfamseq/ ],
-	  prefetch   => [ qw/pfamseq/ ] } );
+  my @species = $c->model("PfamDB::PfamB_reg")
+                  ->search( { auto_pfamB => $auto_pfam },
+                            { join       => [ qw/pfamseq/ ],
+                              prefetch   => [ qw/pfamseq/ ] } );
 
   my %species_unique = map {$_->species => 1} @species;
   $summaryData{numSpecies} = scalar(keys %species_unique);
 
   # number of interactions
 #   $rs = $c->model("PfamDB::Int_pfamAs")->find({ auto_pfamA_A => $auto_pfam },
-# 	{ select => [
-# 				 { count => "auto_pfamA_A" }
-# 				],
-# 	  as => [ qw/NumInts/ ]
+#   { select => [
+#          { count => "auto_pfamA_A" }
+#         ],
+#     as => [ qw/NumInts/ ]
 #     }
 #   );
 #
@@ -169,10 +167,19 @@ sub _getSummaryData : Private {
 }
 
 #-------------------------------------------------------------------------------
+# retrieves database cross-references
 
 sub _getDbXrefs : Private {
   my( $this, $c ) = @_;
 
+  # get just the row from the prodom table, used to get hold of the PRODOM link
+  $c->stash->{prodom} = $c->model("PfamDB::PfamB_database_links")
+                          ->find( { auto_pfamB => $c->stash->{pfam}->auto_pfamB,
+                                    db_id      => "PRODOM" } );
+  $c->log->debug( "PfamB::_getDbXrefs: prodom:  |" . $c->stash->{prodom} . "|" );
+  $c->log->debug( "PfamB::_getDbXrefs: db_link: |" . $c->stash->{prodom}->db_link . "|" );
+
+  # cross references
   my %xRefs;
 
   # stuff in the accession and ID for this entry
@@ -182,76 +189,72 @@ sub _getDbXrefs : Private {
   # PfamB to PfamA links based on PRODOM
   my %btoaPRODOM;
   foreach my $xref ( $c->stash->{pfam}->pfamB_database_links ) {
-	if( $xref->db_id eq "PFAMA_PRODOM" ) {
-	  $btoaPRODOM{$xref->db_link} = $xref;
-	}else {
-	  push @{ $xRefs{$xref->db_id} }, $xref;
-	}
+    if( $xref->db_id eq "PFAMA_PRODOM" ) {
+      $btoaPRODOM{$xref->db_link} = $xref;
+    } else {
+      push @{ $xRefs{$xref->db_id} }, $xref;
+    }
   }
 
   # PfamB to PfamB links based on PRC
   my @btobPRC = $c->model("PfamDB::PfamB2pfamB_PRC_results")
-	->search(
-			 { "pfamB1.pfamB_acc" => $c->stash->{pfam}->pfamB_acc },
-			 { join               => [ qw/pfamB1 pfamB2/ ],
-			   select             => [ qw/pfamB1.pfamB_acc pfamB2.pfamB_acc evalue/ ],
-			   as                 => [ qw/l_pfamB_acc r_pfamB_acc evalue/ ],
-			   order_by           => "pfamB2.auto_pfamB ASC" }
-			);
+                  ->search( { "pfamB1.pfamB_acc" => $c->stash->{pfam}->pfamB_acc },
+                            { join               => [ qw/pfamB1 pfamB2/ ],
+                              select             => [ qw/pfamB1.pfamB_acc pfamB2.pfamB_acc evalue/ ],
+                              as                 => [ qw/l_pfamB_acc r_pfamB_acc evalue/ ],
+                              order_by           => "pfamB2.auto_pfamB ASC" } );
 
   $xRefs{btobPRC} = [];
   foreach ( @btobPRC ) {
-	next if $_->get_column( "evalue" ) <= 0.001;
-	next if $_->get_column("l_pfamB_acc") eq $_->get_column("r_pfamB_acc");
-	push @{$xRefs{btobPRC}}, $_;
+    next if $_->get_column( "evalue" ) <= 0.001;
+    next if $_->get_column("l_pfamB_acc") eq $_->get_column("r_pfamB_acc");
+    push @{$xRefs{btobPRC}}, $_;
   }
 
 #  $xRefs{btobPRC} = \@btobPRC if scalar @btobPRC;
 
   # PfamB to PfamA links based on PRC
   my @btoaPRC = $c->model("PfamDB::PfamB2pfamA_PRC_results")
-	->search(
-			 { "pfamB.pfamB_acc" => $c->stash->{pfam}->pfamB_acc, },
-			 { join      => [ qw/pfamA pfamB/ ],
-			   prefetch  => [ qw/pfamA pfamB/ ] }
-			);
+                  ->search( { "pfamB.pfamB_acc" => $c->stash->{pfam}->pfamB_acc, },
+                            { join      => [ qw/pfamA pfamB/ ],
+                              prefetch  => [ qw/pfamA pfamB/ ] } );
 
   # find the union between PRC and PRODOM PfamB links
   my %btoaPRC;
   foreach ( @btoaPRC ) {
-	$btoaPRC{$_->pfamB_acc} = $_ if $_->evalue <= 0.001;
+    $btoaPRC{$_->pfamB_acc} = $_ if $_->evalue <= 0.001;
   }
 
   my %btoaBOTH;
   foreach ( keys %btoaPRC, keys %btoaPRODOM ) {
-	$btoaBOTH{$_} = $btoaPRC{$_}
-	  if( exists( $btoaPRC{$_} ) and exists( $btoaPRODOM{$_} ) );
+    $btoaBOTH{$_} = $btoaPRC{$_}
+      if( exists( $btoaPRC{$_} ) and exists( $btoaPRODOM{$_} ) );
   }
 
   # and then prune out those accessions that are in both lists
   foreach ( keys %btoaPRC ) {
-	delete $btoaPRC{$_} if exists $btoaBOTH{$_};
+    delete $btoaPRC{$_} if exists $btoaBOTH{$_};
   }
   foreach ( keys %btoaPRODOM ) {
-	delete $btoaPRODOM{$_} if exists $btoaBOTH{$_};
+    delete $btoaPRODOM{$_} if exists $btoaBOTH{$_};
   }
 
   # now populate the hash of xRefs;
   my @btoaPRC_pruned;
   foreach ( sort keys %btoaPRC ) {
-	push @btoaPRC_pruned, $btoaPRC{$_};
+    push @btoaPRC_pruned, $btoaPRC{$_};
   }
   $xRefs{btoaPRC} = \@btoaPRC_pruned if scalar @btoaPRC_pruned;
 
   my @btoaPRODOM;
   foreach ( sort keys %btoaPRODOM ) {
-	push @btoaPRODOM, $btoaPRODOM{$_};
+    push @btoaPRODOM, $btoaPRODOM{$_};
   }
   $xRefs{btoaPRODOM} = \@btoaPRODOM if scalar @btoaPRODOM;
 
   my @btoaBOTH;
   foreach ( sort keys %btoaBOTH ) {
-	push @btoaBOTH, $btoaBOTH{$_};
+    push @btoaBOTH, $btoaBOTH{$_};
   }
   $xRefs{btoaBOTH} = \@btoaBOTH if scalar @btoaBOTH;
 
