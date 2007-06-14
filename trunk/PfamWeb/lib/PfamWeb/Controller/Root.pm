61ada040503f343869bfed3183b4f70a00ec4daf
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-06-14 21:35:00 +0000

    Ditch a couple of redundant methods but beyond that nothing substantive.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
@@ -2,7 +2,7 @@
 # Root.pm
 # jt 20061003 WTSI
 #
-# $Id: Root.pm,v 1.13 2007-05-21 12:51:15 jt6 Exp $
+# $Id: Root.pm,v 1.14 2007-06-14 21:35:00 jt6 Exp $
 
 =head1 NAME
 
@@ -18,14 +18,14 @@ This is the root class for the Pfam website catalyst application. It
 installs global actions for the main site index page and other top-level
 functions.
 
-$Id: Root.pm,v 1.13 2007-05-21 12:51:15 jt6 Exp $
+$Id: Root.pm,v 1.14 2007-06-14 21:35:00 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
-use base "Catalyst::Controller";
+use base 'Catalyst::Controller';
 
 # sets the actions in this controller to be registered with no prefix
 # so they function identically to actions created in MyApp.pm
@@ -45,16 +45,13 @@ sub default : Private {
   my ( $this, $c ) = @_;
 
   # before we go too far, make sure this isn't a broken URL
-  $c->res->redirect( $c->uri_for("/404") )
+  $c->res->redirect( $c->uri_for('/404') )
     unless $c->req->uri eq $c->req->base;
-
-  # set the page to be cached for an hour
-#  $c->cache_page( 3600 );
-
+  
   # tell the navbar where we are
-  $c->stash->{nav} = "home";
+  $c->stash->{nav} = 'home';
 
-  $c->log->debug("PfamWeb::default: generating site index");
+  $c->log->debug('PfamWeb::default: generating site index');
 }
 
 #-------------------------------------------------------------------------------
@@ -66,18 +63,40 @@ appropriate tab name in the stash. That will be picked up by the
 tab_layout.tt view, which will use it to figure out which tab to show
 by default.
 
-Also adds the Pfam version data to the stash, so that it's generally 
-accessible throughout the app.
+Also adds the Pfam version data to the stash, so that it's accessible 
+throughout the site.
 
+Checks for the existence of various files that act as switches, enabling
+different behaviours...
 =cut
 
 sub auto : Private {
   my ( $this, $c ) = @_;
 
+#  # check for the existence of the various files that act as switches for
+#  # various server behaviours
+#  if( -e $this->{BREAK_SWITCH} ) {
+#    $c->log->warn( 'Root::auto: found the break file; redirecting' );
+#    $c->stash->{template} = $this->{BREAK_TEMPLATE}; 
+#    return 0;
+#  }
+#
+#  # add a service message to the index page
+#  if( -e $this->{ANNOUNCE_SWITCH} ) {
+#    $c->log->warn( 'Root::auto: found the announcement switch; adding announcement to index page' );
+#    $c->stash->{serverAnnouncement} = $this->{ANNOUNCE_TEMPLATE};
+#  }
+#
+#  # add a message to the navbar
+#  if( -e $this->{MESSAGE_SWITCH} ) {
+#    $c->log->warn( 'Root::auto: found the message file; adding message to header' );
+#    $c->stash->{serverMessage} = $this->{MESSAGE_TEMPLATE};
+#  }
+
   # pick a tab
   my $tab;
-  ($tab) = $c->req->param("tab") =~ /^(\w+)$/
-    if defined $c->req->param("tab");
+  ($tab) = $c->req->param('tab') =~ /^(\w+)$/
+    if defined $c->req->param('tab');
 
   $c->stash->{showTab} = $1 if defined $tab;
 
@@ -87,11 +106,11 @@ sub auto : Private {
   my $releaseData;
   eval {
     # stash some details of the Pfam release
-    $releaseData = $c->model( "PfamDB::Version" )->find( {} );
+    $releaseData = $c->model( 'PfamDB::Version' )->find( {} );
   };
   if( $@ ) {
-    $c->error( "There appears to be a problem with the Pfam database." );
-    $c->forward("/reportError");
+    $c->error( 'There appears to be a problem with the Pfam database.' );
+    $c->forward('/reportError');
   }
   
   $c->stash->{relData} = $releaseData if $releaseData;
@@ -101,39 +120,9 @@ sub auto : Private {
 
 #-------------------------------------------------------------------------------
 
-=head2 about : Local
-
-Show an "about Pfam" page.
-
-=cut
-
-sub about : Local {
-  my ( $this, $c ) = @_;
-
-  # set the page to be cached for two weeks
-#  $c->cache_page( 1209600 );
-
-  $c->stash->{template} = "components/tools/about.tt";
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 annotate : Local
-
-Show an annotation upload form.
-
-=cut
-
-sub annotate : Local {
-  my ( $this, $c ) = @_;
-
-  $c->stash->{template} = "pages/annotation.tt";
-}
-
-#-------------------------------------------------------------------------------
-
 =head2 browseIndex : Global
 
+Show an index page for the various "browse" pages.
 
 =cut
 
@@ -141,41 +130,37 @@ sub browseIndex : Global {
   my ( $this, $c ) = @_;
 
   # tell the navbar where we are
-  $c->stash->{nav} = "browse";
+  $c->stash->{nav} = 'browse';
 
-  $c->stash->{template} = "pages/browseIndex.tt";
+  $c->stash->{template} = 'pages/browseIndex.tt';
 }
 
 #-------------------------------------------------------------------------------
 
-=head2 fourOhFour : Path( "/404" )
+=head2 fourOhFour : Path( '/404' )
 
-Generates a "404" page.
+Generates a '404' page.
 
 =cut
 
-sub fourOhFour : Path("/404") {
+sub fourOhFour : Path('/404') {
   my ( $this, $c ) = @_;
 
-  # set the page to be cached for two weeks
-#  $c->cache_page( 1209600 );
-
-  #----------------------------------------
   # first, figure out where the broken link was, internal or external
 
-  my $ref = ( defined $c->req->referer ) ? $c->req->referer : "";
-  $c->stash->{where} = ( $ref =~ /sanger/ ) ? "internal" : "external";
+  my $ref = ( defined $c->req->referer ) ? $c->req->referer : '';
+  $c->stash->{where} = ( $ref =~ /sanger/ ) ? 'internal' : 'external';
 
   # record the error
-  $c->error(   "Found a broken "
+  $c->error(   'Found a broken '
              . $c->stash->{where}
-             . " link: \""
+             . ' link: \''
              . $c->req->uri
-             . "\", referer: "
-             . ( $ref eq "" ? "unknown" : "\"" . $ref . "\"" ) );
+             . '\', referer: '
+             . ( $ref eq '' ? 'unknown' : '\'' . $ref . '\'' ) );
 
   # report it
-  $c->forward("/reportError");
+  $c->forward('/reportError');
 
   # and clear the errors before we render the page
   $c->clear_errors;
@@ -184,7 +169,7 @@ sub fourOhFour : Path("/404") {
   # set the HTTP status and point at the 404 page
 
   $c->res->status(404);
-  $c->stash->{template} = "pages/404.tt";
+  $c->stash->{template} = 'pages/404.tt';
 }
 
 #-------------------------------------------------------------------------------
@@ -192,22 +177,19 @@ sub fourOhFour : Path("/404") {
 =head2 end : Private
 
 Renders the index page for the site by default, but the default template can be 
-overridden by setting it in an action (eg for a 404 template).
+overridden by setting it in an action (eg for a 404 template). Patterned on
+the "end" method from the DefaultEnd plugin.
 
 =cut
 
-#sub end : ActionClass("RenderView") {
-#  my ( $this, $c ) = @_;
-#  $c->stash->{template} ||= "pages/index.tt";
-#}
-
 sub end : Private {
   my( $this, $c ) = @_;
-
+  
+  # were there any errors ? If so, render the error page into the response
   if( scalar @{ $c->error } ) {
-    $c->log->debug( "Root::end: found some errors from previous methods" );
+    $c->log->debug( 'Root::end: found some errors from previous methods' );
     $c->stash->{errorMsg} = $c->error;
-    $c->stash->{template} = "pages/error.tt";
+    $c->stash->{template} = 'pages/error.tt';
     
     # make sure the error page isn't cached
     $c->res->header( 'Pragma' => 'no-cache' );
@@ -215,19 +197,21 @@ sub end : Private {
     $c->res->header( 'Cache-Control' => 'no-store, no-cache, must-revalidate,'.
                                         'post-check=0, pre-check=0, max-age=0' );
     
-    $c->forward( "PfamWeb::View::TT" );
+    $c->forward( 'PfamWeb::View::TT' );
     $c->clear_errors;
   }
-    
-  return 1 if $c->response->status =~ /^3\d\d$/;
+
+  # don't render anything else if the response already has content
   return 1 if $c->response->body;
+  return 1 if $c->response->status =~ /^3\d\d$/;
 
-  unless( $c->response->content_type ) {
-    $c->response->content_type( "text/html; charset=utf-8" );
-  }
+  # set the content type to the default of "text/html", unless it's already set
+  $c->response->content_type( 'text/html; charset=utf-8' )
+    unless $c->response->content_type;
 
-  $c->stash->{template} ||= "pages/index.tt";
-  $c->forward( "PfamWeb::View::TT" );
+  # finally, default to the index page template and we're done
+  $c->stash->{template} ||= 'pages/index.tt';
+  $c->forward( 'PfamWeb::View::TT' );
 }
 
 #-------------------------------------------------------------------------------
