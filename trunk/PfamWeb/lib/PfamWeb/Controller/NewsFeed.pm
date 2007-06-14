9b23cbd23b73d38d38d00d92f476d20f0feec4e3
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-06-14 21:32:39 +0000

    Change the caching call to use seconds rather than a number of hours.

--- trunk/PfamWeb/lib/PfamWeb/Controller/NewsFeed.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/NewsFeed.pm
@@ -2,7 +2,7 @@
 # News.pm
 # jt 20061207 WTSI
 #
-# $Id: NewsFeed.pm,v 1.5 2007-04-16 15:58:32 jt6 Exp $
+# $Id: NewsFeed.pm,v 1.6 2007-06-14 21:32:39 jt6 Exp $
 
 =head1 NAME
 
@@ -16,7 +16,7 @@ package PfamWeb::Controller::NewsFeed;
 
 Generates the Pfam news feed RSS.
 
-$Id: NewsFeed.pm,v 1.5 2007-04-16 15:58:32 jt6 Exp $
+$Id: NewsFeed.pm,v 1.6 2007-06-14 21:32:39 jt6 Exp $
 
 =cut
 
@@ -32,17 +32,14 @@ use base "Catalyst::Controller";
 
 =head1 METHODS
 
-=head2 index : Private
+=head2 default : Private
 
 Shows all Pfam news feed items in full.
 
 =cut
 
-sub index : Private {
+sub default : Private {
   my( $this, $c ) = @_;
-
-  # set the page to be cached for an hour
-#  $c->cache_page( 3600 );
 
   # retrieve the news items from the feed
   my @entries = $c->model("WebUser::News")
@@ -84,7 +81,7 @@ sub rss : Global {
     $entries = \@entries;
 
     # cache the news items for later
-    $c->cache->set( "newsFeedEntries", $entries, "1 hour" );
+    $c->cache->set( "newsFeedEntries", $entries, 3600 );
   }
 
   # add each item to the feed
