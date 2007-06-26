7ede47cdda1fd889dcd2f91b599094221d36ab50
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-06-26 11:51:19 +0000

    Move the "structureTab" method in here from Family.pm.

--- PfamWeb/lib/PfamWeb/Controller/Family/Structures.pm
+++ PfamWeb/lib/PfamWeb/Controller/Family/Structures.pm
@@ -5,7 +5,7 @@
 # Controller to build an image of one of the PDB structure for the
 # specified family, along with a form for choosing a different one
 #
-# $Id: Structures.pm,v 1.5 2007-03-15 14:06:10 jt6 Exp $
+# $Id: Structures.pm,v 1.6 2007-06-26 11:51:19 jt6 Exp $
 
 =head1 NAME
 
@@ -42,7 +42,7 @@ parent class will complain otherwise.
 
 Generates a B<page fragment>.
 
-$Id: Structures.pm,v 1.5 2007-03-15 14:06:10 jt6 Exp $
+$Id: Structures.pm,v 1.6 2007-06-26 11:51:19 jt6 Exp $
 
 =cut
 
@@ -55,37 +55,62 @@ use base "PfamWeb::Controller::Family";
 
 =head1 METHODS
 
-=head2 default : Path
+=head2 default : Private
 
 Captures a URL like
 http://localhost:3000/family/structures?acc=PF00067&amp;pdbId=1w9h 
 
 =cut
 
-sub default : Path {
+sub default : Private {
   my( $this, $c ) = @_;
 
   my $id;
-  ( $id ) = $c->req->param("pdbId") =~ /^(\d\w{3})$/
-	if defined $c->req->param( "pdbId" );
+  ( $id ) = $c->req->param('pdbId') =~ /^(\d\w{3})$/
+  	if defined $c->req->param('pdbId');
 
-  $c->stash->{pdbObj} = $c->model("PfamDB::Pdb")->find( { pdb_id   => $id })
-	if defined $id;
+  $c->stash->{pdbObj} = $c->model('PfamDB::Pdb')
+                          ->find( { pdb_id => $id } )
+  	if defined $id;
 
   my @rs;
   if( defined $c->stash->{pfam}->auto_pfamA ) {
-	@rs = $c->model("PfamDB::Pdb_pfamA_reg")
-	  ->search( { auto_pfamA => $c->stash->{pfam}->auto_pfamA},
-				{ join       => [qw/ pdb /],
-				  prefetch   => [qw/ pdb /] } );
+	  @rs = $c->model('PfamDB::Pdb_pfamA_reg')
+            ->search( { auto_pfamA => $c->stash->{pfam}->auto_pfamA},
+			  	            { join       => [qw/ pdb /],
+				                prefetch   => [qw/ pdb /] } );
   }
 
   my %pdbUnique = map{ $_->pdb_id => $_ } @rs;
   $c->stash->{pdbUnique} = \%pdbUnique;
 
   # set up the view and rely on "end" from the parent class to render it
-  $c->stash->{template} = "components/blocks/family/familyStructures.tt";
+  $c->stash->{template} = 'components/blocks/family/familyStructures.tt';
+}
 
+#-------------------------------------------------------------------------------
+
+=head2 structureTab : Path
+
+Populates the stash with the mapping and hands off to the appropriate template.
+
+=cut
+
+sub structureTab : Path('/family/structuretab')  {
+  my($this, $c) = @_;
+
+  $c->log->debug( 'Family::Structures::structureTab: acc: |'
+            		  . $c->stash->{acc}  . '|' .  $c->stash->{entryType}. '|' );
+
+  my @mapping = $c->model('PfamDB::PdbMap')
+                  ->search( { auto_pfam   => $c->stash->{pfam}->auto_pfamA,
+                              pfam_region => 1 },
+                            { join        => [ qw/pdb/ ],
+                              prefetch    => [ qw/pdb/ ]
+                            } );
+  $c->stash->{pfamMaps} = \@mapping;
+
+  $c->stash->{template} = 'components/blocks/family/structureTab.tt';
 }
 
 #-------------------------------------------------------------------------------
