dd61830bbf0b7f2ad21164ec4262c12ddf1327ae
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-07-06 10:02:08 +0000

    Lots of trivial changes, like switching double- to single-quotes.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Family.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Family.pm
@@ -2,7 +2,7 @@
 # Family.pm
 # jt6 20060411 WTSI
 #
-# $Id: Family.pm,v 1.28 2007-06-26 11:53:54 jt6 Exp $
+# $Id: Family.pm,v 1.29 2007-07-06 10:02:08 jt6 Exp $
 
 =head1 NAME
 
@@ -22,17 +22,17 @@ load a Pfam object from the model.
 
 Generates a B<tabbed page>.
 
-$Id: Family.pm,v 1.28 2007-06-26 11:53:54 jt6 Exp $
+$Id: Family.pm,v 1.29 2007-07-06 10:02:08 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
-use base "PfamWeb::Controller::Section";
+use base 'PfamWeb::Controller::Section';
 
 # set the name of the section
-__PACKAGE__->config( SECTION => "family" );
+__PACKAGE__->config( SECTION => 'family' );
 
 #-------------------------------------------------------------------------------
 
@@ -40,16 +40,23 @@ __PACKAGE__->config( SECTION => "family" );
 
 =head2 begin : Private
 
-Extracts the Pfam family ID or accession from the URL and gets the row
+This is the guts of this controller. It's function is to extract
+the Pfam family ID or accession from the URL and get the row
 in the Pfam table for that entry. Expects one of three parameters:
 
 =over
 
-=item acc - a valid Pfam accession, either for an A or B entry
+=item acc
 
-=item id - a valid PfamA accession
+a valid Pfam accession, either for an A or B entry
 
-=item entry - either an ID or accession
+=item id
+
+a valid PfamA accession
+
+=item entry
+
+either an ID or accession
 
 =back
 
@@ -65,73 +72,72 @@ sub begin : Private {
   #----------------------------------------
   # get the accession or ID code
 
-  if( defined $c->req->param("acc") ) {
+  if( defined $c->req->param('acc') and 
+      $c->req->param('acc') =~ m/^(P([FB])\d{5,6})$/i ) {
 
-    $c->req->param("acc") =~ m/^(P([FB])\d{5,6})$/i;
     $c->log->debug( "Family::begin: found accession |$1|, family A / B ? |$2|" );
     
-    # is it a PfamB ?
-    if( $c->req->param("acc") =~ m/^(PB\d{6})$/i ) {
+    # is it a Pfam-A ?
+    if( $c->req->param('acc') =~ m/^(PF\d{5})$/i ) {
 
-      $c->stash->{pfam} = $c->model("PfamDB::PfamB")
-                            ->find( { pfamB_acc => $1 } );
-
-      if( defined $c->stash->{pfam} ) {
-        $c->log->debug( "Family::begin: found a PfamB: |$1|" );
-        $c->stash->{entryType} = "B";
-        $c->stash->{acc}  = $c->stash->{pfam}->pfamB_acc;
-      }
-
-    } elsif( $c->req->param("acc") =~ m/^(PF\d{5})$/i ) {
-
-      # no, must be a PfamA
-      $c->stash->{pfam} = $c->model("PfamDB::Pfam")
+      $c->stash->{pfam} = $c->model('PfamDB::Pfam')
                             ->find( { pfamA_acc => $1 } );
 
-        if( defined $c->stash->{pfam} ) {
-        $c->log->debug( "Family::begin: found a PfamA: |$1|" );
-        $c->stash->{entryType} = "A";
+      if( defined $c->stash->{pfam} ) {
+        $c->log->debug( "Family::begin: found a Pfam-A: |$1|" );
+        $c->stash->{entryType} = 'A';
         $c->stash->{acc} = $c->stash->{pfam}->pfamA_acc;
       }
 
+    } elsif( $c->req->param('acc') =~ m/^(PB\d{6})$/i ) {
+      # no; could be a Pfam-B
+
+      $c->stash->{pfam} = $c->model('PfamDB::PfamB')
+                            ->find( { pfamB_acc => $1 } );
+
+      if( defined $c->stash->{pfam} ) {
+        $c->log->debug( "Family::begin: found a Pfam-B: |$1|" );
+        $c->stash->{entryType} = 'B';
+        $c->stash->{acc}  = $c->stash->{pfam}->pfamB_acc;
+      }
+
     }
 
-  } elsif( defined $c->req->param("id") ) {
-
-    $c->req->param("id") =~ /^([\w_-]+)$/;
+  } elsif( defined $c->req->param('id') and
+           $c->req->param('id') =~ /^([\w_-]+)$/ ) {
     $c->log->debug( "Family::begin: found ID |$1|" );
   
-    $c->stash->{pfam} = $c->model("PfamDB::Pfam")->find( { pfamA_id => $1 } )
-      if defined $1;
+    $c->stash->{pfam} = $c->model('PfamDB::Pfam')
+                          ->find( { pfamA_id => $1 } );
 
     if( defined $c->stash->{pfam} ) {
-      $c->stash->{entryType} = "A";
+      $c->stash->{entryType} = 'A';
       $c->stash->{acc} = $c->stash->{pfam}->pfamA_acc;
     }
     
-  } elsif( defined $c->req->param( "entry" ) ) {
+  } elsif( defined $c->req->param( 'entry' ) ) {
 
-    if( $c->req->param( "entry" ) =~ /^(PF\d{5})$/i ) {
+    if( $c->req->param('entry') =~ /^(PF\d{5})$/i ) {
 
       # looks like a PfamA accession; redirect to this action, appending the accession
       $c->log->debug( "Family::begin: looks like a PfamA accession ($1); redirecting internally" );
-      $c->req->param( "acc" => $1 );
-      $c->detach( "begin" );
+      $c->req->param( 'acc' => $1 );
+      $c->detach( 'begin' );
       return 1;
 
-    } elsif( $c->req->param( "entry" ) =~ /^(PB\d{6})$/i ) {
+    } elsif( $c->req->param( 'entry' ) =~ /^(PB\d{6})$/i ) {
 
       # looks like a PfamA accession; redirect to this action, appending the accession
       $c->log->debug( "Family::begin: looks like a PfamB accession ($1); redirecting" );
-      $c->res->redirect( $c->uri_for( "/pfamb", { acc => $1 } ) );
+      $c->res->redirect( $c->uri_for( '/pfamb', { acc => $1 } ) );
       return 1;
 
-    } elsif( $c->req->param( "entry" ) =~ /^([\w_-]+)$/ ) {
+    } elsif( $c->req->param( 'entry' ) =~ /^([\w_-]+)$/ ) {
 
       # looks like an ID; redirect to this action, appending the ID
-      $c->log->debug( "Family::begin: might be a Pfam ID; redirecting internally" );
-      $c->req->param( "id" => $1 );
-      $c->detach( "begin" );
+      $c->log->debug( 'Family::begin: might be a Pfam ID; redirecting internally' );
+      $c->req->param( 'id' => $1 );
+      $c->detach( 'begin' );
       return 1;
     }
 
@@ -141,10 +147,10 @@ sub begin : Private {
   unless( defined $c->stash->{pfam} ) {
 
     # de-taint the accession or ID
-    my $input = $c->req->param("acc")
-      || $c->req->param("id")
-      || $c->req->param("entry")
-      || "";
+    my $input = $c->req->param('acc')
+      || $c->req->param('id')
+      || $c->req->param('entry')
+      || '';
     $input =~ s/^(\w+)/$1/;
     
     # see if this was an internal link and, if so, report it
@@ -158,7 +164,7 @@ sub begin : Private {
       # report the error as a broken internal link
       $c->error( "Found a broken internal link; no valid Pfam family accession or ID "
                  . "(\"$input\") in \"" . $c->req->referer . "\"" );
-      $c->forward( "/reportError" );
+      $c->forward( '/reportError' );
   
       # now reset the errors array so that we can add the message for
       # public consumption
@@ -167,39 +173,39 @@ sub begin : Private {
     }
 
     # the message that we'll show to the user
-    $c->stash->{errorMsg} = "No valid Pfam family accession or ID";
+    $c->stash->{errorMsg} = 'No valid Pfam family accession or ID';
 
     # log a warning and we're done; drop out to the end method which
     # will put up the standard error page
-    $c->log->warn( "Family::begin: no valid Pfam family ID or accession" );
+    $c->log->warn( 'Family::begin: no valid Pfam family ID or accession' );
 
     return;
   }
 
-  $c->log->debug( "Family::begin: successfully retrieved a pfam object" );
+  $c->log->debug( 'Family::begin: successfully retrieved a pfam object' );
 
   #----------------------------------------
 
   # if this is a PfamA then we need to get more stuff out of the
   # database for it...
 
-  if( $c->stash->{entryType} eq "A" ) {
+  if( $c->stash->{entryType} eq 'A' ) {
 
     # add the clan details, if any
-    $c->stash->{clan} = $c->model("PfamDB::Clans")
-      ->find( { clan_acc => $c->stash->{pfam}->clan_acc } )
+    $c->stash->{clan} = $c->model('PfamDB::Clans')
+                          ->find( { clan_acc => $c->stash->{pfam}->clan_acc } )
         if defined $c->stash->{pfam}->clan_acc;
         
     # if this request originates at the top level of the object hierarchy,
     # i.e. if it's a call on the "default" method of the Family object,
     # then we'll need to do a few extra things
 
-    if( ref $this eq "PfamWeb::Controller::Family" ) {
+    if( ref $this eq 'PfamWeb::Controller::Family' ) {
       
       # add extra data to the stash
-      $c->forward( "_getSummaryData" );
-      $c->forward( "_getDbXrefs" );
-      $c->forward( "_getGoData" );
+      $c->forward( 'getSummaryData' );
+      $c->forward( 'getDbXrefs' );
+      $c->forward( 'getGoData' );
         
     }
   }
@@ -209,99 +215,110 @@ sub begin : Private {
 #- private methods -------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-# get the data items for the overview bar
+=head2 getSummaryData : Private
 
-sub _getSummaryData : Private {
+Retrieves summary data for the family. For most fields this is a simple look-up
+on the PfamA object that we already have, but for the number of interactions
+we have to do one more query.
+
+=cut
+
+sub getSummaryData : Private {
   my( $this, $c ) = @_;
 
-  my %summaryData;
+  my $summaryData = {};
 
   # number of architectures....
-  $summaryData{numArchitectures} = $c->stash->{pfam}->number_archs;
+  $summaryData->{numArchitectures} = $c->stash->{pfam}->number_archs;
 
   # number of sequences in full alignment
-  $summaryData{numSequences} = $c->stash->{pfam}->num_full;
+  $summaryData->{numSequences} = $c->stash->{pfam}->num_full;
 
   # number of structures known for the domain
-  $summaryData{numStructures} = $c->stash->{pfam}->number_structures;
+  $summaryData->{numStructures} = $c->stash->{pfam}->number_structures;
 
   # Number of species
-  $summaryData{numSpecies} = $c->stash->{pfam}->number_species;
+  $summaryData->{numSpecies} = $c->stash->{pfam}->number_species;
 
   # number of interactions
   my $auto_pfamA = $c->stash->{pfam}->auto_pfamA;
-  my $rs = $c->model("PfamDB::Int_pfamAs")
-    ->find( { auto_pfamA_A => $auto_pfamA },
-            { select => [ { count => "auto_pfamA_A" } ],
-              as     => [ qw/numInts/ ]
-            } );
-  $summaryData{numInt} = $rs->get_column( "numInts" );
+  my $rs = $c->model('PfamDB::Int_pfamAs')
+             ->find( { auto_pfamA_A => $auto_pfamA },
+                     { select => [ { count => 'auto_pfamA_A' } ],
+                       as     => [ qw( numInts ) ]
+                     } );
+  $summaryData->{numInt} = $rs->get_column( 'numInts' );
 
-  $c->stash->{summaryData} = \%summaryData;
-
+  $c->stash->{summaryData} = $summaryData;
 }
 
 #-------------------------------------------------------------------------------
-# gets the database cross-references
 
-sub _getDbXrefs : Private {
+=head2 getDbXrefs : Private
+
+Retrieve the database cross-references for the family.
+
+=cut
+
+sub getDbXrefs : Private {
   my( $this, $c ) = @_;
 
-  my %xRefs;
+  my $xRefs = {};
 
   # stuff in the accession and ID for this entry
-  $xRefs{entryAcc} = $c->stash->{pfam}->pfamA_acc;
-  $xRefs{entryId}  = $c->stash->{pfam}->pfamA_id;
+  $xRefs->{entryAcc} = $c->stash->{pfam}->pfamA_acc;
+  $xRefs->{entryId}  = $c->stash->{pfam}->pfamA_id;
 
   # Interpro
-  push @{ $xRefs{interpro} }, $c->stash->{pfam}->interpro_id
+  push @{ $xRefs->{interpro} }, $c->stash->{pfam}->interpro_id
     if $c->stash->{pfam}->interpro_id;
 
   # PDB
-  $xRefs{pdb} = keys %{ $c->stash->{pdbUnique} }
+  $xRefs->{pdb} = keys %{ $c->stash->{pdbUnique} }
     if $c->stash->{summaryData}{numStructures};
 
 
   # PfamA relationship based on SCOOP
-  push @{ $xRefs{scoop} }, $c->model("PfamDB::PfamA2pfamA_scoop_results")
-    ->search( { "auto_pfamA1" => $c->stash->{pfam}->auto_pfamA },
-              { join               => [ qw/pfamA1 pfamA2/ ],
-                select             => [ qw/pfamA1.pfamA_id pfamA2.pfamA_id score/ ],
-                as                 => [ qw/l_pfamA_id r_pfamA_id score/ ]
-              } );
+  push @{ $xRefs->{scoop} },
+       $c->model('PfamDB::PfamA2pfamA_scoop_results')
+         ->search( { auto_pfamA1 => $c->stash->{pfam}->auto_pfamA },
+                   { join        => [ qw( pfamA1 pfamA2 ) ],
+                     select      => [ qw( pfamA1.pfamA_id pfamA2.pfamA_id score ) ],
+                     as          => [ qw( l_pfamA_id r_pfamA_id score ) ]
+                   } );
 
   # PfamA to PfamB links based on PRODOM
   my %atobPRODOM;
   foreach my $xref ( $c->stash->{pfam}->pfamA_database_links ) {
-    if( $xref->db_id eq "PFAMB" ) {
+    if( $xref->db_id eq 'PFAMB' ) {
       $atobPRODOM{$xref->db_link} = $xref;
     } else {
-      push @{ $xRefs{$xref->db_id} }, $xref;
+      push @{ $xRefs->{$xref->db_id} }, $xref;
     }
   }
 
   # PfamA to PfamA links based on PRC
-  my @atoaPRC = $c->model("PfamDB::PfamA2pfamA_PRC_results")
-    ->search( { "pfamA1.pfamA_acc" => $c->stash->{pfam}->pfamA_acc },
-              { join               => [ qw/pfamA1 pfamA2/ ],
-                select             => [ qw/pfamA1.pfamA_id pfamA2.pfamA_id evalue/ ],
-                as                 => [ qw/l_pfamA_id r_pfamA_id evalue/ ],
-                order_by           => "pfamA2.auto_pfamA ASC"
-              } );
+  my @atoaPRC = $c->model('PfamDB::PfamA2pfamA_PRC_results')
+                  ->search( { 'pfamA1.pfamA_acc' => $c->stash->{pfam}->pfamA_acc },
+                            { join               => [ qw( pfamA1 pfamA2 ) ],
+                              select             => [ qw( pfamA1.pfamA_id pfamA2.pfamA_id evalue ) ],
+                              as                 => [ qw( l_pfamA_id r_pfamA_id evalue ) ],
+                              order_by           => 'pfamA2.auto_pfamA ASC'
+                            } );
 
-  $xRefs{atoaPRC} = [];
+  $xRefs->{atoaPRC} = [];
   foreach ( @atoaPRC ) {
-    push @{$xRefs{atoaPRC}}, $_ if $_->get_column( "evalue" ) <= 0.001;
+    push @{ $xRefs->{atoaPRC} }, $_ if $_->get_column( 'evalue' ) <= 0.001;
   }
 
   # $xRefs{atoaPRC} = \@atoaPRC if scalar @atoaPRC;
 
   # PfamB to PfamA links based on PRC
-  my @atobPRC = $c->model("PfamDB::PfamB2pfamA_PRC_results")
-    ->search( { "pfamA.pfamA_acc" => $c->stash->{pfam}->pfamA_acc, },
-              { join      => [ qw/pfamA pfamB/ ],
-                prefetch  => [ qw/pfamA pfamB/ ]
-              } );
+  my @atobPRC = $c->model('PfamDB::PfamB2pfamA_PRC_results')
+                  ->search( { 'pfamA.pfamA_acc' => $c->stash->{pfam}->pfamA_acc, },
+                            { join      => [ qw( pfamA pfamB ) ],
+                              prefetch  => [ qw( pfamA pfamB ) ]
+                            } );
 
   # find the union between PRC and PRODOM PfamB links
   my %atobPRC;
@@ -333,32 +350,36 @@ sub _getDbXrefs : Private {
   foreach ( sort keys %atobPRC ) {
     push @atobPRC_pruned, $atobPRC{$_};
   }
-  $xRefs{atobPRC} = \@atobPRC_pruned if scalar @atobPRC_pruned;
+  $xRefs->{atobPRC} = \@atobPRC_pruned if scalar @atobPRC_pruned;
 
   my @atobPRODOM;
   foreach ( sort keys %atobPRODOM ) {
     push @atobPRODOM, $atobPRODOM{$_};
   }
-  $xRefs{atobPRODOM} = \@atobPRODOM if scalar @atobPRODOM;
+  $xRefs->{atobPRODOM} = \@atobPRODOM if scalar @atobPRODOM;
 
   my @atobBOTH;
   foreach ( sort keys %atobBOTH ) {
     push @atobBOTH, $atobBOTH{$_};
   }
-  $xRefs{atobBOTH} = \@atobBOTH if scalar @atobBOTH;
+  $xRefs->{atobBOTH} = \@atobBOTH if scalar @atobBOTH;
 
-  $c->stash->{xrefs} = \%xRefs;
-
+  $c->stash->{xrefs} = $xRefs;
 }
 
 #-------------------------------------------------------------------------------
-# retrieves GO terms
 
-sub _getGoData : Private {
+=head2 getGoData : Private
+
+Retrieves the gene ontology (GO) data for the family.
+
+=cut
+
+sub getGoData : Private {
   my( $this, $c ) = @_;
 
-  my @goTerms = $c->model("PfamDB::GO")
-                  ->search( { "me.auto_pfamA" => $c->stash->{pfam}->auto_pfamA } );
+  my @goTerms = $c->model('PfamDB::GO')
+                  ->search( { 'me.auto_pfamA' => $c->stash->{pfam}->auto_pfamA } );
 
   $c->stash->{goTerms} = \@goTerms;
 }
