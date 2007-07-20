04c4ba2be299a3c86334731da176bf2c6bb06f29
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-07-20 10:24:38 +0000

    The raw HMMs.

---
+++ trunk/PfamSchemata/PfamDB/PfamA_HMM_fs.pm
@@ -0,0 +1,50 @@
+
+# $Id: PfamA_HMM_fs.pm,v 1.1 2007-07-20 10:24:38 jt6 Exp $
+#
+# $Author: jt6 $
+
+package PfamDB::PfamA_HMM_fs;
+
+use strict;
+use warnings;
+
+use base "DBIx::Class";
+
+__PACKAGE__->load_components( qw/Core/ );
+
+#Set up the table
+__PACKAGE__->table( 'pfamA_HMM_fs' );
+
+#Get the columns that we want to keep
+__PACKAGE__->add_columns( qw/auto_pfamA hmm_fs / );
+
+__PACKAGE__->set_primary_key( 'auto_pfamA' );
+
+__PACKAGE__->has_one( pfam => 'PfamDB::Pfam',
+                      { 'foreign.auto_pfamA' => 'self.auto_pfamA' } );
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+
+=cut
+
+1;
+
