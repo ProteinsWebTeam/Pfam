df78faa266b8fa1c3937805c2df3fbc053229633
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-07-20 10:23:52 +0000

    Replace the main method with one that retrieves the HMM from the DB rather than from file.

--- PfamWeb/lib/PfamWeb/Controller/Family/GetHMM.pm
+++ PfamWeb/lib/PfamWeb/Controller/Family/GetHMM.pm
@@ -2,7 +2,7 @@
 # GetHMM.pm
 # jt6 20061003 WTSI
 #
-# $Id: GetHMM.pm,v 1.4 2007-07-10 19:51:13 jt6 Exp $
+# $Id: GetHMM.pm,v 1.5 2007-07-20 10:23:52 jt6 Exp $
 
 =head1 NAME
 
@@ -18,7 +18,7 @@ Retrieves the raw HMM for a specified Pfam family.
 
 Generates a B<text file>.
 
-$Id: GetHMM.pm,v 1.4 2007-07-10 19:51:13 jt6 Exp $
+$Id: GetHMM.pm,v 1.5 2007-07-20 10:23:52 jt6 Exp $
 
 =cut
 
@@ -33,12 +33,83 @@ use base 'PfamWeb::Controller::Family';
 
 =head2 getHMM : Private
 
-Serve the contents of the HMM file for a Pfam-A entry. Requires the "mode"
-parameter to be set either to ls" or "fs".
+Serve the contents of the HMM for a Pfam-A entry from the database. Requires 
+the "mode" parameter to be set either to "ls" or "fs".
 
 =cut
 
 sub getHMM : Path {
+  my( $this, $c ) = @_;
+
+  return unless defined $c->stash->{pfam};
+
+  # find out which HMM we want...
+  my( $mode ) = $c->req->param( 'mode' ) =~ /^(fs|ls)$/;
+  if( not defined $mode ) {
+    $c->stash->{errorMsg} = 'There was no HMM type specified. '
+                            . '&quot;mode&quot; should be either "ls" or "fs"';
+    return;
+  }
+
+  # retrieve the HMM from the appropriate table
+  my $hmm;
+  if( $mode eq 'ls' ) {
+    my $rs = $c->model('PfamDB::PfamA_HMM_ls')
+               ->find( { auto_pfamA => $c->stash->{pfam}->auto_pfamA } );
+
+    unless( $rs ) {
+      $c->stash->{errorMsg} = "We could not find the &quot;ls&quot; HMM file for " 
+                              . $c->stash->{acc};
+      return;
+    }
+
+    $hmm = $rs->hmm_ls;
+
+  } elsif( $mode eq 'fs' ) {
+    my $rs = $c->model('PfamDB::PfamA_HMM_fs')
+               ->find( { auto_pfamA => $c->stash->{pfam}->auto_pfamA } );
+
+    unless( $rs ) {
+      $c->stash->{errorMsg} = "We could not find the &quot;fs&quot; HMM file for " 
+                              . $c->stash->{acc};
+      return;
+    }    
+
+    $hmm = $rs->hmm_fs;
+  }  
+
+  unless( $hmm ) {
+    $c->stash->{errorMsg} = "We could not find the $mode HMM file for " 
+                            . $c->stash->{acc};
+    return;
+  }    
+
+  my $filename = $c->stash->{pfam}->pfamA_id."_$mode.hmm";
+
+  # set the response headers
+  $c->res->content_type( 'text/plain' );
+  $c->res->headers->header( 'Content-disposition' => "attachment; filename=$filename" );
+
+  # at this point we should have the HMM in hand, so spit it out to the 
+  # response and we're done
+  $c->log->debug( "Family::getHMM:HMM: |$hmm|" );
+  $c->res->write( $hmm );
+
+  # the RenderView action on the end method in Section.pm will spot that there's
+  # content in the response and return without trying to render any templates
+}
+
+#-------------------------------------------------------------------------------
+
+=head2 getHMMFromFile : Private
+
+Serve the contents of the HMM file for a Pfam-A entry. Requires the "mode"
+parameter to be set either to ls" or "fs". This should now be redundant, but
+it's here for sentimental reasons...
+
+=cut
+
+sub getHMMFromFile : Local {
   my( $this, $c ) = @_;
 
   return unless defined $c->stash->{pfam};
@@ -53,7 +124,7 @@ sub getHMM : Path {
 
   # the file
   $c->stash->{HMMFile} = $this->{HMMFileDir} . "/$mode/" . $c->stash->{acc};
-  $c->log->debug( 'Family::getHMM: looking for HMM file |'
+  $c->log->debug( 'Family::getHMMFromFile: looking for HMM file |'
                   . $c->stash->{HMMFile} . '|' );
 
   unless( open( HMM, $c->stash->{HMMFile} ) ) {
