868b5920c3bd9735833b72ac3e4d1645fd8360a7
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-07-27 14:54:00 +0000

    Add caching. Rename some actions.

--- PfamWeb/lib/PfamWeb/Controller/Family/Structures.pm
+++ PfamWeb/lib/PfamWeb/Controller/Family/Structures.pm
@@ -5,7 +5,7 @@
 # Controller to build an image of one of the PDB structure for the
 # specified family, along with a form for choosing a different one
 #
-# $Id: Structures.pm,v 1.7 2007-07-02 09:45:29 jt6 Exp $
+# $Id: Structures.pm,v 1.8 2007-07-27 14:54:00 jt6 Exp $
 
 =head1 NAME
 
@@ -42,37 +42,39 @@ parent class will complain otherwise.
 
 Generates a B<page fragment>.
 
-$Id: Structures.pm,v 1.7 2007-07-02 09:45:29 jt6 Exp $
+$Id: Structures.pm,v 1.8 2007-07-27 14:54:00 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
-use base "PfamWeb::Controller::Family";
+use base 'PfamWeb::Controller::Family';
 
 #-------------------------------------------------------------------------------
 
 =head1 METHODS
 
-=head2 default : Private
+=head2 familyStructures : Path
 
-Captures a URL like
-http://localhost:3000/family/structures?acc=PF00067&amp;pdbId=1w9h 
+Retrieves the list of PDB entries for this family. If a PDB ID is specified,
+the method also retrieves the row of the "pdb" table for that entry.
 
 =cut
 
-sub default : Private {
+sub structures : Path {
   my( $this, $c ) = @_;
 
-  my $id;
-  ( $id ) = $c->req->param('pdbId') =~ /^(\d\w{3})$/
-    if defined $c->req->param('pdbId');
+  # see if we were handed a PDB ID and, if so, put the data for that entry into
+  # the stash
+  if( defined $c->req->param('pdbId') and
+      $c->req->param('pdbId') =~ /^(\d\w{3})$/ ) {
 
-  $c->stash->{pdbObj} = $c->model('PfamDB::Pdb')
-                          ->find( { pdb_id => $id } )
-    if defined $id;
+    $c->stash->{pdbObj} = $c->model('PfamDB::Pdb')
+                            ->find( { pdb_id => $1 } );
+  }
 
+  # retrieve the PDB entries for this family
   my @rs;
   if( defined $c->stash->{pfam}->auto_pfamA ) {
     @rs = $c->model('PfamDB::Pdb_pfamA_reg')
@@ -86,20 +88,24 @@ sub default : Private {
 
   # set up the view and rely on "end" from the parent class to render it
   $c->stash->{template} = 'components/blocks/family/familyStructures.tt';
+
+  # cache the template output for one week
+  $c->cache_page( 604800 );
 }
 
 #-------------------------------------------------------------------------------
 
-=head2 structureTab : Path
+=head2 mapping : Local
 
-Populates the stash with the mapping and hands off to the appropriate template.
+Renders a table showing the mapping between Pfam family, UniProt region and
+PDB residues.
 
 =cut
 
-sub structureTab : Path('/family/structuretab')  {
+sub mapping : Local  {
   my($this, $c) = @_;
 
-  $c->log->debug( 'Family::Structures::structureTab: acc: |'
+  $c->log->debug( 'Family::Structures::mapping: acc: |'
                   . $c->stash->{acc}  . '|' .  $c->stash->{entryType}. '|' );
 
   my @mapping = $c->model('PfamDB::PdbMap')
@@ -111,6 +117,9 @@ sub structureTab : Path('/family/structuretab')  {
   $c->stash->{pfamMaps} = \@mapping;
 
   $c->stash->{template} = 'components/blocks/family/structureTab.tt';
+  
+  # cache the template output for one week
+  $c->cache_page( 604800 );
 }
 
 #-------------------------------------------------------------------------------
