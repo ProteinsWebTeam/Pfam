329fdcd97e2e07cd327e5908f84a2aee6394055a
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2007-08-13 14:40:40 +0000

    Dequeur scripts working on my mac!

--- trunk/PfamBackend/scripts/fastDequeue.pl
+++ trunk/PfamBackend/scripts/fastDequeue.pl
@@ -1,38 +1,70 @@
 #!/usr/bin/perl
+#
+# Authors: Rob Finn & John Tate 
+#
+# This script is designed to take jobs entered into the web_user database
+# and run them.  This dequeuer will only run really fast jobs that take <5 secs.
+# This is the does not run singe sequecne hmmer jobs.  Currently, there is
+# no down weighting of users who submit lots of jobs.  The results of fast jobs
+# are entered in to the database.
+#
+# The executables that this current runs are
+# wu-blastp a query sequence against the pfamb fasta file. 
+#
 
+# Standard Modules
 use strict;
 use warnings;
 use Data::Dumper;
 use IPC::Cmd qw(run);
-use Bio::Pfam::WebServices::PfamQueue::pfamb;
-my $qsout = Bio::Pfam::WebServices::PfamQueue::pfamb->new();
 
-my $debug = 0;
+# Our Modules
+use Bio::Pfam::WebServices::PfamQueue;
+
+#Switch on or off debugging
+our $DEBUG = 0;
+
+# Get a new queue stub of the type fast
+my $qsout = Bio::Pfam::WebServices::PfamQueue->new("fast");
 
 while(1) {
+  #Get a pending job
   my $ref   = $qsout->satisfy_pending_job();
-  $debug && print Dumper($ref);
-  if($ref->{'id'}) {
+  $DEBUG && print Dumper($ref);
+  
+  unless($ref->{'id'}){
+	#No pending jobs
+	sleep 2;
+  }else {
   	my $error = 0;
-    
+	my $cmd;
+  	if($ref->{'job_type'} eq "pfamb"){
 		#Write the users sequence to file
-		open(FA, ">/tmp/".$ref->{job_id}.".fa") ||  ($error .= "Could not open file for writing:[$!].");
+		open(FA, ">".$qsout->tmpDir."/".$ref->{job_id}.".fa") ||  ($error .= "Could not open file for writing:[$!].");
 		print FA ">UserSeq\n";
 		print FA $ref->{'stdin'}."\n";
 		close(FA)  || ($error .= "Could close fasta file:[$!].");
-	   
-	if($error){
-			$qsout->update_job_status($ref->{id}, 'FAIL');
-			$qsout->update_job_stream($ref->{id}, 'stderr', $error);
-			next;
+		   
+		if($error){
+		  $qsout->update_job_status($ref->{id}, 'FAIL');
+		  $qsout->update_job_stream($ref->{id}, 'stderr', $error);
+		  next;
 		}
-		$debug && print STDERR "Executing id=$ref->{'id'}, command=$ref->{'command'}\n";
 		
-     my( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
-          run( command => $ref->{'command'}, verbose => 0 );
-
-	  $debug &&  print STDERR Dumper($success, $error_code, $full_buf, $stdout_buf, $stderr_buf);
+		#There are no options from the user for these jobs at the moment.
+		#Append on the file locations of Pfam-B.fasta and tmp file
+		$cmd = $ref->{'command'}." ".$qsout->dataFileDir."/Pfam-B.fasta ".$qsout->tmpDir.
+				"/".$ref->{job_id}.".fa -cpus ".$qsout->cpus." -gapE=2000 -T=12";
+		
+	}
 	
+	$DEBUG && print STDERR "Executing id=$ref->{'id'}, command=$cmd\n";
+	
+	#Run the executable via IPC::Cmd
+    my( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
+    	run( command => $cmd, verbose => 0 );
+
+	$DEBUG &&  print STDERR Dumper($success, $error_code, $full_buf, $stdout_buf, $stderr_buf);
           
     if($success){
     	$qsout->update_job_status($ref->{'id'}, 'DONE');
@@ -40,14 +72,16 @@ while(1) {
     	$qsout->update_job_status($ref->{'id'}, 'FAIL');
     }
     
+    #Put anything we have captured in stdout and stderr back in the database
     if(scalar(@$stdout_buf)){
     	$qsout->update_job_stream($ref->{id}, 'stdout', join "", @$stdout_buf);
     }     
     if(scalar(@$stderr_buf)){
     	$qsout->update_job_stream($ref->{id}, 'stderr', join "", @$stdout_buf);
-    } 
-    unlink("/tmp/".$ref->{job_id}.".fa") || warn "Could not remove tmp fasta file:[$!]\n";
-  } else {
-    sleep 5;
+    }
+    #Clean up any output files
+    if(-e $qsout->tmpDir."/".$ref->{job_id}.".fa"){
+      unlink($qsout->tmpDir."/".$ref->{job_id}.".fa") || warn "Could not remove tmp fasta file:[$!]\n";
+ 	}
   }
 }
--- trunk/PfamBackend/scripts/hmmerDequeue.pl
+++ trunk/PfamBackend/scripts/hmmerDequeue.pl
@@ -1,39 +1,70 @@
 #!/usr/bin/perl
+#
+# Authors: Rob Finn & John Tate 
+#
+# This script is designed to take jobs entered into the web_user database
+# and run them.  This dequeuer will only run interactive hmmer jobs. 
+# Currently, there is no down weighting of users who submit lots of jobs.  
+# The results of fast jobs are entered in to the database.
+#
+# This only runs pfam_scan.pl which is a wrapper around hmmer and deals with
+# some of the post processing.  We run pfam_scan.pl as this give the users
+# exactly the same results whether they run the batch form of the sequence search.
+#
 
 use strict;
 use warnings;
 use Data::Dumper;
 use IPC::Cmd qw(run);
-use Bio::Pfam::WebServices::PfamQueue::hmmer;
-my $qsout = Bio::Pfam::WebServices::PfamQueue::hmmer->new();
+use Bio::Pfam::WebServices::PfamQueue;
 
-my $debug = 0;
+our $DEBUG = 1;
 
+#Get a queue stub of type hmmer
+my $qsout = Bio::Pfam::WebServices::PfamQueue->new("hmmer");
+
+#An infinite loop to keep submitting jobs.
 while(1) {
+  #See if there are any jobs in the queue
   my $ref   = $qsout->satisfy_pending_job();
-  $debug && print Dumper($ref);
-  if($ref->{'id'}) {
+  $DEBUG && print Dumper($ref);
+  
+  unless($ref->{'id'}){
+  	sleep 2;
+  }else{
   	my $error = 0;
+    my $cmd;
     
-		#Write the users sequence to file
-		open(FA, ">/tmp/".$ref->{job_id}.".fa") ||  ($error .= "Could not open file for writing:[$!].");
-		print FA ">UserSeq\n";
-		print FA $ref->{'stdin'}."\n";
-		close(FA)  || ($error .= "Could close fasta file:[$!].");
+	#Write the users sequence to file
+	open(FA, ">".$qsout->tmpDir."/".$ref->{job_id}.".fa") ||  ($error .= "Could not open file for writing:[$!].");
+	print FA ">UserSeq\n";
+	print FA $ref->{'stdin'}."\n";
+	close(FA)  || ($error .= "Could close fasta file:[$!].");
+	
+	
+	$cmd = $ref->{'command'};
+	if($qsout->pvm){
+		$cmd .= " -pvm"
+	}else{
+		$cmd .= " -cpu ".$qsout->cpus;
+	}
+	$cmd .= " -align -d ".$qsout->dataFileDir;
+	$cmd .= " ".$ref->{'options'};
+	$cmd .= " ".$qsout->tmpDir."/".$ref->{job_id}.".fa";
+
 	   
 	if($error){
-			$qsout->update_job_status($ref->{id}, 'FAIL');
-			$qsout->update_job_stream($ref->{id}, 'stderr', $error);
-			next;
-		}
-		$debug && print STDERR "Executing id=$ref->{'id'}, command=$ref->{'command'}\n";
+	  $qsout->update_job_status($ref->{id}, 'FAIL');
+	  $qsout->update_job_stream($ref->{id}, 'stderr', $error);
+	  next;
+	}
+	$DEBUG && print STDERR "Executing id=$ref->{'id'}, command=$cmd\n";
 		
-     my( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
-          run( command => $ref->{'command'}, verbose => 0 );
+    my( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
+          run( command => $cmd, verbose => 0 );
 
-	  $debug &&  print STDERR Dumper($success, $error_code, $full_buf, $stdout_buf, $stderr_buf);
-	
-          
+	$DEBUG &&  print STDERR Dumper($success, $error_code, $full_buf, $stdout_buf, $stderr_buf);
+	      
     if($success){
     	$qsout->update_job_status($ref->{'id'}, 'DONE');
     }else{
@@ -46,8 +77,6 @@ while(1) {
     if(scalar(@$stderr_buf)){
     	$qsout->update_job_stream($ref->{id}, 'stderr', join "", @$stdout_buf);
     } 
-    unlink("/tmp/".$ref->{job_id}.".fa") || warn "Could not remove tmp fasta file:[$!]\n";
-  } else {
-    sleep 5;
-  }
+    unlink($qsout->tmpDir."/".$ref->{job_id}.".fa") || warn "Could not remove tmp fasta file:[$!]\n";
+  } 
 }
