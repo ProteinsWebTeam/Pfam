8ee0b513527beff250b40183652064fef4ae3037
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-08-30 14:41:52 +0000

    Add "hex_colour" to column list.

--- trunk/PfamSchemata/PfamDB/Pdb_pfamA_reg.pm
+++ trunk/PfamSchemata/PfamDB/Pdb_pfamA_reg.pm
@@ -1,5 +1,5 @@
 
-# $Id: Pdb_pfamA_reg.pm,v 1.3 2007-03-16 11:25:16 jt6 Exp $
+# $Id: Pdb_pfamA_reg.pm,v 1.4 2007-08-30 14:41:52 jt6 Exp $
 #
 # $Author: jt6 $
 
@@ -8,27 +8,39 @@ package PfamDB::Pdb_pfamA_reg;
 use strict;
 use warnings;
 
-use base "DBIx::Class";
+use base 'DBIx::Class';
 
-__PACKAGE__->load_components( qw/Core/ );
-__PACKAGE__->table( "pdb_pfamA_reg" );
-__PACKAGE__->add_columns( qw/auto_pdb_reg auto_pfamA_reg_full auto_pdb auto_pfamA auto_pfamseq chain pdb_res_start pdb_res_end seq_start seq_end/ );
-__PACKAGE__->set_primary_key( "auto_pdb_reg" );
+__PACKAGE__->load_components( qw( Core ) );
+__PACKAGE__->table( 'pdb_pfamA_reg' );
 
-__PACKAGE__->has_one( "pfamA" => "PfamDB::Pfam",
-		      { "foreign.auto_pfamA" => "self.auto_pfamA" },
-		       { proxy => [ qw/pfamA_id pfamA_acc description/ ] } );
+__PACKAGE__->add_columns( qw( auto_pdb_reg 
+                              auto_pfamA_reg_full 
+                              auto_pdb 
+                              auto_pfamA 
+                              auto_pfamseq 
+                              chain 
+                              pdb_res_start 
+                              pdb_res_end 
+                              seq_start 
+                              seq_end
+                              hex_colour ) );
 
-__PACKAGE__->has_one( "pdb"  => "PfamDB::Pdb",
-		      { "foreign.auto_pdb"   => "self.auto_pdb" },
-		      { proxy => [ qw/pdb_id header title pdb_image/ ] } );
+__PACKAGE__->set_primary_key( 'auto_pdb_reg' );
 
-__PACKAGE__->has_one( "pfamseq" => "PfamDB::Pfamseq",
-		      { "foreign.auto_pfamseq" => "self.auto_pfamseq"},
-		      { proxy => [ qw/pfamseq_id pfamseq_acc genome_seq ncbi_code/]});
+__PACKAGE__->has_one( pfamA => 'PfamDB::Pfam',
+                      { 'foreign.auto_pfamA' => 'self.auto_pfamA' },
+                      { proxy => [ qw( pfamA_id pfamA_acc description ) ] } );
 
-__PACKAGE__->has_one( "clanMembers" => "PfamDB::Clan_membership",
-		      { "foreign.auto_pfamA" => "self.auto_pfamA" });
+__PACKAGE__->has_one( pdb  => 'PfamDB::Pdb',
+                      { 'foreign.auto_pdb' => 'self.auto_pdb' },
+                      { proxy => [ qw( pdb_id header title pdb_image ) ] } );
+
+__PACKAGE__->has_one( pfamseq => 'PfamDB::Pfamseq',
+                      { 'foreign.auto_pfamseq' => 'self.auto_pfamseq' },
+                      { proxy => [ qw( pfamseq_id pfamseq_acc genome_seq ncbi_code ) ] } ) ;
+
+__PACKAGE__->has_one( clanMembers => 'PfamDB::Clan_membership',
+                      { 'foreign.auto_pfamA' => 'self.auto_pfamA' } );
 
 =head1 COPYRIGHT
 
