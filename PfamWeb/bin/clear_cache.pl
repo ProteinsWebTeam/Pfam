e68eb4807fdbbef31f06d08deee521c5850716bb
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2007-08-30 13:05:06 +0000

    Add command line options for clearing development, production or all caches.

--- PfamWeb/bin/clear_cache.pl
+++ PfamWeb/bin/clear_cache.pl
@@ -3,18 +3,61 @@
 use strict;
 use warnings;
 
+use Getopt::Std;
+
 use Cache::Memcached;
 
+my @development = qw( 172.17.36.2:11211 );
+my @production  = qw( 172.17.36.3:11211 
+                      172.17.36.4:11211
+                      172.17.36.5:11211
+                      172.17.36.6:11211
+                      172.17.36.7:11211
+                      172.17.36.8:11211
+                      172.17.36.9:11211
+                      172.17.36.10:11211 );
+
+my %options;
+getopt( 'adp', \%options ) or usage();
+
+my @servers;
+if( $options{h} ) {
+  usage();
+} elsif( exists $options{a} ) {
+  print STDERR "clearing all caches:\n";
+  @servers = ( @development, @production );
+} elsif( exists $options{p} ) {
+  print STDERR "clearing production cache:\n";
+  @servers = @production;
+} else {
+  print STDERR "clearing development cache:\n";
+  @servers = @development;
+}
+
+foreach ( @servers ) {
+  print "$_\n";
+}
+
 my $cache = new Cache::Memcached(
-                                  { servers => [ "172.17.36.2:11211",
-                                                 "172.17.36.3:11211",
-                                                 "172.17.36.4:11211",
-                                                 "172.17.36.5:11211",
-                                                 "172.17.36.6:11211",
-                                                 "172.17.36.7:11211",
-                                                 "172.17.36.8:11211",
-                                                 "172.17.36.9:11211",
-                                                 "172.17.36.10:11211" ]
-                                  }
+                                  { servers => \@servers }
                                 );
 $cache->flush_all;
+
+exit;
+
+#-------------------------------------------------------------------------------
+
+sub usage {
+  print <<EOF_help;
+usage: $0 [-adph]
+
+ -a : clear all caches
+ -d : clear development cache only
+ -p : clear production cache only
+ -h : prints this message
+ 
+EOF_help
+
+  exit;
+}
+ 
