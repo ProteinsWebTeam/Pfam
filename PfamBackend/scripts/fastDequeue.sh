a41163aea4e1992f2bce13404a96539aa5f61bbd
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2007-09-11 11:25:45 +0000

    Added general config to shell script

--- PfamBackend/scripts/fastDequeue.sh
+++ PfamBackend/scripts/fastDequeue.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+#
+#You will need to configure this for your system.  
+#General
+export PATH="/sbin:/data/bin:/home/pfamweb/PfamBackend/scripts:$PATH"
+export PERL5LIB="/home/pfamweb/PfamLib:/home/pfamweb/PfamSchemata:/home/pfamweb/BPModules:$PERL5LIB"
 
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-DAEMON=/Users/rdf/Work/PfamWebsite/PfamBackend/scripts/fastDequeue.pl
+#Dequeuer config file location
+export PFAMOFFLINE_CONFIG=/home/pfamweb/PfamBackend/conf/pfam_backend.conf
+
+#Stuff required for binaries.
+export WUBLASTMAT=/Users/rdf/Applications/wu-blast/matrix/
+
+#Config for this shell script
+DAEMON= <PATH-TO-THIS-SCRIPT>/fastDequeue.pl
 NAME=fastDequeue
 DESC="Fast queue daemon"
 
-test -f /home/pfamweb/.bashrc || exit 0
-. /home/pfamweb/.bashrc
+#End of config
 
 test -f $DAEMON || exit 0
 
@@ -14,24 +24,24 @@ set -e
 case "$1" in
   start)
  echo -n "Starting $DESC: "
- start-stop-daemon --start --quiet --pidfile /home/pfamweb/var/run/$NAME.pid \
+ start-stop-daemon --start --quiet --pidfile /var/lock/$NAME.pid \
   --exec /usr/bin/perl --startas $DAEMON
  echo "$NAME."
  ;;
   stop)
  echo -n "Stopping $DESC: "
  # --quiet
- start-stop-daemon --stop --signal 15 --pidfile /home/pfamweb/var/run/$NAME.pid \
+ start-stop-daemon --stop --signal 15 --pidfile /var/lock/$NAME.pid \
   --exec /usr/bin/perl --startas $DAEMON
  echo "$NAME."
  ;;
   restart|force-reload)
  echo -n "Restarting $DESC: "
  start-stop-daemon --stop --quiet --pidfile \
-  /home/pfamweb/var/run/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
+  /var/run/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
  sleep 1
  start-stop-daemon --start --quiet --pidfile \
-  /home/pfamweb/var/run/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
+  /var/run/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
  echo "$NAME."
  ;;
   *)
