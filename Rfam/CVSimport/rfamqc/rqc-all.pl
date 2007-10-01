afb9e4695bdc3cd5678376410e88e1697f61aede
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2007-10-01 09:31:38 +0000

    corrected exe names

--- Rfam/CVSimport/rfamqc/rqc-all.pl
+++ Rfam/CVSimport/rfamqc/rqc-all.pl
@@ -6,17 +6,17 @@ use strict;
 
 foreach my $family (@ARGV){
     print STDERR "\n**SPELL CHECK**\n\n";
-    system ("rqc-spell $family");
+    system ("rqc-spell.pl $family");
     print STDERR "\n**OVERLAP CHECK** - ignoring $family\n\n";
-    system ("rqc-overlap-rdb $family -i $family");
+    system ("rqc-overlap-rdb.pl $family -i $family");
     print STDERR "\n**FORMAT CHECK**\n\n";
-    system ("rqc-format $family");
+    system ("rqc-format.pl $family");
     print STDERR "\n**STRUCTURE CHECK**\n\n";
-    system ("rqc-ss-cons $family");
+    system ("rqc-ss-cons.pl $family");
     print STDERR "\n**MISSING CHECK**\n\n";
-    system ("rqc-check $family");
+    system ("rqc-check.pl $family");
     print STDERR "\n**SEQUENCE CHECK**\n\n";
-    system ("rqc-seqs $family");
+    system ("rqc-seqs.pl $family");
 }
 
 
