da3df73d6423b569e0f2e988713025e8f6000ef9
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2008-01-31 10:37:28 +0000

    First cut of code for queueing up jobs on the farm

---
+++ trunk/PfamLib/Bio/Pfam/PfamJobsDBManager.pm
@@ -0,0 +1,373 @@
+#
+# BioPerl module for Bio::Pfam::PfamJobsDBManager
+#
+# $Author: rdf $
+#
+
+package Bio::Pfam::PfamJobsDBManager;
+use strict;
+use warnings;
+use PfamJobs;
+use Data::Dumper;
+use Carp;
+
+
+=head2 new
+
+  The new method gets a new instance of the PfamJobDBManger and trys to connect to the database.
+  The default options can be overridden by suppling an array of key values pairs.
+  
+  my $pfamJobDB = Bio::Pfam::PfamJobsDBManager->new( [ port, 2187, password, secret ] );
+
+=cut
+
+sub new {
+    my $caller = shift;
+    my $class = ref($caller) || $caller;
+    my %dbiParams = ();
+    my $self = { user      => "pfam",
+		             host      => "127.0.0.1",
+		             port      => "3306",
+		             database  => "pfam_jobs",
+		             driver    => "mysql",
+	               debug     => 0,
+		             @_,};
+		 
+    #print STDERR Dumper($self);
+    
+    eval{
+     $self->{'schema'} = PfamJobs->connect("dbi".
+				       ":".$self->{driver}.
+				       ":".$self->{database}.
+				       ":".$self->{host}.
+				       ":".$self->{port},
+				       $self->{user},
+				       $self->{password},
+				       \%dbiParams);
+    };
+    if($@){
+      croak("Failed to get schema for databse:".$self->database.". Error:[$@]\n");     
+    }
+    
+    #print STDERR "HERE".Dumper($self);
+    
+    return bless($self, $class);
+} 
+
+
+sub getSchema{
+    my $self = shift;
+    return $self->{schema};
+}
+
+sub setStatus{
+  
+  
+}
+
+
+##Select methods should go in here.
+#
+##General shared by all Pfam DB managers
+#sub id2acc {
+# my ($self, $id) = @_;
+# my $result = $self->getSchema
+#                    ->resultset("Pfam")
+#                     ->find({"pfamA_id" => $id});
+# if($result && $result->pfamA_acc){
+#  return ($result->pfamA_acc);
+# }
+#}
+#
+#sub acc2id {
+# my ($self, $acc) = @_;
+# my $result = $self->getSchema
+#                    ->resultset("Pfam")
+#                     ->find({"pfamA_acc" => $acc});
+# if($result && $result->pfamA_id){
+#  return ($result->pfamA_id);
+# }
+#}
+#
+#
+##Try to find in the incoming family is part of a clan
+#sub getClanDataByPfam {
+# my ($self, $family) = @_;
+# 
+# my $result;
+# if($family =~ /PF\d+/){
+#  #Looks like an accession
+#  carp("Looking up information for $family. I think this is an accession") if $self->{'debug'}; 
+#  $result = $self->getSchema
+#                     ->resultset("Clan_membership")
+#                      ->find({ "pfam.pfamA_acc" => $family},
+#                             {join     => [qw/pfam clans/],
+#                              prefetch => [qw/clans/] });
+#  
+# }elsif($family =~ /\S{1,16}/){
+#  #Looks like we have a family id
+#  carp("Looking up information for $family. I think this is an id") if $self->{'debug'}; 
+#  $result = $self->getSchema
+#                     ->resultset("Clan_membership")
+#                      ->find({ "pfam.pfamA_id" => $family},
+#                             {join     => [qw( pfam clans)],
+#                              prefetch => [qw( clans )] });
+#                              
+# }else{
+#  cluck("$family does not look like a pfamA accession or id");
+# }
+# #Return something if we have found something
+# if($result && $result->clan_acc){
+#   carp("Found clan information for\n") if $self->{'debug'};
+#  return ($result);
+# }
+# carp("Did not find any clan information for $family") if $self->{'debug'};
+#}
+#
+#
+#
+#sub getClanData {
+# my($self, $clan) = @_;
+# my $clanData;
+# if($clan =~ /CL\d{4}/){
+# carp("Looking up information for $clan. I think this is an accession") if $self->{'debug'};
+# $clanData = $self->getSchema
+#                    ->resultset("Clans")
+#                     ->find({"clan_acc" => $clan});
+#                     
+# }elsif($clan =~ /\S{1,16}/){
+#  carp("Looking up information for $clan. I think this is an id") if $self->{'debug'};
+#  $clanData = $self->getSchema
+#                    ->resultset("Clans")
+#                     ->find({"clan_id" => $clan});
+# }else{
+#  cluck("$clan does not look like a clan accession or id")
+# }
+# if(ref($clanData)){
+#   carp("Found clan information for $clan") if $self->{'debug'};
+#  return ($clanData);
+# } 
+# carp("Did not find clan information for $clan") if $self->{'debug'};
+#}
+#
+#
+#sub getClanMembership{
+#  my($self, $clan) = @_;
+#  my @clanData;
+#  if($clan =~ /CL\d{4}/){
+#    carp("Looking up information for $clan. I think this is an accession") if $self->{'debug'};
+#    @clanData = $self->getSchema
+#                     ->resultset("Clan_membership")
+#                      ->search({"clans.clan_acc" => $clan},
+#                               { join     => [ qw/clans pfam/],
+#                                 prefetch => [ qw/clans pfam/] });
+#                      
+#  }elsif($clan =~ /\S{1,16}/){
+#   carp("Looking up information for $clan. I think this is an id") if $self->{'debug'};
+#   @clanData = $self->getSchema
+#                     ->resultset("Clan_membership")
+#                      ->search({"clans.clan_id" => $clan},
+#                               { join     =>  qw( clans pfam ),
+#                                 prefetch =>  qw( clans pfam ) });
+#  }else{
+#   cluck("$clan does not look like a clan accession or id")
+#  }
+#  
+#  if($#clanData){
+#    carp("Found clan information for $clan") if $self->{'debug'};
+#   return (\@clanData);
+#  }else{ 
+#    carp("Did not find clan membership for $clan") if $self->{'debug'};
+#  }
+#}
+#
+#sub getAllClanData {
+#  my($self) = shift; 
+#  my @clanData = $self->getSchema
+#                        ->resultset("Clans")
+#                          ->search();
+#  return \@clanData;
+#}
+#
+#sub getPfamData{
+#  my($self, $family) = @_;
+#  my $familyData;
+#  if($family =~ /PF\d{5}/){
+#    carp("Looking up information for $family. I think this is an accession") if $self->{'debug'};
+#    $familyData = $self->getSchema
+#                         ->resultset("Pfam")
+#                           ->find({"pfamA_acc" => $family});
+#                     
+#  }elsif($family =~ /\S{1,16}/){
+#    carp("Looking up information for $family. I think this is an id") if $self->{'debug'};
+#    $familyData = $self->getSchema
+#                      ->resultset("Pfam")
+#                       ->find({"pfamA_id" => $family});
+# }else{
+#  cluck("$family does not look like a family accession or id")
+# }
+# if(ref($familyData)){
+#   carp("Found family information for $family") if $self->{'debug'};
+#  return ($familyData);
+# } 
+# carp("Did not find family information for $family") if $self->{'debug'};
+#}
+#
+#sub getPfamInterPro{
+#  my($self, $family) = @_;
+#  my $familyData;
+#  if($family =~ /PF\d{5}/){
+#    carp("Looking up information for $family. I think this is an accession") if $self->{'debug'};
+#    $familyData = $self->getSchema
+#                         ->resultset("Pfam")
+#                           ->find({"pfamA_acc" => $family},
+#                                  {join        => qw( interpro ),
+#                                   prefetch    => qw( interpro )});
+#                     
+#  }elsif($family =~ /\S{1,16}/){
+#    carp("Looking up information for $family. I think this is an id") if $self->{'debug'};
+#    $familyData = $self->getSchema
+#                      ->resultset("Pfam")
+#                       ->find({"pfamA_id" => $family},
+#                              {join        => qw( interpro ),
+#                               prefetch    => qw( interpro )});
+# }else{
+#  cluck("$family does not look like a family accession or id")
+# }
+# if(ref($familyData)){
+#   carp("Found family information for $family") if $self->{'debug'};
+#  return ($familyData);
+# } 
+# carp("Did not find family information for $family") if $self->{'debug'};
+#}
+#
+#sub getPfamGO{
+#  my($self, $family) = @_;
+#  my @familyGO;
+#  if($family =~ /PF\d{5}/){
+#    carp("Looking up information for $family. I think this is an accession") if $self->{'debug'};
+#    @familyGO = $self->getSchema
+#                         ->resultset("Pfam")
+#                           ->search({"pfamA_acc" => $family},
+#                                    {join        => qw( go ),
+#                                     prefetch    => qw( go )});
+#                     
+#  }elsif($family =~ /\S{1,16}/){
+#    carp("Looking up information for $family. I think this is an id") if $self->{'debug'};
+#    @familyGO = $self->getSchema
+#                         ->resultset("Pfam")
+#                           ->search({"pfamA_id" => $family},
+#                                    {join        => qw( go ),
+#                                     prefetch    => qw( go )});
+# }else{
+#  cluck("$family does not look like a family accession or id")
+# }
+# if(scalar(@familyGO)){
+#   carp("Found family information for $family") if $self->{'debug'};
+#  return (\@familyGO);
+# } 
+# carp("Did not find family information for $family") if $self->{'debug'};
+#}
+#
+#sub getNSEInFull {
+#  my($self, $family) = @_;
+#  my @familyNSE;
+#  if($family =~ /PF\d{5}/){
+#    carp("Looking up information for $family. I think this is an accession") if $self->{'debug'};
+#    @familyNSE = $self->getSchema
+#                         ->resultset("PfamA_reg_full_significant")
+#                           ->search({"pfamA.pfamA_acc" => $family,
+#                                     "in_full"        => 1},
+#                                    {join        => [ qw( pfamA pfamseq ) ] ,
+#                                     prefetch    => [ qw( pfamA pfamseq ) ]});
+#                     
+#  }elsif($family =~ /\S{1,16}/){
+#    carp("Looking up information for $family. I think this is an id") if $self->{'debug'};
+#    @familyNSE = $self->getSchema
+#                         ->resultset("PfamA_reg_full_significant")
+#                           ->search({"pfamA.pfamA_id" => $family,
+#                                     "in_full"        => 1},
+#                                    {join        => [ qw( pfamA pfamseq ) ],
+#                                     prefetch    => [ qw( pfamA pfamseq ) ]});
+#
+#   }else{
+#    cluck("$family does not look like a family accession or id")
+#  }
+#  if(scalar(@familyNSE)){
+#    carp("Found family information for $family") if $self->{'debug'};
+#    return (\@familyNSE);
+#  } 
+#  carp("Did not find family information for $family") if $self->{'debug'};
+#}
+#
+#sub getPfamRegionsForSeq{
+#  my($self, $seq) = @_;
+#  my @pfamRegions;
+#  if($seq =~ /\S{6}/){
+#    carp("Looking up information for $seq. I think this is an accession") if $self->{'debug'};
+#    @pfamRegions = $self->getSchema
+#                         ->resultset("PfamA_reg_full_significant")
+#                           ->search({"pfamseq.pfamseq_acc" => $seq,
+#                                     "in_full"             => 1},
+#                                    {join        => [ qw( pfamA pfamseq ) ],
+#                                     prefetch    => [ qw( pfamA pfamseq ) ]});
+#                     
+#  }elsif($seq =~ /\S+_\S+/){
+#    carp("Looking up Pfam information for $seq. I think this is a seq id") if $self->{'debug'};
+#    @pfamRegions = $self->getSchema
+#                         ->resultset("PfamA_reg_full_significant")
+#                           ->search({"pfamseq.pfamseq_id" => $seq,
+#                                     "in_full"        => 1},
+#                                    {join        => [ qw( pfamA pfamseq ) ],
+#                                     prefetch    => [ qw( pfamA pfamseq ) ]});
+#  
+#  }elsif($seq =~ /\S{33}/){
+#    carp("Looking up Pfam information for $seq. I think this is a seq MD5 checksum") if $self->{'debug'};
+#    @pfamRegions = $self->getSchema
+#                         ->resultset("PfamA_reg_full_significant")
+#                           ->search({"pfamseq.pfamseq_md5" => $seq,
+#                                     "in_full"        => 1},
+#                                    {join        => [ qw( pfamA pfamseq ) ],
+#                                     prefetch    => [ qw( pfamA pfamseq ) ]});
+#  }else{
+#    cluck("$seq does not look like a family accession or id");
+#  }
+#  if(scalar(@pfamRegions)){
+#    carp("Found family information for $seq") if $self->{'debug'};
+#    return (\@pfamRegions);
+#  } 
+#  carp("Did not find family information for $seq") if $self->{'debug'};
+#}
+
+#Specific insert/update methods should go here
+
+
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+
+=cut
+
+
+1;
+
+
+
---
+++ trunk/PfamLib/Bio/Pfam/Queues/IntQueue.pm
@@ -0,0 +1,385 @@
+package Bio::Pfam::Queues::IntQueue;
+#########
+# Author:        rdf
+# Maintainer:    rdf
+# Created:       2007-04-05
+# Last Modified: $Date: 2008-01-31 10:37:28 $
+# Id:            $Id: IntQueue.pm,v 1.1 2008-01-31 10:37:28 rdf Exp $
+#
+# Based on SimpleDB written by Roger Pettett and Jody Clements.
+# Performs Pfam single sequence search database.
+# Database-backed queuing using roughly the same API as LSF
+#
+# command   = target executable
+# priority  = <queuename from subclass>
+#
+use strict;
+use warnings;
+use Sys::Hostname;
+use English qw(-no_match_vars);
+use Carp;
+use Data::UUID;
+use Data::Dumper;
+use File::Basename;
+use Config::General;
+use POSIX qw(setsid);
+use Bio::Pfam::PfamJobsDBManager;
+
+sub new {
+  my ( $caller, $ref ) = @_;
+  my $self = {};
+  
+  my $class = ref($caller) || $caller;
+  print STDERR "$class\n";
+  #Bless $self
+ if($class){
+    bless($self, $class);
+  }else{
+    die "Could not bless object\n";  
+  }
+  
+  
+  #All of this should really go into some sort of config
+  #Lost of current jobs that can be dealt with	
+  $self->knownJobs( { view => 'makepfamview.pl' } );
+  #Get a database connection
+  $self->getSchema({password => 'mafp1' });
+  #Start setting up the local information for setting up the jobs
+  $self->tmpDir("/lustre/scratch1/sanger/pfam/");
+  $self->farmNode("farm-login");
+  
+  #Return the blessed object
+  return ($self);
+}
+
+
+sub getCommand{
+	my($self, $job_type) = @_;
+	if($self->{'jobTypes'}->{$job_type}){
+		return $self->{'jobTypes'}->{$job_type};
+	}else{
+		warn "Unrecognised Job type\n";
+	}
+}
+
+sub knownJobs{
+	my ($self, $jobRefs) = @_;
+	if($jobRefs and ref($jobRefs) eq "HASH"){
+		$self->{'jobTypes'} = $jobRefs
+	}
+}
+
+sub farmNode{
+	my ($self, $farmNode) = @_;
+	if($farmNode){
+		$self->{'farmNode'} = $farmNode
+	}
+}
+
+
+sub knownJobTypes{
+	my $self = shift;
+	if($self->{'jobTypes'}){
+		return (keys %{$self->{'jobTypes'}});
+	}
+}
+
+sub tmpDir {
+	my ($self, $tmpDir) = @_;
+	if($tmpDir){
+		$self->{'tmpDir'} = $tmpDir;
+	}
+	return $self->{'tmpDir'};
+}
+
+#sub job_type {
+#  my ($self, $job_type) = @_;
+#  my $id;
+#  if($self->tracker()) {
+#    ($id) = split /:/mx, $self->tracker();
+#  }elsif($id) {
+#    my $result = $self->getSchema
+#                       ->resultset("jobHistory")
+#                        ->find({id => $id});
+#   
+#    if( $result->job_type ) {
+#      $self->{'command'} = $self->getCommand;
+#    }
+#  }
+#  return $self->{'command'};
+#}
+
+sub getSchema {
+  my ($self, $connectionParams) = @_;
+
+  if(!$self->{'dbSchema'}) {
+    my $pfamJobsDB = Bio::Pfam::PfamJobsDBManager->new( %$connectionParams);
+    $self->{'dbSchema'} = $pfamJobsDB->getSchema;
+  }
+  return $self->{'dbSchema'};
+}
+
+sub statuses {
+  return sort qw(DONE FAIL SUB PEND RUN);
+}
+
+sub submit {
+  my ($self)  = @_;
+  $self->presubmit();
+  my $host    = hostname();
+  my $block   = ($self->blocking() && $self->blocking() ne 'no')?1:0;
+
+  
+    $self->debug() and print {*STDERR} "insert new job PEND\n";
+    my $resultHistory = $self->getSchema
+                        ->resultset('JobHistory')
+                          ->create({'command' => $self->command,
+                                    'priority' => $self->priority,
+                                    'status'  => 'PEND',
+                                    'opened'  => \'NOW()' });
+                                    
+    my $trackerid = $resultHistory->id;
+    print STDERR "tracker id = $trackerid\n";
+    my $resultStream = $self->getSchema
+                              ->resultset('JobStream')
+                                ->create({'id' => $resultHistory->id,
+                                          
+                                         'stdin' => $self->stdin() || q() }); 
+    $self->debug() and print {*STDERR} "insert $trackerid stdin data\n";
+    $self->tracker("$trackerid:");
+    
+    if($self->debug){
+        print STDERR "***** RUNNING QUERY *****\n";
+       my @rs = $self->getSchema
+                      ->resultset( "JobHistory" )
+                        ->search( { status => "PEND",
+                                  id     => { '<',  $resultStream->id },
+                                  job_id => { 'not like', $resultStream->job_id }
+                        
+                                   },
+                                  { select => [
+                                     { count => "id" }
+                                  ],
+                                  as     => [ 'num' ] }
+                                     );
+                                     print STDERR "***** RUN QUERY: Pending jobs=".$rs[0]->get_column("num")."*****\n";
+    }  
+        
+  if($block) {
+    # time out?
+    while(1) {
+      if($self->is_complete()) {
+	last;
+
+      } else {
+	#########
+	# enforced delay so we're not whizzing around in our tight loop
+	#
+	sleep 1;
+      }
+    }
+  }
+
+  #$self->postsubmit();
+
+  $self->debug() and print {*STDERR} qq(submit returning tracker=@{[$self->tracker()]}\n);
+  my ($id) = split /:/mx, $self->tracker();
+  return $id;
+}
+
+#Done
+sub status {
+  my ($self, $id) = @_;
+  my $status = q();
+  if(!$id && $self->tracker()) {
+    ($id) = split /:/mx, $self->tracker();
+  }
+ 
+ my $result = $self->getSchema
+                    ->resultset('JobHistory')
+                     ->find({'id' => $id});         
+  return ($result->status);
+}
+
+#Done
+sub is_complete {
+  my ($self, $index) = @_;
+  return ($self->status($index) !~ /PEND|RUN|SUSP/mx);
+}
+
+sub satisfy_pending_job {
+  my ($self) = @_;
+  return $self->satisfy_pending_jobs(1);
+}
+
+sub numberPendingJobs {
+	my($self, $email ) = @_;
+	print STDERR "Calculating priority for $email\n";
+	my $result = $self->getSchema
+					->resultset('JobHistory')
+						->find( {email  => [ $email],
+								 status => [ qw( PEND RUN ) ] },
+								{select => [
+									{ count => "email" },
+									],
+									as =>  'count'
+									}
+									);
+	my $c = $result->get_column('count');
+	return $c;
+}
+
+sub satisfy_pending_jobs {
+  my ($self, $num) = @_;
+  $num ||= 1;
+  
+  #Get a list of the jobs
+  my @results = $self->getSchema
+                      ->resultset('JobHistory')
+                       ->search({status => "PEND",
+                                 job_type => [$self->knownJobTypes] },
+                                {order_by => 'me.id ASC' });
+   #Take the first job
+   my $result = shift @results;
+  
+    
+   if($result){
+      return ( {
+      'id'         => $result->id,
+      'job_id'     => $result->job_id,
+      'status'     => $result->status,
+      'job_type'   => $result->job_type,
+      'family_id'  => $result->family_id,
+      'family_acc' => $result->family_acc,
+      'options'    => $result->options,
+      'command'    => $self->getCommand($result->job_type)
+    });
+  }
+}
+
+
+
+
+sub get_job {
+	my ($self, $id) = @_;
+	my $result;
+	
+	if($id){
+		$result = $self->getSchema
+                      ->resultset('JobHistory')
+                       ->find({  status   => "PEND",
+                       		     id       => $id },
+                               { join     => [ qw( job_stream )],
+                                 prefetch => [ qw( job_stream )] });
+				
+	}
+
+	if($result){
+		return ( 
+    	{
+      		'id'      => $result->id,
+      		'job_id'  => $result->job_id,
+      		'job_type'=> $result->job_type,
+      		'options' => $result->options,
+      		'stdin'   => $result->stdin,
+      		'stderr'  => $result->stderr,
+      		'status'  => $result->status,
+      		'email'   => $result->email,
+      		'command' => $self->getCommand($result->job_type)
+    });
+		
+	}
+
+}
+
+
+
+
+sub update_job_status {
+  my ($self, $id, $status) = @_;
+
+  if(!$id && $self->tracker()) {
+    ($id) = split /:/mx, $self->tracker();
+  }
+  
+  my $result = $self->getSchema
+                  ->resultset('JobHistory')
+                    ->find({id => $id }); 
+  
+  if($status =~ /^(DONE|FAIL)$/mx) {
+    $result->update({ closed => \'NOW()'});
+  }elsif($status eq 'RUN') {
+    $result->update({ started => \'NOW()'});
+  }
+  
+  $result->update({status => $status});
+  return;
+}
+
+#Done
+sub job_stream {
+  my ($self, $streamid, $id) = @_;
+  
+  if(!$id && $self->tracker()) {
+    ($id) = split /:/mx, $self->tracker();
+  }
+
+  if($streamid !~ /^std(in|out|err)$/mx) {
+    carp qq(PfamSSSDB::update_job_stream: Invalid stream '$streamid');
+    return q();
+  }
+
+  my $results = $self->getSchema
+                      ->resultset('JobStream')
+                        ->find({ id => $id } );
+  
+  return $results->$streamid || q();
+}
+
+
+
+#Done
+sub update_job_stream {
+  my ($self, $id, $streamid, $content) = @_;
+  
+  if(!$id && $self->tracker()) {
+    ($id) = split /:/mx, $self->tracker();
+  }
+
+  if($streamid !~ /^std(out|err)$/mx) {
+    carp qq(PfamSSSDB::update_job_stream: Invalid stream '$streamid');
+    return -1;
+  }
+
+  my $results = $self->getSchema
+                      ->resultset('JobStream')
+                        ->find({ id => $id } );
+  $results->update({ $streamid => $content||q()});
+  return;
+}
+
+
+sub daemonise {
+  my $self = shift;
+  chdir '/var/lock'                 or die "Can't chdir to /: $!";
+  umask 0;
+  open STDIN, '/dev/null'   or die "Can't read /dev/null: $!";
+  open STDOUT, '>/dev/null' or die "Can't write to /dev/null: $!";
+  open STDERR, '>/dev/null' or die "Can't write to /dev/null: $!";
+  defined(my $pid = fork)   or die "Can't fork: $!";
+  
+  my $pidFile = basename($0);
+  $pidFile =~ s/\.pl/\.pid/;
+  
+  if($pid){
+    open PIDFILE, ">$pidFile" or die "can't open $pidFile: $!\n";
+    print PIDFILE $pid;
+    close PIDFILE;
+    exit 0;
+  }
+  setsid                    or die "Can't start a new session: $!"; 
+}
+
+
+
+1;
