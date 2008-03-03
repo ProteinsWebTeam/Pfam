3361e5f90bc8b03d64183f9bf678b0fa3b8b077a
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2008-03-03 13:22:48 +0000

    MD5 change on regeneration.

--- trunk/PfamSchemata/iPfamDB.pm
+++ trunk/PfamSchemata/iPfamDB.pm
@@ -8,8 +8,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04003 @ 2007-10-26 10:08:46
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:9HeqeDQXJ52FiszGQlhrOw
+# Created by DBIx::Class::Schema::Loader v0.04003 @ 2008-02-26 14:01:41
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tvQchb9Wa4uKfRSOzNNpCA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
