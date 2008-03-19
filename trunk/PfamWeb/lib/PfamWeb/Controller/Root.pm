e6e89ac6c36bca0d968ceff580ebbb02dc5a3084
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2008-03-19 13:34:59 +0000

    Strip out common code.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Root.pm
@@ -2,7 +2,7 @@
 # Root.pm
 # jt 20061003 WTSI
 #
-# $Id: Root.pm,v 1.25 2008-03-03 16:47:45 jt6 Exp $
+# $Id: Root.pm,v 1.26 2008-03-19 13:34:32 jt6 Exp $
 
 =head1 NAME
 
@@ -18,20 +18,14 @@ This is the root class for the Pfam website catalyst application. It
 installs global actions for the main site index page and other top-level
 functions.
 
-$Id: Root.pm,v 1.25 2008-03-03 16:47:45 jt6 Exp $
+$Id: Root.pm,v 1.26 2008-03-19 13:34:32 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
-use DateTime;
-
-use base 'Catalyst::Controller';
-
-# set the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-__PACKAGE__->config->{namespace} = '';
+use base 'PfamBase::Controller::Root';
 
 #-------------------------------------------------------------------------------
 
@@ -39,27 +33,14 @@ __PACKAGE__->config->{namespace} = '';
 
 =head2 auto : Private
 
-Checks the request parameters for a "tab" parameter and sets the
-appropriate tab name in the stash. That will be picked up by the
-tab_layout.tt view, which will use it to figure out which tab to show
-by default.
-
-Also adds the Pfam version data to the stash, so that it's accessible 
+Adds the Pfam version data to the stash, so that it's accessible 
 throughout the site.
 
-Checks for the existence of various files that act as switches, enabling
-different behaviours...
 =cut
 
 sub auto : Private {
-  my( $this, $c ) = @_;
-
-  # pick a tab
-  my $tab;
-  ($tab) = $c->req->param('tab') =~ /^(\w+)$/
-    if defined $c->req->param('tab');
-
-  $c->stash->{showTab} = $1 if defined $tab;
+  my $this = shift;
+  my ( $c ) = @_;
 
   # see if we can get a DB ResultSet from the VERSION table, which is 
   # effectively a test of whether we can connect to the DB. If we can't, 
@@ -71,7 +52,7 @@ sub auto : Private {
     $releaseData = $c->model( 'PfamDB::Version' )
                      ->find( {} );
   };
-  if( $@ ) {
+  if ( $@ ) {
     $c->stash->{template} = 'pages/db_down.tt';
 
     # break out of the processing chain now and go straight to the "end" action
@@ -79,306 +60,10 @@ sub auto : Private {
   }
   
   $c->stash->{relData} = $releaseData if $releaseData;
-  
-  return 1;
-}
 
-#-------------------------------------------------------------------------------
-
-=head2 index : Private
-
-Generates the main site index page.
-
-=cut
-
-sub index : Private {
-  my( $this, $c ) = @_;
-
-  # set the page to be cached for one week
-  #$c->cache_page( 604800 );
-
-  # tell the navbar where we are
-  $c->stash->{nav} = 'home';
-
-  $c->log->debug('PfamWeb::index: generating site index');
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 action : Attribute
-
-Description...
-
-fortnight = 1209600
-
-=cut
-
-sub new_features : Local {
-  my( $this, $c ) = @_;
-
-  # get the cookie that stores the state and retrieve the data as a hash with
-  # key/value pairs storing feature timestamps and last-seen dates 
-  my $cookie = $c->req->cookie( 'features' ); 
-  my %last_seen = ();
-  %last_seen = split m/;/, $cookie->value if defined $cookie;
-  
-  # the available changelog entries
-  my $changelog = $c->config->{changelog}->{entries};
-  my @entries = sort keys %$changelog;
-  
-  # work out the range of entries that we should consider
-  my $first = scalar @entries - $c->config->{changelog}->{show_last};
-  $first = 0 if $first < 0;
-  my $last  = scalar @entries - 1;
-
-  my( @times, @features );
-  foreach my $feature_time ( reverse @entries[$first..$last] ) {
-
-    # find out if and when the user last saw this message
-    my $saw_feature_at = $last_seen{$feature_time} || 0;
-
-    # format the timestamp
-    my $ft = DateTime->from_epoch( epoch => $feature_time );
-    my $ts = sprintf( '%02d', $ft->day ) . ' ' . $ft->month_abbr . ' ' . $ft->year();
-
-    # if it's less than, say, two weeks since we saw this entry, show it now
-    if( time - $saw_feature_at < $c->config->{changelog}->{show_for} ) {
-
-      push @features, { id    => $feature_time,
-                        time  => $ts,
-                        entry => $changelog->{$feature_time},
-                        old   => 1 };
-    }
-    # if the feature is previously unseen, show it now
-    elsif( $feature_time > $saw_feature_at ) {
-
-      push @features, { id    => $feature_time,
-                        time  => $ts,
-                        entry => $changelog->{$feature_time} };
-    }
-      
-    # these will be used to set a cookie saying "we saw feature X at time Y"
-    push @times, $feature_time, 
-                 $saw_feature_at > 0 ? $saw_feature_at : time;
-
-  }
-
-  $c->stash->{features} = \@features; 
-
-  # turn the times "hash" into a cookie value
-  my $value = join ';', @times;
-  $c->res->cookies->{features} = { value   => $value,
-                                   expires => 'Fri, 01-Jan-2038 00:00:00 GMT' };
-
-  # if we didn't find any features, set the response status to 204, "No 
-  # content" and let "end" worry about it
-  $c->res->status( 204 ) unless scalar @features;
-
-  # make sure the new features fragment isn't cached
-  $c->res->header( 'Pragma'        => 'no-cache' );
-  $c->res->header( 'Expires'       => 'Thu, 01 Jan 1970 00:00:00 GMT' );
-  $c->res->header( 'Cache-Control' => 'no-store, no-cache, must-revalidate,'.
-                                      'post-check=0, pre-check=0, max-age=0' );
-    
-  # if we did find some features to shout about, "end" should render this 
-  # template
-  $c->stash->{template} = 'components/features.tt';
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 default : Private
-
-Generates a '404' page.
-
-=cut
-
-sub default : Private {
-  my( $this, $c ) = @_;
-
-  # first, figure out where the broken link was, internal or external
-  my $ref = ( defined $c->req->referer ) ? $c->req->referer : '';
-  $c->stash->{where} = ( $ref =~ /sanger/ ) ? 'internal' : 'external';
-
-  # record the error
-  $c->error( 'Found a broken '
-             . $c->stash->{where}
-             . q( link: ')
-             . $c->req->uri
-             . q(', referer: )
-             . ( $ref eq '' ? 'unknown' : qq('$ref') ) );
-
-  # report it...
-  $c->forward('/reportError');
-
-  # ...and clear the errors before we render the page
-  $c->clear_errors;
-
-  # set the HTTP status and point at the 404 page
-  $c->res->status(404);
-  $c->stash->{template} = 'pages/404.tt';
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 reportError : Private
-
-Records site errors in the database. Because we could be getting
-failures constantly, e.g. from SIMAP web service, we want to avoid
-just mailing admins about that, so instead we insert an error message
-into a database table.
-
-The table has four columns:
-
-=over 8
-
-=item message
-
-the raw message from the caller
-
-=item num
-
-the number of times this precise message has been seen
-
-=item first
-
-the timestamp for the first occurrence of the message
-
-=item last
-
-the timestamp for that most recent occurence of the
-message. Automatically updated on insert or update
-
-=back
-
-An external script or plain SQL query should then be able to retrieve
-error logs when required and we can keep track of errors without being
-deluged with mail.
-
-Note that this method does NOT clear_errors. It's up to the caller to decide
-whether that's required or not.
-
-=cut
-
-sub reportError : Private {
-  my( $this, $c ) = @_;
-
-  my $el = $c->model( 'WebUser::ErrorLog' );
-  foreach my $e ( @{$c->error} ) {
-
-    $c->log->error( "PfamWeb::reportError: reporting a site error: |$e|" );
-    # see if we can access the table at all - basically, see if the DB is up 
-    my $rs; 
-    eval {
-      $rs = $el->find( { message => $e } );
-    };
-    if( $@ ) {
-      # really bad; an error while reporting an error...
-      $c->log->error( "PfamWeb::reportError: couldn't create a error log; "
-                      . "couldn't read error table: $@" );
-    }
-  
-    # if we can get a ResultSet, try to add a message
-    if( $rs ) {
-
-      # we've seen this error before; update the error count
-      eval {
-        $rs->update( { num => $rs->num + 1 } );
-      };
-      if( $@ ) {
-        # really bad; an error while reporting an error...
-        $c->log->error( "PfamWeb::reportError: couldn't create a error log; "
-                        . "couldn't increment error count: $@" );
-      }
-
-    } else {
-
-      # no log message like this has been registered so far; add the row 
-      eval {
-        $el->create( { message => $e,
-                       num     => 1,
-                       first   => [ 'CURRENT_TIMESTAMP' ] } );
-      };
-      if( $@ ) {
-        # really bad; an error while reporting an error...
-        $c->log->error( "PfamWeb::reportError: couldn't create a error log; "
-                        . "couldn't create a new error record : $@" );
-      }
-
-    }
-  }
-
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 robots : Path
-
-Serve a "robots.txt" file. We try to retrieve the file from the configuration
-and fall back onto a restrictive generic version that just disallows all robots 
-to all URLs.
-
-=cut
-
-sub robots : Path( '/robots.txt' ) {
-  my( $this, $c ) = @_;
-  
-  # try to get the file from config
-  my $r = $c->config->{robots};
-  
-  # fall back on a generic, restrictive version
-  $r ||= <<'EOF_default_robots';
-User-agent: *
-Disallow: /
-EOF_default_robots
-
-  # put the file into the response and we're done
-  $c->res->content_type( 'text/plain' );
-  $c->res->body( $r );
-}
-
-#-------------------------------------------------------------------------------
-
-=head2 end : Private
-
-Renders the index page for the site by default, but the default template can be 
-overridden by setting it in an action (eg for a 404 template). Patterned on
-the "end" method from the DefaultEnd plugin.
-
-=cut
-
-sub end : Private {
-  my( $this, $c ) = @_;
-  
-  # were there any errors ? If so, render the error page into the response
-  if( scalar @{ $c->error } ) {
-    $c->log->debug( 'Root::end: found some errors from previous methods' );
-    $c->stash->{errorMsg} = $c->error;
-    $c->stash->{template} = 'pages/error.tt';
-    
-    # make sure the error page isn't cached
-    $c->res->header( 'Pragma'        => 'no-cache' );
-    $c->res->header( 'Expires'       => 'Thu, 01 Jan 1970 00:00:00 GMT' );
-    $c->res->header( 'Cache-Control' => 'no-store, no-cache, must-revalidate,'.
-                                        'post-check=0, pre-check=0, max-age=0' );
-    
-    $c->forward( 'PfamWeb::View::TT' );
-    $c->clear_errors;
-  }
-
-  # don't render anything else if the response already has content or if we're
-  # specifically not returning content
-  return 1 if $c->res->body;
-  return 1 if $c->res->status =~ /^3\d\d$/;
-  return 1 if $c->res->status == 204;
-
-  # set the content type to the default of "text/html", unless it's already set
-  $c->response->content_type( 'text/html; charset=utf-8' )
-    unless $c->res->content_type;
-
-  # finally, default to the index page template and we're done
-  $c->stash->{template} ||= 'pages/index.tt';
-  $c->forward( 'PfamWeb::View::TT' );
+  # call the "auto" method on the super-class, which will do the work of
+  # picking the correct tab for us
+  return $c->SUPER::auto( @_ );
 }
 
 #-------------------------------------------------------------------------------
