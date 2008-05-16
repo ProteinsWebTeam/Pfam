bc28a4ec007ff06e710eb9f7823d845a534c912a
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2008-05-16 14:52:48 +0000

    Fix license boilerplate.

--- trunk/PfamBackend/scripts/pfamDNASearch.pl
+++ trunk/PfamBackend/scripts/pfamDNASearch.pl
@@ -9,6 +9,22 @@
 # and make a mini HMM database file. Finally, genewise is called to predicted the
 # precise region(s) of DNA that corresponds to a Pfam family or families.
 
+# Copyright (c) 2007: Genome Research Ltd.
+#
+# Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings; 
