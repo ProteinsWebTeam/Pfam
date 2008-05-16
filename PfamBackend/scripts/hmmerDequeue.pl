7f8f16cad6451f2245d7a110f4e2189fcccd064f
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2008-05-16 14:52:48 +0000

    Fix license boilerplate.

--- PfamBackend/scripts/hmmerDequeue.pl
+++ PfamBackend/scripts/hmmerDequeue.pl
@@ -10,7 +10,23 @@
 # This only runs pfam_scan.pl which is a wrapper around hmmer and deals with
 # some of the post processing.  We run pfam_scan.pl as this give the users
 # exactly the same results whether they run the batch form of the sequence search.
+
+# Copyright (c) 2007: Genome Research Ltd.
 #
+# Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
