9343d37c59dfc936e24d64c28da8958decc808f2
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2008-06-24 08:47:25 +0000

    Wrapper for the Utils controller in PfamBase.

---
+++ RfamWeb/lib/RfamWeb/Controller/Utils.pm
@@ -0,0 +1,58 @@
+
+# Utils.pm
+# jt6 20080104 WTSI
+#
+# $Id: Utils.pm,v 1.1 2008-06-24 08:47:25 jt6 Exp $
+
+=head1 NAME
+
+RfamWeb::Controller::Utils - a set of utility actions
+
+=cut
+
+package RfamWeb::Controller::Utils;
+
+=head1 DESCRIPTION
+
+These are a few actions that can be used in various places around the code. 
+
+$Id: Utils.pm,v 1.1 2008-06-24 08:47:25 jt6 Exp $
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'PfamBase::Controller::Utils';
+
+#-------------------------------------------------------------------------------
+
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+Rob Finn, C<rdf@sanger.ac.uk>
+Paul Gardner, C<pg5@sanger.ac.uk>
+Jennifer Daub, C<jd7@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk),
+         Paul Gardner, C<pg5@sanger.ac.uk>, Jennifer Daub, C<jd7@sanger.ac.uk>
+
+This is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+1;
