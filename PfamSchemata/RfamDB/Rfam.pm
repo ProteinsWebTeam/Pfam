8a8431db0aa824b370f899f212d0fe575dbac391
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2008-07-15 08:47:52 +0000

    Retrofitted boilerplate.

--- PfamSchemata/RfamDB/Rfam.pm
+++ PfamSchemata/RfamDB/Rfam.pm
@@ -177,6 +177,36 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.04004 @ 2008-07-14 20:19:37
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:02dlF/JEau6RmLwYF9l7gA
 
+#-------------------------------------------------------------------------------
 
-# You can replace this text with custom content, and it will be preserved on regeneration
+=head1 AUTHOR
+
+John Tate, C<jt6@sanger.ac.uk>
+
+Paul Gardner, C<pg5@sanger.ac.uk>
+
+Jennifer Daub, C<jd7@sanger.ac.uk>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: John Tate (jt6@sanger.ac.uk), Paul Gardner (pg5@sanger.ac.uk), 
+         Jennifer Daub (jd7@sanger.ac.uk)
+
+This is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
 1;
