cd089b25f38531dcbd7dc1da07a827d425f5012f
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2008-11-12 01:25:00 +0000

    Started to use the external config.  Not great, but starting to get there.

--- PfamScripts/trunk/make/pfbuild.pl
+++ PfamScripts/trunk/make/pfbuild.pl
@@ -1,95 +1,122 @@
-#!/software/bin/perl
+#!/usr/local/bin/perl
 
 use strict;
 use warnings;
 use Getopt::Long;
 
+use Bio::Pfam::Config;
 use Bio::Pfam::AlignPfam;
 
 main( @ARGV ) unless caller(  );
 
 sub main {
-$ENV{PATH} = "/Users/rdf/Work/HMMER3/PreAlpha/hmmer-3.0.snap20081101/src:$ENV{PATH}";
-my ($fname, $hand, $nolsf, $nobuild, $nosplit);
-
-
-&GetOptions( "hand"    => \$hand,
-             "nolsf"   => \$nolsf,
-             "nobuild" => \$nobuild );
-
-#Developement stuff
-$nosplit = 1;
-$nolsf   = 1;
-my $dbsize = 5000000;
-my $pfamseq = '/Users/rdf/Work/HMMER3/Data/23.0/pfamseq';
-
-# Do we have a SEED file
-unless($nobuild){
-  my @buildOpts;
-  unless(-s 'SEED'){
-    die "Could not locate the SEED file:[$!]\n";
+  my $config = Bio::Pfam::Config->new;
+  
+  unless($config){
+    die "Failed to obtain a Pfam Config object, check that the environment variable PFAM_CONFIG is set and the file is there!\n";
   }
-  open( SEED, "SEED" ) or die "FATAL: can't open SEED\n";
-  open( SNEW, ">SEED.$$" ) or die "FATAL: can't write to SEED.$$\n";
-  
-  my $aln = Bio::Pfam::AlignPfam->new;
-  eval{
-    $aln->read_stockholm( \*SEED );
-  };
-  
-  #Problem reading the alignment
-  if($@){
-    warn "SEED alignment not in stockholm format, trying seleex/Pfam format.\n";
-  }
-
-  eval{
-    $aln->read_Pfam( \*SEED );
-  };
-  
-  if($@){
-    die "Failed to parse the SEED alignment\n";
+  unless ($config->location eq 'WTSI' or $config->location eq 'JFRC'){
+    warn "Unkown location.....things will probably break\n"; 
   }
   
-  my $newaln = $aln->allgaps_columns_removed();
-  $newaln->map_chars( '-', '.' );
-  $newaln->write_stockholm( \*SNEW );
-  if($newaln->match_states_string){
-    push(@buildOpts, "--hand");
-  }
-  close SEED;
-  close SNEW;
+  my ($fname, $hand, $local, $nobuild, $nosplit);
 
-  rename( "SEED", "SEED.old.$$" ) or die "FATAL: can't rename SEED\n";
-  rename( "SEED.$$", "SEED" ) or die "FATAL: can't rename SEED.$$\n";
 
-  my $buildline = join( ' ', @buildOpts );
-
-  system("hmmbuild $buildline HMM SEED");
+  &GetOptions( "hand"    => \$hand,
+               "local"   => \$local,
+               "nobuild" => \$nobuild,
+               "nosplit" => \$nosplit );
+               
+  # Do we have a SEED file and can we build an HMM for it.
+  unless($nobuild){
+    my @buildOpts;
+    unless(-s 'SEED'){
+      die "Could not locate the SEED file:[$!]\n";
+    }
+    open( SEED, "SEED" ) or die "FATAL: can't open SEED\n";
+    open( SNEW, ">SEED.$$" ) or die "FATAL: can't write to SEED.$$\n";
   
-}
+    my $aln = Bio::Pfam::AlignPfam->new;
+    eval{
+      $aln->read_stockholm( \*SEED );
+    };
+  
+    #Problem reading the alignment
+    if($@){
+      warn "SEED alignment not in stockholm format, trying seleex/Pfam format.\n";
+    }
+    #Seed if it looks like a SEED in Pfam/Mul format
+    eval{
+      $aln->read_Pfam( \*SEED );
+    };
+  
+    if($@){
+      die "Failed to parse the SEED alignment\n";
+    }
+    
+    #Now reformat the SEED alignment to make all - characters .  
+    my $newaln = $aln->allgaps_columns_removed();
+    $newaln->map_chars( '-', '.' );
+    $newaln->write_stockholm( \*SNEW );
+    if($newaln->match_states_string){
+      push(@buildOpts, "--hand");
+    }
+    close SEED;
+    close SNEW;
 
-# Set the location of pfamseq
-#my $pfamseq;
+    rename( "SEED", "SEED.old.$$" ) or die "FATAL: can't rename SEED\n";
+    rename( "SEED.$$", "SEED" ) or die "FATAL: can't rename SEED.$$\n";
 
-# Skip hmmbuild if no build
+    my $buildline = join( ' ', @buildOpts );
+    unless( -d $config->hmmer3bin){
+      die "Could not find the HMMER3 bin directory,". $config->hmmer3bin ."\n";
+    }
+    
+    system($config->hmmer3bin."/hmmbuild -o hmmbuild.log ".$buildline." HMM SEED") and die "Error building hmm!\n";
+    unless(-s "HMM"){
+      die "Failed to run HMM build. Although it seems to have run successfully, the HMM has no size or is absent\n"; 
+    }
+  }else{
+    # Skip hmmbuild if no build
+    unless( -s "HMM" ){
+      die "Could not find the HMM file:[$!]\n";
+    } 
+  }
 
-  # If there is an HMM already present, grab the build line.
 
-  # Now set any additional parameters
+  # Grab all of the file ownership
 
-# Check the SEED format
-# Reformat gap characters
-# Do we have an RF line
-
-
-# Grab all of the file ownership
-
-# Now build and search
-
-my $options = "-E 1000  -Z $dbsize";
-if($nolsf){
+  # Now build and search
+  my $searchOptions = "-E 1000  -Z ".$config->dbsize;
+  
+  my $cmd;
   if($nosplit){
-     system("hmmsearch $options HMM $pfamseq > PFAMOUT");
+    $cmd =$config->hmmer3bin."/hmmsearch $searchOptions HMM ".$config->pfamseqLoc."/pfamseq > PFAMOUT";
   }
-}
+  
+  if($local){
+    if($nosplit){
+      system($cmd) 
+        and die ;
+    }
+  }else{
+    #We are going to use some sort of farm! 
+    my $farmConfig = $config->farm;
+    unless($farmConfig){
+      die "Failed to get a farm configuration file\n"; 
+    }
+    if($farmConfig->{sge}){
+      if($nosplit){
+        system("qsub -N pfamHmmSearch -j y -o /dev/null -b y -cwd -V \'$cmd\'") and die "Failed to submit job to SGE,qsub -N pfamHmmSearch -j -o /dev/null -b y -cwd -V \'$cmd\' \n";  
+      }else{
+        #TODO split
+        print STDERR "TODO: nothing run:\n"
+      } 
+    }elsif($farmConfig->{lsf}){
+      
+    }else{
+      die "Unknown farm set-up\n"; 
+    }
+  }
+  print STDERR "Finished\n";
 }
