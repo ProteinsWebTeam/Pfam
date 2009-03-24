c7b3676b96ad0741c6f946e140ffbeccbe314c45
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-03-24 10:49:39 +0000

    Rough first draft of the format check script!

---
+++ trunk/PfamScripts/trunk/pfamqc/pqc-format.pl
@@ -0,0 +1,46 @@
+#!/usr/local/bin/perl
+#
+# Perform the basic format checks on a family.
+#
+
+use strict;
+use warnings;
+use Cwd;
+
+use Bio::Pfam::PfamQC;
+
+my $family = shift;
+chomp($family);
+
+unless($family){
+  warn "\n***** No family passed  *****\n\n"; 
+  help();
+}
+
+
+my $familyIO = Bio::Pfam::FamilyIO->new;
+
+#-------------------------------------------------------------------------------
+my $pwd = getcwd;
+
+if( !(-d "$pwd/$family") ) {
+    die "$0: [$pwd/$family] is not a current directory.\nMust be in the parent directory of the family to check in\n";
+}
+
+if( !-w "$pwd/$family" ) {
+    die "$0: I can't write to directory [$pwd/$family].  Check the permissions.\n";
+}
+
+#-------------------------------------------------------------------------------
+
+if( !Bio::Pfam::PfamQC::checkFamilyFiles( $family) ){
+    print "pfci: $family contains errors.  You should rebuild this family.\n";
+    exit(1);
+}
+  
+my $famObj = $familyIO->loadPfamAFromLocalFile($family, $pwd);
+print STDERR "Successfully loaded $family thorugh middleware\n";
+
+unless(Bio::Pfam::PfamQC::passesAllFormatChecks($famObj, $family)){
+    exit(1); 
+}
