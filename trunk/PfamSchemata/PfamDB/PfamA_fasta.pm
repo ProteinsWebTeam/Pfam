ec0e16bda0a1448152c3635949de065f1ebcd562
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-04-06 09:18:29 +0000

    New wrapper for the pfamA_fasta table.

---
+++ trunk/PfamSchemata/PfamDB/PfamA_fasta.pm
@@ -0,0 +1,57 @@
+
+# $Id: PfamA_fasta.pm,v 1.1 2009-04-06 09:18:29 jt6 Exp $
+#
+# $Author: jt6 $
+
+package PfamLive::PfamA_fasta;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components( qw( Core ) );
+
+# the table
+__PACKAGE__->table( qq( pfamA_fasta ) );
+
+# the columns
+__PACKAGE__->add_columns( qw( auto_pfamA
+                              fasta 
+                              nr_threshold 
+                               ) );
+
+# keys
+__PACKAGE__->set_primary_key( qw( auto_pfamA 
+                                  ) );
+
+# relationships
+
+__PACKAGE__->has_one( pfam => 'PfamDB::Pfam',
+                      { 'foreign.auto_pfamA' => 'self.auto_pfamA' } );
+
+
+
+=head1 COPYRIGHT
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
+
+This is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
+
+1;
+
