acb71456ece15f4232a391345be7a364fc286835
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-05-07 14:49:55 +0000

    Starting to add additional functionality.

--- PfamScripts/trunk/svnhooks/pre-commit.pl
+++ PfamScripts/trunk/svnhooks/pre-commit.pl
@@ -74,16 +74,24 @@ foreach my $f (@deleted_files){
 
 if($msg =~ /^PFCI:/){
   $txnlook->commitFamily( $pfamDB );
+elsif($msg =~ /^PFCIATC:(CL\d{4})\:(PF\d{5})/){
+  my($clan, $fam);
+  $clan = $1;
+  $fam  = $2;
+  #Add the clan data to the database
+  $txnlook->updateClanMembership($pfamDB, $clan, $fam);
+  #Then commit the family
+  $txnlook->commitFamily( $pfamDB );
 }elsif( $msg =~ /^PFNEW:/ ){
   $txnlook->commitNewFamily( $pfamDB ); 
 }elsif( $msg =~ /^PFNEWMOV:/ ){
   ;
 }elsif( $msg =~ /^PFANN:/ ) {
-    
+  $txnlook->commitDesc;  
 }elsif( $msg =~ /^PFMOV:/ ) {
   #$txnlook->moveFamily( $pfamDB );  
 }elsif( $msg =~ /^PFKIL:/ ) {
-    
+  
 }else{
   die "Do not know here this commit has come from, [$msg]!\n"; 
 }
