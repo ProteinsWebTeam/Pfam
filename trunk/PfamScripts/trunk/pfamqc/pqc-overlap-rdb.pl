1973877ac35271e483ab8f8316b0b44c692dc720
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-05-07 15:03:03 +0000

    Working copy of pqc-overlap-rdb. Currently it does not deal with clans.....

--- trunk/PfamScripts/trunk/pfamqc/pqc-overlap-rdb.pl
+++ trunk/PfamScripts/trunk/pfamqc/pqc-overlap-rdb.pl
@@ -5,57 +5,33 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Cwd;
 
 use Bio::Pfam::Config;
 use Bio::Pfam::PfamQC;
 use Bio::Pfam::PfamLiveDBManager;
-use Bio::Pfam::Clan::ClanQC;
 
 my ( @ignore, $endpoints_opt, $help, $add_to_clan, $remove_from_clan );
 
 &GetOptions(
   "i=s@"               => \@ignore,
-  "e"                  => \$endpoints_opt,
-  "add_to_clan=s"      => \$add_to_clan,
-  "remove_from_clan=s" => \$remove_from_clan,
   "help"               => \$help
 );
 
 my $family = shift;
+
+if ( !$family ) {
+  print STDERR "ERROR:You have not specified the family\n";
+  help();
+}
+
 if ( $family =~ /^(\S+)\/$/ ) {
   chop $family;
 }
 
 help() if ($help);
-if ( !$family ) {
-  print STDERR "ERROR:You have not specified the family\n";
-  help();
-}
-if ( $remove_from_clan and $add_to_clan ) {
-  print STDERR
-"ERROR: You cannot use the -add_family and -remove_family options together\n";
-}
-
-if ($add_to_clan) {
-  unless ( $add_to_clan =~ /CL\d\d\d\d/ ) {
-    print STDERR
-"ERROR: The clan accession you specified [$add_to_clan] is not in the correct format.\n";
-    help();
-  }
-  print STDERR "Performing overlap checks for adding $family to $add_to_clan\n";
-}
-elsif ($remove_from_clan) {
-  unless ( $remove_from_clan =~ /CL\d\d\d\d/ ) {
-    print STDERR
-"ERROR: The clan accession you specified [$remove_from_clan] is not in the correct format.\n";
-    help();
-  }
-  print STDERR
-    "Performing overlap checks for removing $family from $remove_from_clan\n";
-}
 
 my %ignore;    #Hash used to ignore duplicating entries in @ignore
-
 foreach my $fam (@ignore) {
   $ignore{$fam} = 1;
 }
@@ -71,96 +47,27 @@ my $pfamDB = Bio::Pfam::PfamLiveDBManager->new(
 #Find out if family is in rdb
 my $rdb_family = $pfamDB->getPfamData($family);
 
+my $familyIO = Bio::Pfam::FamilyIO->new;
 
-#Find out what clan (if any) the family already belongs to
-my ( $rdb_clan_acc, $clan_membership );
-if ($rdb_family) {
-  my $clan_result = $pfamDB->getClanDataByPfam("$family");
+#-------------------------------------------------------------------------------
+my $pwd = getcwd;
 
-  if ($clan_result) {
-    $rdb_clan_acc    = $clan_result->clan_acc;
-    $clan_membership = $pfamDB->getClanMembership($rdb_clan_acc);
-
-    print STDERR "$family belongs $rdb_clan_acc.\n" unless ($remove_from_clan);
-
-    if ( $add_to_clan and $rdb_clan_acc ) {
-      die
-"Cannot add $family to $add_to_clan.  $family already belongs to $rdb_clan_acc.\n";
-    }
-    elsif ( $remove_from_clan and $rdb_clan_acc ne $remove_from_clan ) {
-      die
-"$family belongs to $rdb_clan_acc and not to $remove_from_clan.  Incorrect clan accession entered on command line.";
-    }
-    my $num_in_clan = @$clan_membership;
-    if ( $remove_from_clan and $num_in_clan == 2 ) {
-      warn
-"Cannot remove $family from $rdb_clan_acc because there are only two families in this clan and a clan must contain a minimum of two families\n";
-    }
-
-  }
-}
-if ( $add_to_clan and !$rdb_clan_acc ) {    #Family not currently in a clan
-  $clan_membership = $pfamDB->getClanMembership($add_to_clan);
+if( !(-d "$pwd/$family") ) {
+    die "$0: [$pwd/$family] is not a current directory.\nMust be in the parent directory of the family to check in\n";
 }
 
-my $no_nl = 0;
-my $no_ne = 0;
-
-#Look for nested domains if there is a DESC file
-if ( -e "$family/DESC" ) {
-  open( DESC, "$family/DESC" ) or die "Could not open $family/DESC $!";
-  while (<DESC>) {
-    if (/^AC\s+(PF\d\d\d\d\d)/) {
-      my $pfam = $pfamDB->getPfamData($1);
-      push( @ignore, $pfam->pfamA_id ) unless ( $ignore{ $pfam->pfamA_id } );
-      $ignore{ $pfam->pfamA_id } = 1;
-    }
-    if (/^NE\s+(PF\d\d\d\d\d)/) {
-      my $nested = $pfamDB->getPfamData($1);
-      push( @ignore, $nested->pfamA_id )
-        unless ( $ignore{ $nested->pfamA_id } );
-      $ignore{ $nested->pfamA_id } = 1;
-      $no_ne++;
-      warn "CAUTION:: am allowing $family to overlap with $1 !!!\n";
-    }
-    elsif (/^NL\s+/) {
-      $no_nl++;
-    }
-  }
-  close(DESC);
-}
-if ( $no_nl != $no_ne ) {
-  warn "$family:Numbers of nested domains and nested locations do not match";
-  exit(1);
+if( !-w "$pwd/$family" ) {
+    die "$0: I can't write to directory [$pwd/$family].  Check the permissions.\n";
 }
 
-
-#Now look for overlaps
-my ( $overlaps, $clan_seed_overlap );
-if ( $add_to_clan or ( $rdb_clan_acc and !$remove_from_clan ) ) {
-  foreach my $clan_mem (@$clan_membership) {
-    push( @ignore, $clan_mem->pfamA_id )
-      unless ( $ignore{ $clan_mem->pfamA_id } );
-  }
+#-------------------------------------------------------------------------------
   
-  $overlaps =
-    &Bio::Pfam::PfamQC::family_overlaps_with_db( $family, \@ignore,
-    $endpoints_opt, $pfamDB );
-  warn "$family: found $overlaps external overlaps\n";
+my $famObj = $familyIO->loadPfamAFromLocalFile($family, $pwd);
+print STDERR "Successfully loaded $family through middleware\n";
 
-  #Need to also check for clan seed overlaps
-  $clan_seed_overlap =
-    Bio::Pfam::Clan::ClanQC::check_seed_overlap( $pfamDB, $family,
-    $clan_membership, $pfamDB );
-  warn "$family: found $clan_seed_overlap clan seed overlaps\n"
-    if ($clan_seed_overlap);
-}
-else {
-  $overlaps =
-    &Bio::Pfam::PfamQC::family_overlaps_with_db( $family, \@ignore,
-    $endpoints_opt, $pfamDB );
-  warn "$family: found $overlaps overlaps\n";
-}
+my $overlaps =
+  &Bio::Pfam::PfamQC::family_overlaps_with_db( $family, \%ignore, undef, $pfamDB, $famObj );
+  warn "$family: found $overlaps external overlaps\n";
 
 if ($overlaps) {
   exit(1);
