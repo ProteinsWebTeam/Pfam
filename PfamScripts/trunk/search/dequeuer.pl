b0d0ba0f2c2f147eba6ef84b2a7324ab12f7f2ca
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-05-15 09:49:33 +0000

    A first cut of a dequeuer that runs PfamScan directly.

---
+++ PfamScripts/trunk/search/dequeuer.pl
@@ -0,0 +1,92 @@
+#!/software/bin/perl
+
+use strict;
+use warnings;
+
+use Bio::Pfam::Scan::PfamScan;
+use Bio::Pfam::WebServices::PfamQueue;
+use Storable qw( freeze );
+
+use Data::Dump qw( dump );
+
+my $DEBUG = $ENV{DEBUG} || 0;
+$ENV{PFAMOFFLINE_CONFIG} = $ENV{HOME} . '/perl/pfam_scan/pfam_backend.conf';
+
+my $pq = Bio::Pfam::WebServices::PfamQueue->new( 'h3' );
+$pq->daemonise unless $DEBUG;
+
+
+JOB: while ( 1 ) {
+
+  my $job = $pq->satisfy_pending_job;
+
+  unless ( $job->{id} ) {
+    $DEBUG && print STDERR "no job; sleeping\n";
+    sleep 2; # TODO make the sleep delay a configuration variable
+    next JOB;
+  }
+  $DEBUG && print STDERR 'job specification: ' . dump( $job ) . "\n";
+
+  my $sequence = ">UserSeq\n" . $job->{stdin};
+
+  my %input = (
+    -dir      => $pq->dataFileDir, # TODO make "dir" configurable
+    -sequence => $sequence,
+  );
+
+  $DEBUG && print STDERR 'job params: ' . dump( \%input ) . "\n";
+
+  my $ps = Bio::Pfam::Scan::PfamScan->new( %input );
+  $DEBUG && print STDERR "created a PfamScan object\n";
+  # TODO make the PfamScan object stick around; add a "set_input" method
+
+  my $results;
+  eval {
+    $DEBUG && print STDERR "running a search...\n";
+    $ps->search;
+    $DEBUG && print STDERR "done\n";
+    $results = freeze( $ps->results );
+  };
+  if ( $@ ) {
+    $DEBUG && print STDERR "ERROR: search failed: $@\n";
+    $pq->update_job_stream( $job->{id}, 'stderr', $@ );
+    $pq->update_job_status( $job->{id}, 'FAIL' );
+    next JOB;
+  }
+
+  $DEBUG && print STDERR "updating job status\n";
+  $pq->update_job_status( $job->{id}, 'DONE' );
+  $DEBUG && print STDERR "done\n";
+
+  if ( $results ) {
+    $DEBUG && print STDERR "updating job stream with results\n";
+    $pq->update_job_stream( $job->{id}, 'stdout', $results );
+    $DEBUG && print STDERR "done\n";
+  }
+  else {
+    $DEBUG && print STDERR "updating job stream with empty result set\n";
+    $pq->update_job_stream( $job->{id}, 'stdout', 'no matches found' );
+    $DEBUG && print STDERR "done\n";
+  }
+
+  $DEBUG && print STDERR "restarting event loop\n";
+}
+
+exit;
+
+__DATA__
+open ( FASTA, 'multiple.fa' )
+  or die "ERROR: couldn't open fasta file: $!";
+my $sequence = join '', <FASTA>;
+close FASTA;
+
+my %input = (
+  -dir      => $ENV{HOME} . '/perl/pfam_scan/small_library',
+  -sequence => $sequence,
+);
+
+my $ps = Bio::Pfam::Scan::PfamScan->new( %input );
+
+$ps->search;
+my $results = $ps->results;
+print dump( $results );
