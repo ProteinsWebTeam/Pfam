3bbb1e098e62af0237f9e5160887977ee08047b3
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-06-17 12:33:21 +0000

    Added bit to convert ids to accession if at WTSI.

--- trunk/PfamScripts/trunk/svn/pfinfo.pl
+++ trunk/PfamScripts/trunk/svn/pfinfo.pl
@@ -6,6 +6,7 @@ use warnings;
 use Bio::Pfam::Config;
 use Bio::Pfam::SVN::Client;
 use Bio::Pfam::FamilyIO;
+use Bio::Pfam::PfamLiveDBManager;
 
 my $config = Bio::Pfam::Config->new;
 
@@ -16,7 +17,22 @@ unless ( $ARGV[0] ) {
 my $family = shift;
 
 if ( $family !~ /^(PF\d{5})$/ ) {
+ if($config->location eq 'WTSI'){
+  my $connect = $config->pfamlive;
+  my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( 
+    %{ $connect }
+  );
+  my $pfamAcc = $pfamDB->id2acc($family);
+  unless($pfamAcc =~ /PF\d{5}/){
+    warn "You passed in something that did not look like an accession.\n"; 
+    warn "Because you are at WTSI, tried to map it to an accession, but failed.\n";
+    help();
+  }
+  $family = $pfamAcc;   
+ }else{
   warn "Looks like you have passed in an id rather than accession, [$family]\n";
+  help();
+ }
 }
 
 #Check that family exists in svn
@@ -31,7 +47,7 @@ sub help {
 
 usage: $0 <PFAM ACCESSION>
 
-Prints the SVN revision history for the family. 
+Prints the SVN revision history for the family. If you are WTSI, you can use family ids.
 
 EOF
  
