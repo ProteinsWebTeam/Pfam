4013b8caf32f7b24240b40c9419102808e2fcd6d
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-06-22 09:56:38 +0000

    Migrated from old code base and updated to reflect changes.

---
+++ PfamScripts/trunk/make/extend.pl
@@ -0,0 +1,119 @@
+#!/software/bin/perl
+
+unlink ("FA");
+use strict;
+use Getopt::Long;
+use Bio::Pfam::Config;
+use Bio::Pfam::AlignMethods;
+
+my $prog = $0;
+
+my ($opt_align,$opt_n,$opt_c,$length, $t_coffee, $mafft, $clustalw, $probcons, $musclep, $muscle, $mcount, $mask, $pdb, $chain);
+
+# Get options
+&GetOptions('align=s'=> \$opt_align,
+	    'n=i' => \$opt_n,
+	    'c=i' => \$opt_c,
+	    't!'  => \$t_coffee,
+	    'm!'  => \$mafft,
+	    'cl!' => \$clustalw,
+	    'p!'  => \$probcons,
+	    'mu!' => \$muscle,
+	    'mup!'=> \$musclep,
+            "ma!" => \$mask,
+            'pdb=s' => \$pdb,
+            'chain=s' => \$chain,
+	    );
+
+
+my $method;
+
+if($t_coffee) {
+   $method = "t_coffee";
+   $mcount++;
+}
+if($mafft) {
+  $method = "mafft";
+  $mcount++;
+}
+if($clustalw) {
+  $method = "clustalw";
+  $mcount++;
+}
+if($probcons) {
+  $method = "probcons";
+  $mcount++;
+}
+if($mask) {
+  $method = "mask";
+  $mcount++;
+  if(!$pdb) {
+      &AlignPfam::help($prog);
+  }
+}
+if($muscle) {
+  $method = "muscle";
+  $mcount++;
+}
+if($musclep) {
+  $method = "musclep";
+  $mcount++;
+} 
+
+if($mcount == 0) {
+  &AlignMethods::help($prog);
+}
+
+
+if($mcount !=1) {
+    die "More than one alignment method selected!\n";
+}
+
+
+
+if(! $opt_align){
+    warn "Don't know what to extend!\n";
+    &AlignMethods::help($prog);
+}
+
+if (! $opt_n && ! $opt_c){
+    warn "Don't know how far to extend!\n";
+    &AlignMethods::help(@_);
+}
+
+
+my $config = Bio::Pfam::Config->new;      
+my $dbarg  = $config->pfamseqLoc."/pfamseq";
+
+
+
+# If options not set use defaults
+if (! $opt_n){
+    $opt_n=0;
+}
+if (! $opt_c){
+    $opt_c=0;
+}
+
+print STDERR "\nExtending $opt_align by:\n\n";
+print STDERR "$opt_n residues at the N terminus\n";
+print STDERR "$opt_c residues at the C terminus\n";
+
+
+&Bio::Pfam::AlignMethods::extend_alignment($opt_align, $dbarg, $opt_n, $opt_c) ; # prints extended sequences in fasta format to a file called FA
+
+
+my $fasta = "FA";
+
+# Read fasta file and put ref into scalars
+my @foo = &Bio::Pfam::AlignMethods::read_fasta($fasta);
+my @sequence    = @{shift @foo};
+my @description = @{shift @foo};
+
+
+# Create alignment 
+my %hash=&Bio::Pfam::AlignMethods::create_alignment(\@sequence,\@description,$method,$fasta, $pdb, $chain);
+
+
+#Print alignment
+&Bio::Pfam::AlignMethods::print_alignment(\%hash, $method);
