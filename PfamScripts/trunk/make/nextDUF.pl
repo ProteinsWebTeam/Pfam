969044e102f5d6578cf71f8d85bc6e7beb76cf41
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-07-23 16:17:24 +0000

    Fixed the off by one error.

--- PfamScripts/trunk/make/nextDUF.pl
+++ PfamScripts/trunk/make/nextDUF.pl
@@ -24,6 +24,7 @@ foreach my $fam (@{$pfams}, @{$deadFams}){
 }
 
 my @dufNosSort = sort {$b <=> $a} @dufNos;
-my $no = $dufNosSort[0]++;
+
+my $no = $dufNosSort[0] + 1;
 
 print "\n The next available DUF number is: DUF$no\n\n";
