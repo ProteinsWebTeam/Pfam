ff49ac2133d8c76f282ed93498572d050bd31220
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2009-07-27 20:19:20 +0000

    Added help message

--- PfamScripts/trunk/pfamqc/pqc-seqs.pl
+++ PfamScripts/trunk/pfamqc/pqc-seqs.pl
@@ -2,8 +2,20 @@
 
 use strict;
 use warnings;
+use Cwd;
+use Getopt::Long;
 
 use Bio::Pfam::PfamQC;
+
+my ($help);
+
+&GetOptions(
+  "h" => \$help,
+);
+
+if($help or !defined($ARGV[0])){
+  &help; 
+} 
 
 my $family = shift;
 my $pwd = getcwd;
@@ -14,4 +26,20 @@ my $famObj = $familyIO->loadPfamAFromLocalFile( $family, $pwd );
 unless ( Bio::Pfam::PfamQC::sequenceChecker( $family, $famObj ) ) {
   print "$family contains sequence errors.  You should rebuild this family.\n";
   exit(1);
+}
+
+
+sub help {
+
+print<<EOF;
+
+usage: $0 <PFAM ACCESSION>
+
+Checks all of the sequences in the SEED and ALIGN files are present in the current pfamseq.  
+
+EOF
+ 
+
+exit;
+  
 }
