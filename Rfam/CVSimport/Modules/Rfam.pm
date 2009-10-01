18ecc9971e850d13b0f4e1b005f48c4a0c01d1a4
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2009-10-01 14:40:09 +0000

    Update for Infernal 1.0 and Farm-II.

--- Rfam/CVSimport/Modules/Rfam.pm
+++ Rfam/CVSimport/Modules/Rfam.pm
@@ -42,7 +42,19 @@ use vars qw( @ISA
 	     @output_file_set
 	     @scores_file_set
 	     @optional_file_set 
-	     $view_maker );
+	     $view_maker 
+             $infernal_path
+             $scratch_farm
+             $rfamseq_farm2_run_dir
+             %forbidden_family_terms
+             @forbidden_terms
+             $hmmLib
+             $familyDir
+             $prc_path
+             $hmmer2_path
+             $hmmer3_path
+             $cmSeqLib
+);
 
 @ISA    = qw( Exporter );
 
@@ -67,9 +79,6 @@ $lock_file      = "$accession_dir/lock";
 #$rfamseq_root_dir    = "/lustre/pfam/rfam/Production/rfamseq";
 $rfamseq_root_dir    = "/nfs/pfam_nfs/rfam/rfamseq";
 $rfamseq_current_dir = "$rfamseq_root_dir/CURRENT";
-$rfamseq_new_dir     = "$rfamseq_root_dir/NEW";               #NOT FUCKING USED!
-$rfamseq_current_inx = "$rfamseq_current_dir/rfamseq.fa.bpi"; #NOT FUCKING USED!
-$rfamseq_new_inx     = "$rfamseq_new_dir/rfamseq.fa.bpi";     #NOT FUCKING USED!
 $rfamseq             = "$rfamseq_current_dir/rfamseq.fa";
 $rfamseq_run_dir     = "/data/blastdb/Rfam/rfamseq"; 
 
@@ -81,6 +90,35 @@ $rfamseq_farm_current_inx = "$rfamseq_farm_current_dir/rfamseq.fa.bpi";
 $rfamseq_farm_new_inx     = "$rfamseq_farm_new_dir/rfamseq.fa.bpi";
 $rfamseq_farm             = "$rfamseq_farm_current_dir/rfamseq.fa";
 $rfamseq_farm_run_dir     = "/data/blastdb/Rfam/rfamseq"; 
+
+#RFAMSEQ On FARM2:
+#$rfamseq_farm2_run_dir     = "/lustre/scratch103/blastdb/Rfam/rfamseq"; 
+$rfamseq_farm2_run_dir     = "/nfs/pfam_nfs/rfam/rfamseq/CURRENT";
+
+#SCRATCH ON THE FARM:
+#$scratch_farm = "/lustre/scratch1/sanger/rfam";
+$scratch_farm = "/lustre/scratch103/sanger";
+
+#INFERNAL PATH
+$infernal_path = "/software/rfam/share/infernal-1.0/bin";
+
+#HMMER PATH
+$hmmer2_path = "/software/pfam/src/hmmer-2.3.2/src";
+$hmmer3_path = "/software/pfam/src/hmmer-3.0b2/bin";
+
+######################################################################
+#CLAN PATHS:
+$familyDir = "/lustre/pfam/rfam/Curation/RFSEQ10"; #Path to a directory containing all the families
+
+#PRC:
+$hmmLib = "/nfs/pfam_nfs/rfam/HMMLIB";
+$prc_path = "/software/rfam/share/prc-1.5.4_nuc";
+
+#CMSEARCH
+$cmSeqLib = "/nfs/pfam_nfs/rfam/CMSEQLIB";
+
+
+######################################################################
 
 @align_file_set    = ( "SEED", "ALIGN" );
 @view_file_set     = ( "SEED.ann", "ALIGN.ann" ); # must be in same order as @align_file_set
@@ -109,7 +147,70 @@ our $rdbNameDev= "rfam_9_1";
 my $external_rdb_name = "rfam";
 my $switchover_rdb_name = "rfam2";
 our $live_rdb_name = "rfamlive";
-my $temp_rdb_name = "rfam_temp"; ## to be deleted!!
+
+######################################################################
+#Dictionaries of forbidden terms used by rfmake.pl & ALIGN2SEED.pl
+#Terms too common to use as "true" for the histograms & thresholds:
+%forbidden_family_terms = (
+    AND => 1,
+    ARCH => 1,
+    ARCHAEA => 1,
+    ARCHAEAL => 1,
+    BACT => 1,
+    BACTERIA => 1,
+    BACTERIAL => 1,
+    BODY => 1,
+    BOX => 1,
+    CANDIDATE => 1,
+    CD => 1,
+    CHROMOSOME => 1,
+    DATA => 1,
+    DNA => 1,
+    DOMAIN => 1,
+    DS => 1,
+    ELEMENT => 1,
+    EUK => 1,
+    EUKARYOTE => 1,
+    EUKARYOTIC => 1,
+    EXON => 1,
+    FAMILY  => 1,
+    FAMILIES => 1,
+    FOR => 1,
+    FROM => 1,
+    GENE => 1,
+    GENOME => 1,
+    INTERGENIC => 1,
+    INTRON => 1,
+    NUCLEAR => 1,
+    OF => 1,
+    PHAGE => 1,
+    PLANT => 1,
+    PRIMER => 1,
+    PROMOTER => 1,
+    PROTEIN => 1,
+    REG => 1,
+    RNA => 1,
+    SEQ  => 1,
+    SEQUENCE => 1,
+    SMALL   => 1,
+    SUBUNIT => 1,
+    THE => 1,
+    TYPE => 1,
+    UTR => 1,
+    VIRUS => 1
+);
+ 
+@forbidden_terms = qw(
+contaminat
+pseudogene
+pseudo-gene
+repeat
+repetitive
+transpos
+);
+
+
+######################################################################
 
 sub default_db{
     return Rfam::DB::DB_RCS->new( '-current'   => $current_dir,
@@ -156,16 +257,16 @@ sub live_rdb {
 
 }
 
-sub temp_rdb {
-   my ($self) = @_;
-    warn "Depricated method. Call for $temp_rdb_name db: this database doesn't exist";
-  #  return Rfam::DB::DB_RDB->new('-db_name' => $temp_rdb_name,
-#				'-db_driver' => $rdb_driver, 
-#				'-db_host' => $rdb_host,
-#				'-db_user' => $rdb_user,
-#				'-db_password' => $rdb_pass);
+# sub temp_rdb {
+#    my ($self) = @_;
+#     warn "Depricated method. Call for $temp_rdb_name db: this database doesn't exist";
+#   #  return Rfam::DB::DB_RDB->new('-db_name' => $temp_rdb_name,
+# #				'-db_driver' => $rdb_driver, 
+# #				'-db_host' => $rdb_host,
+# #				'-db_user' => $rdb_user,
+# #				'-db_password' => $rdb_pass);
 
-}
+# }
 
 sub external_rdb_update{
     my ($self) = @_;
