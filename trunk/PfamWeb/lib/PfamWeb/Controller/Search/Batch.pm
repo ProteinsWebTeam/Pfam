1ce3328c48feca10aa6cf0bc883bc16b2728ac0e
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-10-07 12:02:06 +0000

    User options have changed slightly for the new search system.

--- trunk/PfamWeb/lib/PfamWeb/Controller/Search/Batch.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Search/Batch.pm
@@ -2,7 +2,7 @@
 # Batch.pm
 # jt6 20061108 WTSI
 #
-# $Id: Batch.pm,v 1.16 2008-09-18 11:51:36 jt6 Exp $
+# $Id: Batch.pm,v 1.17 2009-10-07 12:02:06 jt6 Exp $
 
 =head1 NAME
 
@@ -18,13 +18,15 @@ This controller is responsible for running batch searches for protein sequences.
 It uses the base class L<Batch|PfamWeb::Controller::Search::Batch> to take
 care of queuing the search, but the validation of input etc. is here.
 
-$Id: Batch.pm,v 1.16 2008-09-18 11:51:36 jt6 Exp $
+$Id: Batch.pm,v 1.17 2009-10-07 12:02:06 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
+use Data::Dump qw( dump );
+use JSON;
 use Scalar::Util qw( looks_like_number );
 use Email::Valid;
 
@@ -46,19 +48,19 @@ sub search : Path {
 
   # validate the input
   unless ( $c->forward( 'validate_input' ) ) {
-    $c->stash->{batchSearchError } = $c->stash->{searchError};
+    $c->stash->{batchSearchError} = $c->stash->{searchError};
     return;
   }
 
   #----------------------------------------
 
   # build the command to run
-  $c->stash->{options}  = '';
-  $c->stash->{options} .=  q( --mode ) . $c->stash->{batchOpts} if( $c->stash->{batchOpts} ne 'both' and 
-                                                                    $c->stash->{batchOpts} ne 'bothNoMerge' );
-  $c->stash->{options} .=  q( --no_merge )                      if( $c->stash->{batchOpts} eq 'bothNoMerge' );
-  $c->stash->{options} .=  q( -e )     . $c->stash->{evalue}    if( $c->stash->{evalue} and not $c->stash->{ga} );
-  $c->stash->{options} .=  q( --overlap )                       if( $c->stash->{showOverlap} );
+  # $c->stash->{options}  = '';
+  # $c->stash->{options} .=  q( --mode ) . $c->stash->{batchOpts} if( $c->stash->{batchOpts} ne 'both' and 
+  #                                                                   $c->stash->{batchOpts} ne 'bothNoMerge' );
+  # $c->stash->{options} .=  q( --no_merge )                      if( $c->stash->{batchOpts} eq 'bothNoMerge' );
+  # $c->stash->{options} .=  q( -e )     . $c->stash->{evalue}    if( $c->stash->{evalue} and not $c->stash->{ga} );
+  # $c->stash->{options} .=  q( --overlap )                       if( $c->stash->{showOverlap} );
   
   # finally, before we actually run the search, check we didn't do it recently 
   unless ( $c->forward( 'check_unique' ) ) {
@@ -72,9 +74,14 @@ sub search : Path {
   # set the queue
   $c->stash->{job_type} = 'batch';
 
+  # convert the options hash into JSON
+  $c->log->debug( 'Search::Batch::search: user_options: ', dump( $c->stash->{user_options} ) )
+    if $c->debug;
+  $c->stash->{options} = to_json( $c->stash->{user_options} );
+
   # and submit the job...
   unless ( $c->forward( 'queue_search_transaction' ) ) {
-    $c->stash->{batchSearchError } = $c->stash->{searchError};
+    $c->stash->{batchSearchError} = $c->stash->{searchError};
     return;
   }
 
@@ -92,7 +99,7 @@ sub search : Path {
 }
 
 #-------------------------------------------------------------------------------
-#- private actions--------------------------------------------------------------
+#- private actions -------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
 =head2 validate_input : Private
@@ -104,6 +111,8 @@ Error messages are returned in the stash as "searchError".
 
 sub validate_input : Private {
   my( $this, $c ) = @_;
+
+  $c->stash->{user_options} = {};
 
   # do the quick checks first...
   
@@ -125,12 +134,12 @@ sub validate_input : Private {
       return 0;
     }
     
-    $c->stash->{batchOpts} = $c->req->param('batchOpts');
+    $c->stash->{user_options}->{batchOpts} = $c->req->param('batchOpts');
   }
 
   # default to "both"
   else {
-    $c->stash->{batchOpts} = 'both';
+    $c->stash->{user_options}->{batchOpts} = 'both';
 
     $c->log->debug( 'Search::Batch::validate_input: setting search option to default of "both"' )
       if $c->debug;
@@ -139,7 +148,7 @@ sub validate_input : Private {
   # if we have an E-value, we'll use that, otherwise we'll use the gathering
   # threshold
   if ( defined $c->req->param( 'ga' ) and $c->req->param( 'ga' ) ) {
-    $c->stash->{ga} = 1;
+    $c->stash->{user_options}->{ga} = 1;
   } 
   else {
 
@@ -147,7 +156,7 @@ sub validate_input : Private {
     if ( defined $c->req->param( 'evalue' ) and 
          looks_like_number( $c->req->param( 'evalue' ) ) and
          $c->req->param('evalue') > 0 ) {
-      $c->stash->{evalue} = $c->req->param( 'evalue' );
+      $c->stash->{user_options}->{evalue} = $c->req->param( 'evalue' );
     }
     else {
       $c->stash->{searchError} = 'You did not enter a valid E-value.';
@@ -161,7 +170,9 @@ sub validate_input : Private {
 
   # email address
   if ( Email::Valid->address( -address => $c->req->param('email') ) ) {
-    $c->stash->{email} = $c->req->param('email');
+    $c->stash->{email} = $c->req->param('email'); 
+    # Note: this is not a "user_option" ! Needs to be dropped into it's own column in
+    # the job_history table
   }
   else {
     $c->stash->{searchError} = 'You did not enter a valid email address.';
@@ -172,11 +183,28 @@ sub validate_input : Private {
     return 0;
   }  
 
+  # search for Pfam-Bs ?
+  $c->stash->{user_options}->{searchBs} = ( defined $c->req->param('searchBs') and
+                                            $c->req->param('searchBs') );
+
+  # should we search for Pfam-As or just skip them and search only Pfam-Bs ?
+  if ( defined $c->req->param('skipAs') and
+       $c->req->param('skipAs') ) {
+    
+    $c->log->debug( 'Search::Batch::validate_input: skipping Pfam-A search' )
+      if $c->debug;
+    
+    # flag up the fact that we want to skip Pfam-A searches
+    $c->stash->{user_options}->{skipAs} = 1;
+    
+    # and force a search for Pfam-Bs
+    $c->stash->{user_options}->{searchBs} = 1;
+  }
+
   # finally, the most time-consuming tests: check the sequence upload itself
   unless ( $c->forward( 'parse_upload' ) ) {
 
-    $c->stash->{searchError} = 
-         $c->stash->{searchError}
+    $c->stash->{searchError} = $c->stash->{searchError}
       || 'No valid sequence file found. Please enter a valid FASTA-format file and try again.';
 
     $c->log->debug( 'Search::Batch::validate_input: bad FASTA file; returning to form' )
@@ -189,6 +217,9 @@ sub validate_input : Private {
   $c->log->debug( 'Search::Batch::validate_input: input parameters all validated' )
     if $c->debug;
     
+  $c->log->debug( 'Search::Batch::validate_input: user_options: ', dump( $c->stash->{user_options} ) )
+    if $c->debug;
+
   return 1;
 }
 
