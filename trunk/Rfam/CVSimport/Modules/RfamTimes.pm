b71d1f32a1931b4cee4a939d2592e89c9bfce554
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2009-10-16 15:00:10 +0000

    Modules for Infernal 1.0.

---
+++ trunk/Rfam/CVSimport/Modules/RfamTimes.pm
@@ -0,0 +1,16 @@
+package RfamTimes;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_classes;
+
+
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-08-23 21:54:36
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:QP0YgfuJ8IJiiSqqOQUo3Q
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
