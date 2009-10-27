ebdf2a48bb74b5feff7ae2c990f40a2cfec1a8d5
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-10-27 14:16:32 +0000

    Re-worked to use the new DB and the new domain graphics code.

--- PfamWeb/lib/PfamWeb/Controller/Search/Domain.pm
+++ PfamWeb/lib/PfamWeb/Controller/Search/Domain.pm
@@ -2,7 +2,7 @@
 # Domain.pm
 # jt6 20061108 WTSI
 #
-# $Id: Domain.pm,v 1.6 2009-10-07 12:11:36 jt6 Exp $
+# $Id: Domain.pm,v 1.7 2009-10-27 14:16:32 jt6 Exp $
 
 =head1 NAME
 
@@ -16,14 +16,17 @@ package PfamWeb::Controller::Search::Domain;
 
 Searches for sequence architectures with the specified set of Pfam domains.
 
-$Id: Domain.pm,v 1.6 2009-10-07 12:11:36 jt6 Exp $
+$Id: Domain.pm,v 1.7 2009-10-27 14:16:32 jt6 Exp $
 
 =cut
 
 use strict;
 use warnings;
 
+use Bio::Pfam::Drawing::Layout::LayoutManager;
+
 use Storable qw( thaw );
+use Data::Dump qw(dump);
 
 use base 'PfamWeb::Controller::Search';
 
@@ -31,32 +34,58 @@ use base 'PfamWeb::Controller::Search';
 
 =head1 METHODS
 
-=head2 domainSearch : Path
+=head2 domain_search : Path
 
 Executes a domain query.
 
 =cut
 
-sub domainSearch : Path {
+sub domain_search : Path {
   my ( $this, $c ) = @_;
 
-  $c->log->debug( 'Search::Domain::domainSearch: executing a domain search' )
+  $c->log->debug( 'Search::Domain::domain_search: executing a domain search' )
     if $c->debug;
 
-  # point at the template right away
   $c->stash->{template} = 'components/allArchitectures.tt';
 
+  unless ( $c->forward( 'get_data' ) ) {
+    $c->log->debug( 'Search::Domain::domain_search: problem getting data' )
+        if $c->debug;
+    
+    $c->stash->{domainSearchError} = $c->stash->{errorMessage}
+                                     || 'There was a problem running your query.'; 
+   
+    return; 
+  }
+
+  unless ( $c->forward( 'build_layout' ) ) {
+    $c->log->debug( 'Search::Domain::domain_search: problem getting data' )
+        if $c->debug;
+    
+    $c->stash->{domainSearchError} = $c->stash->{errorMessage}
+                                     || 'There was a problem running your query.'; 
+   
+    return; 
+  }
+
+
+
+}
+
+sub get_data : Private {
+  my ( $this, $c ) = @_;
+  
   my $list = '';
   if ( defined $c->req->param( 'have' ) ) {
-    $c->log->debug( 'Search::Domain::domainSearch: must have:     |' . $c->req->param('have') . '|' )
-      if $c->debug;
+    $c->log->debug( 'Search::Domain::domain_search: must have:     |' 
+                    . $c->req->param('have') . '|' ) if $c->debug;
     foreach ( split /\s+/, $c->req->param('have') ) {
       next unless /(PF\d{5})/;
       $list .= "+$1 ";
     }
   }
   if ( defined $c->req->param( 'not' ) ) {
-    $c->log->debug( 'Search::Domain::domainSearch: must not have: |' . $c->req->param('not') . '|' )
+    $c->log->debug( 'Search::Domain::domain_search: must not have: |' . $c->req->param('not') . '|' )
       if $c->debug;
     foreach ( split /\s+/, $c->req->param('not') ) {
       next unless /(PF\d{5})/;
@@ -64,10 +93,17 @@ sub domainSearch : Path {
     }
   }
 
-  $c->log->debug( "Search::Domain::domainSearch: list: |$list|" )
+  $c->log->debug( "Search::Domain::domain_search: list: |$list|" )
     if $c->debug;
 
-  return unless $list;
+  unless ( $list ) {
+    $c->log->debug( "Search::Domain::get_data: no query terms" ) 
+        if $c->debug;
+
+    $c->stash->{errorMessage} = 'You did not supply any query terms';
+    
+    return 0; 
+  }
 
   my @architectures = $c->model('PfamDB::Architecture')
                         ->search( {},
@@ -79,70 +115,175 @@ sub domainSearch : Path {
                                           $list );
 
   my $sum = 0;
+  map { $sum += $_->no_seqs } @architectures;
+  $c->log->debug( "Search::Domain::get_data: sum using map: $sum" )
+      if $c->debug;
+
+  $sum = 0;
   foreach my $arch ( @architectures ) {
     $sum += $arch->no_seqs;
   }
-
-  $c->log->debug( 'Search::Domain::domainSearch: found ' . scalar @architectures
-                  . ' rows, with a total of $sum sequences' ) if $c->debug;
+  $c->log->debug( "Search::Domain::get_data: sum using for: $sum" )
+      if $c->debug;
 
   $c->stash->{numRows} = scalar @architectures;
   $c->stash->{numSeqs} = $sum;
 
+  $c->log->debug( 'Search::Domain::domain_search: found ' . scalar @architectures
+                  . " rows, with a total of $sum sequences" ) if $c->debug;
+
   # if there are too many results, bail here and let the TT just
   # display the text summary, plus an admonition to the user to
   # restrict their search a bit
-  return if scalar @architectures > 500;
-
-  # build the mappings that we'll need to interpret all this...
-  my( @seqs, %seqInfo );
-  foreach my $arch ( @architectures ) {
-
-    # thaw out the sequence object for this architecture
-    push @seqs, thaw( $arch->annseq_storable );
-  
-    # work out which domains are present on this sequence
-    my @domains = split m/\~/, $arch->architecture;
-    $seqInfo{$arch->pfamseq_id}{arch} = \@domains;
-  
-    # store a mapping between the sequence and the auto_architecture
-    $seqInfo{$arch->pfamseq_id}{auto_arch} = $arch->auto_architecture;
-  
-    # if this is a call to retrieve all of the architectures, we don't
-    # have an auto_architecture, so this won't work
-    $seqInfo{$arch->pfamseq_id}{num} = $arch->no_seqs unless $c->stash->{auto_arch};
-  }
-  $c->log->debug( 'Search::Domain::domainSearch: found ' . scalar @seqs . ' storables' )
-    if $c->debug;
-
-  if( scalar @seqs ) {
-    my $layout = Bio::Pfam::Drawing::Layout::PfamLayoutManager->new;
-    
-    $layout->layout_sequences_with_regions_and_features( \@seqs, { PfamA      => 1,
-                                                                   PfamB      => 1,
-                                                                   noFeatures => 1 } );
-    
-    # should we use a document store rather than temp space for the images ?
-    my $imageset;  
-    if ( $c->config->{use_image_store} ) {
-      $c->log->debug( 'Search::Domain::domainSearch: using document store for image' )
+  if ( scalar @architectures > 500 ) {
+    $c->log->debug( 'Search::Domain::get_data: too many sequences: ' . scalar @architectures )
         if $c->debug;
-      require PfamWeb::ImageSet;
-      $imageset = PfamWeb::ImageSet->new;
+
+    $c->stash->{errorMessage} = 'Your query returned too many sequences';
+    
+    return 0;
+  }
+  
+  my ( @seqs, %seqInfo, @ids );
+  foreach my $row ( @architectures ) {
+
+    # thaw out the sequence object for this architecture and get a handle on
+    # the right DB object
+    push @seqs, thaw( $row->storable->annseq_storable );
+
+    # we're looking at all sequences, so we want just the type example
+    my $seq = $row->type_example;
+
+    # stash the sequence IDs for the type example in an array, so that we can 
+    # access them in the right order in the TT, i.e. ordered by number of 
+    # sequences with the given architecture)
+    my $pfamseq_id = $seq->pfamseq_id;
+    push @ids, $pfamseq_id;
+
+    # work out which domains are present on this sequence
+    my @domains = split m/\~/, $row->architecture;
+    $seqInfo{$pfamseq_id}{arch} = \@domains;
+
+    # how many sequences ?
+    $seqInfo{$pfamseq_id}{num} = $row->no_seqs;
+
+    # store a mapping between the sequence and the auto_architecture
+    $seqInfo{$pfamseq_id}{auto_arch} = $row->auto_architecture;
+
+    # store the sequence description, species name and length of each 
+    # individual sequence
+    $seqInfo{$pfamseq_id}{desc}    = $seq->description;
+    $seqInfo{$pfamseq_id}{species} = $seq->species;
+    $seqInfo{$pfamseq_id}{length}  = $seq->length;
+  }
+
+  $c->log->debug( 'DomainGraphics::get_family_data: retrieved '
+                  . scalar @seqs . ' storables' ) if $c->debug;
+
+  $c->stash->{seqs}    = \@seqs;
+  $c->stash->{ids}     = \@ids;
+  $c->stash->{seqInfo} = \%seqInfo;
+}
+
+sub build_layout : Private {
+  my ( $this, $c ) = @_;
+
+  my $lm = Bio::Pfam::Drawing::Layout::LayoutManager->new;
+  my $pfama = $lm->_getRegionConfigurator('Pfama');
+  my $pfamb = $lm->_getRegionConfigurator('Pfamb');
+  
+  if ( $c->req->param('ac') ) {
+
+    # detaint the param by trying to decode it as JSON
+    my $data;
+    eval {
+      $data = from_json( uri_unescape( $c->req->param('ac') ) );
+    };
+    if ( $@ or not defined $data ) {
+      # decoding failed; don't try to use the data
+      $c->log->warn( 'Search::Domain::build_layout: failed to detaint colours param' )
+        if $c->debug;
     }
     else {
-      $c->log->debug( 'Search::Domain::domainSearch: using temporary directory for store image' )
-        if $c->debug;
-      require Bio::Pfam::Drawing::Image::ImageSet;
-      $imageset = Bio::Pfam::Drawing::Image::ImageSet->new;
+      # decoding worked; set these assigned colours on the layout manager
+      # before generating the new layout
+      my $colours_json = uri_unescape( $c->req->param('ac') );
+      my $colours = from_json( $colours_json );
+      
+      my ( $pfama_colours, $pfamb_colours );
+      # Note that we're not validating the colours here, because they're going
+      # to be passed to the Moose objects, which all have strict type checking
+      # in place. Any broken data will cause an exception when the Moose object
+      # tries to use the data.
+      
+      # split the colours into Pfam-A and Pfam-B colours
+      foreach ( keys %$colours ) {
+        if ( m/^(PF\d{5})$/ ) {
+          $pfama_colours->{$1} = $colours->{$1};
+        }
+        elsif ( m/^(PB\d{6})$/ ) {
+          $pfamb_colours->{$1} = $colours->{$1};
+        }
+      }
+  
+      $c->log->debug( 'Search::Domain::build_layout: pfama_colours: ' 
+                      . dump( $pfama_colours ) ) if $c->debug;
+      $c->log->debug( 'Search::Domain::build_layout: pfamb_colours: ' 
+                      . dump( $pfamb_colours ) ) if $c->debug;
+      
+      # and pre-assign the colours to the respective configurators
+      if ( $pfama_colours ) {
+        $pfama->assignedColours( $pfama_colours );
+        $pfama->colourIndex( scalar( keys %{ $pfama->assignedColours } ) + 1 );
+      }
+
+      if ( $pfamb_colours ) {
+        $pfamb->assignedColours( $pfamb_colours );
+      }
+    }
+  }
+  
+  # let the layout manager build the domain graphics definition from the
+  # sequence objects
+  $lm->layoutSequences( $c->stash->{seqs} );
+
+  $c->log->debug( 'Search::Domain::build_layout: pfama->assignedColours: ' 
+                  . dump( $pfama->assignedColours ) ) if $c->debug;
+  $c->log->debug( 'Search::Domain::build_layout: pfamb->assignedColours: ' 
+                  . dump( $pfamb->assignedColours ) ) if $c->debug;
+
+  # configure the JSON object to correctly stringify the layout manager output
+  my $json = new JSON;
+  # $json->pretty(1);
+  $json->allow_blessed;
+  $json->convert_blessed;
+
+  # encode and stash the sequences as a JSON string
+  $c->stash->{layout} = $json->encode( $c->stash->{seqs} );
+
+  # stash the assigned colours from the layout manager. First we need to merge
+  # the sets of colours from the two configurators
+  if ( defined $pfama and defined $pfamb ) {
+
+    my $valid_colours;
+    my $pfama_colours = $pfama->assignedColours;
+    foreach ( keys %$pfama_colours ) {
+      next unless $_;
+      $valid_colours->{$_} = $pfama_colours->{$_};
+    }
+    my $pfamb_colours = $pfamb->assignedColours;
+    foreach ( keys %$pfamb_colours ) {
+      next unless $_;
+      $valid_colours->{$_} = $pfamb_colours->{$_};
     }
 
-    $imageset->create_images( $layout->layout_to_XMLDOM );
-  
-    $c->stash->{images} = $imageset;
-    $c->stash->{seqInfo}  = \%seqInfo;
+    $c->stash->{assignedColours} = $json->encode( $valid_colours );
+    
+    $c->log->debug( 'Search::Domain::build_layout: assigned colours: ' 
+                    . dump( $c->stash->{assignedColours} ) )
+      if $c->debug;
   }
-
+  
 }
 
 #-------------------------------------------------------------------------------
