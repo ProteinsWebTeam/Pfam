266918db7dd332c8fb9c98f7618c9f037aaee283
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-10-28 11:39:25 +0000

    Rewrote the coercion for colours.

--- trunk/PfamLib/Bio/Pfam/Sequence/Feature.pm
+++ trunk/PfamLib/Bio/Pfam/Sequence/Feature.pm
@@ -4,6 +4,8 @@ use JSON;
 use Moose;
 use Moose::Util::TypeConstraints;
 
+use Data::Dump qw( dump );
+
 subtype 'ColourStr' => as 'Str' => where { $_ =~ /^\#[0-9A-F]{6}$/i } =>
   message { "$_ does not look like a hex number\n" };
 
@@ -40,16 +42,18 @@ coerce 'jsFeatureBoolean'
 coerce 'ColourStr' 
   => from 'Object' 
     => via {
+      # print STDERR "***************** coercing from an Object\n";
       unless ( $_->isa("Convert::Color") ) {
         die "Did not get a Convert::Color object\n";
       }
       return '#' . $_->as_rgb8->hex;
-  } 
+    } 
   => from 'Str' 
     => via {
+      # print STDERR "***************** coercing from a Str\n";
       my $str = $_;
       my $colour;
-      foreach my $tag (qw(hsv rgb rgb8 x11)) {
+      foreach my $tag (qw(hsv rgb8 rgb x11)) {
         $str =~ s/\#//g;
         eval { $colour = Convert::Color->new("$tag:$str"); };
         if ($@) {
@@ -58,42 +62,74 @@ coerce 'ColourStr'
           last;
         }
       }
+    
+      unless ( $colour and $colour->isa("Convert::Color") ) {
+        die "Could not convert colour as string";
+      }
+      return '#' . $colour->as_rgb8->hex;
+    } 
+  => from 'ArrayRef'
+    => via {
+      my $str = join( ",", @$_ );
+      $str =~ s/\#//g;
 
-  unless ( $colour and $colour->isa("Convert::Color") ) {
-    die "Could not convert colour as string";
-  }
-  return '#' . $colour->as_rgb8->hex;
-} => from 'ArrayRef' => via {
-  my $str = join( ",", @$_ );
-  my $colour;
-  foreach my $tag (qw(hsv rgb rgb8 x11)) {
-    $str =~ s/\#//g;
-    eval { $colour = Convert::Color->new("$tag:$str"); };
-    if ($@) {
-      next;
-    }
-    else {
-      last;
-    }
-  }
-  if ( $colour and $colour->isa("Convert::Color") ) {
-    return '#' . $colour->as_rgb8->hex;
-  }
+      my $colour;
+      # *Note* that the order of these tags is important...
+      foreach my $tag ( qw( rgb8 hsv rgb x11 ) ) {
+        eval {
+          $colour = Convert::Color->new("$tag:$str");
+        };
+        if ($@) {
+          next;
+        }
+        else {
+          last;
+        }
+      }
+      if ( $colour and $colour->isa("Convert::Color") ) {
+        return '#' . $colour->as_rgb8->hex;
+      }
+    
+      # Okay, may be a pfam-b. Start again and check to see if we have an array
+      # of colours
+      my @colours;
+      foreach my $c (@$_) {
 
-  #Okay, may be  a pfam-b, so we want/expect and array of Convert::colour
-  my @colours;
-  foreach my $c (@$_) {
-    next unless $c->isa("Convert::Color");
-    push( @colours, '#' . $c->as_rgb8->hex );
-  }
+        if ( ref $c and
+             eval { $c->isa('Convert::Color') } ) {
+          push @colours, '#' . $c->as_rgb8->hex;
+        }
+        else {
 
-  unless ( @colours and scalar(@colours) == 3 ) {
-    die "Could not convert colour array";
-  }
+          my $str = join( ",", @$c );
+          $str =~ s/\#//g;
 
-  return ( \@colours );
+          my $colour;
+          foreach my $tag ( qw( hsv rgb rgb8 x11 ) ) {
+            eval {
+              $colour = Convert::Color->new("$tag:$str");
+            };
+            if ($@) {
+              next;
+            }
+            else {
+              last;
+            }
+          }
 
-};
+          next unless ( ref $colour and
+                        eval { $colour->isa('Convert::Color') } );
+
+          push @colours, '#' . $colour->as_rgb8->hex ;
+        }
+      }
+    
+      unless ( @colours and scalar(@colours) == 3 ) {
+        die "Could not convert colour array";
+      }
+    
+      return \@colours;
+    };
 
 has 'start' => (
   isa      => 'jsInt',
