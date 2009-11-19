79a1b0bf0ed6396abedea48d7da63f0604a540ca
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2009-11-19 13:15:50 +0000

    new ipfamDB class

--- PfamSchemata/iPfamDB.pm
+++ PfamSchemata/iPfamDB.pm
@@ -8,14 +8,15 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04003 @ 2008-02-26 14:01:41
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tvQchb9Wa4uKfRSOzNNpCA
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-11-16 12:00:37
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7aoyTxxCt5LjyHlPcutj5w
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 
-
 =head1 AUTHOR
+
+Prasad Gunasekaran, C<pg6@sanger.ac.uk>
 
 John Tate, C<jt6@sanger.ac.uk>
 
