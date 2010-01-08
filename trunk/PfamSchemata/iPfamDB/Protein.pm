e48345d1b320bb9da42e9659b6847b92c313a426
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-01-08 13:36:29 +0000

    new table protein_annseq added

--- trunk/PfamSchemata/iPfamDB/Protein.pm
+++ trunk/PfamSchemata/iPfamDB/Protein.pm
@@ -62,45 +62,20 @@ __PACKAGE__->belongs_to(
   { "internal_chain_accession" => "accession" },
 );
 __PACKAGE__->has_many(
+  "protein_annseqs",
+  "iPfamDB::ProteinAnnseq",
+  { "foreign.accession" => "self.accession" },
+);
+__PACKAGE__->has_many(
   "protein_int_atoms",
   "iPfamDB::ProteinIntAtoms",
   { "foreign.protein_acc" => "self.accession" },
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-11-16 12:00:37
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:YZe5XZGG6XW0XqtCEYiJGA
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2010-01-08 13:31:47
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:/6FpTg5X20qLw5GetANjoA
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
-
-=head1 AUTHOR
-
-Prasad Gunasekaran, C<pg6@sanger.ac.uk>
-
-John Tate, C<jt6@sanger.ac.uk>
-
-Rob Finn, C<rdf@sanger.ac.uk>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2007: Genome Research Ltd.
-
-Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
-
-This is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-
-=cut
-
 1;
