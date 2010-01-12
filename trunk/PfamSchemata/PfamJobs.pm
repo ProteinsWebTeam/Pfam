e25ca4f9c98b84a5ad9e157b1a332403daf882e2
jm14 <jm14@98333f76-0502-11df-81af-936bbf3a1e26>
2010-01-12 16:30:09 +0000

    Merging rdf repository with public cvs repository

--- trunk/PfamSchemata/PfamJobs.pm
+++ trunk/PfamSchemata/PfamJobs.pm
@@ -1,66 +1,16 @@
-
-# PfamJobs.pm
-# jt 20060912 WTSI
-#
-# $Id: PfamJobs.pm,v 1.2 2008-05-16 15:23:16 jt6 Exp $
-#
-# $Author: jt6 $
-
-=head1 NAME
-
-PfamJobs - DBIC schema definition class for the web_user database
-
-=cut
-
 package PfamJobs;
-
-=head1 DESCRIPTION
-
-The base class for the whole web_user database model. Config comes from the
-catalyst application class.
-
-$Id: PfamJobs.pm,v 1.2 2008-05-16 15:23:16 jt6 Exp $
-
-=cut
 
 use strict;
 use warnings;
 
-use base "DBIx::Class::Schema";
+use base 'DBIx::Class::Schema';
 
-#-------------------------------------------------------------------------------
+__PACKAGE__->load_classes;
 
-__PACKAGE__->load_classes( qw/JobHistory
-                              JobStream
-                              / );
 
-#-------------------------------------------------------------------------------
+# Created by DBIx::Class::Schema::Loader v0.04003 @ 2009-07-22 11:06:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:AG+HJU91GxMt/0IFuFNExw
 
-=head1 AUTHOR
 
-John Tate, C<jt6@sanger.ac.uk>
-
-Rob Finn, C<rdf@sanger.ac.uk>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2007: Genome Research Ltd.
-
-Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
-
-This is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-
-=cut
-
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
--- trunk/PfamSchemata/PfamLive.pm
+++ trunk/PfamSchemata/PfamLive.pm
@@ -1,69 +1,16 @@
-
-# PfamLive.pm
-# jt6 20061109 WTSI
-#
-# $Id: PfamLive.pm,v 1.5 2008-05-16 15:23:16 jt6 Exp $
-#
-# $Author: jt6 $
-
-=head1 NAME
-
-PfamLive - DBIC schema definition class for the pfamlive database
-
-=cut
-
 package PfamLive;
-
-=head1 DESCRIPTION
-
-The base class for the pfamlive database model. Config comes from the catalyst 
-application class.
-
-$Id: PfamLive.pm,v 1.5 2008-05-16 15:23:16 jt6 Exp $
-
-=cut
 
 use strict;
 use warnings;
 
-use base "DBIx::Class::Schema";
+use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_classes();
+__PACKAGE__->load_classes;
 
-#-------------------------------------------------------------------------------
 
-$SIG{INT} = sub {
-   print STDERR "Caught Ctrl-C, trying to cleanly disconnect\n";
-  __PACKAGE__->storage->disconnect;
-};
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-01-17 10:09:48
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jOgHFIe8Z57akm6m0HdQyg
 
-#-------------------------------------------------------------------------------
 
-=head1 AUTHOR
-
-John Tate, C<jt6@sanger.ac.uk>
-
-Rob Finn, C<rdf@sanger.ac.uk>
-
-=head1 COPYRIGHT
-
-Copyright (c) 2007: Genome Research Ltd.
-
-Authors: Rob Finn (rdf@sanger.ac.uk), John Tate (jt6@sanger.ac.uk)
-
-This is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-
-=cut
-
+# You can replace this text with custom content, and it will be preserved on regeneration
 1;
