88ec089034cb904b56a73da8422e547755e61085
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2010-01-19 15:24:20 +0000

    Moved content in trunk up.

--- PfamScripts/trunk/make/pfbuild_farm_post_search.pl
+++
@@ -1,9 +0,0 @@
-#!/software/bin/perl
-
-use strict;
-use warnings;
-use Bio::Pfam::HMM::HMMResultsIO;
-
-my $HMMResultsIO = Bio::Pfam::HMM::HMMResultsIO->new;  
-$HMMResultsIO->convertHMMSearch( "OUTPUT" );
-
