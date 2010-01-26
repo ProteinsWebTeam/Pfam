ec8f9be5cc672b794b75916dddc3e99a41f73080
jm14 <jm14@98333f76-0502-11df-81af-936bbf3a1e26>
2010-01-26 16:54:00 +0000

    Added documentation at top of script

--- PfamScripts/release/calculate_coverage.pl
+++ PfamScripts/release/calculate_coverage.pl
@@ -3,14 +3,18 @@
 use strict;
 use Getopt::Long;
 
-
-#length_file is in format auto, length,
-#regions_file is in format auto_pfamA, start, end, and is sorted by auto_pfamA
+#Script for calculating amino acid coverage for a sequence database
+#Before running this script you need to create two files:
+#length_file.  This file contains the lengths of all the sequences in the database. 
+#The format of the length file is auto, length (e.g. select auto_metaseq, length from metaseq)
+#regions_file.  This file contains all of the pfamA regions found in on sequences in the database.  
+#The format of the regions file is auto_pfamA, start, end (e.g. select auto_pfamA, seq_start, seq_end from meta_pfamA_reg)
+#The regions file MUST be sorted by auto_pfamA (e.g. sort -n <regions_file>)
 
 
 my ($regions_file, $length_file);
 GetOptions( 'regions_file=s' => \$regions_file,
-	    'length_file=s' => \$length_file);
+	    'length_file=s'  => \$length_file);
 
 unless($regions_file and -s $regions_file) {
     die "Need to specify a regions file on the command line\n";
