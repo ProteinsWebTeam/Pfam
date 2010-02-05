bae3691c1e4b878caf0f58a1e584e6d3ca7cddef
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-05 17:06:33 +0000

    Updating table for Rfam 10.0 release

--- PfamSchemata/RfamDB/RfamDatabaseLinks.pm
+++ PfamSchemata/RfamDB/RfamDatabaseLinks.pm
@@ -32,8 +32,8 @@ __PACKAGE__->add_columns(
 __PACKAGE__->belongs_to("auto_rfam", "RfamDB::Rfam", { auto_rfam => "auto_rfam" });
 
 
-# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-01-14 13:54:48
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ILvMU2qGLRjhPGkN38EfUA
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2010-01-12 10:09:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:3T/ZcIQPkobYU/TXhwQpKw
 
 #-------------------------------------------------------------------------------
 
