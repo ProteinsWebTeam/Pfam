6520bbff284233bcc274ac56c8c53b27523a92cf
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-05 17:02:05 +0000

    Updating table for Rfam 10.0 release

--- trunk/PfamSchemata/RfamDB/DeadFamilies.pm
+++ trunk/PfamSchemata/RfamDB/DeadFamilies.pm
@@ -20,11 +20,11 @@ __PACKAGE__->add_columns(
   "forward_to",
   { data_type => "VARCHAR", default_value => undef, is_nullable => 1, size => 7 },
 );
-__PACKAGE__->set_primary_key("rfam_acc");
+__PACKAGE__->add_unique_constraint("rfam_acc", ["rfam_acc"]);
 
 
-# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-01-14 13:54:48
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:AlZm+D0LeATJ0iBYj+8EBA
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2010-01-12 10:09:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:WFS5mI7BQGu1IHkWhR3LtA
 
 #-------------------------------------------------------------------------------
 
