d78620c1f761825729d518c1935d1d51fc59a624
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-05 17:01:46 +0000

    Updating table for Rfam 10.0 release

--- trunk/PfamSchemata/RfamDB/ChromosomeBuild.pm
+++ trunk/PfamSchemata/RfamDB/ChromosomeBuild.pm
@@ -36,8 +36,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-01-14 13:54:47
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:Z1yAhaY5j2tLyUirZrn/3Q
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2010-01-12 10:09:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:SgbmQ+Fwza+Q4SyKodCHBQ
 
 #-------------------------------------------------------------------------------
 
