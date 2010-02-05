aac82cf6a34e2c8767e0f54cf6139b1109c1e5be
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-05 17:11:11 +0000

    Updating table for Rfam 10.0 release

--- PfamSchemata/RfamDB/RfamRegFull.pm
+++ PfamSchemata/RfamDB/RfamRegFull.pm
@@ -9,7 +9,7 @@ __PACKAGE__->load_components("Core");
 __PACKAGE__->table("rfam_reg_full");
 __PACKAGE__->add_columns(
   "auto_rfam",
-  { data_type => "INT", default_value => 0, is_nullable => 0, size => 10 },
+  { data_type => "INT", default_value => undef, is_nullable => 1, size => 10 },
   "auto_rfamseq",
   { data_type => "INT", default_value => 0, is_nullable => 0, size => 10 },
   "auto_genome",
@@ -25,23 +25,32 @@ __PACKAGE__->add_columns(
     is_nullable => 0,
     size => 64,
   },
+  "evalue_score",
+  { data_type => "VARCHAR", default_value => 0, is_nullable => 0, size => 15 },
   "type",
   { data_type => "ENUM", default_value => "full", is_nullable => 1, size => 4 },
   "genome_start",
   { data_type => "BIGINT", default_value => undef, is_nullable => 1, size => 20 },
   "genome_end",
   { data_type => "BIGINT", default_value => undef, is_nullable => 1, size => 20 },
+  "sequence",
+  {
+    data_type => "LONGTEXT",
+    default_value => undef,
+    is_nullable => 1,
+    size => 4294967295,
+  },
 );
+__PACKAGE__->belongs_to("auto_rfam", "RfamDB::Rfam", { auto_rfam => "auto_rfam" });
 __PACKAGE__->belongs_to(
   "auto_rfamseq",
   "RfamDB::Rfamseq",
   { auto_rfamseq => "auto_rfamseq" },
 );
-__PACKAGE__->belongs_to("auto_rfam", "RfamDB::Rfam", { auto_rfam => "auto_rfam" });
 
 
-# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-01-14 13:54:48
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:xjSG4CF5GMpyqpVjjCGmtw
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2010-01-12 10:09:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:/LRN+zLeh8TsWZCZqGLpHQ
 
 __PACKAGE__->belongs_to("auto_genome", "RfamDB::GenomeEntry", { auto_genome => "auto_genome" });
 
