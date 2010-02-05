472563a4dfe4724161e93e5b111294da5236b45a
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-05 17:08:43 +0000

    Updating table for Rfam 10.0 release

--- PfamSchemata/RfamDB/RfamLiteratureReferences.pm
+++ PfamSchemata/RfamDB/RfamLiteratureReferences.pm
@@ -30,8 +30,8 @@ __PACKAGE__->belongs_to(
 __PACKAGE__->belongs_to("auto_rfam", "RfamDB::Rfam", { auto_rfam => "auto_rfam" });
 
 
-# Created by DBIx::Class::Schema::Loader v0.04004 @ 2009-01-14 13:54:48
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:f0LP7GsRZA95pRTfDSrGKQ
+# Created by DBIx::Class::Schema::Loader v0.04004 @ 2010-01-12 10:09:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:4l4mhiXdCNo/U29F1vEw0g
 
 #-------------------------------------------------------------------------------
 
