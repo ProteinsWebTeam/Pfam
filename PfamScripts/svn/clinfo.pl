5f7ec8665428966e5c146eb9ce6ea79bef416495
jm14 <jm14@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-10 11:01:36 +0000

    Allow clan id as input

--- PfamScripts/svn/clinfo.pl
+++ PfamScripts/svn/clinfo.pl
@@ -6,6 +6,7 @@ use warnings;
 use Bio::Pfam::Config;
 use Bio::Pfam::SVN::Client;
 use Bio::Pfam::ClanIO;
+use Bio::Pfam::PfamLiveDBManager;
 
 my $config = Bio::Pfam::Config->new;
 
@@ -16,7 +17,21 @@ unless ( $ARGV[0] ) {
 my $clan = shift;
 
 if ( $clan !~ /^(CL\d{4})$/ ) {
-  warn "Looks like you have passed in an id rather than accession, [$clan]\n";
+    if($config->location eq 'WTSI'){
+	my $connect = $config->pfamlive;
+	my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $connect });
+	my $clanAcc = $pfamDB->clanId2Acc($clan);
+	unless($clanAcc =~ /^CL\d{4}/){
+	    warn "You passed in something that did not look like a clan accession.\n"; 
+	    warn "Because you are at WTSI, tried to map it to a clan accession, but failed.\n";
+	    help();
+	}
+	$clan = $clanAcc;  
+    }
+    else {
+	warn "Looks like you have passed in a clan id rather than a clan accession, [$clan]\n";
+        help();
+    }
 }
 
 #Check that family exists in svn
