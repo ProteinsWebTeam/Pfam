86102a7f615f4de3643d794adbf79c627425412e
jd7 <jd7@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-11 14:31:10 +0000

    added a bit of documentation

--- trunk/Rfam/CVSimport/genomes/parse_ann_con.pl
+++ trunk/Rfam/CVSimport/genomes/parse_ann_con.pl
@@ -1,12 +1,26 @@
 #!/software/bin/perl -w
 
+=head1 NAME
+
+parse_ann_con.pl
+
+=head1 DESCRIPTION
+
+V. simple bit of code to parse the EMBL con and ann files and reduce
+them down to minimal dataset. Greatly increases speed of later code.
+
+=head1 PARAMETERS
+
+parse_ann_con.pl <con file>
+
+=head1 AUTHOR jd7@sanger.ac.uk
+
+=cut
+
 use strict;
 
 
 my $infile=shift;
-
-# get the list of confiles 
-#my @confiles = glob( "$con_ann/rel_*dat" );
 
 my $outfile=$infile.".mini";
 
