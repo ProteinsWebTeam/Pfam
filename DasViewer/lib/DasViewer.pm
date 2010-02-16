b88e96265bbdd388782674e5b418c69446db8ade
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-16 13:27:12 +0000

    das viewer initial import

---
+++ DasViewer/lib/DasViewer.pm
@@ -0,0 +1,65 @@
+package DasViewer;
+
+use strict;
+use warnings;
+
+use Catalyst::Runtime '5.70';
+
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+#   ConfigLoader: will load the configuration from a Config::General file in the
+#                 application's home directory
+# Static::Simple: will serve static files from the application's root 
+#                 directory
+
+use parent qw/Catalyst/;
+use Catalyst qw/-Debug
+                Cache
+                ConfigLoader
+                Static::Simple/;
+our $VERSION = '0.01';
+
+# Configure the application. 
+#
+# Note that settings in dasviewer.conf (or other external
+# configuration file that you set up manually) take precedence
+# over this when using ConfigLoader. Thus configuration
+# details given here can function as a default configuration,
+# with a external configuration file acting as an override for
+# local deployment.
+
+__PACKAGE__->config( name => 'DasViewer' );
+
+# Start the application
+__PACKAGE__->setup();
+
+
+=head1 NAME
+
+DasViewer - Catalyst based application
+
+=head1 SYNOPSIS
+
+    script/dasviewer_server.pl
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 SEE ALSO
+
+L<DasViewer::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+Prasad Gunasekaran,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
