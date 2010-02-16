043688cbdb12cff1295987e3c6325a099549ea27
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-16 10:11:22 +0000

    Added relationship to Pfama so that we can perform a join to both sides.

--- trunk/PfamSchemata/PfamLive/Pfama2pfamaScoopResults.pm
+++ trunk/PfamSchemata/PfamLive/Pfama2pfamaScoopResults.pm
@@ -20,6 +20,17 @@ __PACKAGE__->add_columns(
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-01-17 10:09:48
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:6dB1NrW1CIVL+INW5m1itw
 
+__PACKAGE__->has_one( 
+  'pfamA1',
+  'PfamLive::Pfama',
+  { 'foreign.auto_pfama' => 'self.auto_pfama1' } 
+);
+
+__PACKAGE__->has_one( 
+  'pfamA2',
+  'PfamLive::Pfama',
+  { 'foreign.auto_pfama' => 'self.auto_pfama2' } 
+);
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
