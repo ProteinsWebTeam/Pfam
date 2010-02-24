50e4889dfeda46bb0b871f0571628f231974d410
jd7 <jd7@98333f76-0502-11df-81af-936bbf3a1e26>
2010-02-24 14:40:36 +0000

    remove the spell check as we no longer display the CC lines

--- Rfam/CVSimport/rfamqc/rqc-all.pl
+++ Rfam/CVSimport/rfamqc/rqc-all.pl
@@ -5,8 +5,6 @@
 use strict;
 
 foreach my $family (@ARGV){
-    print STDERR "\n**SPELL CHECK**\n\n";
-    system ("rqc-spell.pl $family");
     print STDERR "\n**OVERLAP CHECK** - ignoring $family\n\n";
     system ("rqc-overlap-rdb.pl $family -i $family");
     print STDERR "\n**FORMAT CHECK**\n\n";
