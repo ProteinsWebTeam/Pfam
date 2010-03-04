9b5a17cdb1c8f5f49e75e626b0741209af69f992
jd7 <jd7@98333f76-0502-11df-81af-936bbf3a1e26>
2010-03-04 13:01:47 +0000

    changed to catch errors from each individual scripts and generate a qc.passed file if the errors are ok. If the family has too many serious errors there will be no qc.passed file generated and the rfci.pl and rfnew.pl code will not allow you to check the family in

--- Rfam/CVSimport/rfamqc/rqc-all.pl
+++ Rfam/CVSimport/rfamqc/rqc-all.pl
@@ -1,21 +1,92 @@
 #! /software/bin/perl -w
 
-# A program to run all checks
+# A program to run all checks on a family prior to checkin
 
 use strict;
+use Getopt::Long;
+use File::Copy;
+use Cwd;
 
-foreach my $family (@ARGV){
-    print STDERR "\n**OVERLAP CHECK** - ignoring $family\n\n";
-    system ("rqc-overlap-rdb.pl $family -i $family");
-    print STDERR "\n**FORMAT CHECK**\n\n";
-    system ("rqc-format.pl $family");
-    print STDERR "\n**STRUCTURE CHECK**\n\n";
-    system ("rqc-ss-cons.pl $family");
-    print STDERR "\n**MISSING CHECK**\n\n";
-    system ("rqc-check.pl $family");
-    print STDERR "\n**SEQUENCE CHECK**\n\n";
-    system ("rqc-seqs.pl $family");
+my $pwd=cwd;
+
+my ($full, $family);
+&GetOptions(
+	    '-full'=> \$full,
+	    '-fam=s'=> \$family
+	    );
+
+$family=shift;
+
+#rqc-passed file
+my $out="$pwd/$family/qc.passed";
+
+if (-e "$out"){
+    print STDERR "qc.passed file already exists-removing it\n";
+    copy($out, $out.'old') ;
+    unlink("$out");
 }
+
+if ($full){
+    print STDERR "Doing a qc-check on families not using the RDB\n";
+}
+
+my ($error,$format);
+my $errlog="$pwd/$family/format.stderr.$$";
+my $outlog="$pwd/$family/format.stdout.$$";
+
+
+print STDERR "\n(1) FORMAT CHECK\n";
+system ("echo '\n**FORMAT ERRS**\n' > $errlog");
+$format=system("~/scripts/rfamqc/rqc-format.pl  $family 1>> $outlog 2>> $errlog");
+if ($format){ $error=1; print STDERR "\t--errors" } else{ print STDERR "\t--format check completed with no major errors";}
+
+print STDERR "\n(2) OVERLAP CHECK - ignoring $family\n";
+system ("echo '\n**OVERLAP ERRS**\n' >> $errlog");
+my $overlap=system ("~/scripts/rfamqc/rqc-overlap-rdb.pl $family -i $family 1>> $outlog 2>> $errlog");
+if ($overlap){ $error=1; print STDERR "\t--errors"} else{ print STDERR "\t--overlap check completed with no major errors";}
+
+print STDERR "\n(3) STRUCTURE CHECK\n";
+system ("echo '\n**SSCONS ERRS**\n' >> $errlog");
+my $sscons=system ("~/scripts/rfamqc/rqc-ss-cons.pl $family  1>> $outlog 2>> $errlog");
+if ($sscons) { $error=1; print STDERR "\t--errors"} else{ print STDERR "\t--sscons check completed with no major errors";}
+
+print STDERR "\n(4) MISSING CHECK\n";
+system ("echo '\n**MISSING ERRS**\n' >> $errlog");
+my $missing=system ("~/scripts/rfamqc/rqc-check.pl $family  1>> $outlog 2>> $errlog");
+if ($missing) { $error=1; print STDERR "\t--errors"} else{ print STDERR "\t--missings check completed";}
+
+print STDERR "\n(45) SEQUENCE CHECK\n";
+system ("echo '\n**SEQUENCE ERRS**\n' >> $errlog");
+my $seqs=system ("~/scripts/rfamqc/rqc-seqs.pl $family  1>> $outlog 2>> $errlog");
+if ($seqs) { $error=1; print STDERR "\t--errors"} else{ print STDERR "\t--sequence check completed with no major errors\n";}
+
+
+
+if ($error){
+    print STDERR "\n\n===Report summaries for each check below===\n";
+    open (ERR, "<$errlog") || die "Cant open the error logs\n";
+    while(<ERR>){
+	print STDERR $_;
+    }
+    print STDERR "\n\n****Family failed rqc-all checks:YOU CANNOT CHECK IT IN****\n";
+    close (ERR);
+}else{
+    open(OUT, ">$pwd/$family/qc.passed") || die "Cant open $pwd/$family/qc.passed $!";
+    print OUT "\nFamily passed with no serious errors\n\n";
+    print STDERR "\n\n===Report summaries for each check below===\n";
+    open (ERR, "<$errlog") || die "Cant open the error logs\n";
+    while(<ERR>){
+	print OUT $_;
+	print STDERR $_;
+    }
+    print STDERR "\n\n****Family passed with no serious errors****\n";
+    close (ERR);
+   close(OUT);
+    
+
+}
+
+
 
 
 
