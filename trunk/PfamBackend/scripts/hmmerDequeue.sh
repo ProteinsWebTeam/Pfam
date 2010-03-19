08a4acab264fba55edb575e2fdfc7694fcc0f11d
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2010-03-19 15:44:55 +0000

    Latest versions of shell scripts. Added a generic bin dir and --signal 15 to commands.

--- trunk/PfamBackend/scripts/hmmerDequeue.sh
+++ trunk/PfamBackend/scripts/hmmerDequeue.sh
@@ -19,14 +19,31 @@
 
 #You will need to configure this for your system.
 #General
-export PATH="/sbin:/data/bin:/home/pfamweb/PfamBackend/scripts:$PATH"
-export PERL5LIB="/home/pfamweb/PfamLib:/home/pfamweb/PfamSchemata:/home/pfamweb/BPModules:$PERL5LIB"
 
-#Stuff required for dequeuer config.
-export PFAMOFFLINE_CONFIG=/home/pfamweb/PfamBackend/conf/pfam_backend.conf
+export DEBUG=0
+export PFAMROOT=/software/pfam
+export PFAMWEBROOT=$PFAMROOT/pfamweb
+export WISECONFIGDIR=$PFAMWEBROOT/src/wise2.2.0/wisecfg 
+export PFAMOFFLINE_CONFIG=$PFAMWEBROOT/PfamBackend/conf/pfam_backend.conf
+export PATH=$PFAMWEBROOT/PfamBackend/scripts:$PFAMWEBROOT/bin:$PFAMWEBROOT/src/hmmer-3.0b3/bin:$PFAMWEBROOT/src/wise2.2.0/src/bin:/sbin:$PATH
+
+# Rfam config
+export RFAMROOT=/software/rfam
+export RFAMWEBROOT=$RFAMROOT/rfamweb 
+export PATH=$RFAMWEBROOT/src/infernal-0.72/src:$RFAMWEBROOT/src/wublast:$PATH
+
+
+HOST=`hostname`
+if [ $HOST = "pfamweb-03" -o $HOST = "pfamweb-04" ]; then
+ . /usr/local/lsf/conf/profile.lsf
+fi
+
+export PERL5LIB=$PFAMWEBROOT/PfamLib:$PFAMWEBROOT/PfamSchemata:$PFAMWEBROOT/bioperl_1.4:$PFAMROOT/perl/lib/5.8.8:$PFAMROOT/perl/lib/perl/5.8.4:$PFAMROOT/perl/lib/site_perl/5.8.8:$PFAMROOT/perl/lib/site_perl/5.8.8/x86_64-linux-thread-multi:$PFAMROOT/perl/share/perl/5.8:$PFAMROOT/perl/share/perl/5.8.8:$PFAMROOT/perl/share/perl/5.8.4
+
 
 #Config for this shell script
-DAEMON= <PATH-TO-THIS-SCRIPT>/hmmerDequeue.pl
+DAEMON0=/software/pfam/pfamweb/PfamBackend/scripts/hmmerDequeue0.pl
+DAEMON1=/software/pfam/pfamweb/PfamBackend/scripts/hmmerDequeue1.pl
 NAME=hmmerDequeue
 DESC="Hmmer queue daemon"
 
@@ -38,25 +55,43 @@ set -e
 case "$1" in
   start)
  echo -n "Starting $DESC: "
- start-stop-daemon --start --quiet --pidfile /var/lock/$NAME.pid \
-  --exec /usr/bin/perl --startas $DAEMON
- echo "$NAME."
+ start-stop-daemon --start --quiet --pidfile /var/lock/${NAME}0.pid \
+  -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON0
+ echo "$NAME0."
+
+ start-stop-daemon --start --quiet --pidfile /var/lock/${NAME}1.pid \
+  -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON1
+ echo "$NAME1."
  ;;
   stop)
  echo -n "Stopping $DESC: "
  # --quiet
- start-stop-daemon --stop --signal 15 --pidfile /var/lock/$NAME.pid \
-  --exec /usr/bin/perl --startas $DAEMON
- echo "$NAME."
+ start-stop-daemon --quiet --stop --signal 15 --pidfile /var/lock/${NAME}0.pid \
+  -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON0
+ echo "${NAME}0."
+
+ start-stop-daemon --quiet --stop --signal 15 --pidfile /var/lock/${NAME}1.pid \
+  -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON1
+ echo "${NAME}1."
+
  ;;
   restart|force-reload)
  echo -n "Restarting $DESC: "
- start-stop-daemon --stop --quiet --pidfile \
-  /var/lock/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
+ start-stop-daemon --stop --singal 15 --quiet  --pidfile \
+  /var/lock/${NAME}0.pid -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON0
+ echo "${NAME}0 stopped."
+
+ start-stop-daemon --stop --signal 15 --quiet  --pidfile \
+  /var/lock/${NAME}1.pid -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON1
+ echo "${NAME}1 stopped."
+
  sleep 1
- start-stop-daemon --start --quiet --pidfile \
-  /var/lock/$NAME.pid --exec /usr/bin/perl --startas $DAEMON
- echo "$NAME."
+ start-stop-daemon --start --quiet  --pidfile /var/lock/${NAME}0.pid \
+ -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON0
+ echo "$NAME0."
+ start-stop-daemon --start --quiet  --pidfile /var/lock/${NAME}1.pid \
+  -c pfamweb --exec /usr/local/bin/perl --startas $DAEMON1
+ echo "$NAME1."
  ;;
   *)
  N=/etc/init.d/$NAME
