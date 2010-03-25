39d808c016297221c01d8bd2adcf8083b78082f1
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2010-03-25 15:14:57 +0000

    Added footer and header.

--- trunk/PfamScripts/svn/clco.pl
+++ trunk/PfamScripts/svn/clco.pl
@@ -1,4 +1,12 @@
 #!/usr/local/bin/perl
+#
+# clco.pl <clan_acc> - check out a clan
+#
+# Author:        rdf
+# Maintainer:    $Author$
+# Version:       $Revision$
+# Created:       Feb 18, 2010
+# Last Modified: $Date$
 
 use strict;
 use warnings;
@@ -9,6 +17,7 @@ use Bio::Pfam::PfamLiveDBManager;
 
 my $config = Bio::Pfam::Config->new;
 
+help() unless($ARGV[0]); 
 
 my $clan = shift;
 
@@ -45,7 +54,7 @@ my $client = Bio::Pfam::SVN::Client->new;
 my $pwd = getcwd();
 my $dest = $pwd; 
 if (-d $dest."/".$clan ){
-  print "The destination directory $dest already exist, remove before checking out a clan\n";
+  print "The destination directory ".$dest."/".$clan." already exist, remove before checking out a clan\n";
   exit(1); 
 }
 
@@ -80,3 +89,41 @@ EOF
 exit;
   
 }
+
+=head1 NAME
+
+ clco.pl - a short description of the script
+
+=cut
+
+=head1 DESCRIPTION
+
+A more detailed description of what this class does and how it does it.
+
+$Author$
+
+=head1 COPYRIGHT
+
+File: clco.pl
+
+Copyright (c) 2007: Genome Research Ltd.
+
+Authors: Rob Finn (rdf@sanger.ac.uk)
+
+ This is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ 
+=cut
+
