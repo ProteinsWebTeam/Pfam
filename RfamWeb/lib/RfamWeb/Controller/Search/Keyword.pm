c8cc2f1d9ab9c234ef5788db28b41f3f83c52ac8
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-03-25 10:20:11 +0000

    Re-factored: common functions moved into PfamBase

--- RfamWeb/lib/RfamWeb/Controller/Search/Keyword.pm
+++ RfamWeb/lib/RfamWeb/Controller/Search/Keyword.pm
@@ -26,310 +26,104 @@ use strict;
 use warnings;
 
 use base 'RfamWeb::Controller::Search';
+use base 'PfamBase::Controller::Search::Keyword';
 
 #-------------------------------------------------------------------------------
 
 =head1 METHODS
 
-=head2 textSearch : Path
+=head2 lookup_term : Private
 
-Forwards immediately to the L<runSearches> action to run the text searches.
-
-=cut
-
-sub textSearch : Path {
-  my( $this, $c ) = @_;
-
-  # if there's no query parameter, we're done here; drop straight to the 
-  # template that will render the search forms
-  return unless $c->req->param('query');
-
-  # get the query
-  my( $terms ) = $c->req->param('query') =~ m/^([\w:\.\-\s]+)/;
-
-  # we're done here unless there's a query specified
-  $c->log->warn( 'Search::Keyword::textSearch: no query terms supplied' ) and return
-    unless defined $terms;
-
-  $c->log->debug( 'Search::Keyword::textSearch: running query with: |' 
-                  . $terms . '|' ) if $c->debug;
-
-  # stash the de-tainted terms so we can safely display them later
-  $c->stash->{rawQueryTerms} = $terms;
-
-  # somewhere for the results of this search
-  $c->stash->{results} = {};
-
-  # hand off to the method which will run the queries
-  $c->forward( 'runSearches', [ 'textSearches' ] );
-}
-
-#-------------------------------------------------------------------------------
-#- private methods -------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-=head2 runSearches : Private
-
-This method is handed the name of a set of searches as its first
-argument. It retrieves the list of search plugins for that search set
-and runs each one in turn. For each plugin it calls the following methods:
-
-=over
-
-=item o C<Plugin::formatTerms>
-
-Intended to convert the simple user input string from the URL into a
-suitable query term for the DB. There's a basic version of this method
-on this class but plugins can override that with their own version if
-they need to do some other processing.
-
-=item o C<Plugin::process>
-
-Executes the database query and returns the results as a L<DBIC
-ResultSet|DBIx::Class::ResultSet>.
-
-=item o C<Search::mergeResults>
-
-Walks the L<ResultSet|DBIx::Class::ResultSet> and adds the results to
-the results of the whole query.
-
-=back
-
-Plugins can run as many queries as necessary, returning the results as
-an array of references to DBIC L<ResultSets|DBIx::Class::ResultSet>.
-The results from multiple C<ResultSets> are merged in the order that
-they were received, so that results from later queries will override
-those from earlier ones.
+Does a quick look-up to see if the search term matches a Pfam ID
+or accession.
 
 =cut
 
-sub runSearches : Private {
-  my( $this, $c, $searchSet ) = @_;
+sub lookup_term : Private {
+  my ( $this, $c ) = @_;
 
-  $c->log->debug( 'Search::Keyword::runSearches: running a search' )
-    if $c->debug;
+   my $rs = $c->model('RfamDB::Rfam')
+              ->search( [ { rfam_acc => $c->stash->{rawQueryTerms} },
+                          { rfam_id  => $c->stash->{rawQueryTerms} } ] );
 
-  # TODO this is a bit sloppy...
-  $c->error( 'You did not supply any valid search terms' )
-    unless $c->stash->{rawQueryTerms};
-
-  my( @plugins, @pluginsReversed, $pluginName, $pluginDesc );
-
-  # get the list of text search plugins from the configuration
-  foreach my $pluginName ( @{ $this->{searchSets}->{$searchSet} } ) {
-
-    unless ( $pluginDesc = $this->{plugins}->{$pluginName} ) {
-      $c->log->debug( "Search::Keyword::runSearches: no description for \"$pluginName\"" )
-        if $c->debug;
-      next;
-    }
-
-    $c->log->debug( 'Search::Keyword::runSearches: adding plugin ' .
-                    "|$searchSet|$pluginName|" ) if $c->debug;
-
-    # keep track of the order of the configured plugins. Store the
-    # list forwards and backwards, since we'll use it both ways
-    push    @plugins,         $pluginName;
-    unshift @pluginsReversed, $pluginName;
-
-    # and drop them (and their descriptions) into a hash too, for easy look-up
-    $c->stash->{pluginsHash}->{$pluginName} = $pluginDesc;
-  }
-
-  # store the (reversed) list of query names
-  $c->stash->{pluginsArray} =         \@plugins;
-  $c->stash->{pluginsArrayReversed} = \@pluginsReversed;
-
-  #----------------------------------------
-
-  # keep track of the number of hits for each query
-  $c->stash->{pluginHits} = {};
-
-  # walk the plugins and run each query in turn. The list of plugins comes
-  # from Module::Pluggable, via our parent class, Search. The plugin object
-  # stringifies to the fully qualified class name, e.g. Search::Plugin::Rfam
-  foreach my $plugin ( $this->plugins ) {
-    my $pluginName = ( split m/\:\:/, $plugin )[-1];
-
-    # check that the plugin is switched on in the config
-    unless ( $c->stash->{pluginsHash}->{$pluginName} ) {
-      $c->log->debug( "Search::Keyword::runSearches: plugin \"$pluginName\" is not enabled" )
-        if $c->debug;
-      next;
-    }
-
-    # check that the plugin is properly formed
-    unless ( $plugin->can( 'process' ) and
-             $plugin->can( 'formatTerms' ) ) {
-      $c->log->debug( "Search::Keyword::runSearches: plugin \"$pluginName\" is not a valid search plugin" )
-        if $c->debug;
-      next;
-    }
-
-    # firkle with the user input if necessary and build a string that
-    # we can pass straight to the DB
-    $c->forward( $plugin, 'formatTerms' );
-
-    # and run the query
-    $c->log->debug( "Search::Keyword::runSearches: running query for plugin $pluginName" )
-      if $c->debug;
-    my $results = $c->forward( $plugin );
-
-    # merge results from the individual query
-    $c->forward( 'mergeResults', [ $pluginName, $results ] );
-  }
-
-  $c->log->debug( 'Search::Keyword::runSearches: found a total of ' .
-          scalar( keys %{$c->stash->{results}} ) . ' rows' ) if $c->debug;
-
-  #----------------------------------------
-
-  # if there are no results, redirect to the error page
-  my $numHits = scalar keys %{$c->stash->{results}};
-  if( $numHits < 1 ) {
-    $c->stash->{template} = 'pages/search/keyword/error.tt';
-    return 0;
-  }
-
-  #----------------------------------------
-
-  # if there's only one result, redirect straight to it
-  if( $numHits == 1 ) {
-    my( $acc ) = keys %{$c->stash->{results}};
-    $c->log->debug( "Search::Keyword::runSearches: found a single hit: |$acc|; redirecting" )
-      if $c->debug;
-    $c->res->redirect( $c->uri_for( '/family', { acc => $acc } ) );
-    return 1;
-  }
-
-  #----------------------------------------
-
-  # sort the results according to the score and rfam accession
-  my @results;
-  my $results = $c->stash->{results};
-  foreach my $acc ( sort { $results->{$b}->{score} <=> $results->{$a}->{score} ||
-                                                $a cmp $b }
-                    keys %{$c->stash->{results}} ) {
-    push @results, $c->stash->{results}->{$acc};
-  }
-
-  $c->stash->{results} = \@results;
-
-  #----------------------------------------
-
-  # do a quick look-up to see if the search term matches a Rfam ID
-  # or accession
-
-  # first, check if there are multiple "words" in the query term,
-  # because if there are, this can't be a unique ID or accession
-  unless( $c->stash->{rawQueryTerms} =~ /![A-Za-z0-9_-]/ ) {
-
-    my $rs = $c->model('RfamDB::Rfam')
-               ->search( [ { rfam_acc => $c->stash->{rawQueryTerms} },
-                           { rfam_id  => $c->stash->{rawQueryTerms} } ] );
-  
-    # we're going to assume that there's only one hit here... we're in
-    # trouble if there's more than one, certainly
-    my $hit = $rs->next;
-    $c->stash->{lookupHit} = $hit if $hit;
-
-  }
-
-  # set the page template and we're done
-  $c->stash->{template} = 'pages/search/keyword/results.tt';
-
+  # we're going to assume that there's only one hit here... we're in
+  # trouble if there's more than one, certainly
+  my $hit = $rs->next;
+  $c->stash->{lookupHit} = $hit if $hit;
 }
 
 #-------------------------------------------------------------------------------
 
-=head2 mergeResults : Private
+=head2 merge
 
-Merges the results of an individual query into the set of results for
-the whole search. The Rfam accession is used as the hash key, so it
-needs to be present in the results of the plugin queries. Also keeps
-track of the number of hits for each plugin query.
+Merges results from plugins. Merging requires that each row of the C<ResultSet>
+has access to an Rfam accession. We'll try to get it using
+
+  $rs->rfam_acc
+
+or
+
+  $rs->auto_rfam->rfam_acc
+
+but if neither method works, the row is skipped.
 
 =cut
 
-sub mergeResults : Private {
-  my( $this, $c, $pluginName, $results ) = @_;
+sub merge : Private {
+  my ( $this, $c, $pluginName, $rs ) = @_;
 
-  if( ref $results eq 'ARRAY' ) {
-    foreach my $rs ( @$results ) {
-      $c->forward( '_mergeResults', [ $pluginName, $rs ] );
+  ROW: while ( my $row = $rs->next ) {
+
+    my ( $acc, $hit );
+
+    TRY: {
+
+      # first try accessing the accession on the table row directly
+      eval {
+        $acc = $row->rfam_acc;
+      };
+      if ( $@ ) {
+        $c->log->debug( 'Search::Keyword::merge: caught an exception when trying '
+                       . " \$row->rfam_acc for plugin |$pluginName|: $@" )
+          if $c->debug;
+      }
+
+      if ( defined $acc ) {
+        $hit = $c->stash->{results}->{$acc} ||= {};
+        $hit->{dbObj} = $row;
+        last TRY;
+      }
+
+      # we couldn't find the accession on the row itself, so try walking down one
+      # possible relationship, "auto_rfam", and see if that gets us to another
+      # table, which hopefully does store the details of the Rfam family
+      #
+      # This is all a bit convoluted, but it means that we shouldn't need to add
+      # proxy columns indiscriminately throughout the model, just so that text
+      # searching can work
+      eval {
+        $acc = $row->auto_rfam->rfam_acc;
+      };
+      if ( $@ ) {
+        $c->log->debug( 'Search::Keyword::merge: caught an exception when trying '
+                       . " \$row->auto_rfam->rfam_acc for plugin |$pluginName|: $@" )
+          if $c->debug;
+      }
+
+      if ( defined $acc ) {
+        $hit = $c->stash->{results}->{$acc} ||= {};
+        $hit->{dbObj} = $row->auto_rfam;
+        # last TRY;
+      }
+
     }
-  } else {
-    $c->forward( '_mergeResults', [ $pluginName, $results ] );
-  }
-}
 
-#-------------------------------------------------------------------------------
-#- methods ---------------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-=head2 _mergeResults
-
-Merges results from plugins. If the plugin returns an array of
-ResultSets, walks down it and merges in each one in turn.
-
-=cut
-
-sub _mergeResults : Private {
-  my( $this, $c, $pluginName, $rs ) = @_;
-
-  # walk the query results and merge them into the overall results
-  my( $acc, $row );
-  while( my $dbObj = $rs->next ) {
-    $acc = $dbObj->rfam_acc;
-
-    $row = $c->stash->{results}->{$acc} ||= {};
-
-    $row->{dbObj} = $dbObj;
-    $row->{query}->{$pluginName} = 1;
-
+    $hit->{query}->{$pluginName} = 1;
     $c->stash->{pluginHits}->{$pluginName} += 1;
 
     # score this hit
-    $this->_computeScore( $c, $row );
+    $this->_computeScore( $c, $hit );
   }
-}
-
-#-------------------------------------------------------------------------------
-
-=head1 NON-ACTION METHODS
-
-=head2 _computeScore
-
-Calculates a simple score for each hit, based on which plugin
-generated the hit.
-
-The score is calculated according to the order of the plugins from
-the config, treating the first one in the list - probably the Rfam
-table search - as the most significant, e.g.
-
- if the order of the plugins is Pfam -> Pdb -> Seq_info -> GO,
- the score is 8 + 4 + 2 + 1 = 15
-
- if the Seq_info plugin doesn't generate a hit, the score is 8 + 4 +
- 0 + 1 = 13, and that hit will sort lower in the results than the
- one above.
-
-Since it's called once per hit, this method is implemented as a
-regular perl function, rather than a Catalyst action, so it's called
-directly instead of using $c->forward.
-
-=cut
-
-sub _computeScore {
-  my( $this, $c, $row ) = @_;
-
-  my $score = 0;
-  my $factor = 1;
-  foreach my $pluginName ( @{ $c->stash->{pluginsArrayReversed} } ) {
-    $score  += $factor if $row->{query}->{$pluginName};
-    $factor *= 2;
-  }
-  $row->{score} = $score;
 }
 
 #-------------------------------------------------------------------------------
