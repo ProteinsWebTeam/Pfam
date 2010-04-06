5c42adb4a80728e06a209618bab0acd823b8f8f5
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-04-06 15:11:42 +0000

    "retrieve_ids" was specific to Pfam. Moved this action into app-specific controller.

--- PfamBase/lib/PfamBase/Controller/Utils.pm
+++ PfamBase/lib/PfamBase/Controller/Utils.pm
@@ -37,44 +37,6 @@ use base 'Catalyst::Controller';
 #- private actions -------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
-=head2 retrieve_ids : Private
-
-Retrieves a list of IDs/accessions from the DB, given a "job ID" for the list.
-This action doesn't attempt to validate the job ID, it just assumes it will
-work. The ID should be checked before calling this action.
-
-=cut
-
-sub retrieve_ids : Private {
-  my( $this, $c, $job_id ) = @_;
-  
-  # retrieve the IDs/accessions for that job ID
-  my $rs = $c->model('WebUser::Species_collection')
-             ->find( $job_id );
-  unless( defined $rs ) {
-    $c->log->debug( 'Utils::retrieve_ids: no accessions for that job ID' )
-      if $c->debug;
-    $c->stash->{errorMsg} = 'No sequence accessions found for that job ID';
-    return;
-  }
-
-  $c->log->debug( 'Utils::retrieve_ids: accessions_list: |'
-                   . $rs->id_list . '|' ) if $c->debug;
-
-  # get the individual accessions
-  my @seqAccs;
-  foreach ( split /\s+/, $rs->id_list ) {
-    next unless  m/^([A-Z]\d[A-Z0-9]{3}\d)$/i;
-    push @seqAccs, $1;
-  }
-  $c->log->debug( 'Utils::retrieve_ids: found |' . scalar @seqAccs
-                  . '| valid sequence accessions' ) if $c->debug;
-
-  return \@seqAccs;
-}
-
-#-------------------------------------------------------------------------------
-
 =head2 get_sequences : Private
 
 This action accepts a "job ID" that identifies a list of sequence accessions
