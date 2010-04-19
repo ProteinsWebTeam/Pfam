06df86ef8b28c72b9e4c1f1861ce9adef870dedc
pg5 <pg5@98333f76-0502-11df-81af-936bbf3a1e26>
2010-04-19 10:48:32 +0000

    Updated to Rfam 10.0.

--- Rfam/CVSimport/wiki/updateWiki.sh
+++ Rfam/CVSimport/wiki/updateWiki.sh
@@ -11,10 +11,9 @@ export http_proxy=http://wwwcache.sanger.ac.uk:3128/
 
 /software/rfam/bin/scrape_cronjob.pl -pages all -update -changes 1 > $log  2> $errorlog
 
+mail -s "wiki cronjob" pg5 agb < $log
 
-mail -s "wiki cronjob" jd7 pg5 agb < $log
-
-/software/rfam/bin/mysqldump -h pfamdb2a -u pfam -pmafp1 -P 3301 rfam_9_1 wikitext     | /software/rfam/bin/mysql -h pfamdb1 -u pfamwebadmin -pmafpwa rfam_9_1
+/software/rfam/bin/mysqldump -h pfamdb2a -u pfam -pmafp1 -P 3301 rfam_10_0 wikitext     | /software/rfam/bin/mysql -h pfamdb1 -u pfamwebadmin -pmafpwa rfam_10_0
 
 cp -f $log /lustre/scratch103/sanger/pg5/wiki_cronjob/Rfamwatchlist
 cp -f $errorlog /lustre/scratch103/sanger/pg5/wiki_cronjob/errors
