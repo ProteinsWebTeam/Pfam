555d8997fb39e9daf1a169e099c758e92558e5a8
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2010-04-30 12:44:39 +0000

    Remove some white space.

--- trunk/PfamLib/Bio/Pfam/Family/PfamA.pm
+++ trunk/PfamLib/Bio/Pfam/Family/PfamA.pm
@@ -62,35 +62,35 @@ use Bio::Pfam::AlignPfam;
 subtype 'AlignPfamA'
   => as Object
   => where { $_->isa('Bio::Pfam::AlignPfam') }
-  => message { "\n\n *** Couldn't find/create a valid Bio::Pfam::AlignPfam object ***\n".
+  => message { "\n\n*** Couldn't find/create a valid Bio::Pfam::AlignPfam object ***\n".
                "There is likely to be something wrong with your ALIGN file.\n".
                "$@\n" };
 
 subtype 'HMMPfamA'
   => as Object
   => where { $_->isa('Bio::Pfam::HMM::HMM') }
-  => message { "\n\n *** Couldn't create a valid Bio::Pfam::HMM::HMM object ***\n".
+  => message { "\n\n*** Couldn't create a valid Bio::Pfam::HMM::HMM object ***\n".
                "There is likely to be something wrong with your HMM file.\n".
                "$@\n" };
 
 subtype 'PfamoutPfamA'
   => as Object
   => where { $_->isa('Bio::Pfam::HMM::HMMResults') }
-  => message { "\n\n *** Couldn't find/create a HMMResults object from your PFAMOUT file ***\n".
+  => message { "\n\n*** Couldn't find/create a HMMResults object from your PFAMOUT file ***\n".
                "There is likely to be something wrong with your PFAMOUT file.\n".
                "$@\n" };
 
 subtype 'scoresPfamA'
   => as Object
   => where { $_->isa('Bio::Pfam::Family::Scores') }
-  => message { "\n\n *** Couldn't create a Bio::Pfam::Family::Scores object ***\n".
+  => message { "\n\n*** Couldn't create a Bio::Pfam::Family::Scores object ***\n".
                "There is likely to be something wrong with your scores file.\n".
                "$@\n" };
 
 subtype 'DESCPfamA'
   => as Object
   => where { $_->isa('Bio::Pfam::Family::DESC') }
-  => message { "\n\n *** Couldn't create a Bio::Pfam::Family::DESC object ***\n".
+  => message { "\n\n*** Couldn't create a Bio::Pfam::Family::DESC object ***\n".
                "There is likely to be something wrong with your DESC file.\n".
                "$@\n" };
   
