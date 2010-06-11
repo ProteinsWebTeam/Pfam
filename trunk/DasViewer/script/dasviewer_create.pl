373fac7de3f0d8e367dacd57d152ecd7414365d3
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-06-11 13:02:18 +0000

    scripts generated using catalyst 5.8

--- trunk/DasViewer/script/dasviewer_create.pl
+++ trunk/DasViewer/script/dasviewer_create.pl
@@ -1,26 +1,10 @@
-#!/software/perl-5.8.8/bin/perl -w
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use Catalyst::Helper;
 
-my $force = 0;
-my $mech  = 0;
-my $help  = 0;
-
-GetOptions(
-    'nonew|force'    => \$force,
-    'mech|mechanize' => \$mech,
-    'help|?'         => \$help
- );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-my $helper = Catalyst::Helper->new( { '.newfiles' => !$force, mech => $mech } );
-
-pod2usage(1) unless $helper->mk_component( 'DasViewer', @ARGV );
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('DasViewer', 'Create');
 
 1;
 
@@ -33,13 +17,12 @@ dasviewer_create.pl - Create a new Catalyst Component
 dasviewer_create.pl [options] model|view|controller name [helper] [options]
 
  Options:
-   -force        don't create a .new file where a file to be created exists
-   -mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
-   -help         display this help and exits
+   --force        don't create a .new file where a file to be created exists
+   --mechanize    use Test::WWW::Mechanize::Catalyst for tests if available
+   --help         display this help and exits
 
  Examples:
    dasviewer_create.pl controller My::Controller
-   dasviewer_create.pl controller My::Controller BindLex
    dasviewer_create.pl -mechanize controller My::Controller
    dasviewer_create.pl view My::View
    dasviewer_create.pl view MyView TT
@@ -68,7 +51,7 @@ Catalyst Contributors, see Catalyst.pm
 
 =head1 COPYRIGHT
 
-This library is free software, you can redistribute it and/or modify
+This library is free software. You can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
