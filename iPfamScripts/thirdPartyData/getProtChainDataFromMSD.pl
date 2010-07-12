dd2b7a39ca3bb3d84dec89c10bd1be80c1fab299
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-07-12 10:55:18 +0000

    first set of changes, might not work

--- iPfamScripts/thirdPartyData/getProtChainDataFromMSD.pl
+++ iPfamScripts/thirdPartyData/getProtChainDataFromMSD.pl
@@ -5,103 +5,127 @@
 
 use strict;
 use warnings;
-use Bio::Pfam;
 use Data::Dumper;
+use Log::Log4perl qw(:easy);
+use Net::SCP;
 use DBI;
 
+use Bio::iPfam::Config;
+
+#-------------------------------------------------------------------------------
+#Initail set up....
+#Start up the logger
+Log::Log4perl->easy_init($DEBUG);
+my $logger = get_logger();
+
 $ENV {"ORACLE_HOME"} = "/software/oracle";
-my $output_dir = "/tmp/";
-#Okay - This is the set up for connecting to the msd database. This should go into a module!!!!
 
+my $status_dir = shift;
+
+unless($statusdir and -d $statusdir){
+  $logger->logdie("You need to pass a statusdir in:[$!]");
+}
+
+#-------------------------------------------------------------------------------
+
+$logger->info( 'writing files to the directory'.$config->localDbsLoc );
+
+#Open up the files
+my $output_dir = $config->localDbsLoc."/msd";
+
+#unless(-e "$statusdir/fetched_pdb_data"){
+open(MSD, ">$output_dir/msd_data.dat") or
+   $logger->logdie("Could not open $output_dir/entryResidueData.dat:[$!]");
+open(MSD2, ">$output_dir/entryData.dat") or
+  $logger->logdie("Could not open $output_dir/entryData.dat:[$!]");
+open(MSD3, ">$output_dir/entryAuthor.dat") or
+  $logger->logdie("Could not open $output_dir/entryAuthor.dat:[$!]");
+  
+#-------------------------------------------------------------------------------
+#Okay - This is the set up for connecting to the msd database. This should go into the config!!!!
+
+$logger->info("Connecting to the PDBe database");
 my $host = "ocs16";
-my $port = "1527";
+my $port = "1530";
 my $db = "msd";
-my $password = "search";
-my $user = "search";
+my $password = "pdbe_ro";
+my $user = "pdbe_ro";
 
-my $dbh = DBI->connect("dbi:Oracle:host=$host;sid=$db;port=$port", $user, $password) or  die "Couldn't connect to database: ".DBI->errstr;
-# We should now have a connection string!
+my $dbh = DBI->connect("dbi:Oracle:host=$host;sid=$db;port=$port", $user, $password)
+ or  $logger->logdie("Couldn't connect to database: ".DBI->errstr);
 
-
-#my $ipfamDbh = DBI->conect("dbi:mysql:host");
-
-###################################
-# Get the PDB entry data from MSD #
-###################################
+#-------------------------------------------------------------------------------
 
 my @entryIds; # We will store all of the entry ids in this array so that we can get the residue mapping entry by entry. 
 
-
-
-#The run the system command.....
-
-#system("mysql -h pfam -u pfam -pmafp1 -e \'load data infile \"$output_dir/msd_data.dat\" into table msd_data\'  $rdb") and die "Error uploading msd_data file to $rdb :[$!]\n";
-
-# select e.accession_code,  e.entry_id, e.header, e.title,  e.creation_date, e.res_val, e.short_experiment_type, e.experiment_type,  pl.pubmedid
-# from
-# (select t.accession_code, t.entry_id, t.pubmedid, t.ordinal
-# from whouse1.pubmedlist t where nvl(t.ordinal,0) = 0 )pl,
-# entry e
-# where
-# pl.accession_code  (+) = e.accession_code
-
+$logger->info("Gettting pdb table information");
 
 #Now get the data for the pdb table
 my $sthEntryData = $dbh->prepare("
-SELECT e.accession_code, 
-       e.entry_id, 
-       e.header, 
-       e.title,  
-       e.creation_date, 
-       e.res_val, 
-       e.short_experiment_type, 
-       e.experiment_type
-FROM entry e");
+select
+e.ID,
+e.KEYWORDS, 
+e.TITLE, 
+e.PDB_REV_DATE_ORIGINAL, 
+e.RESOLUTION, 
+e.METHOD, 
+pl.DATABASE_ID_PUBMED as PUBMEDID
+FROM 
+(
+SELECT  p.database_id_pubmed, p.entry_id, p.id
+from pdbe_ro.citation p where nvl(p.id,0) =0 )pl,
+pdbe_ro.entry e
+where
+e.id = pl.entry_id (+)");
 
-$sthEntryData->execute;
+$sthEntryData->execute or $logger->logdie("Failed to execute entryDataSth:".$dbh->errstr);
 
-my $autoPdb =1;
-my %autoPdb;
-open(MSD2, ">$output_dir/entryData.dat") || die "Could not open entryData.dat:[$!]";
 while (my $hash = $sthEntryData->fetchrow_hashref){
-    print MSD2 $$hash{ENTRY_ID}."\t";
-    print MSD2 $$hash{ACCESSION_CODE}."\t";
-    chomp($$hash{HEADER});
-    $$hash{TITLE} =~ s/\n//g; #erros with unitialised
-    print MSD2 $$hash{HEADER}."\t";
-    print MSD2 $$hash{TITLE}."\t";
-    if($$hash{CREATION_DATE} =~ /(\d{2}\-\S{3}\-\d{2})/){
+    print MSD2 $$hash{ID}."\t";
+    print MSD2 ($$hash{KEYWORDS} ? $$hash{KEYWORDS} : '\N')."\t";
+
+    if($$hash{TITLE}){
+      $$hash{TITLE} =~ s/\n//g; #erros with unitialised
+      print MSD2 $$hash{TITLE};
+    }
+    print MSD2 "\t";
+    if($$hash{PDB_REV_DATE_ORIGINAL} =~ /(\d{2}\-\w{3}\-\d{2})/){
 	     print MSD2 "$1\t";
     }else{
-	     print MSD2 "\t";
+	     print MSD2 '\N'."\t";
     }
-    if($$hash{RES_VAL}){
-      print MSD2 $$hash{RES_VAL}."\t"; #Put in sprintf
+    if($$hash{RESOLUTION}){
+      print MSD2 $$hash{RESOLUTION}."\t"; #Put in sprintf
     }else{
-      print MSD2 "\t"; 
+      print MSD2 '\N'."\t"; 
     }
-    print MSD2 $$hash{SHORT_EXPERIMENT_TYPE}."\t";
-    print MSD2 $$hash{EXPERIMENT_TYPE}."\t\t\n";
-    #print MSD2 $$hash{PUBMEDID}."\t";
-    push(@entryIds, $$hash{ENTRY_ID});
+    print MSD2 $$hash{METHOD}."\t";
+    if($$hash{PUBMEDID}){
+      print MSD2 $$hash{PUBMEDID}."\n";
+    }else{
+      print MSD2 '\N'."\n";
+    }
+    push(@entryIds, $$hash{ID});
 }
 $sthEntryData->finish;
 close(MSD2);
 
+=out
+#-------------------------------------------------------------------------------
 
 ###########################
 # Get the PDB author data #
 ###########################
 
+$logger->info("Getting the PDB author data");
 #Now get the data for the pdb_author table
-my $sthAuthor = $dbh->prepare("SELECT entry_id, ordinal, last_name, name_initials FROM author");
+my $sthAuthor = $dbh->prepare("SELECT ENTRY_ID, ORDINAL, NAME FROM PDBE_RO.AUDIT_AUTHOR");
 $sthAuthor->execute;
-open(MSD3, ">$output_dir/entryAuthor.dat") || die "Could not open entryAuthor.dat:[$!]";
+
 while (my $hash = $sthAuthor->fetchrow_hashref){
 	print MSD3 $$hash{ENTRY_ID}."\t";
-	print MSD3 $$hash{ORDINAL}."\t";
-	print MSD3 $$hash{LAST_NAME}."\t";
-	print MSD3 $$hash{NAME_INITIALS}."\n";
+	print MSD3 ($$hash{ORDINAL} ? $$hash{ORDINAL} : '')."\t";
+	print MSD3 ($$hash{NAME} ? $$hash{NAME}: '')."\n";
 }
 $sthAuthor->finish;
 close(MSD3);
@@ -110,63 +134,61 @@ close(MSD3);
 # Finally get the residue by residue mapping and the DSSP code #
 ################################################################
 
-open(MSD, ">$output_dir/msd_data.dat") || die "Could not open msd_data.dat:[$!]";
-my $sthDssp = $dbh->prepare("SELECT residue_id, dssp_symbol from residue_data where entry_id = ?");
-#Now get the swissprot mapping
+#Get the residue by residue mapping
+$logger->info("Going to get the residue by residue mapping between PDB and UniProt");
+
+
+#Now get the uniprot mapping
 my $sthMapping =$dbh->prepare(
 "SELECT 
 S.ENTRY_ID,
-S.RESIDUE_ID,
-S.CHAIN_CODE, 
-S.CHAIN_PDB_CODE, 
-S.RESIDUE_SERIAL, 
-S.RESIDUE_PDB_CODE, 
-S.RESIDUE_PDB_SEQ, 
-S.SP_PRIMARY_ID, 
-S.SP_1_LETTER_CODE, 
-S.SP_SERIAL,
-S.ACCESSION_CODE
+S.STRUCT_ASYM_ID,
+S.AUTH_ASYM_ID, 
+S.RESIDUE_ID, 
+S.CHEM_COMP_ID, 
+S.AUTH_SEQ_ID,
+S.PDB_INS_CODE,
+S.ACCESSION,
+U.SEQ_VERSION, 
+S.ONE_LETTER_CODE, 
+S.UNP_SERIAL,
+S.DSSP_SYMBOL
 FROM 
-SWISS_PROT_MAPPING S
-WHERE S.ENTRY_ID = ?
+PDBE_RO.XREF_RESIDUE S,
+PDBE_RO.UNP_ENTITY U
+WHERE S.ENTRY_ID=U.ENTRY_ID and S.ACCESSION=U.ACCESSION AND S.ENTRY_ID = ?
 order by 
 S.ENTRY_ID, 
-S.CHAIN_CODE, 
-S.RESIDUE_SERIAL ASC");
+S.STRUCT_ASYM_ID, 
+S.RESIDUE_ID ASC"
+);
 
-
+my ($c, $total);
 foreach my $entryId (@entryIds){
-
-  #First get the residue dssp code;
-  $sthDssp->execute($entryId);
-  my $dsspRes = $sthDssp->fetchall_arrayref;
-  my %dssp = map{$$_[0] => $$_[1]}@{$dsspRes};
-
+  $c++;
+  if($c == 1000){
+    $total += $c;
+    $c = 0;
+    $logger->info("Getting the $total entry out of ".scalar(@entryIds)." entries");
+  }
   $sthMapping->execute($entryId);
-  while (my $hash = $sthMapping->fetchrow_hashref){
-    if($$hash{ACCESSION_CODE}  && defined($$hash{RESIDUE_PDB_SEQ})){#Something is unitialised here
-      #
-      print MSD "$$hash{ENTRY_ID}\t";
-      print MSD "$$hash{ACCESSION_CODE}\t";
-      
-
-      if($$hash{CHAIN_PDB_CODE}){
-      	print MSD "$$hash{CHAIN_PDB_CODE}\t";
-      }
-      print MSD "$$hash{SP_PRIMARY_ID}\t";
-      
-      print MSD "$$hash{RESIDUE_PDB_SEQ}\t";
-      print MSD "$$hash{RESIDUE_PDB_CODE}\t";
-      if($dssp{$$hash{RESIDUE_ID}}){
-      	print MSD "$dssp{$$hash{RESIDUE_ID}}\t";
-      }else{
-	      print MSD "\t";
-      }
-      print MSD "$$hash{SP_SERIAL}\t";
-      print MSD "$$hash{SP_1_LETTER_CODE}\n";
-      
+  my $map = $sthMapping->fetchall_arrayref;
+  foreach my $row (@{$map}){
+    #Make sure the sequence is in pfamseq
+    next unless($acc2auto{$row->[7]});
+    #Add the auto pfamseq reference to the accession.  
+    $row->[7] .= "\t".$acc2auto{$row->[7]};
+    my $rowString;
+    foreach my $e (@$row){
+      $rowString .= (defined($e) ? $e : '\N')."\t";    
     }
+    print MSD $rowString."\n";  
   }
 }
 $sthMapping->finish;
 close(MSD);
+system("touch $statusdir/fetched_pdb_data") and 
+  $logger->logdie("Could not touch $statusdir/fetched_pdb_data");
+#}else{
+#  $logger->info("Already fetched the pdb data\n");
+#}
