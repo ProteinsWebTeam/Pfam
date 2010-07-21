b16140bca7e9573be5703b3e7e6d82032a70a14b
pg6 <pg6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-07-21 15:32:43 +0000

    new schema definition for iPfam release

--- PfamSchemata/iPfamDB.pm
+++ PfamSchemata/iPfamDB.pm
@@ -8,8 +8,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2010-07-21 15:16:33
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:F2BYjwERe2xD01Pm6AAi1Q
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2010-07-21 16:29:00
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:AmeRZicJPhSgZs+EIqZEeQ
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
