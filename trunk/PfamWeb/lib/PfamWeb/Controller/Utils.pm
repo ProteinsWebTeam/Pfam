c245ed1eddcb1a72e4ecc12ad3053f6b58d31a96
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2010-09-20 12:09:28 +0000

    Move methods here from PfamBase

--- trunk/PfamWeb/lib/PfamWeb/Controller/Utils.pm
+++ trunk/PfamWeb/lib/PfamWeb/Controller/Utils.pm
@@ -23,7 +23,108 @@ $Id: Utils.pm,v 1.5 2008-05-16 15:29:28 jt6 Exp $
 use strict;
 use warnings;
 
-use base 'PfamBase::Controller::Utils';
+use Text::Wrap;
+
+# use base 'PfamBase::Controller::Utils';
+use base 'Catalyst::Controller';
+
+#-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+=cut
+
+#-------------------------------------------------------------------------------
+#- private actions -------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+=head2 retrieve_ids : Private
+
+Retrieves a list of IDs/accessions from the DB, given a "job ID" for the list.
+This action doesn't attempt to validate the job ID, it just assumes it will
+work. The ID should be checked before calling this action.
+
+=cut
+
+sub retrieve_ids : Private {
+  my( $this, $c, $job_id ) = @_;
+  
+  # retrieve the IDs/accessions for that job ID
+  my $rs = $c->model('WebUser::Species_collection')
+             ->find( $job_id );
+  unless( defined $rs ) {
+    $c->log->debug( 'Utils::retrieve_ids: no accessions for that job ID' )
+      if $c->debug;
+    $c->stash->{errorMsg} = 'No sequence accessions found for that job ID';
+    return;
+  }
+
+  $c->log->debug( 'Utils::retrieve_ids: accessions_list: |'
+                   . $rs->id_list . '|' ) if $c->debug;
+
+  # get the individual accessions
+  my @seqAccs;
+  foreach ( split /\s+/, $rs->id_list ) {
+    next unless  m/^([A-Z]\d[A-Z0-9]{3}\d)$/i;
+    push @seqAccs, $1;
+  }
+  $c->log->debug( 'Utils::retrieve_ids: found |' . scalar @seqAccs
+                  . '| valid sequence accessions' ) if $c->debug;
+
+  return \@seqAccs;
+}
+
+#-------------------------------------------------------------------------------
+
+=head2 get_sequences : Private
+
+This action accepts a "job ID" that identifies a list of sequence accessions
+and returns those sequences as a fasta-format file.
+
+=cut
+
+sub get_sequences : Private {
+  my( $this, $c, $job_id, $pfam ) = @_;
+  
+  # retrieve the accessions for that job ID
+  my $accession_list = $c->forward( '/utils/retrieve_ids', [ $job_id ] );
+  unless( $accession_list ) {
+    $c->stash->{errorMsg} ||= 'Could not retrieve sequences for that job ID';
+    return;
+  }
+  
+  $c->log->debug( 'Utils::get_sequences: found |' 
+                  . scalar @$accession_list . '| valid sequence accessions' )
+    if $c->debug;
+  $c->stash->{selectedSeqAccs} = $accession_list;
+
+  # get each of the sequences in turn and turn them into FASTA format
+  # (note that we take the auto-number for the family from "$c->stash->{entry}"
+  # because the begin method on SpeciesTree can handle Pfam-As, Pfam-Bs and 
+  # clans, so "entry" is more appropriate than "pfam", which we usually use.) 
+  my $fasta = '';
+  foreach my $seqAcc ( @$accession_list ) {
+    my @rows = $c->model('PfamDB::PfamaRegFullSignificant')
+                 ->search( { 'pfamseq.pfamseq_acc' => $seqAcc,
+                             auto_pfama            => $pfam->auto_pfama,
+                             in_full               => 1 },
+                           { prefetch              => [ qw( pfamseq ) ] } );
+    $c->stash->{numRows} = scalar @rows;
+
+    # need to remember the final character in each row, the "\n"
+    $Text::Wrap::columns = 61;
+
+    foreach my $r (@rows){
+      $fasta .= '>'.$r->pfamseq_acc.'/'.$r->seq_start.'-'.$r->seq_end."\n";
+      $fasta .= wrap( '', '', 
+                      substr( $r->sequence,
+                              $r->seq_start - 1,
+                              $r->seq_end - $r->seq_start + 1 )."\n" );
+    }
+  }
+
+  return $fasta;
+}
 
 #-------------------------------------------------------------------------------
 
