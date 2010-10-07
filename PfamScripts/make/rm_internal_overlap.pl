a413dc604b797ab4162514a285a7b7ef59a09da1
agb <agb@98333f76-0502-11df-81af-936bbf3a1e26>
2010-10-07 16:10:15 +0000

    Fixed several bugs, hopefully did not make any more

--- PfamScripts/make/rm_internal_overlap.pl
+++ PfamScripts/make/rm_internal_overlap.pl
@@ -17,24 +17,25 @@ EOF
 exit 0;
 }
 
-system ("belvu -S a -o mul $align > sorted.align");
+system ("belvu -S a -o mul $align > $$.align");
 
-open (FH, "sorted.align")or die "cannot open sorted.align";
-my ($id, $from, $to, $old_id, $old_from, $old_to);
+open (FH, "$$.align")or die "cannot open sorted $$.align file";
+my ($id, $from, $to, $old_from, $old_to);
+my $old_id="";
 while(<FH>){
     my $line=$_;
+
+    next if (/\/\//);
     if (/^(\S+)\/(\d+)-(\d+)/){
-	$old_id=$id;
-	$old_from=$from;
-	$old_to=$to;
+	if ($id){
+	    $old_id=$id;
+	    $old_from=$from;
+	    $old_to=$to;
+	}
 
 	$id=$1;
 	$from=$2;
 	$to=$3;
-
-	if (! $old_id){ # Always print out first line
-	    print $line;
-	}
 
 	if ($old_id eq $id){ # Domain in same sequence. Check for overlap
 	    if ($old_to>=$from){
@@ -44,8 +45,7 @@ while(<FH>){
 	    } else {
 		print $line;
 	    }
-
-	} else { # Different IDs so cannot overlap.
+	} else { # Different IDs so cannot overlap. Also catches first line!
 		print $line;
 	}
     }
