96e39d7ab25056c82673935eb0cfe540a7c26ade
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2011-01-07 14:22:02 +0000

    Set up a many-to-many relationship with Pfama (via PfamaWiki)

--- PfamSchemata/PfamLive/Wikipedia.pm
+++ PfamSchemata/PfamLive/Wikipedia.pm
@@ -38,5 +38,8 @@ __PACKAGE__->has_many(
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:ZwxUU71nu+CBkf344DLdeQ
 
 
-# You can replace this text with custom content, and it will be preserved on regeneration
+__PACKAGE__->many_to_many(
+  'pfams' => 'pfama_wikis', 'auto_pfama'
+);
+ 
 1;
