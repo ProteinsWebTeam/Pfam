8b707ad0890b89bad9ce296a92e1e44912425f65
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2011-01-21 11:29:19 +0000

    Add a relationship to the wikitext table

--- PfamSchemata/WebUser/ArticleMapping.pm
+++ PfamSchemata/WebUser/ArticleMapping.pm
@@ -16,7 +16,12 @@ __PACKAGE__->add_columns(
   { data_type => 'TINYTEXT', default_value => '', is_nullable => 0, size => 255 },
 );
 
-__PACKAGE__->set_primary_key('accession');
+__PACKAGE__->set_primary_key('accession','title');
+
+__PACKAGE__->might_have(
+  wikitext => 'WebUser::Wikitext',
+  { 'foreign.title' => 'self.title' }
+);
 
 1;
 
@@ -24,12 +29,11 @@ __END__
 
 This table has the same name as one in the WikiApprove database, but this
 version doesn't need the "db" column. Its use from here out doesn't require
-that column. We also remove the second column (title) from the primary key,
-since we're interested in updating titles associated with entries:
+that column.
 
 CREATE TABLE `article_mapping` (
   `accession` varchar(7) NOT NULL,
   `title` tinytext NOT NULL,
-  PRIMARY KEY  (`accession`)
+  PRIMARY KEY  (`accession`,`title`(256))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
