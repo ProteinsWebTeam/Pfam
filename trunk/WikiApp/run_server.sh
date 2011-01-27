6ce5dcdba9668e0fe03f2dd525c372df29a739fc
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2011-01-27 11:04:49 +0000

    Fix problems with disconnecting tty and add PfamSchemata to @INC

--- trunk/WikiApp/run_server.sh
+++ trunk/WikiApp/run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export PERL5LIB=/path/to/PfamLib:/path/to/wikiapp_perl5/lib/perl5
-export WIKIAPP_CONFIG=/path/to//WikiApp/wikiapp.conf
+export PERL5LIB=/software/pfam/Modules/PfamLib:/software/pfam/wikiapp_perl5/lib/perl5
+export WIKIAPP_CONFIG=/software/pfam/WikiApp/wiki_app.conf
 
-/path/to/perl script/wikiapp_server.pl
+/software/bin/perl script/wikiapp_server.pl < /dev/null > /dev/null 2>&1 &
 
