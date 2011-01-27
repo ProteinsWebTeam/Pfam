7acb346e80c773fd89373428547636b3182dc467
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2011-01-27 11:08:00 +0000

    Clean up paths

--- WikiApp/run_server.sh
+++ WikiApp/run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export PERL5LIB=/software/pfam/Modules/PfamLib:/software/pfam/wikiapp_perl5/lib/perl5
-export WIKIAPP_CONFIG=/software/pfam/WikiApp/wiki_app.conf
+export PERL5LIB=/path/to/PfamLib:/path/to/PfamSchemata:/path/to/wikiapp_perl5/lib/perl5
+export WIKIAPP_CONFIG=/path/to/WikiApp/wiki_app.conf
 
-/software/bin/perl script/wikiapp_server.pl < /dev/null > /dev/null 2>&1 &
+/usr/bin/perl script/wikiapp_server.pl < /dev/null > /dev/null 2>&1 &
 
