b91e9c11e3ad54808bc5e1aeb41085817b4c1ebb
agb <agb@98333f76-0502-11df-81af-936bbf3a1e26>
2011-05-09 11:57:42 +0000

    Added DESC editing to script

--- PfamScripts/make/nextDUF.pl
+++ PfamScripts/make/nextDUF.pl
@@ -26,5 +26,38 @@ foreach my $fam (@{$pfams}, @{$deadFams}){
 my @dufNosSort = sort {$b <=> $a} @dufNos;
 
 my $no = $dufNosSort[0] + 1;
+my $id="DUF$no";
 
-print "\n The next available DUF number is: DUF$no\n\n";
+print "\n The next available DUF number is: $id\n\n";
+
+# Now check if in family dir overwrite DESC info
+if (! -e "DESC"){exit 0;}
+
+open (DESC, "DESC") or die "Cannot open DESC";
+open (DESCNEW, "> DESCNEW") or die "Cannot write to file DESCNEW";
+
+print DESCNEW "ID   $id\nDE   Domain of unknown function ($id)\n";
+
+while(<DESC>){
+    if (/^ID/){
+	next;
+    }
+    if (/^DE/){
+	next;
+    }
+    if (/^WK/){
+	next;
+    }
+
+    print DESCNEW;
+
+    if (/^TP/){
+        print DESCNEW "WK   Domain_of_unknown_function\n";
+    }
+}
+close DESC;
+close DESCNEW;
+
+# Do file moves
+system("cp DESC DESCOLD");
+system("cp DESCNEW DESC");
