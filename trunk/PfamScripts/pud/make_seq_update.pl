39124ea9c5dd387fa9788072113257fdb76517ba
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-07-01 20:28:27 +0000

    Moved the ncbi update to before the mapping. This was yet another cockup in a long line of things going wrong for Pfam 26.0.

--- trunk/PfamScripts/pud/make_seq_update.pl
+++ trunk/PfamScripts/pud/make_seq_update.pl
@@ -192,6 +192,18 @@ unless(-e "$statusdir/updated_pfamseq"){
   $logger->info("Already updated pfamseq");   
 }
 
+
+#-------------------------------------------------------------------------------
+## get GenPept from NCBI - takes about 1 hour to download. 12 hours to upload 
+unless(-e "$statusdir/ncbi"){  
+  system("pud-ncbi.pl -version $newrelease_num") and 
+    $logger->logdie("Failed to run pud-ncbi.pl:[$!]");
+  system("touch $statusdir/ncbi") and $logger->logdie("couldn't touch $statusdir/ncbi:[$!]");
+  $logger->info("Run ncbi seq update");
+}else{
+  $logger->info("Already updated ncbi sequence database");
+}
+
 #Now map between uniprot and ncbi - (takes about 1.5 hours)
 unless(-e "$statusdir/updated_ncbi_map"){
   #Add Jaina's stuff in here!!!!
@@ -232,17 +244,6 @@ unless(-e "$statusdir/uploaded_proteomes"){
   system("touch $statusdir/uploaded_proteomes") and $logger->logdie(die "couldn't touch $statusdir/uploaded_proteomes:[$!]");
 }else{
   $logger->info("Already uploaded proteomes");  
-}
-
-#-------------------------------------------------------------------------------
-## get GenPept from NCBI - takes about 1 hour to download. 12 hours to upload 
-unless(-e "$statusdir/ncbi"){  
-  system("pud-ncbi.pl -version $newrelease_num") and 
-    $logger->logdie("Failed to run pud-ncbi.pl:[$!]");
-  system("touch $statusdir/ncbi") and $logger->logdie("couldn't touch $statusdir/ncbi:[$!]");
-  $logger->info("Run ncbi seq update");
-}else{
-  $logger->info("Already updated ncbi sequence database");
 }
 
 #-------------------------------------------------------------------------------
