e0680f6daa2d32f13da2df94639a80a36d83f698
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-07-19 20:12:46 +0000

    Fixed the fact that taxid can not be nullable. This was causing all sorts of warnings;

--- trunk/PfamSchemata/PfamDB/Taxonomy.pm
+++ trunk/PfamSchemata/PfamDB/Taxonomy.pm
@@ -9,7 +9,7 @@ __PACKAGE__->load_components("Core");
 __PACKAGE__->table("taxonomy");
 __PACKAGE__->add_columns(
   "ncbi_taxid",
-  { data_type => "INT", default_value => 0, is_nullable => 1, size => 10 },
+  { data_type => "INT", default_value => 0, is_nullable => 0, size => 10 },
   "species",
   {
     data_type => "VARCHAR",
