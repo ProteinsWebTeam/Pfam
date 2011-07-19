de097d118bc6816ece4a5df78c1b1e102099b9bd
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-07-19 20:13:51 +0000

    Added a relationship - pairwise.

--- PfamSchemata/PfamDB/Pfama2pfamaHhsearchResults.pm
+++ PfamSchemata/PfamDB/Pfama2pfamaHhsearchResults.pm
@@ -29,7 +29,7 @@ __PACKAGE__->belongs_to(
 
 # Created by DBIx::Class::Schema::Loader v0.04003 @ 2009-11-16 14:52:41
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:aPCgnutLXO6dtpfU4WSgfQ
-
+__PACKAGE__->add_unique_constraint("pairwise", [qw(auto_pfama1 auto_pfama2)]);
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
