4c811c68ad2b419b441ea2cd3ed77a2f97513045
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-10-12 19:27:14 +0000

    First cut at trying to add sunbursts for Pfam-Bs.

---
+++ trunk/PfamSchemata/PfamLive/PfambSpeciesTree.pm
@@ -0,0 +1,33 @@
+package PfamLive::PfambSpeciesTree;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("Core");
+__PACKAGE__->table("pfamb_species_tree");
+__PACKAGE__->add_columns(
+  "auto_pfamb",
+  { data_type => "INT", default_value => "", is_nullable => 0, size => 5 },
+  "json_string",
+  {
+    data_type => "LONGTEXT",
+    default_value => "",
+    is_nullable => 0,
+    size => 16777215,
+  },
+);
+__PACKAGE__->belongs_to(
+  "auto_pfamb",
+  "PfamLive::Pfamb",
+  { auto_pfama => "auto_pfamb" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.04003 @ 2010-06-18 14:21:30
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:XfajFCpxLLWe3IruJUuMfA
+__PACKAGE__->set_primary_key("auto_pfamb");
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
