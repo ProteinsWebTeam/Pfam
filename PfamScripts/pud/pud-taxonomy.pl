e414e64ab491e64ca1a9636263e09e775edfbc72
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-10-14 02:29:28 +0000

    Modified to make the parent id to be ncbi_taxid rather than text. Makes faster for building species trees.

--- PfamScripts/pud/pud-taxonomy.pl
+++ PfamScripts/pud/pud-taxonomy.pl
@@ -1,51 +1,144 @@
 #!/usr/bin/env perl
 
+#
+# Script to download the taxonomy data from NCBI and to build the taxonomic
+# tree, strip out the levels that are not so meaningful to generate a minimal
+# tree. This 
+#
+
 use strict;
 use warnings;
 
 use DBI;
 use Data::Dumper;
 use Getopt::Long;
-
 use Log::Log4perl;
+
+use Data::Dump qw(dump);
+use Time::HiRes qw(gettimeofday tv_interval);
+
 use Bio::Pfam::Config;
 use Bio::Pfam::PfamLiveDBManager;
 
 
 #-------------------------------------------------------------------------------
 # configure logging
-
 my $logger_conf = q(
-  log4perl.logger                   = WARN, Screen
+  log4perl.logger                   = DEBUG, Screen
   log4perl.appender.Screen          = Log::Log4perl::Appender::Screen
   log4perl.appender.Screen.layout   = Log::Log4perl::Layout::PatternLayout
   log4perl.appender.Screen.layout.ConversionPattern = %M:%L %p: %m%n
 );
 
 Log::Log4perl->init( \$logger_conf );
-my $log = Log::Log4perl->get_logger;
+my $logger = Log::Log4perl->get_logger;
 
 #-------------------------------------------------------------------------------
-# check input
+#Get the config for the site
 
 my $config = Bio::Pfam::Config->new;
 my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( 
   %{  $config->pfamlive }
 );
 
-my ( $help );
 
-GetOptions(
-
-  "help"     => \$help
-);
+my ($help);
+GetOptions( "help" => \$help );
 
 usage() and exit if $help;
 
+#-------------------------------------------------------------------------------
+#- main ------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-my $nodeFile = "nodes.dmp";
-my $nameFile = "names.dmp";
+              
+#-------------------------------------------------------------------------------
+# Build up all the ncbi nodes. Read into a hash array
+$logger->info('Parsing ncbi data (nodes.dmp)');
 
+my %names;
+my %minNames;
+open( _NAMES, $config->localDbsLoc . '/ncbi/names.dmp' )
+  or die "Could not open " . $config->localDbsLoc . "/ncbi/names.dmp: [$!]\n";
+
+#------------------------------------------------------------------------------
+#This is a bit of a hack, but we want to keep these levels so we can hang
+#all sequences off this
+
+#Move to config
+my %promote;
+foreach
+  my $l ( "Viroids", "Viruses", "unclassified sequences", "other sequences" )
+{
+  $promote{$l}++;
+}
+
+my %promoteTaxIds;
+
+#------------------------------------------------------------------------------
+# Extract just the scientific names (for the time being, it may be useful to
+# have synonyms and lay terms ).
+#
+while (<_NAMES>) {
+  next unless (/scientific name/);
+  @_ = split( /\|/, $_ );
+  my ($taxid) = $_[0] =~ /(\d+)/;
+  my ($name)  = $_[1] =~ /\s+(.*)\s+/;
+  $names{$taxid} = $name;
+  $promoteTaxIds{$taxid}++ if ( $promote{$name} );
+}
+
+
+
+
+#------------------------------------------------------------------------------
+#Now parse the nodes file
+#
+open( _NODES,  $config->localDbsLoc . '/ncbi/nodes.dmp'  )
+  or $logger->logdie("Could not open nodes.dmp: [$!]");
+
+my $nodes = [];    #This will be out store for all the nodes in the tree
+while (<_NODES>) {
+  @_ = split( /\|/, $_ );
+  my ($taxid)  = $_[0] =~ /(\d+)/;
+  my ($parent) = $_[1] =~ /(\d+)/;
+  my ($rank)   = $_[2] =~ /(\S+)/;
+
+  #Shunt those special ids to be superkingdoms.
+  $rank = 'superkingdom' if ( $promoteTaxIds{$taxid} );
+
+  $nodes->[$taxid] = {
+                       taxid  => $taxid,
+                       parent => $parent,
+                       rank   => $rank,
+                       name   => $names{$taxid} };
+  
+}
+close(_NAMES);
+close(_NODES);
+
+#------------------------------------------------------------------------------
+$pfamDB->getSchema->resultset('Taxonomy')->delete;
+
+my $dbh = $pfamDB->getSchema->storage->dbh;
+$logger->debug( 'successfully connected to database' );
+
+my $insertTaxSth = $dbh->prepare( 
+                "INSERT INTO taxonomy VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
+
+
+
+
+#Now build the fuill tree as we get it from NCBI
+#
+$logger->info('Building full taxonomic tree');
+my $tree = {};
+buildTree( $tree, $nodes );
+
+#------------------------------------------------------------------------------
+# Cut out the levels that are excessive by reassigning parentage
+#
+$logger->info('Removing excessive nodes');
 my %ranks = ( superkingdom => 1,
               kingdom      => 1,
               phylum       => 1,
@@ -55,232 +148,170 @@ my %ranks = ( superkingdom => 1,
               genus        => 1,
               species      => 1 );
 
-my %names;
-my %minNames;
-open( _NAMES, $config->localDbsLoc . '/ncbi/names.dmp' )
-  or die "Could not open " . $config->localDbsLoc . "/ncbi/names.dmp: [$!]\n";
-while(<_NAMES>){
-  next unless(/scientific name/);
-  @_ = split(/\|/, $_);
-  my ($lineNumber) = $_[0] =~ /(\d+)/;
-  my ($lineName)   = $_[1] =~ /\s+(.*)\s+/;
-  $names{$lineNumber} = $lineName;
+my $parent = 1;                         #The root id
+walkAndCut( $tree, $nodes, $parent, \%ranks );
+
+#------------------------------------------------------------------------------
+# Now rebuild the minimal tree from the nodes.
+#
+my $t0 = [gettimeofday];
+$logger->info('Building minimal taxonomic tree');
+my $minTree = {};
+buildTree( $minTree, $nodes );
+my $elapsed = tv_interval( $t0 );
+$logger->info( "Minimal tree building took $elapsed");
+
+#------------------------------------------------------------------------------
+# To mkae the storage/quereies in the database faster we need to traverse the
+# tree to store the
+#
+$logger->info('Traversing tree');
+my $count = 0;
+traverseTree( $minTree, $nodes, $count );
+
+#------------------------------------------------------------------------------
+# Print tree out/store it in the database.
+#
+
+#$dbh->do("DELETE FROM taxonomy");
+
+my $taxString = '';
+$logger->info("Updating taxonomy table");
+traverseTreeAndStore( $minTree, $nodes, $taxString, $insertTaxSth );
+
+
+#------------------------------------------------------------------------------
+# subroutines ----------------------------------------------------------------- 
+#------------------------------------------------------------------------------
+
+sub traverseTree {
+  my ( $hash, $nodes, $count ) = @_;
+  foreach my $k ( keys %{$hash} ) {
+    $nodes->[$k]->{lft} = $count++;
+    $count = traverseTree( $hash->{$k}, $nodes, $count );
+    $nodes->[$k]->{rgt} = $count++;
+  }
+  return $count;
 }
 
-open( _NODES, $config->localDbsLoc . '/ncbi/nodes.dmp' )
-  or die "Could not open " . $config->localDbsLoc . "/ncbi/nodes.dmp: [$!]\n";
-while(<_NODES>){
-  @_ = split(/\|/, $_);
-  my ($lineNumber) = $_[0] =~ /(\d+)/;
-  my ($lineRank)   = $_[2] =~ /(\S+)/;
-  $lineRank = lc($lineRank);
-  if($names{$lineNumber} and $ranks{$lineRank}){
-    $minNames{ $names{$lineNumber} } = $lineRank;
+
+sub traverseTreeAndStore {
+  my ( $hash, $nodes, $taxString, $insertTaxSth ) = @_;
+  
+  foreach my $k ( keys %{$hash} ) {
+    my $thisTaxString = $taxString;
+    if(defined $nodes->[$k]->{name} ){
+    $thisTaxString .= $nodes->[$k]->{name} . ';';
+    $insertTaxSth->execute(
+        $k,
+        $nodes->[$k]->{rank} eq 'species' ? $nodes->[$k]->{name} : undef,
+        $thisTaxString,
+        $nodes->[$k]->{lft},
+        $nodes->[$k]->{rgt}, 
+        $nodes->[$k]->{parent},
+        $nodes->[$k]->{name},
+        1, 
+        $nodes->[$k]->{rank});
+    }
+    traverseTreeAndStore( $hash->{$k}, $nodes, $thisTaxString, $insertTaxSth );
   }
 }
 
-close(_NAMES);
-close(_NODES);
-
-#Now put the manual fixes in for the other things in the classification.
-foreach my $l ( "Viroids","Viruses", "unclassified", "unclassified sequences", "other sequences"){
-  $minNames{$l} = 'superkingdom';
+sub traverseTreeAndPrint {
+  my ( $hash, $nodes, $taxString ) = @_;
+  foreach my $k ( keys %{$hash} ) {
+    my $thisTaxString = $taxString;
+    $thisTaxString .= $nodes->[$k]->{name} . ';';
+    print $k. "\t"
+      . $nodes->[$k]->{parent} . "\t"
+      . $nodes->[$k]->{rank} . "\t"
+      . $nodes->[$k]->{lft} . "\t"
+      . $nodes->[$k]->{rgt} . "\t"
+      . $nodes->[$k]->{name} . "\t"
+      . $thisTaxString . "\n";
+    traverseTreeAndPrint( $hash->{$k}, $nodes, $thisTaxString );
+  }
 }
 
-#-------------------------------------------------------------------------------
-#- main ------------------------------------------------------------------------
-#-------------------------------------------------------------------------------
+sub walkAndCut {
+  my ( $hash, $nodesRef, $parent, $ranks ) = @_;
 
-# prepare the database access and the queries that we'll use. We expect to 
+  foreach my $k ( keys %{$hash} ) {
+    my $thisParent = $parent;
+    $nodesRef->[$k]->{parent} = $parent;
+    if ( $ranks->{ $nodesRef->[$k]->{rank} } ) {
+      $thisParent = $nodesRef->[$k]->{taxid};
+    }
+    walkAndCut( $hash->{$k}, $nodesRef, $thisParent, $ranks );
+  }
+}
+
+sub buildTree {
+  my ( $tree, $nodes ) = @_;
+
+  foreach my $node (@$nodes) {
+    next unless ($node);
+    next unless ( $node->{rank} eq 'species' );
+
+    my @speciesNodes;
+    push( @speciesNodes, $node );
+    my $pnode = $node;
+    until ( $pnode->{parent} == $pnode->{taxid} ) {
+      push( @speciesNodes, $nodes->[ $pnode->{parent} ] );
+      $pnode = $nodes->[ $pnode->{parent} ];
+    }
+
+    my $parent = $tree;
+    #Now walk down buiding up the tree.
+    for ( my $i = $#speciesNodes ; $i >= 0 ; $i-- ) {
+      $parent->{ $speciesNodes[$i]->{taxid} } = {}
+        unless exists $parent->{ $speciesNodes[$i]->{taxid} };
+      $parent = $parent->{ $speciesNodes[$i]->{taxid} };
+    }
+  }
+
+}
+
+
+
+# prepare the database access and the queries that we'll use. We expect to
 # write to a table like this:
 #
-# CREATE TABLE taxonomy (
-#   ncbi_taxid INT(10)      DEFAULT 0,
-#   species   VARCHAR(100) DEFAULT NULL,
-#   taxonomy  MEDIUMTEXT,
-#   lft       INT(10),
-#   rgt       INT(10),
-#   parent    VARCHAR(100),
-#   level     VARCHAR(100)
-# );
+# CREATE TABLE `taxonomy` (
+#  `taxid` int(10) NOT NULL,
+#  `parent_taxid` int(11) NOT NULL,
+#  `rank` varchar(16) DEFAULT NULL,
+#  `lft` int(10) NOT NULL,
+#  `rgt` int(10) NOT NULL,
+#  `name` varchar(100) NOT NULL,
+#  `taxonomy` mediumtext,
+#  PRIMARY KEY (`taxid`)
+#) ENGINE=InnoDB
 #
 # query the table with something like:
-# SELECT ncbi_taxid, 
+# SELECT ncbi_taxid,
 #        species,
-#        lft, 
-#        rgt, 
-#        parent, 
-#        level 
-# FROM   taxonomy 
-# WHERE  lft <= 98440 
-# AND    rgt >= 98441 
+#        lft,
+#        rgt,
+#        parent,
+#        level
+# FROM   taxonomy
+# WHERE  lft <= 98440
+# AND    rgt >= 98441
 # ORDER BT lft ASC;
-
-# connect
-$pfamDB->getSchema->resultset('Taxonomy')->delete;
-
-my $dbh = $pfamDB->getSchema->storage->dbh;
-$log->debug( 'successfully connected to database' );
-
-# just prepare the insert statement
-my $insertSth = $dbh->prepare( "INSERT INTO taxonomy VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )" );
-$log->debug( 'prepared INSERT statement' );
-
-# prepare and execute the select
-my $extractSth = $dbh->prepare( "SELECT ncbi_taxid, species, taxonomy FROM ncbi_taxonomy WHERE species != ''" );
-$extractSth->execute;
-$log->debug( 'executed SELECT statement to retrieve tree' );
-
-my $tree = {};
-while ( my $row = $extractSth->fetchrow_arrayref ) {
-  
-  my $parent = $tree;
-  foreach ( split( /;/, $row->[2] )) {
-    $parent->{$_} = {} unless exists $parent->{$_};
-    $parent = $parent->{$_};
-  }
-  $parent->{$row->[1]."-IsLeaf"} = {};
-  $parent->{$row->[1]."-IsLeaf"}->{NCBI} = $row->[0];
-  $parent->{$row->[1]."-IsLeaf"}->{TAXONOMY} = $row->[2];
-  
-}
-
-# open( TREE, ">tree.log" )
-#   or die "Couldn't open \"tree.log\" for write: $!";
-# print TREE Dumper( $tree );
-# close TREE;
-
-#my $indent = "";
-
-$log->info( 'starting to build the tree' );
-my $count  = 1;
-build( $tree, "root" );
-
-# open( TREE, ">tree_numbered.log" )
-#   or die "Couldn't open \"tree.log\" for write: $!";
-# print TREE Dumper( $tree );
-# close TREE;
-
-# open( WALK, ">walk.log" )
-#   or die "Couldn't open \"walk.log\" for write: $!";
-
-# $indent = "";
-
-$log->info( 'starting to walk the tree' );
-walk( $tree, \%minNames );
-
-exit;
-
-#-------------------------------------------------------------------------------
-#- subroutines -----------------------------------------------------------------
-#-------------------------------------------------------------------------------
-
-sub build {
-  my ( $hash, $parent ) = @_;
-
-  foreach my $nodeName ( keys %$hash ) {
-    #   print STDERR $indent . "key/value: |$nodeName|" . $hash->{$nodeName} . "| ";
-    #    if( ref $hash->{$nodeName} ) {
-    #      print STDERR "found a hash with " .
-    #     scalar( keys %{$hash->{$nodeName}} ) . " keys; walking\n";
-    #    } else {
-    #      print STDERR "not a hash\n";
-    #    }
-    next unless ref $hash->{$nodeName};
-
-    my $node = $hash->{$nodeName};
-    $node->{PARENT} = $parent;
-    if ( exists $node->{NCBI} ) {
-      $node->{LEFT}  = $count++;
-      $node->{RIGHT} = $count++;
-    }
-    else {
-      $node->{LEFT} = $count++;
-      # $indent .= "  ";
-      build( $node, $nodeName );
-      # $indent = substr( $indent, 2 );
-      $node->{RIGHT} = $count++;
-    }
-
-  } # end of foreach node
-
-}
-
-#-------------------------------------------------------------------------------
-
-sub walk {
-  my $hash = shift;
-  my $minNamesRef = shift;
-
-  my ( $left, $right, $parent );
-  foreach my $nodeName ( keys %$hash ) {
-
-    $left   = $hash->{$nodeName}->{LEFT};   delete $hash->{$nodeName}->{LEFT};
-    $right  = $hash->{$nodeName}->{RIGHT};  delete $hash->{$nodeName}->{RIGHT};
-    $parent = $hash->{$nodeName}->{PARENT}; delete $hash->{$nodeName}->{PARENT};
-
-    #   print WALK $indent, "name:      |$nodeName|\n";
-    #   print WALK $indent, "left:      |$left|\n";
-    #   print WALK $indent, "right:     |$right|\n";
-    #   print WALK $indent, "parent:    |$parent|\n";
-    $insertSth->bind_param( 4, $left );
-    $insertSth->bind_param( 5, $right );
-    $insertSth->bind_param( 6, $parent );
-
-    if ( exists $hash->{$nodeName}->{NCBI} ) {
-      #    print WALK $indent, "NCBI code: |".$hash->{$nodeName}->{NCBI}."|\n";
-      #    print WALK $indent, "tax:       |".$hash->{$nodeName}->{TAXONOMY}."|\n";
-      $insertSth->bind_param( 1, $hash->{$nodeName}->{NCBI} );
-      $nodeName =~ /^(.*?)\-IsLeaf$/;
-      $insertSth->bind_param( 2, $1 );
-      $insertSth->bind_param( 3, $hash->{$nodeName}->{TAXONOMY} );
-      $insertSth->bind_param( 7, "NULL" );
-      $insertSth->bind_param( 8, 1);
-      $insertSth->bind_param( 9, "species");
-
-
-
-
-      $insertSth->execute;
-    }
-    else {
-      $insertSth->bind_param( 1, "0" );
-      $insertSth->bind_param( 2, "NULL" );
-      $insertSth->bind_param( 3, "NULL" );
-      $insertSth->bind_param( 7, $nodeName );
-      $insertSth->bind_param( 8, (defined($minNamesRef->{$nodeName}) ? 1 : 0 ));
-      $insertSth->bind_param( 9, (defined($minNamesRef->{$nodeName}) ? $minNamesRef->{$nodeName} : "NULL" ));
-
-      $insertSth->execute;
-      #    $indent .= "  ";
-      walk( $hash->{$nodeName}, $minNamesRef );
-      #    $indent = substr( $indent, 2 );
-    }
-
-  }
-}
+;
 
 #-------------------------------------------------------------------------------
 
 sub usage {
 
-print <<'EOF_help';
-Usage: tree.pl [OPTION]
+  print <<'EOF_help';
+Usage: $0 
 
 Build a database table of the taxonomic tree.
 
-Example: tree.pl -h pfamdb2a --rdb pfam_35_0 -u pfam -p wibble --port 3301 
-
-Database parameters:
-  -r, --rdb      database name
-  -h, --host     database host name
-      --port     database port number
-  -u, --user     database account user name
-  -p, --pass     database account password
-      --help     show this message
-
 EOF_help
- 
+
 }
 
 #-------------------------------------------------------------------------------
