80ee896e4590e555e2295901d6c9899db82d25f5
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2011-11-17 17:12:28 +0000

    Modified this old legacy script to populate the MySQL database with the release stats.

--- PfamScripts/release/flatfile_stats.pl
+++ PfamScripts/release/flatfile_stats.pl
@@ -2,17 +2,44 @@
 
 # A script to make coverage stats from flatfiles.
 
-# Usage: flatfile_stats.pl <Pfam-A flatfile> <Pfam-B flatfile>
+# Usage: flatfile_stats.pl <Pfam-A flatfile> <Pfam-B flatfile> <num_seqs> <num_res>
 
 use strict;
 use warnings;
-
+use Bio::Pfam::Config;
+use Bio::Pfam::PfamLiveDBManager;
 
 
 $| = 1;
 
+my $config = Bio::Pfam::Config->new;
+my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamlive } );
+
+
 my $pfama_flat=shift;
 my $pfamb_flat=shift;
+my $pfamseq_sequences=shift;
+my $pfamseq_residues=shift;
+
+unless($pfama_flat and -s $pfama_flat){ 
+  die "Please specify the location of the Pfam-A file.\n";
+}
+
+unless($pfamb_flat and -s $pfamb_flat){ 
+  die "Please specify the location of the Pfam-B file.\n";
+}
+
+unless( $pfamseq_sequences and $pfamseq_sequences =~ /^\d+$/){ 
+  die "Please specify the number of sequences in pfamseq.\n";
+}
+
+unless( $pfamseq_residues and $pfamseq_residues =~ /^\d+$/){ 
+  die "Please specify the number of residues in pfamseq.\n";
+}
+
+unless( $pfamseq_residues > $pfamseq_sequences){ 
+  die "The number of sequences is greater than the number of residues!!!\n";
+}
 
 my ($db,
     $id,
@@ -26,8 +53,6 @@ my ($db,
     $pfamb_sequences,
     $pfamb_only_seqs,
     $pfamb_residues,
-    $pfamseq_residues,
-    $pfamseq_sequences,
     $both_a_and_b_seqs,
     $B2Alinks,
     $A2Blinks, 
@@ -40,6 +65,9 @@ my ($db,
     %pfamb_id);
 
 open (A, "$pfama_flat") || die"Can't open $pfama_flat\n";
+open (B, "$pfamb_flat") || die"Can't open $pfamb_flat\n";
+
+
 while (<A>){
     if (/\#=GF\s+ID\s+(\S+)/) {
 	$pfamAfamilies++;
@@ -118,7 +146,7 @@ foreach my $id_a (keys %pfama_id)
 $all_residues = $pfama_residues;
 
 
-open (B, "$pfamb_flat")||die"Can't open $pfamb_flat\n";
+
 while (<B>){
     if (/\#=GF\s+ID\s+(\S+)/) {
 	$pfamBfamilies++;
@@ -167,10 +195,18 @@ $all_sequences = $pfama_only_seqs + $pfamb_only_seqs + $both_a_and_b_seqs;
 $pfama_sequences = $pfama_only_seqs + $both_a_and_b_seqs;
 $pfamb_sequences = $pfamb_only_seqs + $both_a_and_b_seqs;
 
+my $pfama_seq_cov =  sprintf("%.2f", $pfama_sequences/$pfamseq_sequences*100);
+my $pfama_res_cov = sprintf("%.2f", $pfama_residues/$pfamseq_residues*100);
 
-# Now find stats for underlying pfamseq
-$db = &Bio::Pfam::default_db();
-($pfamseq_residues,$pfamseq_sequences) = $db->pfamseq_stats();
+my $pfamb_seq_cov = sprintf("%.2f", $pfamb_sequences/$pfamseq_sequences*100);
+my $pfamb_res_cov = sprintf("%.2f", $pfamb_residues/$pfamseq_residues*100);
+
+my $total_seq_cov = sprintf("%.2f", $all_sequences/$pfamseq_sequences*100);
+my $total_res_cov = sprintf("%.2f", $all_residues/$pfamseq_residues*100);
+
+my $pfamb_add_seq_cov = sprintf("%.2f", $total_seq_cov - $pfama_seq_cov); 
+my $pfamb_add_res_cov = sprintf("%.2f", $total_res_cov - $pfama_res_cov); 
+
 
 # Now calculate coverages
 
@@ -183,21 +219,25 @@ print "Pfam-A\n======\n\n";
 print "\tNumber of families = $pfamAfamilies\n";
 print "\tResidues = $pfama_residues\n";
 print "\tSequences = $pfama_sequences (with Pfam-A domains only = $pfama_only_seqs)\n";
-print "\tPercent pfamseq residues =",$pfama_residues/$pfamseq_residues*100,"\n";
-print "\tPercent Pfamseq sequences =",$pfama_sequences/$pfamseq_sequences*100,"\n\n";
+print "\tPercent pfamseq residues =",$pfama_res_cov,"\n";
+print "\tPercent Pfamseq sequences =",$pfama_seq_cov,"\n\n";
 
 print "Pfam-B\n======\n\n";
 print "\tNumber of families: $pfamBfamilies\n";
 print "\tResidues: $pfamb_residues\n";
 print "\tSequences: $pfamb_sequences (with Pfam-B domains only = $pfamb_only_seqs)\n";
-print "\tPercent Pfamseq residues =",$pfamb_residues/$pfamseq_residues*100,"\n";
-print "\tPercent Pfamseq sequences =",$pfamb_sequences/$pfamseq_sequences*100,"\n\n";
+print "\tPercent Pfamseq residues =",$pfamb_res_cov,"\n";
+print "\tPercent Pfamseq sequences =",$pfamb_res_cov,"\n";
+print "\tPercent Pfamseq residues additional =",$pfamb_add_res_cov,"\n";
+print "\tPercent Pfamseq sequences additional =",$pfamb_add_res_cov,"\n\n";
+
+
 
 print "Combined stats\n===============\n\n";
 print "\tResidues: $all_residues\n";
 print "\tSequences: $all_sequences (with both Pfam-A and Pfam-B domains = $both_a_and_b_seqs)\n";
-print "\tPercent Pfamseq residues =",$all_residues/$pfamseq_residues*100,"\n";
-print "\tPercent Pfamseq sequences =",$all_sequences/$pfamseq_sequences*100,"\n";
+print "\tPercent Pfamseq residues =",$total_res_cov,"\n";
+print "\tPercent Pfamseq sequences =",$total_seq_cov,"\n";
 
 print "\n\nLinks from Pfam-B to Pfam-A\n";
 print "---------------------------\n";
@@ -212,3 +252,12 @@ print "---------------------------\n";
 foreach my $item (sort { $a <=> $b } keys %A2Blinknums) {
     print "Number of families with $item Pfam-B links: $A2Blinknums{$item}\n";
 }
+
+
+
+my $version = $pfamDB->getSchema->resultset('Version')->search()->first;
+$version->upadte({ pfamA_coverage => sprintf("%.1f", $pfama_seq_cov),
+                   pfamB_additional_coverage => sprintf("%.1f", $pfamb_add_seq_cov),
+                   pfamA_residue_coverage => sprintf("%.1f", $pfama_res_cov),
+                   pfamB_additional_residue_coverage => sprintf("%.1f", $pfamb_add_res_cov), 
+                   number_families => $pfamAfamilies });
