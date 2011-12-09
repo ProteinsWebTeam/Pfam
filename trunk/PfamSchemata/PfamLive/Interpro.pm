74cfccab12433f6a5b05e4dcc5e54ab86cb60d5a
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2011-12-09 13:48:48 +0000

    Add unique constraint

--- trunk/PfamSchemata/PfamLive/Interpro.pm
+++ trunk/PfamSchemata/PfamLive/Interpro.pm
@@ -36,5 +36,9 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-01-17 10:09:48
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:lI3rC1SwrkXzLdm5LNyuDA
 
+__PACKAGE__->add_unique_constraint( 
+  auto_ip_unq => [ qw( auto_pfama interpro_id ) ] 
+); 
+
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
