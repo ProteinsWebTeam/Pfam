adc5f93629744132a1616bf2328f169aef475c35
jm14 <jm14@98333f76-0502-11df-81af-936bbf3a1e26>
2012-01-06 10:33:01 +0000

    New script to wrap CC lines in DESC files

---
+++ PfamScripts/make/CC_wrap.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Text::Wrap;
+
+$Text::Wrap::columns = 70;
+
+my $desc_file = "DESC";
+my $tmp_desc = "DESC.b4.CCedit.$$";
+
+unless(-s $desc_file) {
+  print "Can't find DESC file in cwd\n";
+}
+
+rename($desc_file, $tmp_desc) or die "Couldn't rename $desc_file to $tmp_desc, $!";
+
+my $CC_line;
+my $found_CC;
+open(TMP, $tmp_desc) or die "Couldn't open $tmp_desc, $!";
+open(DESC, ">$desc_file") or die "Couldn't open $desc_file for writing, $!";
+while(<TMP>) {
+  if(/^CC\s+(.+)/) {
+    $CC_line .=$1;
+    $found_CC=1;
+  }
+  else {
+    if($found_CC) {
+      my $text = wrap( "CC   ", "CC   ", $CC_line . ".\n" );
+      print DESC "$text";
+      $found_CC="";
+    }
+    else {
+      print DESC $_;
+    }
+  }
+}
+
+if($found_CC) {
+  my $text = wrap( "CC   ", "CC   ", $CC_line . "\n" );
+  print DESC "$text";
+}
+
+close TMP;
+close DESC;
