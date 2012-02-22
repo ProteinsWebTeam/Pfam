8f81548ee6c45bdefc9e0f42b273f0c484172a43
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2012-02-22 13:16:16 +0000

    Fix minor bug with handling of "output" param.

--- PfamBase/lib/PfamBase/Controller/Search.pm
+++ PfamBase/lib/PfamBase/Controller/Search.pm
@@ -50,7 +50,7 @@ sub begin : Private {
   # decide what format to emit. The default is HTML, in which case
   # we don't set a template here, but just let the "end" method on
   # the Section controller take care of us
-  $c->stash->{output_xml} = ( $c->req->param('output') || '' eq 'xml' );
+  $c->stash->{output_xml} = ( $c->req->param('output') || '' ) eq 'xml';
 
 }
 
