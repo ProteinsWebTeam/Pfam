d903f6008aada31ec2c91cd6166be64cbc538b26
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2012-10-02 20:35:09 +0000

    Tidied up paths, etc.

--- PfamBackend/scripts/fastDequeue.sh
+++ PfamBackend/scripts/fastDequeue.sh
@@ -30,19 +30,30 @@
 # Description:       Pfam queue listener
 ### END INIT INFO
 
-#You will need to configure this for your system.
-#General
+# General
+export DEBUG=0
+export PATH=/usr/local/bin:/software/bin:/usr/bin:/bin
 
+# Pfam config
 export PFAMROOT=/software/pfam
 export PFAMWEBROOT=$PFAMROOT/pfamweb
 export WISECONFIGDIR=$PFAMWEBROOT/wisecfg 
 export PFAMOFFLINE_CONFIG=$PFAMWEBROOT/PfamBackend/conf/pfam_backend.conf
-export PATH=$PFAMWEBROOT/PfamBackend/scripts:$PFAMWEBROOT/bin:$PFAMWEBROOT/src/wise2.2.0/src/bin:/sbin:/usr/bin:$PATH
+export PATH=$PFAMWEBROOT/PfamBackend/scripts:$PFAMWEBROOT/bin:$PFAMWEBROOT/src/wise2.2.0/src/bin:/software/pfam/bin:$PATH
+export PERL5LIB=$PFAMWEBROOT/PfamLib:$PFAMWEBROOT/PfamSchemata:$PFAMWEBROOT/bioperl_1.4
 
 # Rfam config
 export RFAMROOT=/software/rfam
 export RFAMWEBROOT=$RFAMROOT/rfamweb 
-export PATH=$RFAMWEBROOT/bin:/software/rfam/bin:$RFAMWEBROOT/src/wublast:$PATH
+export PATH=$RFAMWEBROOT/bin:$RFAMWEBROOT/src/wublast:$PATH
+# export PATH=$RFAMWEBROOT/bin:/software/rfam/bin:$RFAMWEBROOT/src/wublast:$PATH
+
+
+HOST=`hostname`
+if [ $HOST = "pfamweb-03" -o $HOST = "pfamweb-04" ]; then
+ . /usr/local/lsf/conf/profile.lsf
+fi
+
 
 # MPI
 if [ "x$LD_LIBRARY_PATH" == "x" ]; then
@@ -51,24 +62,19 @@ else
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi-1.4.3/lib
 fi
 
-export PATH=/software/openmpi-1.4.3/bin:$PATH
 
-#This is very WTSI specific.
-HOST=`hostname`
-if [ $HOST = "pfamweb-03" -o $HOST = "pfamweb-04" ]; then
- . /usr/local/lsf/conf/profile.lsf
-fi
+# finalise paths
+export PATH=/software/openmpi-1.4.3/bin:/software/bin:/sbin:/usr/bin:$PATH
+export PERL5LIB=/software/pfam/pfamweb/perl5/lib/perl5:/software/pfam/pfamweb/perl5/x86_64-linux-thread-multi:$PERL5LIB
 
-export PERL5LIB=$PFAMWEBROOT/perl5/lib/perl5/x86_64-linux-thread-multi:$PFAMWEBROOT/perl5/lib/perl5:$PFAMWEBROOT/PfamLib:$PFAMWEBROOT/PfamSchemata:$PFAMWEBROOT/bioperl_1.4:$PFAMROOT/perl/lib/5.8.8:$PFAMROOT/perl/lib/perl/5.8.4:$PFAMROOT/perl/lib/site_perl/5.8.8:$PFAMROOT/perl/lib/site_perl/5.8.8/x86_64-linux-thread-multi:$PFAMROOT/perl/share/perl/5.8:$PFAMROOT/perl/share/perl/5.8.8:$PFAMROOT/perl/share/perl/5.8.4
+# export PERL5LIB=$PFAMWEBROOT/perl5/lib/perl5/x86_64-linux-thread-multi:$PFAMWEBROOT/perl5/lib/perl5:$PFAMWEBROOT/PfamLib:$PFAMWEBROOT/PfamSchemata:$PFAMWEBROOT/bioperl_1.4:$PFAMROOT/perl/lib/5.8.8:$PFAMROOT/perl/lib/perl/5.8.4:$PFAMROOT/perl/lib/site_perl/5.8.8:$PFAMROOT/perl/lib/site_perl/5.8.8/x86_64-linux-thread-multi:$PFAMROOT/perl/share/perl/5.8:$PFAMROOT/perl/share/perl/5.8.8:$PFAMROOT/perl/share/perl/5.8.4
 
 
 #Config for this shell script
-DAEMON=/software/pfam/pfamweb/PfamBackend/scripts/fastDequeue.pl
+DAEMON=$PFAMWEBROOT/PfamBackend/scripts/fastDequeue.pl
 NAME=fastDequeue
 DESC="Fast queue daemon"
-PERLBIN=/usr/local/bin/perl
-
-export DEBUG=0;
+PERLBIN=/software/bin/perl
 
 #End of config
 
@@ -76,34 +82,39 @@ test -f $DAEMON || exit 0
 
 set -e
 case "$1" in
+
   start)
- echo -n "Starting $DESC: "
- start-stop-daemon --start --quiet --pidfile /var/lock/${NAME}.pid \
-   -c pfamweb --exec $PERLBIN --startas $PERLBIN $DAEMON 
- echo "$NAME."
- ;;
+    echo -n "Starting $DESC: "
+    start-stop-daemon --start --quiet --pidfile /var/lock/${NAME}.pid \
+      -c pfamweb --exec $PERLBIN --startas $DAEMON 
+    echo "$NAME."
+    ;;
+
   stop)
- echo -n "Stopping $DESC: "
- # --quiet
- start-stop-daemon --stop --signal 15 --pidfile /var/lock/${NAME}.pid \
-  -c pfamweb --exec $PERLBIN --startas $PERLBIN $DAEMON
- echo "$NAME."
- ;;
+    echo -n "Stopping $DESC: "
+    start-stop-daemon --stop --signal 15 --quiet --pidfile /var/lock/${NAME}.pid \
+      -c pfamweb --exec $PERLBIN --startas $DAEMON
+    echo "$NAME."
+    ;;
+
   restart|force-reload)
- echo -n "Restarting $DESC: "
- start-stop-daemon --stop --signal 15 --quiet --pidfile \
-  /var/lock/${NAME}.pid  -c pfamweb --exec $PERLBIN --startas $PERLBIN $DAEMON 
- sleep 1
- start-stop-daemon --start --quiet --pidfile \
-  /var/lock/${NAME}.pid -c pfamweb --exec $PERLBIN --startas $PERLBIN $DAEMON
- echo "$NAME."
- ;;
+    echo -n "Restarting $DESC: "
+    start-stop-daemon --stop --signal 15 --quiet --pidfile /var/lock/${NAME}.pid \
+      -c pfamweb --exec $PERLBIN --startas $DAEMON 
+
+    sleep 1
+
+    start-stop-daemon --start --quiet --pidfile /var/lock/${NAME}.pid \
+      -c pfamweb --exec $PERLBIN --startas $DAEMON
+    echo "$NAME."
+    ;;
+
   *)
- N=/etc/init.d/$NAME
- # echo "Usage: $N {start|stop|restart|reload|force-reload}" >&2
- echo "Usage: $N {start|stop|restart|force-reload}" >&2
- exit 1
- ;;
+    N=/etc/init.d/$NAME
+    echo "Usage: $N {start|stop|restart|force-reload}" >&2
+    exit 1
+    ;;
+
 esac
 
 exit 0
