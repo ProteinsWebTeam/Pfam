7dc4d3bc5349ba1c4000ac8082910fef703bb147
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2012-11-27 13:52:11 +0000

    New scoop OO view object to perform the scoop analysis for the release.

---
+++ trunk/PfamLib/Bio/Pfam/ViewProcess/Scoop.pm
@@ -0,0 +1,235 @@
+package Bio::Pfam::ViewProcess::Scoop;
+
+use strict;
+use warnings;
+use Moose;
+use Moose::Util::TypeConstraints;
+
+extends 'Bio::Pfam::ViewProcess::Architecture';
+
+has '+statusFile' => (
+  default => 'runScoop',
+);
+
+sub runScoop {
+  my ( $self ) = @_;
+  
+  my $dbh = $self->pfamdb->getSchema->storage->dbh;
+  my $scp = Net::SCP->new( { "host"=> $self->config->{pfamlive}->{host} } );
+  if(!$self->statusCheck("sigReg.txt")){
+    $dbh->do(
+      "SELECT auto_pfamseq, auto_pfamA seq_start, seq_end, domain_evalue_score "
+    . "INTO OUTFILE '/tmp/sigReg.txt' FROM pfamA_reg_full_significant" );
+    $scp->get('/tmp/sigReg.txt', $self->statusdir.'/sigReg.txt')
+  }
+  
+  if(!$self->statusCheck("insigReg.txt")){
+    $dbh->do(
+      "SELECT auto_pfamseq, auto_pfamA seq_start, seq_end, domain_evalue_score "
+    . "INTO OUTFILE '/tmp/insigReg.txt' FROM pfamA_reg_full_insignificant" );
+    $scp->get('/tmp/insigReg.txt', $self->statusdir.'/insigReg.txt')
+  }
+  
+  #Now sort the regions
+  if(!$self->statusCheck("allReg.txt")){
+    system("cat ".$self->statusdir."/sigReg.txt ".$self->statusdir."/insigReg.txt >".$self->statusdir."/allReg.txt");
+  }
+  if(!$self->statusCheck("allRegSort.txt")){
+    system("sort ".$self->statusdir."/allReg.txt > ".$self->statusdir."/allRegSort.txt") 
+       and $self->logger->logdie("Failed to sort the regions:[$!]");
+  }
+  
+   if(!$self->statusCheck("runScoop")){
+    open( my $upload, '>', $self->statusdir."/scoopRes.4upload.txt" )
+      or $self->logger->logdie("Failed to open file $self->statusdir/scoopRes.4upload.txt:[$!]");
+    
+    $self->scoop($self->statusdir."/allRegSort.txt", $upload);
+    close($upload);
+    $self->touchStatuc("runScoop");
+   }
+  
+  $self->pfamdb->getSchema->resultset('Pfama2pfamaScoopResults')->delete;
+
+  $scp->put( $self->statusdir."/scoopRes.4upload.txt", "/tmp/scoopRes.4upload.txt" )
+    or $self->logger->logdie("Failed to scp results file to instance");
+  $dbh->do("LOAD DATA INFILE '/tmp/scoopRes.4upload.txt' INTO TABLE pfamA2pfamA_scoop_results");
+}
+
+sub scoop {
+  my ( $self, $file, $fh, $tempDir ) = @_;
+
+  $self->logger->info("Reading regions");
+  my $total_region = 0;
+  my %family_total;    # Counts matches per family
+  my %uber_matrix;
+
+  open( FH, $file ) or die "Cannot open $file";
+  my $old_id;
+  my ( $set, $new_set );
+  foreach my $line (<FH>) {
+    chomp $line;
+    my @line = split( /\s+/, $line );
+    my $id   = $line[0];
+
+    # Make new region object
+    my $region = new Bio::SCOOP::Region(
+      'id'          => $id,
+      'start'       => $line[2],
+      'end'         => $line[3],
+      'family'      => $line[1],
+      'evalue'      => $line[4],
+      'significant' => $line[5]
+    );
+
+    if ( $id ne $old_id and $old_id ) {
+      # Now add old set to uber_matrix
+      $self->add_set_to_matrix( $set, \%family_total, \%uber_matrix );
+      
+      # Make new set
+      $set = new Bio::SCOOP::RegionSet($id);
+      $set->add($region);
+      $total_region++;
+    }
+    elsif ( $id eq $old_id ) {
+      $set->add($region);
+      $total_region++;
+    }
+    elsif ( $id ne $old_id and !$old_id ) {
+      $set = new Bio::SCOOP::RegionSet($id);
+      $set->add($region);
+      $total_region++;
+
+    }
+    $old_id = $id;
+  }
+  $self->logger->info("Finished processing regions");
+
+  # Now do final processing
+  $self->logger->info( "Total regions:$total_region");
+
+  # Variables used to calculate the average score per family
+  my %family_score_total;
+  my %family_score_denominator;
+
+  # Loop over every pair of families in uber matrix and calculate score
+  open( FH, ">$tempDir/old_scores" ) or die "cannot write $tempDir/old_scores:[$!]";
+  foreach my $x ( sort keys %uber_matrix ) {
+    my %hash = %{ $uber_matrix{$x} };
+    foreach my $y ( sort keys %hash ) {
+      my $expected = 1 + $family_total{$x} * $family_total{$y} / $total_region;
+      my $observed = $uber_matrix{$x}{$y};
+      my $test     = 0;
+      my $score;
+      if ( !$test ) {
+        $score = $observed / $expected;
+      }
+      else {
+        $score = $uber_matrix{$x}{$y};
+      }
+      print FH "$score $x $y $family_total{$y} Both $observed\n";
+
+      # Store for each family the total score and the number of entries.
+      $family_score_total{$x} += $score;
+      $family_score_total{$y} += $score;
+      $family_score_denominator{$x}++;
+      $family_score_denominator{$y}++;
+    }
+  }
+
+  # Print out average score data
+  foreach my $x ( keys %family_score_total ) {
+
+    # Method d
+    my $average_score =
+      $family_score_total{$x} / ( 100 + $family_score_denominator{$x} );
+    print FH
+"# Average score for $x is $average_score denominator is $family_score_denominator{$x}\n";
+  }
+  print FH "# TOTAL REGIONS $total_region\n";
+  close FH;
+
+  $self->logger->info("Second pass over Uber matrix\n");
+  # Loop over every pair of families in uber matrix and calculate score
+  foreach my $x ( sort keys %uber_matrix ) {
+    foreach my $y ( sort keys %{ $uber_matrix{$x} } ) {
+      my $average_score_x =
+        $family_score_total{$x} / ( 100 + $family_score_denominator{$x} );
+      my $average_score_y =
+        $family_score_total{$y} / ( 100 + $family_score_denominator{$y} );
+      my $max_score;
+      if ( $average_score_x > $average_score_y ) {
+        $max_score = $average_score_x;
+      }
+      else {
+        $max_score = $average_score_y;
+      }
+
+      # The below line is current best method
+      my $expected =
+        1 + $max_score * $family_total{$x} * $family_total{$y} / $total_region;
+      my $observed = $uber_matrix{$x}{$y};
+      my $test     = 0;
+      my $score;
+
+      $score = $observed / ($expected);
+
+      print $fh "$x\t$y\t$score\n";
+
+    }
+  }
+
+}
+
+# OK now have all regions loaded up
+# Loop over each protein and populate uber matrix.
+
+# This routine takes a RegionSet and does comparison and adds data to uber_matrix
+sub _add_set_to_matrix {
+  my ( $self, $set, $family_total, $uber_matrix ) = @_;
+  my @regions = $set->each();
+  my $n       = @regions;
+
+  #print STDERR "Looking at protein $id with $n regions\n";
+  # Now loop over diagonal half matrix
+  if ( $n == 1 ) {
+    $family_total->{ $regions[0]->family() }++;    # Still add to family counts
+    return ();
+  }
+
+  for ( my $i = 0 ; $i < $n ; $i++ ) {
+
+    # For every region add to family count
+    if ( !$regions[$i] ) {
+      warn "No region for $i\n";
+    }
+
+    $family_total->{ $regions[$i]->family() }++;
+
+    for ( my $j = $i + 1 ; $j < $n ; $j++ ) {
+      if ( !$regions[$j] ) {
+        warn "No region for $j\n";
+      }
+
+      # Are regions the same?
+      if ( $regions[$i]->overlap( $regions[$j] ) ) {
+
+# Also test whether either region has counted towards a match between these families already.
+        if (  !$regions[$i]->match( $regions[$j]->family() )
+          and !$regions[$j]->match( $regions[$i]->family() ) )
+        {
+
+          # OK increment counts here
+          $uber_matrix->{ $regions[$i]->family() }{ $regions[$j]->family() }++;
+
+          # Add to list of outputs matched in region object
+          $regions[$i]->add_match( $regions[$j]->family() );
+          $regions[$j]->add_match( $regions[$i]->family() );
+        }
+      }
+    }
+  }
+  return;
+}
+
+
+1;
