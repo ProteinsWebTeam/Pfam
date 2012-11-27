b62c6c98b72220af5d63316c81f9822a1dc68a2f
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2012-11-27 22:27:40 +0000

    Whole series of minor fixes to get the code running.

--- trunk/PfamScripts/view/performScoopAnalysis.pl
+++ trunk/PfamScripts/view/performScoopAnalysis.pl
@@ -3,18 +3,15 @@
 use strict;
 use warnings;
 use Data::Printer;
-use Bio::Pfam::ViewProcess;
 use Bio::Pfam::ViewProcess::Scoop;
 
 #-------------------------------------------------------------------------------
 
 #Lets get a new Pfam view object
-my $view = Bio::Pfam::ViewProcess->new;
-my $storableView = Bio::Pfam::ViewProcess::Storable->new;
+my $scoopView = Bio::Pfam::ViewProcess::Scoop->new;
 
 #run scoop
-if(! $view->statusCheck('doneScoop')){
-  my $scoopView = Bio::PfamViewProcess::Scoop->new;
+if(! $scoopView->statusCheck('doneScoop')){
   #Grab the regions from the database and perform the SCOOP analysis
   $scoopView->runScoop;
   $scoopView->touchStatus('doneScoop')
