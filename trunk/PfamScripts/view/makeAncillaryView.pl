693b08d3dbca88469215bf207a95f5eb92b54064
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2012-12-06 20:50:12 +0000

    Major updates to the ancillary view process code - now encapsulated the proteome updates.

--- trunk/PfamScripts/view/makeAncillaryView.pl
+++ trunk/PfamScripts/view/makeAncillaryView.pl
@@ -3,15 +3,17 @@
 use strict;
 use warnings;
 use Data::Printer;
+use Bio::Pfam::ViewProcess;
 use Bio::Pfam::ViewProcess::Architecture;
 use Bio::Pfam::ViewProcess::Storable;
+use Bio::Pfam::ViewProcess::Scoop;
+
 #-------------------------------------------------------------------------------
 
 #Lets get a new Pfam view object
 my $view = Bio::Pfam::ViewProcess->new;
 my $archView = Bio::Pfam::ViewProcess::Architecture->new;
 my $storableView = Bio::Pfam::ViewProcess::Storable->new;
-
 #-------------------------------------------------------------------------------
 # Now update the VERSION table with the number of PfamA
 
@@ -58,9 +60,134 @@ if(exists($archView->options->{acc}) and $archView->options->{acc}){
   }
   
   #Now make the structure images
+  if(! $pdbImageView->statusCheck('donePdbImages')){
+    #Submit the architecture calulcations to the farm. This will not return until
+    #all jobs have completed.
+    $pdbImageView->submitToFarm(150);
+    #Reset all of the counts in the architecture table and pfamA table
+    $pdbImageView->touchStatus('donePdbImages');
+  }
   
-  #run scoop
+  #Now make run the models against the 'other' sequence databases
+  if(! $searchView->statusCheck('doneOtherSearches')){
+    #Submit the architecture calulcations to the farm. This will not return until
+    #all jobs have completed.
+    $searchView->submitToFarm(75);
+    #Reset all of the counts in the architecture table and pfamA table
+    $searchView->touchStatus('doneOtherSearches');
+  }
+}
+ 
+ 
+ 
+#All of these steps are affected whether one family is changed or all as they
+#involve all by all comparisons. 
+#run scoop
+my $scoopView = Bio::Pfam::ViewProcess::Scoop->new;
+  if(! $scoopView->statusCheck('doneScoop')){
+    exit;
+    #Grab the regions from the database and perform the SCOOP analysis
+    $scoopView->runScoop;
+    $scoopView->touchStatus('doneScoop')
+  }else{
+    $scoopView->logger->info('Done Scoop');
+  }
+
+  #run HHsearch
+  my $hhsearchView = Bio::Pfam::ViewProcess::HHsearch->new;
+  if(! $hhsearchView->statusCheck('doneHHsearch')){
+    $hhsearchView->options->{newlib} = 1;
+    $hhsearchView->makeHHLib;
+    $hhsearchView->upload(1);
+    $hhsearchView->submitToFarm(100);
+    $hhsearchView->touchStatus('doneHHsearch')
+  }
+
+  #Proteome data.....
+  my $proteomeView = Bio::Pfam::ViewProcess::Proteome->new;
+  if(! $proteomeView->statusCheck('doneProteome')){
+    my $protDbh = $proteomeView->pfamdb->getSchema->storage;
+    if(! $proteomeView->statusCheck('updateProteomeArch')){
+      $protDbh->do("set FOREIGN_KEY_CHECKS=0");
+    
+      #Update the proteome_architecture, then update the stats
+      $protDbh->do("DELETE FROM proteome_architecture");
+      $protDbh->do("INSERT INTO proteome_architecture ".
+                      "SELECT p.auto_proteome, s.auto_architecture, s.auto_pfamseq, count(s.auto_pfamseq) ".
+                      "FROM proteome_pfamseq p, pfamseq s ".
+                      "WHERE s.auto_pfamseq=p.auto_pfamseq ".
+                      "GROUP BY auto_architecture, auto_proteome");
+      
+      $protDbh->do("set FOREIGN_KEY_CHECKS=1");
+      $proteomeView->touchStatus('updateProteomeArch');
+    }
+    
+    if(! $proteomeView->statusCheck('updateProteomeStats')){
+      $protDbh->do("UPDATE complete_proteomes c ".
+                   "SET num_total_regions = ( ".
+                   "SELECT sum(count) FROM proteome_regions r ".
+                   "WHERE r.auto_proteome=c.auto_proteome)");
+      
+      $protDbh->do("UPDATE complete_proteomes c ".
+                    "SET num_proteins= ( SELECT count( distinct r.auto_pfamseq) ".
+                    "FROM proteome_regions r ".
+                    "WHERE r.auto_proteome=c.auto_proteome)") ;
+                    
+      $protDbh->do("UPDATE complete_proteomes c SET total_seqs_covered = ( ".
+                    "SELECT count( distinct r.auto_pfamseq) FROM proteome_regions r ".
+                    "WHERE r.auto_proteome=c.auto_proteome)") ;
+      $protDbh->do("UPDATE complete_proteomes SET ".
+                    "sequence_coverage = ((total_seqs_covered/total_genome_proteins)*100)");
+      $protDbh->do("UPDATE complete_proteomes c ".
+                    "SET total_aa_length=( ".
+                    "SELECT sum(length) ".
+                    "FROM proteome_pfamseq p , pfamseq s ".
+                    "WHERE s.auto_pfamseq=p.auto_pfamseq ".
+                    "AND p.auto_proteome=c.auto_proteome)");
+                    
+      $protDbh->do("UPDATE complete_proteomes c SET total_aa_covered=( ".
+                    "SELECT sum(seq_end - seq_start + 1) ".
+                    "FROM proteome_pfamseq p, pfamA_reg_full_significant s ".
+                    "WHERE s.auto_pfamseq=p.auto_pfamseq ". 
+                    "AND p.auto_proteome=c.auto_proteome and in_full=1)");
+                    
+      $protDbh->do("UPDATE complete_proteomes ".
+                   "SET residue_coverage = ((total_aa_covered/total_aa_length)*100)");
+      
+      $protDbh->do("DELETE FROM complete_proteomes WHERE num_proteins=0");
+      
+      $proteomeView->touchStatus('updateProteomeStats');
+    }
+    
+    if(! $proteomeView->statusCheck('updateProteomeGrp')){
+      my @rs = $proteomeView->pfamdb
+                              ->getSchema
+                                ->resultset('Taxonomy')
+                                  ->search({ rank => 'superkingdom' });
+      my $sth = $protDbh->prepare("UPDATE complete_proteomes c, taxonomy t ".
+                                    "SET grouping=? ".
+                                    "WHERE c.ncbi_taxid=t.ncbi_taxid and t.lft> ? and t.rgt< ? ");
+      foreach my $row (@rs){
+        $sth->execute($row->level, $row->lft, $row->rgt);
+      }
+      my $rs = $proteomeView->pfamdb
+                              ->getSchema
+                                ->resultset('Taxonomy')
+                                  ->search({ grouping => 'NULL' });
+      if($rs->count > 0){
+        $proteomeView->logger->logdie("Some complete_proteomes have grouping of null!");
+      }
+      $proteomeView->touchStatus('updateProteomeGrp');
+    }
+    $proteomeView->touchStatus('doneProteome');
+  }
   
+  if(! $proteomeView->statusCheck('doneProteomeTSV')){
+    #Each proteome to be searched
+    $proteomeView->submitToFarm(75);
+    #
+    $proteomeView->touchStatus('doneProteomeTSV');
+  }
 }
 
 
@@ -76,19 +203,3 @@ if(exists($archView->options->{acc}) and $archView->options->{acc}){
 
 #Now get a list of all sequences belongs to these families 
 
-#Generate the storables for each sequence
-
-#Look to see if the architecture is known for this sequence.  If not add it to the architecture table.
-
-#Foreach family update the number of architectures
-
-#Foreach clan update the number of architectures
-
-# Now add in Kristofers bit to add the domain order
-
-#-------------------------------------------------------------------------------
-#Now on to the profile:profile comparisons
-#
-# Run hhsearch.
-
-# 
