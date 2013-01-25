535f6f1090edb1cae6c2eba98e8a1b0ebd118f49
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-01-25 15:44:23 +0000

    Templates for objects that will represent each file in Rfam.

---
+++ Rfam/Lib/Bio/Rfam/Family/CM.pm
@@ -0,0 +1,34 @@
+=head1 NAME
+
+MODULENAME - a module that 
+
+=cut
+
+package Bio::Rfam::Family::CM;
+
+=head1 DESCRIPTION
+
+A more detailed description of what this class does and how it does it.
+
+=head1 COPYRIGHT
+
+File: CM.pm 
+
+Copyright (c) 2013: 
+
+
+Author: Rob Finn (rdf 'at' ebi.ac.uk or finnr 'at' janelia.hhmi.org)
+Incept: finnr, Jan 25, 2013 8:32:16 AM
+
+=cut
+
+use strict;
+use warnings;
+
+#-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+=cut
+
+1;
