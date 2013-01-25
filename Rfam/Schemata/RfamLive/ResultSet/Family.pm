71bd635e559e16e3251f76d65bdbb3818da60a3e
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-01-25 02:10:46 +0000

    Trying to push some methods on to the schemata.

---
+++ Rfam/Schemata/RfamLive/ResultSet/Family.pm
@@ -0,0 +1,14 @@
+package RfamLive::ResultSet::Family;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+sub id2acc {
+  my ($self, $id) = @_;
+
+  my $fam = $self->find({rfam_id=> $id});
+
+  return $fam->rfam_acc();
+}
