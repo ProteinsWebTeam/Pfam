887c9d7951662a378f6763be7a4f476f40a79440
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-01-31 09:49:36 +0000

    Template for Rfam QC.

---
+++ Rfam/Lib/Bio/Rfam/QC.pm
@@ -0,0 +1,34 @@
+=head1 NAME
+
+Bio::Rfam::QC - a module that performs QC on Rfam data.
+
+=cut
+
+package Bio::Rfam::QC;
+
+=head1 DESCRIPTION
+
+A more detailed description of what this class does and how it does it.
+
+=head1 COPYRIGHT
+
+File: QC.pm 
+
+Copyright (c) 2013: 
+
+
+Author: Rob Finn (rdf 'at' ebi.ac.uk or finnr 'at' janelia.hhmi.org)
+Incept: finnr, Jan 25, 2013 8:43:56 AM
+
+=cut
+
+use strict;
+use warnings;
+
+#-------------------------------------------------------------------------------
+
+=head1 METHODS
+
+=cut
+
+1;
