31e366213afcee1c6ff4e8a1b2a54daa444e316b
nawrockie <nawrockie@98333f76-0502-11df-81af-936bbf3a1e26>
2013-02-01 23:00:54 +0000

    Added code to output average percent id, sqlen of SEED, plus output 'seed-annot.sto', an annotated version of the seed, with annotation from DESC.

--- trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Alignment.pm
+++ trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Alignment.pm
@@ -9,10 +9,52 @@ has foo => (
 );
 
 sub process {
-  my $this = shift;
-  print "In Bio::Rfam::View::Plugin::Alignment::process\n";
-  print 'Family alignment stuff ' . $this->parent->family->DESC->AC . "\n";
+  my $self   = shift;
+  my $family = $self->parent->family;
+  my $msa    = $family->SEED;
+  my $desc   = $family->DESC;
+
+  write_avg_pid   ($msa);
+  write_avg_sqlen ($msa);
+  write_seed_annot($msa, "seed-annot.sto");
+
+  # TODO: output 'seed-annot-species.sto', same as 'seed-annot..sto'
+  # but with species names
+}
+sub write_avg_pid { 
+    my $msa = shift;
+    printf("Average ID:     %5.2f\n", ($msa->average_pid * 100));
+    return;
 }
 
-1;
+sub write_avg_sqlen { 
+    my $msa = shift;
+    printf("Average length: %.2f\n",  ($msa->average_sqlen));
+    return;
+}
+
+sub write_seed_annot {
+    my $msa  = shift;
+    my $desc = shift;
+
+    my $io = Bio::Rfam::FamilyIO->new;
+
+    # TODO, write writeDESC2Array function, no need to output to file
+    $io->writeDESC($desc);
+    open(IN, "DESC") || die "ERROR unable to open DESC"; 
+    my $line;
+    my $tag;
+    my $value;
+    while($line = <IN>) { 
+	chomp $line;
+	if($line =~ /(^\S+)\s+(.*$)/) { 
+	    ($tag, $value) = ($1, $2);
+	    $msa->addGF($tag, $value);
+	}
+	else { 
+	    die "ERROR unable to parse DESC line $line"; 
+	}
+    }
+    $msa->write_msa("$family.sto.ann");
+}
 1;
