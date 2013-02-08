a936cff6f5e9bb1b683f5d49ee5dd165cea56597
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-02-08 19:07:20 +0000

    First pass at code for adding a new Rfam entry into the repository.

---
+++ Rfam/Scripts/svn/rfnew.pl
@@ -0,0 +1,220 @@
+#!/usr/bin/env perl
+#
+# rfnew.pl - This script allows one to check in a family to the SVN repository
+# containing rfam families. The family that you wish to add to the database
+# should be passed in as a parameter.
+#
+
+use strict;
+use warnings;
+use Cwd;
+use Data::Printer;
+use Getopt::Long;
+
+use Bio::Rfam::SVN::Client;
+use Bio::Rfam::FamilyIO;
+use Bio::Rfam::QC;
+
+#-------------------------------------------------------------------------------
+# Deal with all of the options
+
+my ( $message, $ignore, $addToClan, $help );
+
+&GetOptions(
+  "m=s"         => \$message,
+  "i"           => \$ignore,
+  "help"        => \$help
+) or die "Error fetching options\n";
+
+my $family = shift;
+
+unless ($family) {
+  warn "\n***** No family passed  *****\n\n";
+  help();
+}
+chomp($family);
+
+if (@ARGV) {
+  warn "\n***** $0 does not support multiple family check-ins *****\n\n";
+  help();
+}
+
+help() if ($help);
+
+#-------------------------------------------------------------------------------
+my $pwd = getcwd;
+
+if ( !( -d "$pwd/$family" ) ) {
+  die
+"$0: [$pwd/$family] is not a current directory.\nMust be in the parent directory of the family to check in\n";
+}
+
+if ( !-w "$pwd/$family" ) {
+  die
+    "$0: I can't write to directory [$pwd/$family].  Check the permissions.\n";
+}
+
+#-------------------------------------------------------------------------------
+# If a message is supplied, then write it to file such that the code reference
+# that deals with the SVN log message can grab it.
+
+if ( -s ".default" . $$ . "rfnew" ) {
+  unlink( ".default" . $$ . "rfnew" )
+    or die "Could not remove old default check-in message\n";
+}
+
+#-------------------------------------------------------------------------------
+#Initial SVN stuff
+
+my $config = Bio::Rfam::Config->new;
+
+#Check that family exists in svn
+my $client = Bio::Rfam::SVN::Client->new;
+
+#-------------------------------------------------------------------------------
+#First QC step is to check the timestamps on the files
+
+if ( !Bio::Rfam::QC::checkModelFiles($family) ) {
+  print "$0: $family contains errors.  You should rebuild this family.\n";
+  exit(1);
+}
+
+#
+#-------------------------------------------------------------------------------
+# Load the family from disk and svn through the middleware
+
+my $familyIO = Bio::Rfam::FamilyIO->new;
+my $newFamObj = $familyIO->loadRfamFromLocalFile( $family, $pwd );
+print STDERR "Successfully loaded $family through middleware\n";
+
+
+#-------------------------------------------------------------------------------
+#Check DESC file for ID/AC 
+
+if ( $newFamObj->DESC->AC ) {
+  die
+"Your family appears to have an accession, but you are using rfnew! Either remove and "
+    . "let the database automatically assign the accession or use rfci\n";
+}
+
+unless ( $newFamObj->DESC->ID ) {
+  die "Your family does not appear have an identifier!"
+    . "The check-in process will automatically assign the accession "
+    . " and position it in the repository for you!\n";
+}
+
+#-------------------------------------------------------------------------------
+#Check that the pending model does ot already exist. Parnoid check as new models
+#should be removed immediately into the main respository.
+
+$client->checkNewFamilyDoesNotExists( $newFamObj->DESC->ID );
+
+#-------------------------------------------------------------------------------
+
+#These are more sanity checks
+unless ($ignore) {
+
+  #If we are at sanger, perform an overlap check against the database.
+  if ( $config->location eq "EBI" ) {
+    my $rfamdb= $config->rfamlive;
+
+    #Find out if family is in rdb
+    #my $rdb_family = $dfamDB->getDfamData($family);
+    #my %ignore;
+
+    #Need to populate the ignore hash with clan and nesting data......
+
+    my $overlaps =
+      &Bio::Rfam::QC::family_overlaps_with_db( );
+      #$family, \%ignore, undef,
+    #  $dfamDB, $newFamObj );
+    if ($overlaps) {
+      print "Looks like your family contains overlaps.\n";
+      exit(1);
+    }
+  }
+
+  Bio::Rfam::QC::checkDESCSpell( $family, $familyIO );
+
+  unless ( Bio::Rfam::QC::sequenceChecker( $family, $newFamObj ) ) {
+    print "dfnew: $family contains errors.  You should rebuild this family.\n";
+    exit(1);
+  }
+
+  #rqc-check $family
+  #unless ( Bio::Rfam::QC::noFragsInSeed( $family, $newFamObj ) ) {
+  #  exit(1);
+  #}
+
+  #unless ( Bio::Rfam::QC::nonRaggedSeed( $family, $newFamObj ) ) {
+  #  exit;
+  #}
+}
+
+#NEED TO CHECK THAT ASSURTIONS COVER ALL FORMAT CHECKS.....
+unless ( Bio::Rfam::QC::passesAllFormatChecks( $newFamObj, $family ) ) {
+  exit(1);
+}
+
+#Automatically write the 'new' message and add it the binding.
+open( M, ">.default" . $$ . "rfnew" )
+  or die "Could not open .default" . $$ . "rfnew:[$!]\n";
+print M $newFamObj->DESC->ID . " deposited\n";
+close M;
+$client->addRFNEWLog();
+
+#-------------------------------------------------------------------------------
+#If we get here, then great! We can now add the family!
+my $caught_cntrl_c;
+$SIG{INT} = sub { $caught_cntrl_c = 1; };    # don't allow control C for a bit!
+
+$client->addFamily( $family, $newFamObj->DESC->ID );
+
+#Remove any file containing the check-in message
+if ( -s ".default" . $$ . "rfnew" ) {
+  unlink( ".default" . $$ . "rfnew" )
+    or die "Could not remove old default check-in message\n";
+}
+
+#
+if ($caught_cntrl_c) {
+  print STDERR "\n** You hit cntrl-c while the operation was in progress.".
+               "\n** The script has tried to ignore this and recover".
+               "\n** but this could be very bad.  You really must tell ".
+               "\n** someone about this so they can check everything is okay!\n";
+}
+
+
+#It may be nice to report the accession of the new family.....
+#This will only work where the database is based, as this is the only place 
+#where the database sits. Could replace with a webservice!
+
+if ( $config->location eq "EBI" ) {
+  my $rfamdb   = $config->rfamlive;
+  my $rfamEntry = $rfamdb->resultset('Family')->find({rfam_id => $newFamObj->DESC->ID });
+  print STDERR "This family has been asssigned the accession:"
+    . $rfamEntry->rfam_acc . "\n"
+    if ( $rfamEntry->rfam_acc );
+}
+
+exit(0);
+
+sub help {
+  
+print<<EOF;
+
+  usage: $0 <directory>
+  
+  Where the directory contains the files that consitute a Rfam entry.
+  
+  Aim: To perform quality control checks on a new family and add it to the SVN repository.
+  
+  -i                - Ignore some of the QC steps to speed up check-in.
+  -m                - Specify the message that describes the changes you have made to this family 
+                      on the command line, avoid being prompted for it at a later satge.                   
+                      
+EOF
+
+exit(1);
+
+}
---
+++ Rfam/Scripts/svnhooks/post-commit.pl
@@ -0,0 +1,155 @@
+#!/usr/bin/env perl
+#
+# This script is run by the svn repository as part of the post-commmit hook. The
+# methods in this script deal automatically adding accessions to the respository 
+# and moving new entries into the repository.
+#
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Bio::Rfam::Config;
+use Bio::Rfam::SVN::Commit;
+use Bio::Rfam::SVN::Client;
+use Mail::Mailer;
+use Cwd;
+
+my ( $rev, $repos );
+GetOptions(
+  "rev=s"   => \$rev,
+  "repos=s" => \$repos
+);
+
+my $revlook;
+eval {
+  unless ( $rev and $repos )
+  {
+    die "Need to define both rev and repos: try $0 -help";
+  }
+
+  my %params;
+  $params{rev}   = $rev;
+  $params{repos} = $repos;
+
+  $revlook = Bio::Rfam::SVN::Commit->new( \%params );
+  unless ( $revlook and $revlook->isa('SVN::Look') ) {
+    die "Failed to get a SVN::Look object for rev:$rev and repos:$repos\n";
+  }
+
+  my $config = Bio::Rfam::Config->new;
+
+  #Now see what has just been done for this revision.
+  my $logMessage = $revlook->log_msg;
+  print STDERR $logMessage."\n";
+  # The following actions will trigger a SVN post-commit
+  #------------------------------------------------------
+
+# 1. Adding a new model to the repository.
+#    This will add the accession to the DESC file and trigger off the view process.
+
+  if ( $logMessage =~ /^NEW\:(\S+)/ ) {
+    
+    my $family = $1;
+    
+    print STDERR "Working on $family\n";
+    
+    my $rfamdb = $config->rfamlive;
+    my $entry = $rfamdb->resultset('Family')->find({rfam_id => $family });
+    
+    
+    my $client = Bio::Rfam::SVN::Client->new;
+    my $cwd = getcwd;
+    
+    my $tmpDir = File::Temp->newdir( 'CLEANUP' => 1 );
+    
+    #Move the entry
+    my $dest = $tmpDir->dirname;
+    chdir($dest) or die "Could not change into $dest dir:$!";
+    open( M, ">.default" . $$ . "rfnewmove" )
+      or die "Could not open $dest/.default" . $$ . "rfnewmove:$!";
+    print M "Moving from pending to main repository.";
+    close(M);
+    print STDERR $entry->rfam_acc."\n";
+    $client->addRFNEWMOVELog;
+    $client->moveNewEntry( $family, $entry->rfam_acc );
+
+    #Now checkout and automatically add the accession to the DESC file
+    open( M, ">.default" . $$ . "rfnewmove" )
+      or die "Could not open $dest/.default" . $$ . "rfnewmove:$!";
+    print M "Automatically adding accession.";
+    close(M);
+    $client->addRFNEWMOVELog;
+    #Now checkout and add the accession to the DESC file!
+    $client->checkoutFamily( $entry->rfam_acc, $dest );
+
+    #parse the DESC file
+    my $familyIO = Bio::Rfam::FamilyIO->new;
+    my $descObj = $familyIO->parseDESC("$dest/DESC");
+    $descObj->AC( $entry->rfam_acc );
+    $familyIO->writeDESC( $descObj, $dest );
+
+    #Commit back in
+    $client->commitFamily($dest);
+    chdir($cwd);
+  }
+  else {
+    #No other commits require post-commit processing
+    ;
+  }
+};
+
+if ($@) {
+  
+  my %header = (
+    To      => 'finnr@janelia.hhmi.org',
+    From    => 'rfam@ebi.ac.uk',
+    Subject => 'Error in post-commit '
+  );
+  $header{Cc} = $revlook->author . '@ebi.ac.uk' if ($revlook);
+
+  my $message = "Error with the post commit\n";
+  $message .= "Revision: $rev "    if ($rev);
+  $message .= "Repository: $repos" if ($repos);
+  $message .= "\n$@\n";
+  
+  my $mailer = Mail::Mailer->new();
+  $mailer->open( \%header );
+  print $mailer $message;
+  $mailer->close;
+
+  exit(1);
+}
+
+=head1 COPYRIGHT
+
+File: post-commit.pl
+
+Author: Rob Finn (finnr@janelia.hhmi.org)
+
+Copyright (c) 2010, Howard Hughes Medical Institute, All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, 
+   this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright notice, 
+   this list of conditions and the following disclaimer in the documentation 
+   and/or other materials provided with the distribution.
+   3. Neither the name of the Howard Hughes Medical Institute nor the names of 
+   its contributors may be used to endorse or promote products derived from 
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, ANY 
+IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A 
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; REASONABLE ROYALTIES; 
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+SUCH DAMAGE. 
+
+=cut
