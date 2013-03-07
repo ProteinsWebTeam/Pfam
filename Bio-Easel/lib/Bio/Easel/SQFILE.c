5faa07be15ac6def341a1c8e3f75f57527a48e1c
nawrockie <nawrockie@98333f76-0502-11df-81af-936bbf3a1e26>
2013-03-07 18:56:43 +0000

    Extended SQFILE.c::_c_fetch_seq_to_string() to allow unlimited or arbitrary line lengths, and added informative 'croak' statements for errors.

--- Bio-Easel/lib/Bio/Easel/SQFILE.c
+++ Bio-Easel/lib/Bio/Easel/SQFILE.c
@@ -64,57 +64,92 @@ int _c_open_ssi (ESL_SQFILE *sqfp)
 
 /* Function:  _c_fetch_seq_to_fasta_string()
  * Incept:    EPN, Mon Mar  4 15:03:11 2013
- * Synopsis:  Fetch a sequence from an open sequence file and return it as a string.
+ * Synopsis:  Fetch a sequence from an open sequence file and return it as a FASTA
+ *            formatted string.
+ * Args:      sqfp  - open ESL_SQFILE to fetch seq from
+ *            key   - name or accession of sequence to fetch
+ *            textw - width for each sequence of FASTA record, -1 for unlimited.
  * Returns:   A pointer to a string that is the sequence in FASTA format.
  */
 
-char *_c_fetch_seq_to_fasta_string (ESL_SQFILE *sqfp, char *key)
+char *_c_fetch_seq_to_fasta_string (ESL_SQFILE *sqfp, char *key, int textw)
 {
+
   int     status;                /* Easel status code */
   ESL_SQ *sq = esl_sq_Create();  /* the sequence */
   char   *seqstring = NULL;      /* the sequence string */
-  int     n  = 0;                /* position in string */
-  int     n2 = 0;                /* position in string */
+  int     n   = 0;               /* position in string */
+  int     n2  = 0;               /* position in string */
+  int     pos = 0;               /* position in sq->seq */
 
-  /* make sure we're not in digital mode */
-  if(sq->dsq)                       goto ERROR; 
-  if (sqfp->data.ascii.ssi == NULL) goto ERROR;
+  /* make sure textw makes sense */
+  if(textw < 0 && textw != -1) croak("invalid value for textw\n"); 
+  /* make sure we're not in digital mode, and SSI is valid */
+  if (sq->dsq)                      croak("sequence file is unexpectedly digitized\n");
+  if (sqfp->data.ascii.ssi == NULL) croak("sequence file has no SSI information\n"); 
 
   /* from esl-sfetch.c's onefetch(), caller will output informative error message based on status returned here */
   status = esl_sqfile_PositionByKey(sqfp, key);
-  if      (status == eslENOTFOUND) goto ERROR;
-  else if (status == eslEFORMAT)   goto ERROR;
-  else if (status != eslOK)        goto ERROR;
+  if      (status == eslENOTFOUND) croak("seq %s not found in SSI index for file %s\n", key, sqfp->filename); 
+  else if (status == eslEFORMAT)   croak("Failed to parse SSI index for %s\n", sqfp->filename);
+  else if (status != eslOK)        croak("Failed to look up location of seq %s in SSI index of file %s\n", key, sqfp->filename);
     
   status = esl_sqio_Read(sqfp, sq);
-  if      (status == eslEFORMAT) goto ERROR;
-  else if (status == eslEOF)     goto ERROR;
-  else if (status != eslOK)      goto ERROR;
+  if      (status == eslEFORMAT) croak("Parse failed (sequence file %s):\n%s\n",  sqfp->filename, esl_sqfile_GetErrorBuf(sqfp));
+  else if (status == eslEOF)     croak("Unexpected EOF reading sequence file %s\n", sqfp->filename);
+  else if (status != eslOK)      croak("Unexpected error %d reading sequence file %s\n", status, sqfp->filename);
 
-  if (strcmp(key, sq->name) != 0 && strcmp(key, sq->acc) != 0) goto ERROR;
+  if (strcmp(key, sq->name) != 0 && strcmp(key, sq->acc) != 0) 
+    croak("whoa, internal error; found the wrong sequence %s, not %s\n", sq->name, key);
     
   /* create seqstring  */
-  n = strlen(sq->name);
-  if (esl_strdup(sq->name, n, &seqstring) != eslOK) goto ERROR;
-  if (sq->acc) { 
-    n2 = strlen(sq->acc);
-    if (esl_strcat(&seqstring, n, sq->acc, n2) != eslOK) goto ERROR;
-    n += n2;
-  }
-  if (sq->desc) { 
-    n2 = strlen(sq->desc);
-    if (esl_strcat(&seqstring, n, sq->desc, n2) != eslOK) goto ERROR;
-    n += n2;
-  }
-  /* add newline */
-  if (esl_strcat(&seqstring, n, "\n", 1) != eslOK) goto ERROR;
+  /* '>' character */
+  n = 0;
+  if (esl_strdup(">", 1, &seqstring)              != eslOK) croak("out of memory while fetching sequence %s\n", key);
   n++;
-  if (esl_strcat(&seqstring, n, sq->seq, sq->n) != eslOK) goto ERROR;
-
+  /* name */
+  n2 = strlen(sq->name);
+  if (esl_strcat(&seqstring, n, sq->name, n2)     != eslOK) croak("out of memory while fetching sequence %s\n", key);
+  n += n2;
+  /* accession */
+  if (sq->acc[0] != 0) { 
+    if (esl_strcat(&seqstring, n, " ", 1)         != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n++;
+    n2 = strlen(sq->acc);
+    if (esl_strcat(&seqstring, n, sq->acc, n2)    != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n += n2;
+  }
+  /* desc */
+  if (sq->desc[0] != 0) { 
+    if (esl_strcat(&seqstring, n, " ", 1)         != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n++;
+    n2 = strlen(sq->desc);
+    if (esl_strcat(&seqstring, n, sq->desc, n2)   != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n += n2;
+  }
+  /* newline */
+  if (esl_strcat(&seqstring, n, "\n", 1)          != eslOK) croak("out of memory while fetching sequence %s\n", key);
+  n++;
+  /* sequence */
+  if (textw == -1) { /* unlimited line length */
+    if (esl_strcat(&seqstring, n, sq->seq, sq->n) != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n += sq->n;
+    /* newline */
+    if (esl_strcat(&seqstring, n, "\n", 1)        != eslOK) croak("out of memory while fetching sequence %s\n", key);
+    n++;
+  }
+  else { /* textw != -1, limit line length to textw */
+    for (pos = 0; pos < sq->n; pos += textw) { 
+      n2 = ESL_MIN(textw, sq->n - pos);
+      if (esl_strcat(&seqstring, n, sq->seq+pos, n2) != eslOK) croak("out of memory while fetching sequence %s\n", key);
+      n += n2;
+      /* newline */
+      if (esl_strcat(&seqstring, n, "\n", 1) != eslOK) croak("out of memory while fetching sequence %s\n", key);
+      n++;
+    }
+  }
+  /* fprintf(stderr, "in C, seqstring: %s\n", seqstring); */
   esl_sq_Destroy(sq);
 
   return seqstring;
-
- ERROR: 
-  return NULL;
 }    
--- Bio-Easel/t/05-sqfile-use.t
+++ Bio-Easel/t/05-sqfile-use.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings FATAL => 'all';
-use Test::More tests => 5;
+use Test::More tests => 6;
 
 
 BEGIN {
@@ -22,8 +22,13 @@ isa_ok($sqfile2, "ESL_SQFILE");
 my $path = $sqfile->path;
 is($path, "./t/data/test.fa");
 
-# test fetch_seq_to_fasta_string
+# test fetch_seq_to_fasta_string with no line length
 my $seqstring = $sqfile->fetch_seq_to_fasta_string("CP000857.1/1802194-1802277");
-is ($seqstring, "CP000857.1/1802194-1802277\nCUCACAUCAGAUUUCCUGGUGUAACGAAUUUUCAAGUGCUUCUUGCAUAAGCAAGUUUGAUCCCGACCCGUAGGGCCGGGAUUU");
+is ($seqstring, ">CP000857.1/1802194-1802277\nCUCACAUCAGAUUUCCUGGUGUAACGAAUUUUCAAGUGCUUCUUGCAUAAGCAAGUUUGA\nUCCCGACCCGUAGGGCCGGGAUUU\n");
+
+# test fetch_seq_to_fasta_string with unlimited line length
+$seqstring = $sqfile->fetch_seq_to_fasta_string("CP000857.1/1802194-1802277", -1);
+is ($seqstring, ">CP000857.1/1802194-1802277\nCUCACAUCAGAUUUCCUGGUGUAACGAAUUUUCAAGUGCUUCUUGCAUAAGCAAGUUUGAUCCCGACCCGUAGGGCCGGGAUUU\n");
+
 
 
