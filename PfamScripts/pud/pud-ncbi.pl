bf47e3ce63af7bba2e3961c6656f75b48a4310ae
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-08-28 21:21:21 +0000

    Removed a load of crappy old stuff that broke the release. We only need to download the file now.

--- PfamScripts/pud/pud-ncbi.pl
+++ PfamScripts/pud/pud-ncbi.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Cwd;
 use LWP::Simple;
 use Log::Log4perl qw( :easy );
 use Data::Dump qw( dump );
@@ -9,13 +10,16 @@ use Digest::MD5 qw( md5_hex );
 use Config::General qw( SaveConfig );
 use Getopt::Long;
 use File::Copy;
+
 use Bio::Pfam::Config;
+use Bio::Pfam::PfamLiveDBManager;
 # get the version of pfam as a command line argument;
 
-my $VERSION;
+my ($version, $skip);
 
 GetOptions (
-  'version=i' =>  \$VERSION 
+  'version=i' =>  \$version,
+  'skip'      =>  \$skip
 ); 
 
 
@@ -23,56 +27,72 @@ GetOptions (
 Log::Log4perl->easy_init();
 my $logger = get_logger();
 
-unless($VERSION){
+unless($version){
   $logger->logdie("You need to provide the version number as a parameter");  
 }
 
 # get the config file for the pfam.
 my $config = Bio::Pfam::Config->new;
+my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamliveAdmin });
+my $dbh = $pfamDB->getSchema->storage->dbh;
 
-my $NCBI_PATH = 'ftp://ftp.ncbi.nih.gov/ncbi-asn1/protein_fasta';
+my $NCBI_PATH = 'ftp://ftp.ncbi.nih.gov/ncbi-asn1';
 my $store_dir = $config->localDbsLoc."/ncbi" || $logger->logdie( "cant get the localDbsLoc from the config file;");
 my $production_location = $config->{ productionLocation } || $logger->logdie( "cant get productionLocation from config file");
 my $nfs_location = $config->{ ncbi }->{ location } || $logger->logdie( "cant get the location( nfs) from the config file");
+my $pfamseqDir = $production_location."/pfamseq".$version;
+my $pfamseqNcbiFile = $pfamseqDir.'/ncbi';
+my $pfamseqNcbiFileIdx = $pfamseqNcbiFile.'.ssi';
 
 $logger->debug( "Production-Location :$production_location |nfs-location: $nfs_location ");
 
-# downloading the ncbi_index file using get method;
-my $content = get( $NCBI_PATH ) || $logger->logdie( "cant download the Ncbi_index file from the path: $NCBI_PATH");
-  
-$logger->info( "Downloading file release number.....");
-getstore( $NCBI_PATH."/GB_Release_Number","$store_dir/GB_Release_Number" ) || $logger->logdie( "cant store the file GB_Release_Number in $store_dir/GB_Release_Number");    
+$logger->info( "Downloading file release number.....[ $store_dir]");
+getstore( $NCBI_PATH."/GB_Release_Number","$store_dir/GB_Release_Number" ) 
+  || $logger->logdie( "cant store the file GB_Release_Number in $store_dir/GB_Release_Number");    
 
-$logger->info( "Downloading file nr.gz.....");
-$NCBI_PATH = 'ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/';
-mirror($NCBI_PATH."/nr.gz", "$store_dir/nr.gz") || $logger->logdie("Can not store nr.gz in $store_dir/nr.gz");
-$logger->info("downloaded nr.");
-# when it reaches here all the files are present in the ncbi directory and now parse each file to get the fasta sequence;
-
-my $counter = 1;
-my $total_seqs = {};
+#Download the fasta file from NCBI
+my $file = "$store_dir/nr.gz";
+unless($skip){
+  $logger->info( "Downloading file nr.gz.....");
+  $NCBI_PATH = 'ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/';
+  getstore($NCBI_PATH."/nr.gz", $file) 
+    || $logger->logdie("Can not store nr.gz in $file");
+  $logger->info("downloaded nr.");
+}
 
 # now get all the files from the directory and parse it to append the fasta sequences to make a single file.
-my $file = "$store_dir/nr.gz";
-$logger->info( "now working on file number $counter: $file");
-
-#This separates out the NR header lines into one per sequence..... 
-my $num_seq = gz2string( $file, $logger );
-  
-
-# now index the file using esl-sfetch;
-chdir("$production_location/pfamseq$VERSION") or 
-  $logger->logdie("Could not chdir to $production_location/pfamseq$VERSION:[$!]"); 
-if(-e "ncbi.ssi"){
-  unlink("ncbi.ssi");
+#This separates out the NR header lines into one per sequence.....
+if(!-e $pfamseqNcbiFile){
+  parseAndWriteFasta( $file, $logger, $pfamseqDir );
 }
-system( "esl-sfetch --index ncbi ") and $logger->logdie( "cant index the file using esl-sfetch");
+
+if(! -e $pfamseqNcbiFileIdx){
+  # now index the file using esl-sfetch;
+  my $cwd = getcwd;
+  chdir($pfamseqDir) or 
+    $logger->logdie("Could not chdir to $pfamseqDir:[$!]");
+  
+  system( "esl-sfetch --index ncbi ") 
+    and $logger->logdie( "cant index the file using esl-sfetch");
+}
+
+my $numberSeqs = 0;
+open(STATS, "esl-seqstat $pfamseqNcbiFile |") or 
+  $logger->logdie("Could not run esl-seqstat on $pfamseqNcbiFile:[$!]");
+while(<STATS>){
+  chomp;
+  if(/Number of sequences\:\s+(\d+)/){
+    $numberSeqs = $1;
+    last;
+  }
+}
+close(STATS);
 
 # copy the sequence file, index file and dat file to /nfs/pfam_nfs/pfam/ncbi/
-copy( "$production_location/pfamseq".$VERSION."/ncbi","$nfs_location/ncbi") || 
-  $logger->logdie( "cant copy the file $production_location/pfamseq.$VERSION/ncbi");
-copy( "$production_location/pfamseq".$VERSION."/ncbi.ssi","$nfs_location/ncbi.ssi") || 
-  $logger->logdie( "cant copy the file $production_location/pfamseq.$VERSION/ncbi.ssi");
+copy( $pfamseqNcbiFile,"$nfs_location/ncbi") || 
+  $logger->logdie( "cant copy the file $pfamseqNcbiFile");
+copy( $pfamseqNcbiFileIdx,"$nfs_location/ncbi.ssi") || 
+  $logger->logdie( "cant copy the file $pfamseqNcbiFile");
 
 # now change the config file;
 $logger->info("Changing pfam config file\n");
@@ -81,8 +101,8 @@ $logger->info("Changing pfam config file\n");
   my %ac = $configNew->getall; 
   
   my $pfam_config = $ENV{PFAM_CONFIG};
-  move( $pfam_config, "$pfam_config.$$.old") || $logger->logdie("Could not move config file");;
-  $ac{ ncbi }->{ dbsize } = $num_seq; 
+  copy( $pfam_config, "$pfam_config.$$.old") || $logger->logdie("Could not move config file");;
+  $ac{ ncbi }->{ dbsize } = $numberSeqs; 
 
   SaveConfig( $pfam_config, \%ac );
   
@@ -101,15 +121,15 @@ exit;
 
 
 #---------------------------------------------------------------------------------------------------------------------------------
-# subroutine gz2string uncompress the fasta file and parses the contents to create two kinds of output,
+# subroutine uncompress the fasta file and parses the contents to create two kinds of output,
 # as tab delimited file and other as fasta file with all sequences in single file.
 
-sub gz2string {
-  my ( $file, $logger ) = @_;
-
-  open( OUT1,">$production_location/pfamseq".$VERSION."/ncbi") || 
+sub parseAndWriteFasta {
+  my ( $file, $logger, $dir ) = @_;
+  
+  open( OUT1,">$dir/ncbi") || 
     $logger->logdie( "cant open the file ncbi for writing");
-  my($head, $sequence, $gis, $line_count);
+  my($head, $sequence, $gi_lines, $seq_lines, $line_count);
   open( IN, "gunzip -c $file | ") || $logger->logdie( "cant open the file $file for reading ");
   while( <IN> ){
     $line_count++;
@@ -119,8 +139,9 @@ sub gz2string {
       
       my $ter_acc = '\N';
       if(defined($head) and defined($sequence)){ 
-        $gis += parseAndWrite($head, $sequence, \*OUT1);
-        
+        parseAndWrite($head, $sequence, \*OUT1);
+        $gi_lines++;
+        $seq_lines++;
       }
       $sequence = undef;
       $head = $_;
@@ -133,34 +154,32 @@ sub gz2string {
     
   } # end of while
   close IN;
-  $gis += parseAndWrite($head, $sequence, \*OUT1)
+  parseAndWrite($head, $sequence, \*OUT1)
       if(defined($head) and defined($sequence));
   close OUT1;
-  return $gis;
+  return ( $gi_lines,$seq_lines ) ;
 }
 
 sub parseAndWrite {
-  my($head, $sequence, $fh) = @_; 
+  my($head, $sequence, $fh1) = @_; 
   
   my $md5 = md5_hex( $sequence ) if( defined $sequence );
   my $l = length( $sequence );
   my @heads = split( /\001/, $head );
   my @pheads;
-  my $count =0;
   foreach my $h (@heads) {
     if ( my ( $gi, $gb_prefix, $gb, $ter_acc, $desc, $os ) =
       $h =~ /^\S?gi\|(\d+)\|(\S+)\|(\S+)?\|(\S+)? (.*?)(\[.*\])?$/ )
     {
-      $gb = ($gb ? $gb : '');
+      $ter_acc = ($ter_acc ? $ter_acc : '\N');
+      $gb = ($gb ? $gb : '\N');
       $desc .= " $os" if($os); 
       #print out redundant fasta file:
-      print $fh ">$gi $gb_prefix|$gb $desc\n$sequence\n";
-      $count++;
+      print $fh1 ">$gi $gb_prefix|$gb $desc\n$sequence\n";
     }
     else {
       die "NR header $h did not match\n";
     }
     last;
   }
-  return $count;
 }
