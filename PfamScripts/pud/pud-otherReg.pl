8098a037d6202eda3f59ca98d0a0e3267ef6bd5e
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-08-29 14:44:42 +0000

    Adding in some code from last release that was ommitted.

--- PfamScripts/pud/pud-otherReg.pl
+++ PfamScripts/pud/pud-otherReg.pl
@@ -8,6 +8,8 @@ use IO::File;
 use Sys::Hostname;
 use Data::UUID;
 use Net::SCP;
+use Getopt::Long;
+use File::Touch;
 
 use Bio::Pfam::Config;
 use Bio::Pfam::PfamLiveDBManager;
@@ -15,66 +17,108 @@ use Bio::Pfam::PfamLiveDBManager;
 #Get the current working directory
 my $pwd = getcwd;
 
-$ENV{'COILSDIR'} = '/usr/local/ensembl/data/coils';
-
 #Start up the logger
 Log::Log4perl->easy_init($DEBUG);
 my $logger = get_logger();
 
+my ( $statusdir, $pfamseqDir, $split );
+$split = 500;
+
+&GetOptions(
+  "statusdir=s"  => \$statusdir,
+  "pfamseqdir=s" => \$pfamseqDir,
+  "split=i"      => \$split
+  ) or $logger->logdie("Invalid option!");
+
+#Get the connection to the pfam database.
 my $config = Bio::Pfam::Config->new;
 my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamliveAdmin } );
 my $dbh    = $pfamDB->getSchema->storage->dbh;
 
-my $pfamseqDir = shift;
-
 unless ($pfamseqDir) {
-  $logger->logdie("You need to supply the directory name:[$!]");
+  $logger->logdie("You need to supply the pfamseq directory name:[$!]");
 }
+unless ($statusdir) {
+  $logger->logdie("You need to supply the status directory name:[$!]");
+}
+#-------------------------------------------------------------------------------
+#Hash of sub refs that maps region data to a parsing sub routine.
+my %subs = (
+  ncoils  => \&parseNcoils,
+  seg     => \&parseSeg,
+  phobius => \&parsePhobius,
+  iupred  => \&parseIupred
+);
+
 
 #-------------------------------------------------------------------------------
+unless ( -d "$statusdir/otherReg" ) {
+  mkdir("$statusdir/otherReg")
+    or $logger->logdie("Could not make directory $statusdir/otherReg: [$!]");
+}
+
 #Make and change and into the otherReg dir in the lastest pfamseqdir
-unless ( -e "$pfamseqDir/otherReg" ) {
+unless ( -d "$pfamseqDir/otherReg" ) {
   mkdir("$pfamseqDir/otherReg")
     or $logger->logdie("Could not make directory $pfamseqDir/otherReg: [$!]");
 }
 chdir("$pfamseqDir/otherReg")
-  or $logger->logdie(
-  "Could not change into directory $pfamseqDir/otherReg: [$!]");
+  or
+  $logger->logdie("Could not change into directory $pfamseqDir/otherReg: [$!]");
 
 #-------------------------------------------------------------------------------
 #Open and spit pfamseq into bits;
 my $n = 1;
-unless(-e "$pfamseqDir/otherReg/doneSplit"){
-my $bit = int( ( $config->dbsize ) / 100 );
-$bit++;
-
-$logger->debug(
-  "Going to split pfamseq into 100 files containing $bit sequences");
-
-open( B, ">pfamseq.$n" )
-  or $logger->logdie("Could not open pfamseq.$n:[$!]\n");
-open( P, "$pfamseqDir/pfamseq" )
-  or $logger->logdie("Could not open $pfamseqDir/pfamseq:[$!]\n");
-
-my $c;
-while (<P>) {
-  if (/^>/) {
-    $c++;
-    if ( $c > $bit ) {
-      close(B);
-      $n++;
-      $c = 0;
-      open( B, ">pfamseq.$n" );
+unless ( -e "$statusdir/otherReg/doneSplit" ) {
+  my $dbsize;
+  if(-e "$pfamseqDir/DBSIZE"){
+    open(D, "<", "$pfamseqDir/DBSIZE") or $logger->logdie("Could not open $pfamseqDir/DBSIZE");
+    while(<D>){
+      if(/(\d+)/){
+        $dbsize = $1;
+        last;
+      }
     }
+    close(D);
+  }else{
+    my $rs = $pfamDB->getSchema->resultset('Pfamseq')->search({});
+    $dbsize = $rs->count;
   }
- print B $_;
+  
+  my $bit = int( ( $dbsize ) / $split );
+  $bit++;
+
+  $logger->debug(
+    "Going to split pfamseq into $split files containing $bit sequences");
+
+  open( B, ">pfamseq.$n" )
+    or $logger->logdie("Could not open pfamseq.$n:[$!]\n");
+  open( P, "$pfamseqDir/pfamseq" )
+    or $logger->logdie("Could not open $pfamseqDir/pfamseq:[$!]\n");
+
+  my $c;
+  while (<P>) {
+    if (/^>/) {
+      $c++;
+      if ( $c > $bit ) {
+        close(B);
+        $n++;
+        $c = 0;
+        open( B, ">pfamseq.$n" );
+      }
+    }
+    print B $_;
+  }
+  close(B);
+  close(P);
+  $logger->info("Made $n files");
+  touch("$statusdir/otherReg/doneSplit");
+}else{
+  my @files = glob("pfamseq.*");
+  $n = scalar(@files); 
 }
-close(B);
-close(P);
-$logger->info("Made $n files");
-system("touch $pfamseqDir/otherReg/doneSplit");
-}
-$n =100;
+$logger->info("There are $n files to process!");
+
 ##-------------------------------------------------------------------------------
 ##Now submit the searches to the farm
 my $farmConfig = $config->farm;
@@ -84,43 +128,37 @@ my $uuid       = $ug->to_string( $ug->create() );
 my $fh         = IO::File->new();
 my $user       = $ENV{USER};
 
+unless ( -e "$statusdir/otherReg/doneFarm" ) {
+  $logger->info("Submitting jobs to the farm");
 
-unless(-e "$pfamseqDir/otherReg/doneFarm"){
-$logger->info("Submitting jobs to the farm");
+  #Set up the job and copy the files over;
+  $fh->open( "| bsub -q "
+      . $farmConfig->{lsf}->{queue}
+      . " -o $statusdir/otherReg/$uuid.log  -JotherRegs\"[1-$n]\" " );
+ 
+  #Change into the directory containing the shattered pfamseq files
+  $fh->print("cd $pfamseqDir/otherReg\n");
 
-#Set up the job and copy the files over;
-$fh->open( "| bsub -q "
-    . $farmConfig->{lsf}->{queue}
-    . " -o /tmp/$$.log  -JotherRegs\"[1-$n]\" " );
-#$fh->print( "mkdir " . $farmConfig->{lsf}->{scratch} . "/$user/$uuid\n" );
-$fh->print( "cd $pfamseqDir/otherReg/pfamseq.\$\{LSB_JOBINDEX\}\n"); 
-#r/otherReg/pfamseq.\$\{LSB_JOBINDEX\} $fh->print(
-#      "/usr/bin/scp -p $phost:$pfamseqDir/otherReg/pfamseq.\$\{LSB_JOBINDEX\} "
-#    . $farmConfig->{lsf}->{scratch}
-#    . "/$user/$uuid/pfamseq.\$\{LSB_JOBINDEX\}\n" );
-#
 #To calculate these other regions
 #seg
-#ncoils - This needs the environment variable COILSDIR to be set.
+#ncoils - This needs the environment variable COILSDIR to be set. Done via cshrc.pfam
 #phobius
-$fh->print(
-  "ncoils -f < pfamseq.\$\{LSB_JOBINDEX\} > ncoils.\$\{LSB_JOBINDEX\}\n");
-$fh->print("seg pfamseq.\$\{LSB_JOBINDEX\} -l > seg.\$\{LSB_JOBINDEX\}\n");
-$fh->print(
-  "phobius.pl pfamseq.\$\{LSB_JOBINDEX\} > phobius.\$\{LSB_JOBINDEX\}\n");
+#iupred - This needs the environment variable IUPred_PATH to be set. Done via cshrc.pfam
+ 
+  $fh->print(
+    "ncoils -f < pfamseq.\$\{LSB_JOBINDEX\} > ncoils.\$\{LSB_JOBINDEX\}\n");
+  $fh->print("seg pfamseq.\$\{LSB_JOBINDEX\} -l > seg.\$\{LSB_JOBINDEX\}\n");
+ # $fh->print(
+ #   "phobius.pl pfamseq.\$\{LSB_JOBINDEX\} > phobius.\$\{LSB_JOBINDEX\}\n");
+  $fh->print(
+"iupred_multifasta pfamseq.\$\{LSB_JOBINDEX\} long > iupred.\$\{LSB_JOBINDEX\}\n"
+  );
+  $fh->close;
 
-#Now bring back all of the files
-#foreach my $f (qw(ncoils seg phobius)) {
-#  $fh->print(
-#"/usr/bin/scp -p $f.\$\{LSB_JOBINDEX\} $phost:$pfamseqDir/otherReg/$f.\$\{LSB_JOBINDEX\}\n"
-#  );
-#}
-#$fh->print("rm -f pfamseq.\$\{LSB_JOBINDEX\}\n");
-$fh->close;
-
-sleep(30); #Give them change to get on to the farm queue!
-system("touch $pfamseqDir/otherReg/doneFarm");
+  sleep(60);    #Give them change to get on to the farm queue!
+  system("touch $pfamseqDir/otherReg/doneFarm");
 }
+
 #-------------------------------------------------------------------------------
 ##Have all of the jobs finished
 $logger->info("Waiting for jobs to finish on the farm");
@@ -137,22 +175,21 @@ while ( !$finished ) {
 
   if ($jobnum) {
     $logger->info(
-"Will not continue until your $jobnum outstanding jobs have completed. Will check again in five minutes"
-    );
-    sleep(300);
+      "Will not continue until your $jobnum outstanding jobs have completed."
+        . "Will check again in ten minutes" );
+    sleep(600);
   }
   else {
     $finished = 1;
   }
 }
 
-#my $n = 100;
 #-------------------------------------------------------------------------------
 #Now we should have every thing back to be joined together and uploaded.
 $logger->info("Checking all of the files have been retieved from the farm");
 my $error;
 for ( my $i = 1 ; $i <= $n ; $i++ ) {
-  foreach my $f (qw(seg phobius ncoils)) {
+  foreach my $f (keys %subs) {
     unless ( -s "$pfamseqDir/otherReg/$f.$i" ) {
       $logger->warn("$pfamseqDir/otherReg/$f.$i is missing");
       $error++;
@@ -169,55 +206,67 @@ $logger->logdie('Some of the files are other region files are missing')
 my $orDir = "$pfamseqDir/otherReg";
 
 #-------------------------------------------------------------------------------
-#Get auto_pfamase mapping
-$logger->info("Getting pfamseq accessions/auto_pfamseq");
-
-my $newSeqSth =
-  $dbh->prepare("select pfamseq_acc, auto_pfamseq from pfamseq")
-  or $logger->logdie('select prepare failed:'. $dbh->errstr );
-$newSeqSth->execute or $logger->logdie('select execution failed:'. $dbh->errstr );
-my $res = $newSeqSth->fetchall_arrayref;
-my $newSeqs;
-foreach my $r (@$res) {
-  $newSeqs->{ $r->[0] } = $r->[1];
-}
-
-#-------------------------------------------------------------------------------
 #Now parse the data and write to the file
 
-my %subs = ( ncoils  => \&parseNcoils,
-             seg     => \&parseSeg,
-             phobius => \&parsePhobius );
-#my ($fh);
-open($fh, ">$orDir/allOtherReg.dat") or $logger->logdie("Could not open allOtherReg.dat");
+my $fhOut;
+open( $fhOut, ">$orDir/allOtherReg.dat" )
+  or $logger->logdie("Could not open allOtherReg.dat");
 
-for( my $m =1; $m <= $n; $m++){
-  foreach my $f (qw(ncoils phobius seg)){
+for ( my $m = 1 ; $m <= $n ; $m++ ) {
+  my $pfamseq = getAutos($m, $orDir, $dbh);
+  
+  foreach my $f (keys %subs) {
     $logger->info("Parsing $f.$m");
-    $subs{$f}( $orDir, "$f.$m", $fh, $newSeqs );
+    $subs{$f}( $orDir, "$f.$m", $fhOut, $pfamseq );
   }
 }
 
 #-------------------------------------------------------------------------------
 #Copy file to the mysql instance so that it can be uploaded
 #
-my $scp = Net::SCP->new( { "host"=> $pfamDB->{host} } );
+
+$pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamliveAdmin } );
+$dbh    = $pfamDB->getSchema->storage->dbh;
+
+my $scp = Net::SCP->new( { "host" => $pfamDB->{host} } );
 my $tmp = "/tmp";
 $logger->info("scp data file to instance");
 
-$scp->put("$orDir/allOtherReg.dat", "$tmp/allOtherReg.dat") or $logger->logdie("Could not scp file ($orDir/allOtherReg.dat) to ". $config->pfamliveAdmin->{host}." because: ". $scp->{errstr});
+$scp->put( "$orDir/allOtherReg.dat", "$tmp/allOtherReg.dat" )
+  or $logger->logdie( "Could not scp file ($orDir/allOtherReg.dat) to "
+    . $config->pfamliveAdmin->{host}
+    . " because: "
+    . $scp->{errstr} );
 
 $logger->info('preparing to upload the file');
 $dbh->do("delete from other_reg");
 
-my $sth = $dbh->prepare( "LOAD DATA INFILE '/tmp/allOtherReg.dat' INTO TABLE other_reg" ) 
-   or $logger->logdie('select prepare failed:'. $dbh->errstr );
+$dbh->do("LOAD DATA INFILE '/tmp/allOtherReg.dat' INTO TABLE other_reg")
+  or $logger->logdie( 'file upload failed:' . $dbh->errstr );
 
-$sth->execute or $logger->logdie('select execution failed:'. $dbh->errstr ); 
 
 #-------------------------------------------------------------------------------
 #Subroutines that parse each file type......
 #
+
+sub getAutos {
+  my ($m, $orDir, $dbh) = @_;  
+  my $sth = $dbh->prepare("select auto_pfamseq from pfamseq where pfamseq_acc=?") or
+          $logger->logdie("Error preparing statement:". $dbh->errstr);
+  
+  my $pfamseq = {};
+  open(P, "<", $orDir."/pfamseq.".$m) or $logger->logdie("Failed to open $orDir/pfamseq.$m :[$!]");
+  while(<P>){
+    if(/^>(\S+)\.\d+/){
+      $sth->execute($1);
+      my $row = $sth->fetchrow_arrayref;
+      $pfamseq->{$1} = $row->[0];  
+    }
+  }
+  close(P);
+  return($pfamseq); 
+}
+
 
 sub parseNcoils {
   my ( $dir, $file, $fh, $pfamseq_autos ) = @_;
@@ -247,28 +296,31 @@ sub parseNcoils {
   $/ = "\n";
 }
 
-sub findCoils{
-    my($acc_auto, $seq, $fh) = @_;
-    my $start;
-    my $end;
-    my $prev = -1;
-    while( $seq =~ m/x/g){
-        my $x_pos = pos $seq;
-        if(($prev + 1) == $x_pos){
-            #$end = $x_pos;
-        }else{
-            $end = $prev;
-            if($start){
-                #print "\t$acc_auto\t$start\t$end\tcoiled_coil\tncoils\t\t\n";
-                print $fh "\t$acc_auto\t$start\t$end\tcoiled_coil\tncoils\t\t\n";
-            }
-            $start = $x_pos;
-        }
-        $prev = $x_pos;
+sub findCoils {
+  my ( $acc_auto, $seq, $fh ) = @_;
+  my $start;
+  my $end;
+  my $prev = -1;
+  while ( $seq =~ m/x/g ) {
+    my $x_pos = pos $seq;
+    if ( ( $prev + 1 ) == $x_pos ) {
+
+      #$end = $x_pos;
     }
-    if($start){
-        print $fh "\t$acc_auto\t$start\t$prev\tcoiled_coil\tncoils\t\t\n";     
+    else {
+      $end = $prev;
+      if ($start) {
+
+        #print "\t$acc_auto\t$start\t$end\tcoiled_coil\tncoils\t\t\n";
+        print $fh "\\N\t$acc_auto\t$start\t$end\tcoiled_coil\tncoils\t\\N\t\\N\n";
+      }
+      $start = $x_pos;
     }
+    $prev = $x_pos;
+  }
+  if ($start) {
+    print $fh "\\N\t$acc_auto\t$start\t$prev\tcoiled_coil\tncoils\t\\N\t\\N\n";
+  }
 }
 
 sub parseSeg {
@@ -281,7 +333,7 @@ sub parseSeg {
       my $end   = $3;
       my $score = $4;
       print $fh
-        "\t$$pfamseq_autos{$acc}\t$start\t$end\tlow_complexity\tseg\t$score\t\n";
+"\\N\t$$pfamseq_autos{$acc}\t$start\t$end\tlow_complexity\tseg\t$score\t\\N\n";
     }
   }
   close(SEG);
@@ -297,21 +349,69 @@ sub parsePhobius {
   while (<PHOB>) {
     if (/^ID\s+(\S+)\.\d+/) {
       $acc = $1;
+      unless ( exists( $pfamseq_auto->{$acc} ) ) {
+        $logger->logdie("$acc is not in the pfamseq mapping!");
+      }
+      
     }
     elsif (/^FT\s+SIGNAL\s+(\d+)\s+(\d+)/) {
       my $start = $1;
       my $end   = $2;
-      print $fh 
-        "\t" . $$pfamseq_auto{$acc} . "\t$start\t$end\tsig_p\tPhobius\t\t\n";
+      print $fh "\\N\t"
+        . $pfamseq_auto->{$acc}
+        . "\t$start\t$end\tsig_p\tPhobius\t\\N\t\\N\n";
     }
     elsif (/^FT\s+TRANSMEM\s+(\d+)\s+(\d+)/) {
       my $start = $1;
       my $end   = $2;
       print $fh
-        "\t$$pfamseq_auto{$acc}\t$start\t$end\ttransmembrane\tPhobius\t\t\n";
+        "\\N\t".$pfamseq_auto->{$acc}."\t$start\t$end\ttransmembrane\tPhobius\t\\N\t\\N\n";
     }
     elsif (/^\/\//) {
       $acc = "";
+    }
+  }
+}
+
+sub parseIupred {
+  my ( $dir, $iupred_file, $fh, $pfamseq_auto ) = @_;
+
+  open( DIS, "<", $iupred_file )
+    or $logger->logdie("Failed to open $iupred_file for reading:[$!]");
+
+  my ($acc);
+
+  #Need to parse something like this:
+
+  #ID Q6GZX3.1
+  #FT  IUPred     218     224  DISORDERED
+  #FT  IUPred     261     298  DISORDERED
+  #//
+
+  while (<DIS>) {
+    if (/^ID\s+(\S+)\.\d+/) {
+      $acc = $1;
+      unless ( exists( $pfamseq_auto->{$acc} ) ) {
+        $logger->logdie("$acc is not in the pfamseq mapping!");
+      }
+    }
+    elsif (/^FT\s+IUPred\s+(\d+)\s+(\d+)/) {
+      my $start = $1;
+      my $end   = $2;
+      print $fh "\\N\t"
+        . $pfamseq_auto->{$acc}
+        . "\t$start\t$end\tdisorder\tIUPred\t\\N\t\\N\n";
+    }
+    elsif (/^\/\//) {
+      $acc = "";
+    }
+    elsif (/^#/) {
+
+      #comment block
+      next;
+    }
+    else {
+      $logger->logdie("Failed to parse line, $_");
     }
   }
 }
