c29cedac17f0b57260a06d9da3a3aa76617bcf13
rdf <rdf@98333f76-0502-11df-81af-936bbf3a1e26>
2013-08-29 18:03:18 +0000

    Updated script for getting complete proteomes.

--- PfamScripts/pud/pud-getProteome.pl
+++ PfamScripts/pud/pud-getProteome.pl
@@ -3,40 +3,56 @@
 use strict;
 use warnings;
 use LWP::UserAgent;
-use File::Temp qw(tempdir);
-use IO::Uncompress::Gunzip qw(gunzip $GunzipError);
-use IO::Compress::Gzip qw(gzip);
-use Data::Printer;
-use Log::Log4perl qw( :easy );
+use HTTP::Date;
 
-use Bio::Pfam::Config;
-use Bio::Pfam::PfamLiveDBManager;
+my $proteome =  'complete:yes';
+my $keyword =  'keyword:181';
 
-#Start up the logger
-Log::Log4perl->easy_init($DEBUG);
-my $logger = get_logger();
-
-my $dir = 
-
-my $proteome = 'complete:yes';
-my $url = 'www.uniprot.org';
 my $agent = LWP::UserAgent->new;
+$agent->env_proxy;
+
+my $output_dir = shift;
+chdir "$output_dir" or die "couln't change dir";
+
 # Get a list of all taxons below the top node with a complete/reference proteome.
-my $query_list = 'http://'.$url."/taxonomy/?query=$proteome&format=list";
+my $query_list = "http://www.uniprot.org/taxonomy/?query=$proteome&format=list";
+
 my $response_list = $agent->get($query_list);
-$logger->logdie( 'Failed, got ' . $response_list->status_line .
-  ' for ' . $response_list->request->uri ) unless $response_list->is_success;
+die 'Failed, got ' . $response_list->status_line .
+  ' for ' . $response_list->request->uri . "\n"
+  unless $response_list->is_success;
+
+
+my $summary = "http://www.uniprot.org/taxonomy/?query=$proteome&format=tab";
+
+$response_list = $agent->get($summary);
+die 'Failed, got ' . $response_list->status_line .
+  ' for ' . $response_list->request->uri . "\n"
+  unless $response_list->is_success;
+
+open(L,">", "taxonomy_list") or die;
+print L $response_list->content;
+close(L);
+exit;
+
 
 # For each taxon, mirror its proteome set in FASTA format.
+for my $taxon (split(/\n/, $response_list->content)) {
+  my $file = $taxon . '.fasta';
+  my $query_taxon = "http://www.uniprot.org/uniprot/?query=organism:$taxon+$keyword&format=fasta&include=yes";
+  my $response_taxon = $agent->mirror($query_taxon, $file);
 
-  my $version = 0;
-  for my $taxon (split(/\n/, $response_list->content)) {
-    my $file = $taxon . '.fasta';
-    my $query_taxon = 'http://'.$url."/uniprot/?query=organism:$taxon&format=fasta&include=yes";
-    my $response_taxon = $agent->mirror($query_taxon, $file);
-    if ($response_taxon->is_success) {
-      $logger->debug("Got file");
-    }else{
-      $logger->logdie('Failed, got ' . $response_taxon->status_line .' for ' . $response_taxon->request->uri);
-    }
+  if ($response_taxon->is_success) {
+    my $results = $response_taxon->header('X-Total-Results');
+    my $release = $response_taxon->header('X-UniProt-Release');
+    my $date = sprintf("%4d-%02d-%02d", HTTP::Date::parse_date($response_taxon->header('Last-Modified')));
+    print "File $file: downloaded $results entries of UniProt release $release ($date)\n";
   }
+  elsif ($response_taxon->code == HTTP::Status::RC_NOT_MODIFIED) {
+    print "File $file: up-to-date\n";
+  }
+  else {
+    die 'Failed, got ' . $response_taxon->status_line .
+      ' for ' . $response_taxon->request->uri . "\n";
+  }
+}
