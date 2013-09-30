358fab6f67972a3a7c8e80544b538863a0543f72
jm14 <jm14@98333f76-0502-11df-81af-936bbf3a1e26>
2013-09-30 13:16:54 +0000

    Added code to concatenate output files into one

--- trunk/PfamScripts/search/pfam_scan_farm.pl
+++ trunk/PfamScripts/search/pfam_scan_farm.pl
@@ -2,7 +2,7 @@
 
 #Script to split a fasta file into smaller chunks, and run pfam_scan.pl on each chunk
 #Input is fasta file
-#Optional: Location of HMMs to use, and the number of sequences to spilt this file into
+#Optional: Location of HMMs to use, the number of sequences to spilt this file into, name of ouput file
 #Output is in cwd
 
 use strict;
@@ -11,15 +11,19 @@ use Getopt::Long;
 use IO::File;
 
 #Get user options
-my ($help, $fasta, $split_number, $hmm_dir);
+my ($help, $fasta, $split_number, $hmm_dir, $output);
 &GetOptions ("fasta=s" => \$fasta,
              "split=i" => \$split_number,
-	     "hmm_dir=s" => \$hmm_dir );
+	     "hmm_dir=s" => \$hmm_dir, 
+	     "output=s"  => \$output);
 
 #Does fasta file exist
 unless($fasta and -s $fasta) {
   help();
 }
+
+#Define output file if not defined already
+$output="pfam_scan_result" unless($output);
 
 #Check split number is sensible, if not specified use default
 if($split_number) {
@@ -86,7 +90,7 @@ close FASTA;
 close FH;
 
 #Run pfam_scan.pl on farm using job arrays, submit to long queue, select 2Gb memory
-print STDERR "Submitting $j pfam_scan.pl jobs to farm\n";
+print STDERR "Submitting $j+1 pfam_scan.pl jobs to farm\n";
 my $num_files = "1-$j";
 my $fh = IO::File->new();
 
@@ -95,24 +99,38 @@ $fh->print( "pfam_scan.pl -dir $hmm_dir -fasta $name\$\{LSB_JOBINDEX} > $name\$\
 $fh->close;
 
 
+#Concatenate output files into one
+#Remove intermediate files
+my $fh2 = IO::File->new();
+
+$fh2->open( "| bsub -q normal -o $name.log -J$name -w\'done($name)\'") or die "Couldn't open file handle\n";
+$fh2->print("cat $name*out > $output\n");
+$fh2->print("rm -fr $name*\[0-9\]\n");
+$fh2->print("rm -fr $name*out\n");
+$fh2->close;
+
+
 sub help {
 print STDERR << "EOF";
 
 This program splits a fasta file into smaller chunks named <fasta>1,
 <fasta>2 etc. It runs pfam_scan.pl using Pfam GA thresholds on each
-chunk on the Sanger farm. The output will be pfam_scan.pl output files
-named <fasta>1.out, <fasta>2.out etc. in the cwd.
+chunk on the Sanger farm. If all chunks run on the farm successfully,
+the script will concantenate all output files into a single file
+called 'pfam_scan_result', and delete the intermediate files.
 
 EXAMPLE:
 
 $0 -fasta NR_PDB.fasta
 
 OPTIONS:
-  -split <integer>  Number of sequences to put in each smaller file 
-                    (value must be >=50 and <=10000, default is 1000)
+  -split <integer>    Number of sequences to put in each smaller file 
+                      (value must be >=50 and <=10000, default: 1000)
 
-  -hmm_dir <dir>    Directory location of HMMs 
-                    (default is HMMs from current release)
+  -hmm_dir <dir>      Directory location of HMMs 
+                      (default: HMMs from current release)
+
+  -output <filename>  Name of output file (default: pfam_scan_result)
  
 EOF
  
