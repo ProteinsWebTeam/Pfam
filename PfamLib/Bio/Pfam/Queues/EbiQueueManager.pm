e0e322926f0383b230e43ec023cf1b271a20b7f0
jt6 <jt6@98333f76-0502-11df-81af-936bbf3a1e26>
2014-01-17 14:51:07 +0000

    First cut of a queue manager module for use in EBI

---
+++ PfamLib/Bio/Pfam/Queues/EbiQueueManager.pm
@@ -0,0 +1,516 @@
+
+# EbiQueueMananger.pm
+# jgt 20131202
+
+package Bio::Pfam::Queues::EbiQueueManager;
+
+use Moose;
+use Log::Log4perl qw(get_logger :levels);
+use POSIX qw(setsid);
+use File::Basename;
+use Carp;
+use JSON;
+use Storable qw( nfreeze );
+use Data::Printer;
+use File::Temp;
+use File::Find;
+use Mail::Send;
+
+use WebUser;
+use Bio::Pfam::EbiRestClient;
+
+#-------------------------------------------------------------------------------
+#- attributes ------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+# public attributes
+
+has 'config' => (
+  is       => 'ro',
+  isa      => 'Bio::Pfam::Config',
+  required => 1,
+);
+
+has 'queue' => (
+  is       => 'ro',
+  isa      => 'Str',
+  required => 1,
+);
+
+has 'async' => (
+  is  => 'ro',
+  isa => 'Bool',
+);
+
+#-------------------------------------------------------------------------------
+# private attributes
+
+has '_log' => (
+  is      => 'ro',
+  default => sub { get_logger() }, # TODO this needs to be more robust when the 
+                                   # calling script doesn't init Log4perl
+);
+
+has '_schema' => (
+  is      => 'ro',
+  lazy    => 1,
+  default => sub {
+    my $self = shift;
+    my $db_params = $self->config->{Model}->{WebUser};
+    WebUser->connect( 
+      'dbi:' . $db_params->{driver} . ':'
+             . $db_params->{database} . ':'
+             . $db_params->{host} . ':'
+             . $db_params->{port},
+      $db_params->{user},
+      $db_params->{password}
+    );
+  },
+);
+
+has '_queue_spec' => (
+  is => 'rw',
+  isa => 'HashRef',
+);
+
+has '_job_type' => (
+  is => 'rw',
+  isa => 'Str',
+);
+
+# RESTful search interface client
+has '_rc' => (
+  is => 'ro',
+  isa => 'Bio::Pfam::EbiRestClient',
+  default => sub { Bio::Pfam::EbiRestClient->new },
+  lazy => 1,
+);
+
+#-------------------------------------------------------------------------------
+# post-construction checks
+
+sub BUILD {
+  my $self = shift;
+
+  # see if we should enable debugging messages
+  $self->_log->level($DEBUG) if $ENV{DEBUG};
+
+  # check that the specified queue is valid, i.e. available in the config
+  my $queue_config = $self->config->{search}->{queues};
+  my %known_queues = map { $_ => 1 } keys %$queue_config;
+ 
+  croak 'ERROR: not a valid queue; known queues (from configuration) are: ' 
+        . join( ', ', keys %known_queues ) . "\n"
+    unless $known_queues{$self->queue};
+
+  # as a convenience, retrieve the queue specification from the configuration, 
+  # based on the queue name that we get from arguments. Also store the job
+  # type directly
+  $self->_queue_spec( $queue_config->{$self->queue} );
+  $self->_job_type( $self->_queue_spec->{job_type} );
+
+  # set up the REST client
+  $self->_rc->base_url( $self->_queue_spec->{base_url} );
+}
+
+#-------------------------------------------------------------------------------
+#- public methods --------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+sub daemonise {
+  my $self = shift;
+
+  my $lock_dir = $self->config->{search}->{lock_dir};
+  my( $pid_file, $dir, $suffix ) = fileparse($0);
+  $pid_file = "${lock_dir}/${pid_file}.pid";
+
+  $self->_log->info( "daemonising script; lock file '$pid_file'" );
+
+  umask 0;
+  open STDIN,   '/dev/null' or $self->_log->logdie( "can't read from /dev/null: $!" );
+  open STDOUT, '>/dev/null' or $self->_log->logdie( "can't write to /dev/null: $!" );
+  open STDERR, '>/dev/null' or $self->_log->logdie( "can't write to /dev/null: $!" );
+  defined( my $pid = fork ) or $self->_log->logdie( "can't fork: $!" );
+  
+  if ( $pid ) {
+    open PIDFILE, ">$pid_file"
+      or $self->_log->logdie( "can't open $pid_file: $!\n" );
+    print PIDFILE $pid;
+    close PIDFILE;
+    exit 0;
+  }
+
+  setsid or $self->_log->logdie( "can't start a new session: $!" ); 
+}
+
+#-------------------------------------------------------------------------------
+
+sub start_polling {
+  my $self = shift;
+
+  $self->_log->info( 'starting polling loop' );
+  my $delay = $self->_queue_spec->{polling_interval};
+
+  while ( 1 ) {
+
+    $self->_log->info( 'polling for jobs of type "' . $self->_queue_spec->{job_type} . '"' );
+
+    $self->_enqueue_pending_jobs;
+    $self->_check_running_jobs;
+
+    $self->_log->debug( "sleeping for $delay seconds" );
+    sleep $delay
+
+  } # end of endless while loop
+
+}
+
+#-------------------------------------------------------------------------------
+#- private methods -------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
+sub _enqueue_pending_jobs {
+  my $self = shift;
+
+  my @jobs = $self->_schema
+               ->resultset('JobHistory')
+                 ->get_pending_jobs( $self->_job_type );
+
+  $self->_log->info( 'found ' . scalar @jobs . ' pending jobs' )
+    if scalar @jobs;
+
+  JOB: foreach my $job ( @jobs ) {
+
+    # these are the options specified by the user who submitted the search
+    my $job_options = from_json $job->options;
+    $self->_log->debug( "job options: ", p( $job_options ) );
+    
+    # these are the options that we're going to hand to the EBI search service
+    my $search_options = { database => $self->_queue_spec->{database} };
+    $search_options->{evalue} = $job_options->{evalue}
+      if $job_options->{evalue};
+
+    $self->_log->info( 'search options: ', p( $search_options ) );
+
+    my $rv;
+    if ( $self->_queue_spec->{async} ) {
+      # batch job...
+      $rv = $self->_submit_batch_job( $job, $search_options );
+    }
+    else {
+      $rv = $self->_submit_interactive_job( $job, $search_options );
+    }
+
+    if ( $rv ) {
+      $job->update( { status => 'RUN',
+                      started => \'NOW()', } );
+    }
+    else {
+      $job->update( { status => 'FAIL',
+                      closed => \'NOW()',
+                      job_stream => [
+                        { stderr => 'There was a problem submitting your job.' }
+                      ]
+                    } );
+    }
+  }
+
+}
+
+#-------------------------------------------------------------------------------
+
+sub _check_running_jobs {
+  my $self = shift;
+
+  my @jobs = $self->_schema->resultset('JobHistory')
+               ->get_running_jobs( $self->_job_type );
+
+  $self->_log->info( 'found ' . scalar @jobs . ' running jobs' )
+    if scalar @jobs;
+
+  JOB: foreach my $job ( @jobs ) {
+    my $job_id = $job->job_id;
+
+    my $results;
+    if ( $self->_queue_spec->{async} ) {
+      $results = $self->_handle_batch_job( $job );
+    }
+    else {
+      $results = $self->_handle_interactive_job( $job );
+    }
+
+    if ( $results ) {
+
+      $self->_log->debug( "results of job $job_id:\n$results" );
+
+      # temporarily turn off query debugging, because job_stream can contain
+      # binary strings that will screw up the terminal
+      $self->_schema->storage->debug(0);
+
+      $job->job_stream->update( { stdout => $results } );
+      # re-enable debugging if necessary
+      $self->_schema->storage->debug(1)
+        if $ENV{DBIC_TRACE};
+
+      $job->update( { status => 'DONE',
+                      closed => \'NOW()' } );
+
+      $self->_log->debug( "stored results for job $job_id" );
+    }
+    else {
+      $self->_log->debug( "no results (yet) for job $job_id" );
+    }
+
+  } # end of "foreach job"
+
+}
+
+#-------------------------------------------------------------------------------
+
+sub _handle_batch_job {
+  my ( $self, $job ) = @_;
+  
+  my $job_id = $job->job_id;
+
+  my $all_chunks_rs  = $self->_schema->resultset('JobHistory')
+                         ->get_chunks_for_job( $job_id );
+
+  my $running_chunks_rs = $all_chunks_rs->search( { status => 'RUN' } );
+  if ( $running_chunks_rs->count > 0 ) {
+    $self->_log->debug( "batch job $job_id still has "
+                        . $running_chunks_rs->count . ' running chunks' );
+
+    # check for status of running chunks
+    my $num_done = $running_chunks_rs->count;
+    foreach my $chunk ( $running_chunks_rs->all ) {
+      my $ebi_id = $chunk->ebi_id;
+      my $chunk_status = $self->_rc->status( $ebi_id );
+      $self->_log->debug( "chunk $ebi_id has status $chunk_status" );
+
+      # update status for completed chunks
+      if ( $chunk_status eq 'FINISHED' ) {
+        my $chunk_results = $self->_rc->result( $ebi_id );
+        $chunk->update( { status => 'DONE',
+                          closed => \'NOW()' } );
+        $chunk->job_stream->update( { stdout => $chunk_results } );
+
+        $self->_log->debug( "marked chunk $ebi_id as DONE" );
+        $num_done--;
+      }
+    }
+
+    $self->_log->debug( "still $num_done chunks running" );
+
+    # if there are still jobs running, bail out and come back to check
+    # their status on the next polling cycle
+    return if $num_done > 0;
+  }
+
+  $self->_log->debug( "all chunks complete for batch job $job_id" );
+  # TODO not taking into account failed chunks
+
+  # stick the chunks back together
+  my $results;
+  foreach my $chunk ( $all_chunks_rs->all ) {
+    my $ebi_id = $chunk->ebi_id;
+    $self->_log->debug( "getting results for chunk $ebi_id" );
+
+    my $chunk_results = $chunk->job_stream->stdout;
+    # TODO check for failed job
+
+    $self->_log->debug( 'results for chunk with EBI job ID ' . $ebi_id 
+                        . ":\n$chunk_results" );
+
+    if ( defined $results ) {
+      # grep out just the result rows
+      $results .= join( "\n", grep( !/^#/, split( m/\n/, $chunk_results ) ) );
+    }
+    else {
+      # take the whole log from the first job, since we want the header too
+      $results = $chunk_results;
+    }
+
+  }
+  $results =~ s/^\s*\n//mg; # strip blank lines
+
+  # since all chunks are done, delete the rows from the DB
+  $all_chunks_rs->delete;
+
+  # mail results
+  $self->_mail_batch_results( $job, $results );
+
+  return $results;
+}
+
+#-------------------------------------------------------------------------------
+
+sub _mail_batch_results {
+  my ( $self, $job, $results ) = @_;
+
+  my $job_id = $job->job_id;
+  my $email  = $job->email;
+
+  $self->_log->info( "mailing results of job $job_id to $email" );
+ 
+  my $msg = Mail::Send->new;
+  $msg->to( $email );
+  $msg->subject( "Results for batch search job $job_id" );
+  $msg->bcc( $self->_queue_spec->{admin_email} );
+
+  my $body_fh = $msg->open;
+  print $body_fh $results;
+  $body_fh->close
+    or $self->_log->warn( "failed to send email to $email with results of job $job_id" );
+}
+
+#-------------------------------------------------------------------------------
+
+sub _handle_interactive_job {
+  my ( $self, $job ) = @_;
+
+  my $ebi_id = $job->ebi_id;
+  $self->_log->debug( "found running job with internal EBI job ID $ebi_id" );
+    
+  # retrieve the status of the job from the EBI queuing system
+  my $status = $self->_rc->status( $ebi_id );
+
+  my $results;
+
+  if ( $status eq 'RUNNING' ) {
+    $self->_log->debug( "interactive job $ebi_id is still RUNNING" );
+  }
+  elsif ( $status eq 'FINISHED' ) {
+    $self->_log->info( "interactive job $ebi_id is FINISHED; retrieving results" );
+
+    my $json_results = from_json( $self->_rc->result($ebi_id) );
+    unless ( defined $json_results ) {
+      $self->_log->warn( "there was a problem deserialising the results for EBI job $ebi_id" );
+      return;
+    }
+
+    $results = nfreeze $json_results;
+    unless ( $results ) {
+      $self->_log->warn( "there was a problem freezing the results for EBI job $ebi_id" );
+      return;
+    }
+      
+    $self->_log->debug( "successfully retrieved and froze results for EBI job $ebi_id" );
+
+  }
+  else {
+    $self->_log->warn( "something went wrong with job $ebi_id" );
+    die 'There was a problem running your job';
+  }
+
+  return $results;
+}
+
+#-------------------------------------------------------------------------------
+
+sub _submit_interactive_job {
+  my ( $self, $job, $search_options ) = @_;
+
+  my $sequence = $job->job_stream->stdin;
+  $self->_log->debug( "sequence to search: $sequence" );
+
+  $search_options->{sequence} = $sequence;
+
+  my $ebi_id = $self->_rc->search( $search_options);
+
+  $job->update( { ebi_id  => $ebi_id,
+                  status  => 'RUN',
+                  started => \'NOW()' } );
+
+  $self->_log->debug( "submitted search; internal EBI job ID: $ebi_id" );
+
+  return 1;
+}
+
+#-------------------------------------------------------------------------------
+
+sub _submit_batch_job {
+  my ( $self, $job, $search_options ) = @_;
+
+  # bail unless there's a valid email address
+  my $email = $job->email;
+  unless ( $email and Email::Valid->address( $email ) ) {
+    $self->_log->logwarn( 'no valid email address supplied' );
+    return 0;
+  }
+  
+  $search_options->{format} = 'txt';
+  $search_options->{email} = 'xfam@ebi.ac.uk';
+
+  # retrieve and split up the input sequences
+  my $fasta = $job->job_stream->stdin;
+
+  my @sequences = split(/\n\>/, "\n".$fasta);
+  shift @sequences;
+
+  my @chunks;
+  while ( scalar @sequences > 0 ) {
+    # Rob wrote the next line. He's a god.
+    push @chunks, join( '', map{ ">$_\n" } splice( @sequences, 0, $self->_queue_spec->{chunk_size} ) );
+  }
+
+  $self->_log->info( 'batch file was split into ' . scalar @chunks . ' chunks' );
+
+  my $jh = $self->_schema->resultset('JobHistory');
+  my $js = $self->_schema->resultset('JobStream');
+
+  my $i = 0;
+  foreach my $chunk ( @chunks ) {
+
+    $search_options->{sequence} = $chunk;
+
+    my $ebi_id;
+    eval {
+      $ebi_id = $self->_rc->search( $search_options );
+      $self->_log->debug( "got job ID $ebi_id" );
+    };
+    if ( $@ ) {
+      $self->_log->logwarn( "there was a problem submitting chunk $i for job "
+                            . $job->job_id );
+      return 0;
+    }
+
+    $self->_log->debug( "submitted batch chunk $i, given EBI job ID $ebi_id" );
+
+    my $jh_row = $jh->create( {
+      job_id   => $job->job_id,
+      ebi_id   => $ebi_id,
+      status   => 'RUN',
+      options  => $job->options,
+      opened   => $job->opened,
+      started  => \'NOW()',
+      job_type => 'chunk',
+    } );
+
+    unless ( $jh_row ) {
+      $self->_log->logwarn( "there was a problem storing job info for chunk $chunk for batch job "
+                            . $job->job_id );
+      return 0;
+    }
+
+    my $rv = $js->create( {
+      id    => $jh_row->id,
+      stdin => $chunk,
+    } );
+
+    unless ( $rv ) {
+      $self->_log->logwarn( "there was a problem storing the sequence for chunk $chunk for batch job "
+                            . $job->job_id );
+      return 0;
+    }
+
+    $i++;
+  }
+
+  return 1;
+}
+
+#-------------------------------------------------------------------------------
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
