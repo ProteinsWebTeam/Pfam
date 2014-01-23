57493fd793eac3c780a3975a01a6ebd30e7576da
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-01-23 10:37:57 +0000

    Fix "is_nullable" for "type".

--- trunk/PfamSchemata/PfamDB/AlignmentsAndTrees.pm
+++ trunk/PfamSchemata/PfamDB/AlignmentsAndTrees.pm
@@ -39,7 +39,7 @@ __PACKAGE__->add_columns(
     size => 4294967295,
   },
   "type",
-  { data_type => "ENUM", default_value => undef, is_nullable => 1, size => 4 },
+  { data_type => "ENUM", default_value => undef, is_nullable => 0, size => 4 },
 );
 __PACKAGE__->add_unique_constraint("UQ_alignments_and_trees_1", ["auto_pfama", "type"]);
 __PACKAGE__->belongs_to(
