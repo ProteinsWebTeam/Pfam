ce19705407a12d1e1b64264d45986732c9d4e47a
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-01-28 14:59:00 +0000

    Use esl-reformat rather than sreformat

--- PfamScripts/view/heatMap.pl
+++ PfamScripts/view/heatMap.pl
@@ -91,8 +91,10 @@ if ( $option{n} ) {
 #-------------------------------------------------------------------------------
 #
 unlink("fa.ann");
-system("sreformat fasta ALIGN.ann > fa.ann") and die "Failed to run sreformat:[$!]\n";
-system($config->hmmer3bin."/hmmalign HMM fa.ann > ALIGN.pp.wsto") and die "Failed to run hmmalign:[$!]\n";
+system($config->binLocation . '/esl-reformat fasta ALIGN.ann > fa.ann') == 0 
+  or die "Failed to run esl-reformat:[$!]\n";
+system($config->binLocation . '/hmmalign HMM fa.ann > ALIGN.pp.wsto') == 0
+  or die "Failed to run hmmalign:[$!]\n";
 unlink("fa.ann");
 my ($aliHash, $aliOrder) = read_alignment(); 
 my ($aliHM);
