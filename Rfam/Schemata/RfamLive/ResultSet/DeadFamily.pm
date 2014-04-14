46967d4f283582892e9a1b9e47cba4e5f335253d
swb <swb@98333f76-0502-11df-81af-936bbf3a1e26>
2014-04-14 11:59:04 +0000

    started adding createFromFamilyRow sub

--- Rfam/Schemata/RfamLive/ResultSet/DeadFamily.pm
+++ Rfam/Schemata/RfamLive/ResultSet/DeadFamily.pm
@@ -17,5 +17,27 @@ sub getAllDeadFamilyAcc {
   return($accs);
 }
 
+sub createFromFamilyRow{
+	my ($entry, $comment, $forward, $user) =@_;
+	
+	my $fam = $self->find({ rfam_acc =>$familyObj->DESC->AC} );
+				
+	unless ($fam) {
+		croak ("Failed to find a database entry in the family table for "
+			. $familyObj->DESC->AC
+			. ". This is really bad!\n");
+	}	
+   
+	my @row;
+
+	push (@row, {	rfam_acc	=> $rfam_acc,
+					rfam_id		=> $rfam_id,
+					comment		=> $comment,
+					forward_to	=> $forward,
+					title		=> $title,
+					user		=> $user } );
+	$self->populate(\@row);
+	
+}
 
 1;
