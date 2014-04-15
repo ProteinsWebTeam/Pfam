7a5d40b98faf8679c713289f0605e075d1d4a753
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-04-15 15:00:12 +0000

    Added verbose logging, in an attempt to pin down the problems with committing large families

--- trunk/Rfam/Scripts/svnhooks/pre-commit.pl
+++ trunk/Rfam/Scripts/svnhooks/pre-commit.pl
@@ -13,6 +13,21 @@ use Bio::Rfam::Config;
 use Bio::Rfam::SVN::Commit;
 use Data::Printer;
 
+use Log::Log4perl qw(:easy);
+ 
+ # set up logging
+my $logger_conf = q(
+  log4perl.logger                         = DEBUG, FileAppender
+  log4perl.appender.FileAppender          = Log::Log4perl::Appender::File
+  log4perl.appender.FileAppender.filename = /tmp/pre-commit.log
+  log4perl.appender.FileAppender.layout   = Log::Log4perl::Layout::PatternLayout
+  log4perl.appender.FileAppender.layout.ConversionPattern = %M:%L %p: %m%n
+);
+
+Log::Log4perl->init( \$logger_conf );
+
+my $log = get_logger(); 
+
 my $DEBUG = defined( $ENV{DEBUG} ) ? $ENV{DEBUG} : 0;
 
 my ( $rev, $txn, $repos, $debug, $help );
@@ -22,6 +37,8 @@ GetOptions(
   "txn=s"   => \$txn,
   "repos=s" => \$repos
 );
+
+$log->debug('successfully processed options');
 
 if ( $rev and $txn ) {
   die "Can not define both and revision and a transaction\n";
@@ -37,6 +54,8 @@ else {
 
 $params{repos} = $repos;
 
+$log->debug("got revision number ($rev) and transaction ($txn) from command-line options");
+
 my $txnlook = Bio::Rfam::SVN::Commit->new( \%params );
 
 unless ( $txnlook and $txnlook->isa('SVN::Look') ) {
@@ -48,8 +67,13 @@ unless ($msg) {
   die "No commit message passed in!\n";
 }
 
+$log->debug( qw(got log message from transaction: "$msg") );
+
 my $config  = Bio::Rfam::Config->new;
+$log->debug('got config object');
+
 my $rfamdb = $config->rfamlive;
+$log->debug('got database connection');
 
 if ($DEBUG) {
   print STDERR "*** $msg ***\n";
@@ -72,9 +96,11 @@ if ($DEBUG) {
     print STDERR "Deleted:" . $f . "\n";
   }
 }
+$log->debug('printed modified file list');
 
 #Make sure the database isn't locked
 my $lock = $txnlook->allowCommit($rfamdb);
+$log->debug('retrieved lock status');
 my $allow_commit;
 if ($lock) {
 
@@ -100,19 +126,31 @@ unless ($allow_commit) {
   die "The database is currently locked by " . $lock->locker . "\n";
 }
 
+$log->debug('database is not locked (or committer has permission to commit anyway)');
+
 if ( $msg =~ /^CI:/ ) {
+  $log->debug('got a commit');
   $txnlook->commitEntry;
+  $log->debug('committed');
 }
 elsif ( $msg =~ /^NEW:/ ) {
+  $log->debug('got a new entry');
   $txnlook->commitNewEntry;
+  $log->debug('committed');
 }elsif( $msg =~ /CIDESC:/){
+  $log->debug('got a modified DESC');
   $txnlook->commitEntryDESC;   
+  $log->debug('committed');
 }elsif ( $msg =~ /^NEWMOV:/ ) {
+  $log->debug('got a NEWMOV message');
   ;
 }elsif ( $msg =~ /^MOV:/ ) {
+  $log->debug('got a move');
   $txnlook->moveFamily();
+  $log->debug('committed');
 }
 elsif ( $msg =~ /^KILL:/ ) {
+  $log->debug('got a kill');
   my ( $comment, $forward );
   if ( $msg =~ /^KILL:Comment;(.*)\nKILL:Forward;(.*)/ ) {
     $comment = $1;
@@ -125,23 +163,32 @@ elsif ( $msg =~ /^KILL:/ ) {
   else {
     die "In KILL message, did not parse $msg\n";
   }
+  $log->debug( qw(kill comment: "$comment") );
+  $log->debug( qw(forward to:   "$forward") );
 
   #Find out the author so we know who has done this.
   my $author = $txnlook->author();
+  $log->debug( qw(kill author:  "$author") );
   #Go and delete the family.
   $txnlook->deleteFamily( $comment, $forward, $author );
+  $log->debug('killed family');
 }
 elsif ( $msg =~ /SEQUP/ ) {
+  $log->debug('got a sequence update');
   foreach my $file ( $txnlook->updated() ) {
+    $log->debug("changed file: $file");
     unless ( $file =~ /Sequences/ ) {
+      $log->debug('dying; not a valid sequence update file');
       die "Got sequence update message, but the file ($file) does not look"
         . " like it has come from the sequence part of the repository\n";
     }
   }
 }elsif( $msg =~ /ADMINBYPASS/ ) {
+  $log->debug('admin bypass');
   ; 
 }
 else {
+  $log->debug( qw(unknown commit message: "$msg") );
   die "Do not know here this commit has come from, [$msg]!\n";
 }
 
