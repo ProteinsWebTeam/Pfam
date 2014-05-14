74d94d067b42730aec66f870c929d3252ddcc3bc
swb <swb@98333f76-0502-11df-81af-936bbf3a1e26>
2014-05-14 15:02:03 +0000

    Commented on the code and general tidying up

--- trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Tree.pm
+++ trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Tree.pm
@@ -15,29 +15,32 @@ has foo => (
 sub process {
   my $self = shift;
   $self->makeSeedTrees;
-  #$self->makeSeedTrees('tax');
 }
 
 #Make phylogenetic tree for seed
 #
 sub makeSeedTrees {
-	my ($self,$tax) = @_;
+	my ($self) = @_;
 	my ($treeMethod,$cmd,$taxcmd,$infile,$outfile,$type);
 
 	my $rfamdb = $self->parent->config->rfamlive;
 	my $rfam_acc = $self->parent->family->DESC->AC;
-	
+#Set up some locations for writing files to:
+#
 	my $location = "/nfs/nobackup2/xfam/rfam";
 	my $seedfile = "$location/$rfam_acc";
 	my $tree = "$location/$rfam_acc.outtree";
 	my $taxtree = "$location/$rfam_acc.taxtree";
+
+#Get SEED file, and check the family exists in the database:
 	my $msa = $self->parent->family->SEED;
 	
 	my $famRow = $rfamdb->resultset('Family')->find( {rfam_acc => $rfam_acc} );
 	if (!defined($famRow)) {
 		croak ("Failed to find entry in the family table for $rfam_acc!\n");
 	}
-	
+#FastTree needs an aligned fasta file, so convert the seed file:
+#
     $treeMethod = 'fasttree';
 	$msa->write_msa("$seedfile","afa");
 	
@@ -45,11 +48,18 @@ sub makeSeedTrees {
 
 	$cmd = "FastTree -nt -nj -boot 100 < $seedfile > $tree 2> /dev/null";
 	system ($cmd) == 0 or croak ("Problem with generating the seed tree....this is not good!\n");
+
+#We generate two trees: one with the sequence accessions as labels, and one with the species. We can't use the seed file
+#labelled with the species names to generate a tree, as FastTree can't cope with non-unique sequence names, so we convert
+#the sequence accession tree:
+#
 	specifyTree($msa, $rfamdb ,$tree,$taxtree);	
 	
 	my ($fileGzipped,$taxGzipped);
 	gzip $tree => \$fileGzipped;
 	gzip $taxtree => \$taxGzipped;
+
+#Update the database:
 	
 	my $row = $rfamdb->resultset('AlignmentAndTree')->find_or_create(
 		{
@@ -75,16 +85,21 @@ sub makeSeedTrees {
 					{key => 'rfam_acc_and_type'}
 				);
 
+#Cleanup:
+#
 	unlink($seedfile);
 	unlink($tree);
 	unlink($taxtree);
 }
 #This bit of code is based on Eric's seqToSpeciesNames. Needed as FastTree won't allow non-unique names in a tree
-#This renders it unable to build trees when we use species names, where one species has more than one sequence in the seed
+
 sub specifyTree {
 	my ($self, $rfamdb, $tree,$taxtree) = @_;
 	my %seenSpecies;
 	my %accToSpecies;
+
+#Get sequence identifiers from the seed and query the database for the species (align_display_name)	
+
 	my $sth = $rfamdb->prepare_seqaccToTaxon;
 	for( my $i = 0; $i < $self->nseq; $i++){
 		 my $nse = $self->get_sqname($i);
@@ -102,6 +117,9 @@ sub specifyTree {
 	$accToSpecies{$name} = $speciesName;
 	$seenSpecies{$row->{align_display_name}}++;
 	}
+
+#Now get the original tree and replace all the accessions with the species:
+
 	open(TREE, "$tree") or croak ("Can't open tree file to convert seqs to species...this is not good!\n");
 	my @ar =<TREE>;
 	my $t = join("", @ar);
