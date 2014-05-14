b9e6a0b238887292f77708f41ea0b78663e82388
swb <swb@98333f76-0502-11df-81af-936bbf3a1e26>
2014-05-14 13:48:00 +0000

    Added some cleaning up to SecondaryStructure.pm
    Significant changes to Tree.pm. FastTree will not work whenever the sequence identifiers are the same, so we can't build a tree on the seed where we have replaced the NSE with the species, as there are many families where they have more than one sequence from a single species. Therefore, we need to search and replace on the tree to replace all the sequence accessions with species; there is now a subroutine (specifyTree) which does this.
    
    All trees loaded into the database

--- trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Tree.pm
+++ trunk/Rfam/Lib/Bio/Rfam/View/Plugin/Tree.pm
@@ -5,6 +5,8 @@ with 'MooseX::Role::Pluggable::Plugin';
 
 use IO::Compress::Gzip qw(gzip $GzipError);
 use Data::Dumper;
+use Carp;
+ 
 has foo => (
   is  => 'rw',
   isa => 'Int'
@@ -13,13 +15,14 @@ has foo => (
 sub process {
   my $self = shift;
   $self->makeSeedTrees;
+  #$self->makeSeedTrees('tax');
 }
 
 #Make phylogenetic tree for seed
 #
 sub makeSeedTrees {
-	my ($self) = @_;
-	my ($treeMethod,$cmd,$taxcmd,$infile,$outfile);
+	my ($self,$tax) = @_;
+	my ($treeMethod,$cmd,$taxcmd,$infile,$outfile,$type);
 
 	my $rfamdb = $self->parent->config->rfamlive;
 	my $rfam_acc = $self->parent->family->DESC->AC;
@@ -27,10 +30,9 @@ sub makeSeedTrees {
 	my $location = "/nfs/nobackup2/xfam/rfam";
 	my $seedfile = "$location/$rfam_acc";
 	my $tree = "$location/$rfam_acc.outtree";
-	my $taxseed = "$location/$rfam_acc.tax";
 	my $taxtree = "$location/$rfam_acc.taxtree";
 	my $msa = $self->parent->family->SEED;
-
+	
 	my $famRow = $rfamdb->resultset('Family')->find( {rfam_acc => $rfam_acc} );
 	if (!defined($famRow)) {
 		croak ("Failed to find entry in the family table for $rfam_acc!\n");
@@ -38,56 +40,93 @@ sub makeSeedTrees {
 	
     $treeMethod = 'fasttree';
 	$msa->write_msa("$seedfile","afa");
-	$msa->seqToSpeciesNames($rfamdb);
-	$msa->write_msa("$taxseed","afa");
-	$cmd = "FastTree -nt -nj -boot 100 < $seedfile > $tree 2> /dev/null";
-    $taxcmd = "FastTree -nt -nj -boot 100 <$taxseed> $taxtree 2> /dev/null";
+	
+	print "Making trees for $rfam_acc\n";
 
+	$cmd = "FastTree -nt -nj -boot 100 < $seedfile > $tree 2> /dev/null";
 	system ($cmd) == 0 or croak ("Problem with generating the seed tree....this is not good!\n");
-	system ($taxcmd) == 0 or croak ("Problem with generating the seedtax tree...this is not good!\n");
+	specifyTree($msa, $rfamdb ,$tree,$taxtree);	
 	
 	my ($fileGzipped,$taxGzipped);
 	gzip $tree => \$fileGzipped;
-	gzip $taxtree => \$fileGzipped;
+	gzip $taxtree => \$taxGzipped;
 	
-	my $row = $rfamdb->resultset('AlignmentAndTree')->find(
+	my $row = $rfamdb->resultset('AlignmentAndTree')->find_or_create(
 		{
-			rfam_acc => $rfam_acc
-		});
-	if (!$row) {
-		$rfamdb->resultset('AlignmentAndTree')->create({ rfam_acc => $rfam_acc,
-					type =>'seed',
-					tree => $fileGzipped,
-					treemethod => $treeMethod
-	
-		});
-		$rfamdb->resultset('AlignmentAndTree')->create({	rfam_acc => $rfam_acc,
-					type => 'seedtax',
-					tree => $taxGzipped,
-					treemethod => $treeMethod
-		});
-	} else {
-		$row->update({ rfam_acc => $rfam_acc,
-					type =>'seed',
-					tree => $fileGzipped,
-					treemethod => $treeMethod
-	
-		});
-		$row->update({	rfam_acc => $rfam_acc,
-					type => 'seedtax',
-					tree => $taxGzipped,
-					treemethod => $treeMethod
-		});
+			rfam_acc => $rfam_acc,
+			type => 'seed',
+		},
+		{key => 'rfam_acc_and_type'}
+		);
+	$row->update( { tree => $fileGzipped,
+					type => 'seed',},
+					{key => 'rfam_acc_and_type'});
 
-	}
+	my $taxrow = $rfamdb->resultset('AlignmentAndTree')->find_or_create(
+		{
+			rfam_acc => $rfam_acc,
+			type => 'seedTax',
+		},
+		{ key => 'rfam_acc_and_type'}
+		);
+	
+	$taxrow->update( { tree => $taxGzipped,
+					type => 'seedTax',},
+					{key => 'rfam_acc_and_type'}
+				);
+
 	unlink($seedfile);
-	unlink($taxseed);
 	unlink($tree);
 	unlink($taxtree);
 }
+#This bit of code is based on Eric's seqToSpeciesNames. Needed as FastTree won't allow non-unique names in a tree
+#This renders it unable to build trees when we use species names, where one species has more than one sequence in the seed
+sub specifyTree {
+	my ($self, $rfamdb, $tree,$taxtree) = @_;
+	my %seenSpecies;
+	my %accToSpecies;
+	my $sth = $rfamdb->prepare_seqaccToTaxon;
+	for( my $i = 0; $i < $self->nseq; $i++){
+		 my $nse = $self->get_sqname($i);
+
+	my ($is_nse, $name, $start, $end) = Bio::Rfam::Utils::nse_breakdown($nse);
+	if(! $is_nse) { die "ERROR $nse not in name/start-end format"; }
+	
+	$sth->execute($name);
+	my $row = $sth->fetchrow_hashref;
+	if(!exists($seenSpecies{$row->{align_display_name}})){
+		$seenSpecies{$row->{align_display_name}} = 1;
+	}
+	my $speciesName = $row->{align_display_name}.'.'.$seenSpecies{$row->{align_display_name}};
+	
+	$accToSpecies{$name} = $speciesName;
+	$seenSpecies{$row->{align_display_name}}++;
+	}
+	open(TREE, "$tree") or croak ("Can't open tree file to convert seqs to species...this is not good!\n");
+	my @ar =<TREE>;
+	my $t = join("", @ar);
+	foreach my $k (keys %accToSpecies){
+        $t =~ s/$k/$accToSpecies{$k}/g;
+    }
+	open(TAXTREE, ">>$taxtree");
+	print TAXTREE "$t\n";
+}
+
+
 
 1;
 1;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
