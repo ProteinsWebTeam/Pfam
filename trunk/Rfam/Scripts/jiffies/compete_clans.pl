fa944c6e31f3e872f771aa3d531e7d7536241e5c
swb <swb@98333f76-0502-11df-81af-936bbf3a1e26>
2014-06-05 11:53:27 +0000

    Fully working script for competing the clans. Checks for any clan which has been updated in the last week, runs the competing clans overlap check and updates the is_significant flag accordingly.  We need some way of tracking when this was last run and results from it though - maybe sending email?

--- trunk/Rfam/Scripts/jiffies/compete_clans.pl
+++ trunk/Rfam/Scripts/jiffies/compete_clans.pl
@@ -10,6 +10,7 @@ use Bio::Rfam::View;
 use Bio::Rfam::SVN::Client;
 use Bio::Rfam::QC;
 use Scalar::Util qw(reftype);
+use DateTime;
 
 my $config = Bio::Rfam::Config->new;
 my $rfamdb = $config->rfamlive;
@@ -19,16 +20,31 @@ my $client = Bio::Rfam::SVN::Client->new({config => $config});
 my %clans;
 my @family_in_clan;
 my @overlaps;
+my @clans_to_update;
 
-my $clan_rs = $rfamdb->resultset('ClanMembership')->search;
 
-while (my $row = $clan_rs->next) {
-	my $clan_acc = $row->clan_acc->clan_acc;
-	my $rfam_acc = $row->rfam_acc->rfam_acc;
-	push @{$clans{$clan_acc}}, $rfam_acc;
-	push @family_in_clan, $rfam_acc;
+my $dt = DateTime->now;
+
+my $time = join ' ', $dt->ymd, $dt->hms;
+
+print "$time\n";
+
+my $interval = "> now() - interval 1 week";
+
+my $clan_update = $rfamdb->resultset('Clan')->search({updated => \$interval});
+
+
+while (my $cl = $clan_update->next) {
+	push @clans_to_update,  $cl->clan_acc;
 }
 
+for my $clan (@clans_to_update) {
+	my $clan_rs = $rfamdb->resultset('ClanMembership')->search({clan_acc => $clan});
+	while (my $row = $clan_rs->next) {
+		my ($clan_acc, $rfam_acc) = ($row->clan_acc->clan_acc, $row->rfam_acc->rfam_acc);
+		push @{$clans{$clan_acc}}, $rfam_acc;
+	}
+}
 
 for my $clan (keys %clans) {
 	my $start_family = shift @{$clans{$clan}};
@@ -37,6 +53,9 @@ for my $clan (keys %clans) {
 	my $familyObj = $familyIO->loadRfamFromSVN($start_family, $client);
     my $error = Bio::Rfam::QC::findClanOverlaps($familyObj, $rfamdb, $config,  \@{$clans{$clan}});
 }
+
 exit;
+
+
 
 
