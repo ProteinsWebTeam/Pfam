7ea849842b4c6ce02b30d9af16fc951f50b244f0
swb <swb@98333f76-0502-11df-81af-936bbf3a1e26>
2014-06-10 14:58:18 +0000

    Plugin to scan the PDB against each family. Loads results into DB. Working, but needs log files added

---
+++ trunk/Rfam/Lib/Bio/Rfam/View/Plugin/PDB.pm
@@ -0,0 +1,101 @@
+package Bio::Rfam::View::Plugin::PDB;
+
+use Moose;
+with 'MooseX::Role::Pluggable::Plugin';
+
+use Data::Printer;
+use Carp;
+use File::Temp;
+sub process{
+	my $self = shift;
+	$self->mapPDB;
+}
+#
+
+sub mapPDB {
+
+	my ($self) = @_;	
+	#Sorry Rob, a hardcoded path. Need to put this someplace more permanent.
+	#This file is the fasta file of all the non-protein nucleic acid sequences in the PDB.
+	#Those with modified bases have been excluded.
+	#Documentation on Confluence as to how to produce this fasta file.
+	#
+	my $PDB_fasta = '/nfs/production/xfam/rfam/CURATION/PDB/pdb_trimmed_noillegals.fa';	
+	my $config = $self->parent->config;
+	my $client = Bio::Rfam::SVN::Client->new({config => $config});
+	my $familyIO = Bio::Rfam::FamilyIO->new;	
+	my $familyObj = $self->parent->family;
+	my $rfam_acc = $familyObj->DESC->AC;
+	my $prefix = "$rfam_acc.XXXX";
+
+	# Generate a bunch of temporary files for the cmscan job:
+	#
+	my $out = File::Temp->new(TEMPLATE => $prefix,
+							 DIR => '/tmp',
+							 SUFFIX => '.out');
+	my $tblout = File::Temp->new(TEMPLATE => $prefix,
+								DIR => '/tmp',
+								SUFFIX => '.tblout');
+
+	my $logfile =  File::Temp->new(TEMPLATE => $prefix,
+									DIR => '/tmp',
+									SUFFIX => '.log');
+
+	my $rfamdb = $self->parent->config->rfamlive;
+
+	# File to put the CM which we extract from the SVN:
+	#
+	my $cm_file = "/tmp/$prefix.CM";
+	my $cm = $familyObj->CM;
+    $familyIO->writeAnnotatedCM($cm, $familyObj,$cm_file);
+	
+	# cmpress the CM and run the cmscan job:
+	#
+	my $cmpress = "cmpress -F $cm_file";
+	system ($cmpress) ;
+	my $cmscan_cmd = "cmscan -o  $out --tblout $tblout --cut_ga $cm_file $PDB_fasta >>$logfile";
+	system ( $cmscan_cmd) ;
+ 	unlink glob "/tmp/$prefix.CM.*";	
+	#Array to store our PDB matches in:
+	my @pdbs;
+
+ 	open (CMSCAN,"<$tblout") or die "Can't open CMscan results\n";
+	while (<CMSCAN>) {
+		my $line = $_;
+		chomp $line;
+		next if ($line =~ /^#/);
+		my @result = split (/\s+/, $line);
+		my ($pdb_id, $chain) = split(/_/, $result[2]);
+		push @pdbs, {rfam_acc => $rfam_acc,
+					pdb_id =>$pdb_id,
+					chain => $chain,
+					pdb_start => $result[7],
+					pdb_end => $result[8],
+					bit_score => $result[14],
+					evalue_score => $result[15],
+					cm_start => $result[5],
+					cm_end => $result[6] };	
+	} 
+	my $resultset = $rfamdb->resultset('PdbFullRegion')->search({rfam_acc => $rfam_acc});
+ 	
+	#Delete all existing matches so we don't have duplicates:
+	#
+	$resultset->delete_all;
+	
+	#Load new PDB hits into DB:	
+	for my $hit (@pdbs) {
+		$resultset->create( {rfam_acc =>$hit->{rfam_acc},
+							pdb_id => $hit->{pdb_id},
+							chain => $hit->{chain},
+							pdb_start => $hit->{pdb_start},
+							pdb_end => $hit->{pdb_end},
+							bit_score => $hit->{bit_score},
+							evalue_score => $hit->{evalue_score},
+							cm_start => $hit->{cm_start},
+							cm_end => $hit->{cm_end}
+							});
+	}
+
+} 
+	
+	
