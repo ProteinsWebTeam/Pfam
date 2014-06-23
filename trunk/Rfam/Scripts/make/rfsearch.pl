27320c7deeb8ca518dffb5ce40883f8fc1591d74
nawrockie <nawrockie@98333f76-0502-11df-81af-936bbf3a1e26>
2014-06-23 18:24:43 +0000

    Removing debugging print statements accidentally left in.

--- trunk/Rfam/Scripts/make/rfsearch.pl
+++ trunk/Rfam/Scripts/make/rfsearch.pl
@@ -824,7 +824,6 @@ if ((! $only_build) && ((! $no_search) || ($allow_no_desc))) {
   my @rev_errOA    = (); # names of error files for reversed searches
 
   submit_cmsearch_jobs($config, $ndbfiles, "s.",  $searchopts, $cmfile, \@dbfileA, \@jobnameA, \@tblOA, \@cmsOA, \@errOA, $ssopt_str, $q_opt);
-  exit 0;
   if($rev_ndbfiles > 0) { 
     submit_cmsearch_jobs($config, $rev_ndbfiles, "rs.", $rev_searchopts, $cmfile, \@rev_dbfileA, \@rev_jobnameA, \@rev_tblOA, \@rev_cmsOA, \@rev_errOA, $ssopt_str, $q_opt);
   }
