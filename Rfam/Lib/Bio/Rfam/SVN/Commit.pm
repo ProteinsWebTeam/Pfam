bd83edb473236aa7a51e4b2857d75efef45c27cc
evan <evan@98333f76-0502-11df-81af-936bbf3a1e26>
2014-07-02 15:06:38 +0000

    Fixed bugs with inserting litereature from motifs into RfamLive. Now it is consistent with families and clans

--- Rfam/Lib/Bio/Rfam/SVN/Commit.pm
+++ Rfam/Lib/Bio/Rfam/SVN/Commit.pm
@@ -659,7 +659,7 @@ sub commitMotif {
     $rfamdb->resultset('Motif')->createMotifFromObj($motifObj);
   }
   
-  $rfamdb->resultset('MotifLiterature')->find_or_createFromMotifObj( $motifObj );
+  $rfamdb->resultset('LiteratureReference')->find_or_createFromMotifObj( $motifObj );
   #$rfamdb->resultset('DatabaseLink')->find_or_createFromMotifObj( $motifObj );  
   
   #Finish the transaction.
--- Rfam/Schemata/RfamLive/ResultSet/LiteratureReference.pm
+++ Rfam/Schemata/RfamLive/ResultSet/LiteratureReference.pm
@@ -50,4 +50,26 @@ sub find_or_createFromClanObj {
   }
 }
 
+sub find_or_createFromMotifObj {
+  my ($self, $motifObj) = @_;
+
+  if(!$motifObj or !$motifObj->isa('Bio::Rfam::Motif')){
+    croak('Either the Bio::Rfam::Motif object was undefined or not an object of that type.');
+  }
+  if(defined($motifObj->DESC->REFS)){
+    foreach my $ref (@{$motifObj->DESC->REFS}){
+      my $lit = $self->find_or_create( { pmid => $ref->{RM},
+                                         title => $ref->{RT},
+                                         author => $ref->{RA},
+                                         journal => $ref->{RL}
+      } );
+      $lit->update_or_create_related( 'motif_literature_references', #the relationship
+                                      { motif_acc    => $motifObj->DESC->AC,
+                                        pmid         => $ref->{RM},
+                                        comment      => defined($ref->{RC}) ? $ref->{RC} : '',
+                                        order_added  => $ref->{RN}} );
+    }
+  }
+}
+
 1;
