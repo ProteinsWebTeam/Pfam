9fea35132a962e4806c4aaf17f5411b6dbeb1ba1
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-07-21 13:21:10 +0000

    DBIC wrapper for the rfam_jobs database.

---
+++ Rfam/Schemata/RfamJobs.pm
@@ -0,0 +1,20 @@
+use utf8;
+package RfamJobs;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07033 @ 2014-07-10 13:01:16
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:pOmNltUuQ7sbnVWzoABjGg
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
