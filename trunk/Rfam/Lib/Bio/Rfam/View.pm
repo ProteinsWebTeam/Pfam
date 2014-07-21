c5458d52955b265a6c9078413bcd58c8cf776323
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-07-21 13:24:13 +0000

    A new, generic view process script, plus a tweaked Bio::Rfam::View class with some of the cargo-culted code commented but not yet removed. We'll see what breaks with it commented and remove it once we're sure nothing too terrible happens.

--- trunk/Rfam/Lib/Bio/Rfam/View.pm
+++ trunk/Rfam/Lib/Bio/Rfam/View.pm
@@ -1,14 +1,13 @@
 package Bio::Rfam::View;
 
-use strict;
-use warnings;
-use Carp;
 use Moose;
 use Moose::Util::TypeConstraints;
 
+use Carp;
+
 use Bio::Rfam::Family;
-use Bio::Rfam::FamilyIO;
 use Bio::Rfam::MooseTypes;
+# use Bio::Rfam::FamilyIO;
 
 with 'MooseX::Role::Pluggable';
 
@@ -36,34 +35,35 @@ has job => (
   isa => 'Object'
 );
 
+# the enum here has to marry up with the enum in the database table
 has seqdb => (
   is => 'ro',
-  isa => enum(['rfamseq']),
+  isa => enum( [ qw( rfamseq genome refseq ) ] ),
   required => 1
 );
 
-sub example_method_on_view_object {
-  my $this = shift;
-  print 'Bio::Rfam::View::View::view: db: ' . $this->db . "\n";;
-  print 'Bio::Rfam::View::View::view: family: ' . $this->family . "\n";;
-}
-
-sub startJob {
-  my ($self) = @_;
-
-  if(!defined($self->job)){
-    my $jobRow = $self->config
-                       ->rfamlive
-                        ->resultset('PostProcess')
-                          ->search({ rfam_acc => $self->family->DESC->AC,
-                                     uuid     => $self->job_uuid })->first;
-   if(defined($jobRow)){
-     $self->job($jobRow);
-   }else{
-     croak("Falied to find job for ".$self->family->DESC->AC." and uuid ".$self->job_uuid);
-   }
-  }
-  $self->job->start;
-}
+# sub example_method_on_view_object {
+#   my $this = shift;
+#   print 'Bio::Rfam::View::View::view: db: ' . $this->db . "\n";;
+#   print 'Bio::Rfam::View::View::view: family: ' . $this->family . "\n";;
+# }
+#
+# sub startJob {
+#   my ($self) = @_;
+#
+#   if(!defined($self->job)){
+#     my $jobRow = $self->config
+#                        ->rfamlive
+#                         ->resultset('PostProcess')
+#                           ->search({ rfam_acc => $self->family->DESC->AC,
+#                                      uuid     => $self->job_uuid })->first;
+#    if(defined($jobRow)){
+#      $self->job($jobRow);
+#    }else{
+#      croak("Falied to find job for ".$self->family->DESC->AC." and uuid ".$self->job_uuid);
+#    }
+#   }
+#   $self->job->start;
+# }
 
 1;
