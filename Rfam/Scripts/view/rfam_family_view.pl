fde867e06df4b6a4c38101c7acc76d9c5c6572cd
jgt <jgt@98333f76-0502-11df-81af-936bbf3a1e26>
2014-07-23 14:10:18 +0000

    Added support for multiple view sets. Specify one or more view plugin sets, which have to be listed in the config, and the script will run all of the plugins from all of the sets. This hasn't been much tested, so early results will need a bit of checking.

--- Rfam/Scripts/view/rfam_family_view.pl
+++ Rfam/Scripts/view/rfam_family_view.pl
@@ -28,25 +28,21 @@ pod2usage( -exitval => 1, -verbose => 2 ) if $help;
 die "ERROR: you must specify both a job UUID (-id) and a family accession (-f)\n"
   unless ( defined $job_uuid and defined $rfam_acc );
 
-die "not a valid UUID ($job_uuid)\n"
+die "ERROR: not a valid UUID ($job_uuid)\n"
   unless ( $job_uuid =~ m/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i );
 
-die "not a valid Rfam family accession ($rfam_acc)\n"
+die "ERROR: not a valid Rfam family accession ($rfam_acc)\n"
   unless ( $rfam_acc =~ m/^RF\d{5}$/i );
+
+# anything else on the command line will be taken to be the name of a plugin 
+# set
+die "ERROR: you must specify at least one plugin set\n"
+  unless scalar @ARGV;
 
 #-------------------------------------------------------------------------------
 # setup 
 
 my $config  = Bio::Rfam::Config->new;
-my $plugins = $config->viewPluginSets('family');
-
-my $view = Bio::Rfam::View->new ( {
-  plugins   => $plugins,
-  config    => $config,
-  family    => $rfam_acc,
-  job_uuid  => $job_uuid,
-  seqdb     => 'rfamseq'
-} );
 
 # we need to get the row in the job_history table for this job, so that we can
 # update its status before and after running the plugins. It's also a good
@@ -77,17 +73,30 @@ die "couldn't find a row for this job (job ID $job_uuid) in the tracking table"
 #-------------------------------------------------------------------------------
 # call the plugins
 
-$job->run;
+foreach my $plugin_set ( @ARGV ) {
 
-foreach my $plugin ( @{ $view->plugin_list } ) {
-  $plugin->process;
+  my $plugins = $config->viewPluginSets( $plugin_set );
+
+  my $view = Bio::Rfam::View->new ( {
+    plugins   => $plugins,
+    config    => $config,
+    family    => $rfam_acc,
+    job_uuid  => $job_uuid,
+    seqdb     => 'rfamseq'
+  } );
+
+  $job->run;
+
+  foreach my $plugin ( @{ $view->plugin_list } ) {
+    $plugin->process;
+  }
+
+  $job->done;
+
+  # (the $job row object has methods "run", "fail" and "done, which set the
+  # status for that row and update the "closed" time stamp too. See
+  # RfamJobs/Result/JobHistory.pm.)
 }
-
-$job->done;
-
-# (the $job row object has methods "run", "fail" and "done, which set the
-# status for that row and update the "closed" time stamp too. See
-# RfamJobs/Result/JobHistory.pm.)
 
 exit;
 
@@ -103,7 +112,8 @@ rfam_family_view.pl - run the "view process" for an Rfam family
 
 =head1 SYNOPSIS
 
-  rfam_family_view.pl -id 5EF780FE-2D0E-11E3-8C47-A34D104C94EC -family RF00504
+  # runs the plugins in the "family" plugin set for Rfam family RF00504
+  rfam_family_view.pl -id 5EF780FE-2D0E-11E3-8C47-A34D104C94EC -f RF00504 family
 
 =head1 DESCRIPTION
 
@@ -111,7 +121,29 @@ This is a script to run the Rfam view process for a given family. It's intended
 to be run by the job dequeuer, which polls the rfam_jobs.job_history table for
 pending view process jobs, and runs this script on the farm via "bsub".
 
-=head1 OPTIONS
+We have the concept of plugin sets, so that we can group together view plugins
+which have common features. For example, there may be plugins that are based on
+sequence data, versus others that use genome data. These can be collected
+together in separate sets and then run separately or as part of the same view
+process job. 
+
+The configuration file should define the view sets in a section like this:
+
+  <view_sets> 
+    rfamseq Alignment
+    rfamseq Species
+    rfamseq SecondaryStructure
+    rfamseq Tree
+    genome Search
+    genome Tree
+  </view_sets> 
+
+This block defined two plugin sets, I<rfamseq> and I<genome>. To run both sets
+in the same view process run, add both to the command line arguments:
+
+  rfam_family_view.pl -id 5EF780FE-2D0E-11E3-8C47-A34D104C94EC -f RF00504 rfamseq genome
+
+=head1 REQUIRED OPTIONS
 
 =over 8
 
@@ -131,6 +163,13 @@ print this help text
 
 The UUID should normally come from the tracking database, having been generated
 by the commit script that commissioned the view process job originally.
+
+=head1 ARGUMENTS
+
+The script needs to know which sets of plugins to run. The available sets
+should be configured in the C<view_sets> section of the configuration. You
+can run more than one set of plugins by adding multiple view set names to
+the command line.
 
 =head1 CONFIGURATION
 
