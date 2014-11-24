49004be36bc41b88911cb691fdcc7b199499e403
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2014-11-24 12:40:55 +0000

    added primary key

--- trunk/PfamSchemata/PfamLive/Result/ClanMembership.pm
+++ trunk/PfamSchemata/PfamLive/Result/ClanMembership.pm
@@ -82,6 +82,7 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:YXwndJ1etAuPLEO+Dfp4AQ
 
+ __PACKAGE__->set_primary_key( "clan_acc", "pfama_acc");
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
