2a7a17e41e86b763ce6189282d785095efac1481
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2014-12-12 08:59:47 +0000

    hashed out final QC step for family commit temporarily as overlap check not yet working

--- trunk/PfamLib/Bio/Pfam/SVN/Commit.pm
+++ trunk/PfamLib/Bio/Pfam/SVN/Commit.pm
@@ -372,14 +372,15 @@ sub _qualityControlFamily {
      
   }  
 
-  if(defined($msg) and $msg !~ /Release \S+ update/){
-    my $compete = 1;
-    my $overlaps = Bio::Pfam::PfamQC::family_overlaps_with_db( $family,\%ignore , undef, $pfamDB, $famObj, $compete );
-    warn "$family: found $overlaps overlaps\n";
-    if ($overlaps) {
-      confess("Found overlaps\n");
-    }
-  }
+#TODO reinstate this block
+#  if(defined($msg) and $msg !~ /Release \S+ update/){
+#    my $compete = 1;
+#    my $overlaps = Bio::Pfam::PfamQC::family_overlaps_with_db( $family,\%ignore , undef, $pfamDB, $famObj, $compete );
+#    warn "$family: found $overlaps overlaps\n";
+#    if ($overlaps) {
+#      confess("Found overlaps\n");
+#    }
+#  }
 }
 
 sub moveClan {
