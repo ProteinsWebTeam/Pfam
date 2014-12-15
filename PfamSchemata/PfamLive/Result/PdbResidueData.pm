2cdf6918b8016cbb779854fa956aa4d0ec275d36
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2014-12-15 15:57:22 +0000

    added _PACKAGE__->might_have( pfam_a_reg_full_significants ....

--- PfamSchemata/PfamLive/Result/PdbResidueData.pm
+++ PfamSchemata/PfamLive/Result/PdbResidueData.pm
@@ -150,6 +150,8 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:9xe1hFhfREWFjn7vjCqjAw
 
+__PACKAGE__->might_have( pfam_a_reg_full_significants => "PfamLive::Result::PfamARegFullSignificant",
+ { "foreign.pfamseq_acc" => "self.pfamseq_acc"});
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
