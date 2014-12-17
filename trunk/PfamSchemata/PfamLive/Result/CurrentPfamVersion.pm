68206c9e2c65ef9be3c387113a165c898357951b
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2014-12-17 14:13:48 +0000

    added primary key and unique constraint

--- trunk/PfamSchemata/PfamLive/Result/CurrentPfamVersion.pm
+++ trunk/PfamSchemata/PfamLive/Result/CurrentPfamVersion.pm
@@ -90,6 +90,8 @@ __PACKAGE__->belongs_to(
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:7R0/zPDFyJK6Vw22i1iS8w
 
+__PACKAGE__->set_primary_key("pfama_acc");
+__PACKAGE__->add_unique_constraint("pfamA_acc", ["pfama_acc"]);
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
