a624f31ab77f393f8ad49ec98f59f6b07296ebae
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2015-01-02 09:54:54 +0000

    added added PACKAGE__->might_have clan_membership

--- trunk/PfamSchemata/PfamLive/Result/PfamARegFullSignificant.pm
+++ trunk/PfamSchemata/PfamLive/Result/PfamARegFullSignificant.pm
@@ -250,7 +250,9 @@ __PACKAGE__->belongs_to(
 
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:LDMV0uCiWmzSQ9+eaL+cpA
-
+__PACKAGE__->might_have(
+	"clan_membership" => 'PfamLive::Result::ClanMembership',
+ 	{ 'foreign.pfama_acc' => 'self.pfama_acc' } );
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
