880fbaffa76594416c39d80290bc90885226b496
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2015-01-13 09:15:28 +0000

    sychronised the database objects used by Perl with those in the database schema.

--- trunk/PfamSchemata/PfamLive.pm
+++ trunk/PfamSchemata/PfamLive.pm
@@ -12,8 +12,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:LwHhTNADw9PhaQW68m+7ow
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-01-13 08:53:22
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jLtiSxHflgOiiKyoaHei+A
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
