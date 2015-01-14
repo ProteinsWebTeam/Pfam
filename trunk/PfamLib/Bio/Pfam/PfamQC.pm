241bd0a688835996b63bbe93997d799d597b26fa
testuser <testuser@98333f76-0502-11df-81af-936bbf3a1e26>
2015-01-14 10:26:27 +0000

    Modified the pfamDBAdmin connection in family_overlaps_with_db

--- trunk/PfamLib/Bio/Pfam/PfamQC.pm
+++ trunk/PfamLib/Bio/Pfam/PfamQC.pm
@@ -950,10 +950,21 @@ sub family_overlaps_with_db {
     foreach my $seq (@seedAccs) {
       $tempAccs{$seq->id}=1;
     }
-    
-    my $dbh = $pfamDBAdmin->getSchema->storage->dbh;
-    my $db_name=$config->{Model}->{Pfamlive}->{database};
-    
+
+    #my $dbh = $pfamDBAdmin->getSchema->storage->dbh;
+    #my $db_name=$config->{Model}->{Pfamlive}->{database};
+    my $db_name="pfam_live";
+    my $dbh = DBI->connect( "dbi:mysql:database="
+                              . $config->pfamlive->{database}
+                              . ";host="
+                              . $config->pfamlive->{host}
+                              . ";port="
+                              . $config->pfamlive->{port},
+                            $config->pfamlive->{adminuser},
+                            $config->pfamlive->{adminpassword},
+                                {AutoCommit => 0}
+                          ) || die "Could not connect to database: $DBI::errstr";
+
     my $query = "CREATE TEMPORARY TABLE $db_name.tempAccs ( pfamseq_acc VARCHAR(10) NOT NULL,   PRIMARY KEY (pfamseq_acc));";
     my $sth = $dbh->prepare($query);
     $sth->execute() or die "Can't execute statement: $DBI::errstr";
