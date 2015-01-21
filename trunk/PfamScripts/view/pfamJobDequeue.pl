3cddc9993d40fa4e30d50e1513bf7e7186648b52
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2015-01-21 11:01:47 +0000

    updated bsub parameters to work on EBI farm

--- trunk/PfamScripts/view/pfamJobDequeue.pl
+++ trunk/PfamScripts/view/pfamJobDequeue.pl
@@ -20,6 +20,7 @@ use IO::File;
 use Getopt::Long;
 use File::Rsync;
 use LSF::Job;
+use DDP;
 
 # Our Module Found in Pfam-Core
 use Bio::Pfam::Queues::IntQueue;
@@ -50,6 +51,7 @@ while (1) {
     #Step 1 - There is a job to run!
     my $error = 0;
     my ( @cmds, $memory, $tmpDir, $queue, $resource );
+    $queue = 'production-rh6'; #queue will always be production-rh6
 
     #Step 2 - Build up the resource requirements!
     if ( $ref->{'job_type'} eq "family" ) {
@@ -58,39 +60,31 @@ while (1) {
       #Depending on the size of the family, dictates where the job should be scheduled to!
       if ( $ref->{'entity_size'} ) {
         if ( $ref->{'entity_size'} < 3000 ) {
-          #Use small
-          $memory = '4000000';
-          $resource = "select[mypfamlive2<200 && type==X86_64 && mem>4000] rusage[mypfamlive2=10:mem=4000]";
+          $memory = '4000';
+          $resource = "rusage[mem=4000]"; 
           $tmpDir = $qsout->tmpDir;
-          $queue  = 'normal';
         }elsif( $ref->{'entity_size'} >= 3000 and $ref->{'entity_size'} < 10000 ){
-          #Use normal
-          $resource = 'select[mem>4000 && mypfamlive2<200 && type==X86_64] rusage[mypfamlive2=10:mem=4000]';
-          $memory = '4000000'; #Request 4 GB of memory
+          $resource = "rusage[mem=4000]"; 
+          $memory = '4000';
           $tmpDir = $qsout->tmpDir;
-          $queue  = 'normal';
         }elsif( $ref->{'entity_size'} > 30000 ){
-          #Use normal
-          $resource = 'select[mem>63000 && mypfamlive2<200 && model==BL460C_2_26 ] rusage[mypfamlive2=10:mem=63000]';
-          $memory = '63000000'; #Request 64 GB of memory
+          $resource = "rusage[mem=63000]"; 
+          $memory = '63000';
           $tmpDir = $qsout->tmpDir;
-          $queue  = 'hugemem';
         
         }else {
-          #Higher memory requirements and run on long
-          $memory = '15000000'; #Request 7 GB of memory
-          $resource = 'select[type==X86_64 && mem>15000 && mypfamlive2<200] rusage[mypfamlive2=10:mem=15000]';
+          #Higher memory requirements 
+          $memory = '15000';
+          $resource = "rusage[mem=15000]"; 
           $tmpDir = $qsout->tmpDir;
-          $queue  = "long";
         }
       }
       else {
 
         #If we do not have a size of family, this will work in most cases.
-        $memory = '7000000'; #We want 7GB of memory
-        $resource = 'select[type==X86_64 && mem>7000 && mypfamlive2<200] rusage[mypfamlive2=10:mem=7000]';
+        $memory = '7000';
+        $resource = "rusage[mem=7000]"; 
         $tmpDir = $qsout->tmpDir;
-        $queue  = 'long';
       }
 
       #Step 3 - Build up the command that we want to run
@@ -100,15 +94,14 @@ while (1) {
       $cmd .= " && rm -fr " . $tmpDir . "/".$user."/" . $ref->{'job_id'};
       push( @cmds, $cmd );
 
-    }
+    } #end of job type = family
     elsif ( $ref->{'job_type'} eq 'clan' ) {
       #Repeat steps 2 and 3 if we have a clan view process to run.  
 
       #Step 2 - build up the LSF resource requirements
-      $memory   = '7000000'; #We want 7GB of memory
-      $resource = 'select[type==X86_64 && mem>7000 && mypfamlive2<200] rusage[mypfamlive2=10:mem=7000]';
+      $memory = '7000';
+      $resource = "rusage[mem=7000]"; 
       $tmpDir   = $qsout->tmpDir;
-      $queue    = 'long';
 
       #Step 3 - Build up the command that we want to run
       my $cmd = $ref->{'command'};
