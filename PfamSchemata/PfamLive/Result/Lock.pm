2412c092b763ed11f92bcb484be052bc784dfcee
ruthe <ruthe@98333f76-0502-11df-81af-936bbf3a1e26>
2015-02-18 16:04:41 +0000

    added primary key

--- PfamSchemata/PfamLive/Result/Lock.pm
+++ PfamSchemata/PfamLive/Result/Lock.pm
@@ -61,6 +61,9 @@ __PACKAGE__->add_columns(
 # Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:V4/+7th/dp04faOPYaUz6A
 
+__PACKAGE__->set_primary_key(
+    "locked"
+);
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
