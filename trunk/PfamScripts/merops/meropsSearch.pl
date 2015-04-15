5dfc145363d53a240aa2ec6da51e8d292ba69c66
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-04-15 12:01:34 +0000

    Use envelope co-ordinates in ALIGN (previously used aligment co-ordinates)

--- trunk/PfamScripts/merops/meropsSearch.pl
+++ trunk/PfamScripts/merops/meropsSearch.pl
@@ -6,13 +6,13 @@ use IO::File;
 use Cwd;
 
 #Set up the parameters
-my $hmmerBin="/nfs/production/xfam/pfam/software/bin";
+my $hmmerBin="/nfs/production/xfam/users/jaina/merops/bin/";
 my $cpu = 4;
 my $seqDB="/nfs/production/xfam/users/jaina/merops/db/uniprot_trembl_sprot.fasta";
 my $queue="production-rh6";
 my $memory_mb=4000;
 my $evalue=0.1;
-
+my $dom_se_script="/nfs/production/xfam/pfam/software/Scripts/PfamScripts/merops/dom_se.pl";
 
 #Run hmmbuild
 my $hmmbuildCmd="$hmmerBin/hmmbuild -o /dev/null HMM SEED";
@@ -28,14 +28,15 @@ $fh->open( "| bsub -q $queue -n $cpu -R \"rusage[mem=$memory_mb]\" -M $memory_mb
 $fh->print( "cd $cwd\n" );
 
 #Run hmmsearch
-my $hmmsearchCmd="$hmmerBin/hmmsearch -E $evalue --domtblout hmmsearchTbl --cpu 4 -o hmmsearchOut -A hmmsearchAln HMM $seqDB";
+my $hmmsearchCmd="$hmmerBin/hmmsearch -E $evalue --domtblout hmmsearchTbl --cpu 4 -o hmmsearchOut HMM $seqDB";
 $fh->print("$hmmsearchCmd\n");
   
-#Make fasta file of sequences in hmmsearchAln    
-my $eslCmd="$hmmerBin/esl-reformat -o FA fasta hmmsearchAln";
+#Make fasta file of sequences in hmmsearch output    
+$fh->print("$dom_se_script\n"); #This script creates a file of domain envelope start ends called dom_se
+my $eslCmd="$hmmerBin/esl-sfetch -o FA -Cf $seqDB dom_se"; #Create fasta file FA
 $fh->print("$eslCmd\n");
 
-#Run hmmalign on sequnces and include SEED alignment
+#Run hmmalign on FA and include SEED alignment
 my $hmmalignCmd="$hmmerBin/hmmalign -o tmpAln --outformat pfam --mapali SEED HMM FA";
 $fh->print("$hmmalignCmd\n");
 
