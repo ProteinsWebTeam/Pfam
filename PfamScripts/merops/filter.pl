5b2a985fac48e244e0111b568cbcdaa8f1f46a66
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-05-08 13:42:18 +0000

    Filter out domains that are outcompeted Tidied formatting a little

--- PfamScripts/merops/filter.pl
+++ PfamScripts/merops/filter.pl
@@ -40,52 +40,71 @@ my $st_subfamily = $dbh->prepare("SELECT Name, MERNO, PU, Features.AC, Features.
 my $hmmerBin="/nfs/production/xfam/users/jaina/merops/bin/"; 
 
 #Go through each family and filter based match length and presence of active sites
+#Ignore domains that are outcompeted, if any
 foreach my $fam (@families)  {
+  next unless(-d $fam);
+
   print STDERR "Doing $fam\n";
 
   my $hmmLength=hmmLength("$fam/HMM");
+
+  my %outcompeted;
+  if(-s "$fam/outcompeted.txt") {
+    open(OUTCOM, "$fam/outcompeted.txt") or die "Couldn't open fh to $fam/outcompeted, $!";
+    while(<OUTCOM>) {
+      if(/^\S+ \S+ \((\S+\/\d+-\d+)\,/) {
+         $outcompeted{$1}=1; 
+       }
+       else {
+         print STDERR "Failed to parse this line in $fam/outcompeted.txt:\n$_";
+       }
+    }
+    close OUTCOM;
+  }
   
   #Now store active site data for holotype sequences, and store sequences from hmmer search in a hash for later
-  my (%sequences, %alignment, %seed, %uniquePattern, %asPattern, %acc_se, %alignment2);
+  my (%sequences, %alignment, %seed, %uniquePattern, %asPattern, %acc_se);
   my $align="$fam/ALIGN";
   open(ALIGN, $align) or die "Couldn't open fh to $align, $!";
   while(<ALIGN>) {
     if(/^((\S+)\/(\d+)-(\d+))\s+(\S+)/) {
       my ($acc_se, $acc, $start, $end, $seq)=($1, $2, $3, $4, $5);
-
       #Store a shorter version of sprot and trembl accession
       #sp|Q9XEC4|APA3_ARATH/70-508 becomes Q9XEC4/70-508
       #tr|A0A067GI16|A0A067GI16_CITSI/72-514 becomes A0A067GI16/72-514
       #Use the shortened version for printing alignments later
-      my $short_acc;
+      my $short_acc_se;
       if($acc_se =~ /[tr|sp]\|(\S+)\|\S+/) {
-	$short_acc="$1/$start-$end"
+        $short_acc_se="$1/$start-$end"
       }
       else {
-	$short_acc="$acc/$start-$end";
+        $short_acc_se="$acc/$start-$end";
       }
-      $short_acc=sprintf('%-30s', $short_acc); #Uniprot acc can be 10 char, so 30 characters for the acc/start-ends should give plenty of room
+
+      next if(exists($outcompeted{$short_acc_se})); #Ignore domains that are outcompeted
+     
+      $short_acc_se=sprintf('%-30s', $short_acc_se); #Uniprot acc can be 10 char, so 30 characters for the acc/start-ends should give plenty of room
       
       if($acc =~ /^H\|/) { #It's a holotype seq
-	#Extract holotypes active sites for holotype seq from db
-	#Populate hash %uniquePattern - the keys are unique active site patterns in the form '67C:308[S|T]:311H:' 
-	#Populate hash %asPatterns - in the form $asPattern{$acc_se}->{$colNumber}=$residue
+        #Extract holotypes active sites for holotype seq from db
+        #Populate hash %uniquePattern - the keys are unique active site patterns in the form '67C:308[S|T]:311H:' 
+        #Populate hash %asPatterns - in the form $asPattern{$acc_se}->{$colNumber}=$residue
 	
-	my $seqObj = Bio::LocatableSeq->new('-id'=>$acc, '-start'=>$start, '-end'=>$end, 'seq' => $seq);
-	if($fam =~ /[A-Z]\d+[A-Z]/) {  #It's a subfamily
-	  getActSites($seqObj, $st_subfamily, $fam, $acc, $acc_se, \%uniquePattern, \%asPattern, $start, $end);
-	}
-	else { #It's a family
- 	  getActSites($seqObj, $st_family, $fam, $acc, $acc_se, \%uniquePattern, \%asPattern, $start, $end);
-	}
-	$seed{$acc_se}=$short_acc."$seq\n";
+        my $seqObj = Bio::LocatableSeq->new('-id'=>$acc, '-start'=>$start, '-end'=>$end, 'seq' => $seq);
+        if($fam =~ /[A-Z]\d+[A-Z]/) {  #It's a subfamily
+          getActSites($seqObj, $st_subfamily, $fam, $acc, $acc_se, \%uniquePattern, \%asPattern, $start, $end);
+        }
+        else { #It's a family
+          getActSites($seqObj, $st_family, $fam, $acc, $acc_se, \%uniquePattern, \%asPattern, $start, $end);
+        }
+        $seed{$acc_se}=$short_acc_se."$seq\n";
       }
       elsif($acc =~ /^MER\d+/) { #It's from the original clustal phylum alignment from Neil
-	$seed{$acc_se}=$short_acc."$seq\n";
+        $seed{$acc_se}=$short_acc_se."$seq\n";
       }
       else { #It's from the hmmer3 search against uniprot, store for later
-	$sequences{$acc_se}=$seq;
-	$alignment{$acc_se}=$short_acc."$seq\n";
+        $sequences{$acc_se}=$seq;
+        $alignment{$acc_se}=$short_acc_se."$seq\n";
       }
     }
     elsif(/^\/\//) {
@@ -102,7 +121,7 @@ foreach my $fam (@families)  {
   #Filter sequences based on match length
   #If active site data - filter on whether seq contains active site pattern
   #Sequences that do not pass filters go into a file called additionalHits
-  #Sequences that pass filter go into a file called homologues
+  #Sequences that pass filter go into a file called homologues.aln
   open(ADDITIONAL, ">$fam/additionalHits") or die "Couldn't open fh to $fam/additionalHits, $!";
   open(HOMOLOGUES, ">$fam/homologues.aln") or die "Couldn't open fh to $fam/homologues.aln, $!";
 
@@ -135,14 +154,14 @@ foreach my $fam (@families)  {
       
       my $match=lookForActSite($sequences{$acc_se}, \%asPattern, $fam);
       if($match) {
-	print HOMOLOGUES $alignment{$acc_se};
+        print HOMOLOGUES $alignment{$acc_se};
       }
       else {
-	print ADDITIONAL $alignment{$acc_se};
+        print ADDITIONAL $alignment{$acc_se};
       }
     }
     else {
-      #It's passed the length filter, and there is no active site data
+      #It's passed the length filter, and there are no active site data
       print HOMOLOGUES $alignment{$acc_se}
     }
   }
@@ -174,6 +193,7 @@ sub hmmLength{
     die "Couldn't get length of HMM from $hmm" unless($hmmLength);
   }
 }
+
 sub getActSites {
   my ($seqObj, $sth, $fam, $acc, $acc_se, $uniquePattern, $asPattern, $start, $end) = @_;
 
@@ -196,72 +216,71 @@ sub getActSites {
       
  
       for(my $i=0; $i<@asPos; $i++) {
-	next unless($asPos[$i] and $asRes[$i]); #Ignore if there is residue position ($asPos) but no residue ($asRes)
-	my $resNum;
-	if($asPos[$i] =~ /\S(\d+)/) {
-	  $resNum=$1;
-	}
-	else {
-	  die "Couldn't parse residue number for $acc_se (@asPos), array position $i ($asPos[$i])\n" unless($resNum);
-	}
-	
-	unless($resNum == 0 or ($resNum < $start or $resNum > $end)) { #Unless the residue falls outside the alignment
-	  my $colNumber = $seqObj->column_from_residue_number($resNum);
-	  #What to do if position is 0? Ignore all active sites on holotype?
-	  
-	  my $residue;
-	  if($asRes[$i] =~ /\//) {
-	    $residue = "[".$asRes[$i]."]";
-	    $residue =~ s/\//|/g;  #S/T becomes [S|T];
-	  }
-	  else {
-	    $residue = $asRes[$i];
-	  }
-	  
-	  $pattern.=$colNumber.$residue.":";    
-	  $tmpHash{$colNumber}=$residue;
-	}
+        next unless($asPos[$i] and $asRes[$i]); #Ignore if there is residue position ($asPos) but no residue ($asRes)
+        my $resNum;
+        if($asPos[$i] =~ /\S(\d+)/) {
+          $resNum=$1;
+        }
+        else {
+          die "Couldn't parse residue number for $acc_se (@asPos), array position $i ($asPos[$i])\n" unless($resNum);
+        }
+
+        unless($resNum == 0 or ($resNum < $start or $resNum > $end)) { #Unless residue is 0 (we don't know the position) or the residue falls outside the alignment
+          my $colNumber = $seqObj->column_from_residue_number($resNum);
+
+          my $residue;
+          if($asRes[$i] =~ /\//) {
+            $residue = "[".$asRes[$i]."]";
+            $residue =~ s/\//|/g;  #S/T becomes [S|T];
+          }
+          else {
+            $residue = $asRes[$i];
+          }
+
+          $pattern.=$colNumber.$residue.":";    
+          $tmpHash{$colNumber}=$residue;
+        }
       }
     }
-    
+
     if($fam =~ /^M/) { #It's a metallopeptidase, so need to look for metal binding sites too
       if($metLig and $acceptMetLig) {
-	my @metPos=split(/\, /, $metLig); #S270, S332
-	my @metRes=split(/, /, $acceptMetLig); #C, S/T
+        my @metPos=split(/\, /, $metLig); #S270, S332
+        my @metRes=split(/, /, $acceptMetLig); #C, S/T
 	
-	for(my $i=0; $i<@metPos; $i++) {
-	  my $metResNum;
-	  if($metPos[$i] =~ /\S(\d+)/) {
-	    $metResNum=$1;
-	  }
-	  else {
-	    die "Couldn't parse residue number for $acc_se (@metPos), array position $i ($metPos[$i])\n" unless($metResNum);
-	  }
-	  
-	  unless($metResNum == 0 or ($metResNum < $start or $metResNum > $end) ) {
-	    my $colNumber = $seqObj->column_from_residue_number($metResNum);
-	    
-	    my $residue;
-	    if($metRes[$i] =~ /\//) {
-	      $residue = "[".$metRes[$i]."]";
-	      $residue =~ s/\//|/g;  #S/T becomes [S|T];
-	    }
-	    else {
-	      $residue = $metRes[$i];
-	    }
-	    $pattern.=$colNumber.$residue.":";
-	    $tmpHash{$colNumber}=$residue;  
-	  }
-	}
+        for(my $i=0; $i<@metPos; $i++) {
+          my $metResNum;
+          if($metPos[$i] =~ /\S(\d+)/) {
+            $metResNum=$1;
+          }
+          else {
+            die "Couldn't parse residue number for $acc_se (@metPos), array position $i ($metPos[$i])\n" unless($metResNum);
+          }
+
+          unless($metResNum == 0 or ($metResNum < $start or $metResNum > $end) ) { #Unless residue is 0 (we don't know the position) or the residue falls outside the alignment
+            my $colNumber = $seqObj->column_from_residue_number($metResNum);
+
+            my $residue;
+            if($metRes[$i] =~ /\//) {
+              $residue = "[".$metRes[$i]."]";
+              $residue =~ s/\//|/g;  #S/T becomes [S|T];
+            }
+            else {
+              $residue = $metRes[$i];
+            }
+            $pattern.=$colNumber.$residue.":";
+            $tmpHash{$colNumber}=$residue;  
+          }
+        }
       }
     }
-    
+
     #Only keep patterns we haven't seen before
     if($pattern) {
       unless(exists($uniquePattern->{$pattern})) {
-	#print STDERR "$pattern\n";
-	$uniquePattern->{$pattern}=1;
-	$asPattern->{$acc_se}=\%tmpHash; 
+        #print STDERR "$pattern\n";
+        $uniquePattern->{$pattern}=1;
+        $asPattern->{$acc_se}=\%tmpHash; 
       }
     }
   }
@@ -297,8 +316,8 @@ sub lookForActSite {
     foreach my $asCol (sort { $a <=> $b } keys %{$asPattern->{$asSeq}}) { #asCol is the column position of the active site residue
       my $residue = substr($sequence, $asCol-1, 1); #Extract residue that is at the active site position
       if(!$residue or uc($residue) !~ /$asPattern->{$asSeq}->{$asCol}/) {  #eg /[S|T]/, see if residue is same as the allowed active site residue
-	$match=0;
-	last;
+        $match=0;
+        last;
       }
     }
     
@@ -309,7 +328,7 @@ sub lookForActSite {
       $sequenceNoGaps =~ s/\-//g;
       $sequenceNoGaps =~ s/\.//g;
       unless($sequenceNoGaps =~ /HE\w\wH/) {
-	$match=0;
+        $match=0;
       }
     }
     if($match) {
@@ -324,7 +343,7 @@ sub lookForActSite {
       $sequenceNoGaps =~ s/\-//g;
       $sequenceNoGaps =~ s/\.//g;
       unless($sequenceNoGaps =~ /HE\w\wH/) {
-	$match=0;
+        $match=0;
       }
     }
   }
