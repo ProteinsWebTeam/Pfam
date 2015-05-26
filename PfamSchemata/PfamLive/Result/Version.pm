d9739fb8540f987198cbed2fdb822a0d55eb1e05
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-05-26 11:04:13 +0000

    Updated to remove additional fields that were added during last update.

--- PfamSchemata/PfamLive/Result/Version.pm
+++ PfamSchemata/PfamLive/Result/Version.pm
@@ -88,9 +88,8 @@ __PACKAGE__->add_columns(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07039 @ 2014-05-19 08:45:26
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:OaYXzAdOEmBfOq4XaxFIXg
-
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-05-26 11:59:58
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:hxGDDkrxVBwlDQbPr6fuSg
 __PACKAGE__->set_primary_key( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
