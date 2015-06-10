a0762a6fc9c2f78a113d06655f8f968f82e3d847
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-06-10 15:21:02 +0000

    Debugged script to get it working:  Added missing modules and removed unneeded ones  Moved chdir statements to be in the correct place so script can find/write files  Stopped each chunk overwriting the previous chunks fasta file
    Commented out the section that moves pfamseq to /nfs - will revisit this later. This needs to happen after any antifam seq are removed from pfamseq. 
    -This line, and those below, will be ignored--
    
    M    pud-make_pfamseq_fasta.pl

--- trunk/PfamScripts/pud/pud-make_pfamseq_fasta.pl
+++ trunk/PfamScripts/pud/pud-make_pfamseq_fasta.pl
@@ -4,14 +4,11 @@
 
 use strict;
 use warnings;
-use Cwd;
-use LWP::UserAgent;
 use Getopt::Long;
 use Log::Log4perl qw(:easy);
-use Archive::Tar;
-use File::Touch;
 use File::Copy;
 use Bio::Pfam::Config;
+use Bio::Pfam::PfamLiveDBManager;
 
 my ( $statusdir, $pfamseq_dir );
 
@@ -37,25 +34,21 @@ unless ( $pfamseq_dir and -e $pfamseq_dir ) {
   help();
 }
 
-my $pwd = cwd;
-chdir($pfamseq_dir)
-  or $logger->logdie("Couldn't change directory into $pfamseq_dir $!");
-
-
 #get DBSIZE file
-open(DB, "DBSIZE") or $logger->logdie("Could not open DBSIZE:[$!]");
+open(DB, "$pfamseq_dir/DBSIZE") or $logger->logdie("Could not open $pfamseq_dir/DBSIZE:[$!]");
 my $dbsize;
 while(<DB>){
     ($dbsize) = $_ =~/(\S+)/;
     last;
 }
+close DB;
 
 #get sequences from pfamseq
 #if this is too slow/fails it could be worth trying a smaller offset or sending it to the farm with plenty of memory
 my $offset = 0;
 my $n = 1;
 my $total = 0;
-
+open (FA, ">$pfamseq_dir/pfamseq") or $logger->logdie("Cannot open pfamseq file to write");
 while ($total < $dbsize){
     $logger->debug("Querying database.... chunk $n offset $offset ");
     my $st = $dbh->prepare("select pfamseq_acc, seq_version, pfamseq_id, description, sequence from pfamseq limit 1000000 offset $offset") or $logger->logdie("Failed to prepare statement:".$dbh->errstr);
@@ -63,18 +56,16 @@ while ($total < $dbsize){
     my $rowno = $st->rows;
 
     my $array_ref = $st->fetchall_arrayref();
-    open (FA, ">pfamseq") or $logger->logdie("Cannot open pfamseq file to write");
     $logger->debug("Fetching results...");
     foreach my $row (@$array_ref) {
-
-	print FA ">" . $row->[0] . "." . $row->[1] . " " . $row->[2] . " " . $row->[3] . "\n" . $row->[4] . "\n";
+      print FA ">" . $row->[0] . "." . $row->[1] . " " . $row->[2] . " " . $row->[3] . "\n" . $row->[4] . "\n";
     }
-    close FA;
     $offset = $offset + 1000000;
     $total = $total + $rowno;
     $n++;
     $logger->debug("$total rows retrieved");
 }
+close FA;
 $dbh->disconnect;
 
 #make easel indexes
@@ -82,49 +73,51 @@ if(-e "$statusdir/esl-indexes") {
     $logger->debug("Already make easel indexes");
 } else {
     $logger->debug("Making easel indices for pfamseq");
+    chdir($pfamseq_dir) or $logger->logdie("Couldn't chdir into $pfamseq_dir:[$!]");
     system ("esl-sfetch --index pfamseq") and $logger->logdie("Couldn't make easel indices for pfamseq:[$!]");
     $logger->debug("Making NCBI indices for pfamseq");
     system ("formatdb -p T -i pfamseq") and $logger->logdie("Couldn't make NCBI-blast indices for pfamseq:[$!]");
+    chdir("../") or $logger->logdie("Couldn't chdir up from $pfamseq_dir:[$!]");
     system("touch $statusdir/esl-indexes") and $logger->logdie("Couldn't touch $statusdir/esl-indexes:[$!]\n");
 }
 
-#copy pfamseq to nfs
-if ("$statusdir/moved_pfamseq"){
-    $logger->debug("Already moved pfamseq to nfs");
-} else {
-    $logger->info("Copying pfamseq to nfs directory");
-    my $pfamseq_nfs = $config->{pfamseq}->{location};
+##copy pfamseq to nfs
+#if ("$statusdir/moved_pfamseq"){
+#    $logger->debug("Already moved pfamseq to nfs");
+#} else {
+#    $logger->info("Copying pfamseq to nfs directory");
+#    my $pfamseq_nfs = $config->{pfamseq}->{location};
+# 
+#    unlink glob("$pfamseq_nfs/*") or $logger->logdie("Problem deleting files in $pfamseq_nfs");
+#    my @pfamseq_files = qw(pfamseq pfamseq.pal pfamseq.ssi);
+#    open(PAL, "$pfamseq_dir/pfamseq.pal") or $logger->logdie("Could not open $pfamseq_dir/pfamseq.pal:[$!]");
+#    while(<PAL>){
+#	if(/^DBLIST\s+(.*)/){
+#	    my $l = $1; #$l contains the list of virtual databases;
+#	    foreach my $bit (split(/\s+/, $l)){
+#		foreach my $ext (qw(phr pin psq)){
+#		    push(@pfamseq_files, $bit.".".$ext);
+#		}
+#	    }
+#	}
+#    }
+#    close(PAL);
+#
+#
+#    foreach my $f (@pfamseq_files) {i
+#	$logger->debug("Copying $f");
+#	copy("$pfamseq_dir/$f", "$pfamseq_nfs/$f") or $logger->logdie("Copy $pfamseq_dir/$f to $pfamseq_nfs failed: $!");
+#    }
+#    system("touch $statusdir/moved_pfamseq") and $logger->logdie("Couldn't touch $statusdir/moved_pfamseq:[$!]\n");
+#}
+#
+##update DBSIZE file
+#move ("$pfamseq_dir/DSIZE", "$pfamseq_dir/DBSIZE_preantifam") or die "Cannot move $pfamseq_dir/DBSIZE to $pfamseq_dir/DBSIZE_preantifam\n";
+#open (DBSIZE, ">$pfamseq_dir/DBSIZENEW") or die "Cannot open new $pfamseq_dir/DBSIZE file to write\n";
+#print DBSIZENEW "$total\n";
+#close DBSIZENEW;
 
-    unlink glob("$pfamseq_nfs/*") or $logger->logdie("Problem deleting files in $pfamseq_nfs");
-    my @pfamseq_files = qw(pfamseq pfamseq.pal pfamseq.ssi);
-    open(PAL, "pfamseq.pal") or $logger->logdie("Could not open pfamseq.pal:[$!]");
-    while(<PAL>){
-	if(/^DBLIST\s+(.*)/){
-	    my $l = $1; #$l contains the list of virtual databases;
-	    foreach my $bit (split(/\s+/, $l)){
-		foreach my $ext (qw(phr pin psq)){
-		    push(@pfamseq_files, $bit.".".$ext);
-		}
-	    }
-	}
-    }
-    close(PAL);
 
-
-    foreach my $f (@pfamseq_files) {
-	$logger->debug("Copying $f");
-	copy($f, $pfamseq_nfs."/".$f) or $logger->logdie("Copy $f to $pfamseq_nfs failed: $!");
-    }
-    system("touch $statusdir/moved_pfamseq") and $logger->logdie("Couldn't touch $statusdir/moved_pfmaseq:[$!]\n");
-}
-
-#update DBSIZE file
-move ("DSIZE", "DBSIZE_preantifam") or die "Cannot move file\n";
-open (DBSIZENEW, ">DBSIZE") or die "Cannot open new DBSIZE file to write\n";
-print DBSIZENEW "$total\n";
-close DBSIZENEW;
-
-chdir($pwd);
 
 sub help{
 
