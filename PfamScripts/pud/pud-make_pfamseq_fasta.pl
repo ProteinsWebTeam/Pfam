93732e270e41c9f9dc73ef0756f8757bf935aba5
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-06-12 15:59:09 +0000

    Made changing the dbsize in pfam config and moving pfamseq to production location an option

--- PfamScripts/pud/pud-make_pfamseq_fasta.pl
+++ PfamScripts/pud/pud-make_pfamseq_fasta.pl
@@ -1,16 +1,17 @@
 #!/usr/bin/env perl
 
 #make the pfamseq fasta file and SSI index
-
+#optionally move pfamseq fasta to production location and update pfam config with database size
 use strict;
 use warnings;
 use Getopt::Long;
 use Log::Log4perl qw(:easy);
 use File::Copy;
+use Cwd;
+use Config::General qw(SaveConfig);
 use Bio::Pfam::Config;
 use Bio::Pfam::PfamLiveDBManager;
 
-my ( $statusdir, $pfamseq_dir );
 
 #Start up the logger
 Log::Log4perl->easy_init();
@@ -21,117 +22,149 @@ my $config = Bio::Pfam::Config->new;
 my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamliveAdmin } );
 my $dbh = $pfamDB->getSchema->storage->dbh;
 
+#User options
+my ($status_dir, $pfamseq_dir, $move);
 &GetOptions(
-  "status_dir=s"  => \$statusdir,
-  "pfamseq_dir=s" => \$pfamseq_dir
-  )
-  or $logger->logdie("Invalid option!");
+  "status_dir=s"  => \$status_dir,
+  "pfamseq_dir=s" => \$pfamseq_dir,
+  "move"          => \$move);
 
-unless ( $statusdir and -e $statusdir ) {
+unless ( $status_dir and -e $status_dir ) {
   help();
 }
 unless ( $pfamseq_dir and -e $pfamseq_dir ) {
   help();
 }
+my $cwd = getcwd();
 
-#get DBSIZE file
-open(DB, "$pfamseq_dir/DBSIZE") or $logger->logdie("Could not open $pfamseq_dir/DBSIZE:[$!]");
-my $dbsize;
-while(<DB>){
-    ($dbsize) = $_ =~/(\S+)/;
-    last;
-}
-close DB;
 
-#get sequences from pfamseq
+#Create fasta file from pfamseq table
 #if this is too slow/fails it could be worth trying a smaller offset or sending it to the farm with plenty of memory
-my $offset = 0;
-my $n = 1;
-my $total = 0;
-open (FA, ">$pfamseq_dir/pfamseq") or $logger->logdie("Cannot open pfamseq file to write");
-while ($total < $dbsize){
-    $logger->debug("Querying database.... chunk $n offset $offset ");
+my $total=0;
+if(-s "$pfamseq_dir/pfamseq") {
+  $logger->debug("Already made pfamseq fasta file");
+  my $sth=$dbh->prepare("select count(*) from pfamseq") or $logger->logdie("Failed to prepare statement:".$dbh->errstr);
+  $sth->execute() or $logger->logdie("Couldn't execute statement ".$sth->errstr);
+  $total=$sth->fetchrow();
+}
+else {
+  $logger->debug("Going to make fasta file from pfamseq table");
+  my $offset = 0;
+  my $n = 1;
+  open (FA, ">$pfamseq_dir/pfamseq") or $logger->logdie("Cannot open $pfamseq_dir/pfamseq file to write");
+  while (1){
+    $logger->debug("Querying pfamseq table in database.... chunk $n offset $offset ");
     my $st = $dbh->prepare("select pfamseq_acc, seq_version, pfamseq_id, description, sequence from pfamseq limit 1000000 offset $offset") or $logger->logdie("Failed to prepare statement:".$dbh->errstr);
     $st->execute() or $logger->logdie("Couldn't execute statement ".$st->errstr);
     my $rowno = $st->rows;
-
+    last unless($rowno);
     my $array_ref = $st->fetchall_arrayref();
     $logger->debug("Fetching results...");
     foreach my $row (@$array_ref) {
       print FA ">" . $row->[0] . "." . $row->[1] . " " . $row->[2] . " " . $row->[3] . "\n" . $row->[4] . "\n";
     }
-    $offset = $offset + 1000000;
-    $total = $total + $rowno;
+    $offset += 1000000;
+    $total += $rowno;
     $n++;
     $logger->debug("$total rows retrieved");
+  }
+  close FA;
+  $dbh->disconnect;
 }
-close FA;
-$dbh->disconnect;
 
-#make easel indexes
-if(-e "$statusdir/esl-indexes") { 
-    $logger->debug("Already make easel indexes");
+my $dbsize=$total;
+
+#Make easel indexes
+if(-e "$status_dir/esl-indexes") { 
+  $logger->debug("Already make easel indexes");
 } else {
-    $logger->debug("Making easel indices for pfamseq");
-    chdir($pfamseq_dir) or $logger->logdie("Couldn't chdir into $pfamseq_dir:[$!]");
-    system ("esl-sfetch --index pfamseq") and $logger->logdie("Couldn't make easel indices for pfamseq:[$!]");
-    $logger->debug("Making NCBI indices for pfamseq");
-    system ("formatdb -p T -i pfamseq") and $logger->logdie("Couldn't make NCBI-blast indices for pfamseq:[$!]");
-    chdir("../") or $logger->logdie("Couldn't chdir up from $pfamseq_dir:[$!]");
-    system("touch $statusdir/esl-indexes") and $logger->logdie("Couldn't touch $statusdir/esl-indexes:[$!]\n");
+  $logger->debug("Making easel indices for pfamseq");
+  chdir($pfamseq_dir) or $logger->logdie("Couldn't chdir into $pfamseq_dir:[$!]");
+  system ("esl-sfetch --index pfamseq") and $logger->logdie("Couldn't make easel indices for pfamseq:[$!]");
+  chdir($cwd) or $logger->logdie("Couldn't chdir to $cwd:[$!]");
+  system("touch $status_dir/esl-indexes") and $logger->logdie("Couldn't touch $status_dir/esl-indexes:[$!]\n");
 }
 
-##copy pfamseq to nfs
-#if ("$statusdir/moved_pfamseq"){
-#    $logger->debug("Already moved pfamseq to nfs");
-#} else {
-#    $logger->info("Copying pfamseq to nfs directory");
-#    my $pfamseq_nfs = $config->{pfamseq}->{location};
-# 
-#    unlink glob("$pfamseq_nfs/*") or $logger->logdie("Problem deleting files in $pfamseq_nfs");
-#    my @pfamseq_files = qw(pfamseq pfamseq.pal pfamseq.ssi);
-#    open(PAL, "$pfamseq_dir/pfamseq.pal") or $logger->logdie("Could not open $pfamseq_dir/pfamseq.pal:[$!]");
-#    while(<PAL>){
-#	if(/^DBLIST\s+(.*)/){
-#	    my $l = $1; #$l contains the list of virtual databases;
-#	    foreach my $bit (split(/\s+/, $l)){
-#		foreach my $ext (qw(phr pin psq)){
-#		    push(@pfamseq_files, $bit.".".$ext);
-#		}
-#	    }
-#	}
-#    }
-#    close(PAL);
-#
-#
-#    foreach my $f (@pfamseq_files) {i
-#	$logger->debug("Copying $f");
-#	copy("$pfamseq_dir/$f", "$pfamseq_nfs/$f") or $logger->logdie("Copy $pfamseq_dir/$f to $pfamseq_nfs failed: $!");
-#    }
-#    system("touch $statusdir/moved_pfamseq") and $logger->logdie("Couldn't touch $statusdir/moved_pfamseq:[$!]\n");
-#}
-#
-##update DBSIZE file
-#move ("$pfamseq_dir/DSIZE", "$pfamseq_dir/DBSIZE_preantifam") or die "Cannot move $pfamseq_dir/DBSIZE to $pfamseq_dir/DBSIZE_preantifam\n";
-#open (DBSIZE, ">$pfamseq_dir/DBSIZENEW") or die "Cannot open new $pfamseq_dir/DBSIZE file to write\n";
-#print DBSIZENEW "$total\n";
-#close DBSIZENEW;
+#Copy pfamseq to production location
+if($move) { 
+  if (-e "$status_dir/moved_pfamseq"){
+    $logger->debug("Already moved pfamseq to nfs");
+  } 
+  else {
+    $logger->info("Copying pfamseq to nfs directory");
+    my $pfamseq_nfs = $config->{pfamseq}->{location};
+
+    my @pfamseq_files = qw(pfamseq pfamseq.ssi);
+
+    foreach my $f (@pfamseq_files) {
+      $logger->debug("Deleting old $f from $pfamseq_nfs");  
+      unlink("$pfamseq_nfs/$f");
+      $logger->debug("Copying new $f to $pfamseq_nfs");
+      copy("$pfamseq_dir/$f", "$pfamseq_nfs/$f") or $logger->logdie("Copy $pfamseq_dir/$f to $pfamseq_nfs failed: $!");
+    }
+    system("touch $status_dir/moved_pfamseq") and $logger->logdie("Couldn't touch $status_dir/moved_pfamseq:[$!]\n");
+  }
+
+  #Change PFAM_CONFIG
+  if(-e "$status_dir/changed_pfam_config") {
+    $logger->info("Already changed database size in Pfam config file\n");
+  }
+  else {
+    $logger->info("Updating database size in Pfam config file\n");
+
+    my $c = new Config::General($ENV{PFAM_CONFIG}); 
+    my %ac = $c->getall; 
+
+    my $pfam_config = $ENV{PFAM_CONFIG};
+    move($pfam_config, "$pfam_config.old") or $logger->logdie("Could not move config file");;
+    $ac{pfamseq}->{dbsize} = $dbsize; 
+
+    SaveConfig($pfam_config, \%ac);
+
+    my $newConfig;
+    eval { 
+      $newConfig = Bio::Pfam::Config->new; 
+
+    };  
+    if($@) { 
+      $logger->logdie("Problem modifying the pfam_config ($pfam_config) file");
+    }
+    $config = $newConfig;
+
+    system("touch $status_dir/changed_pfam_config") and $logger->logdie("Couldn't touch $status_dir/changed_pfam_config:[$!]\n"); 
+  }
+
+
+}
 
 
 
 sub help{
 
-print STDERR << "EOF";
+  print STDERR << "EOF";
 
-This script extracts sequences from pfamseq, writes a fasta file and easel indexes, and moves to nfs.
+This script creates a fasta file from the sequences in the pfamseq
+table in the database. There is an option to move the pfamseq fasta
+file to the production location in the Pfam config file, and update 
+the config file with the database size. If you are removing Anifam
+matches from the pfamseq table, you will need to run this script twice,
+once to create a fasta file to run the Antifam HMMs against, and once to
+create a fasta file once the Antifam sequences have been removed from
+the pfamseq table.
+
 Usage:
 
   $0 -status_dir <status_dir> -pfamseq_dir <pfamseq_dir>
 
+Options
+  -help  :Prints this help message
+  -move  :Move the pfamseq fasta file to the production location,
+          and update the database size in Pfam config file
+
 Both the status directory and pfamseq_directory must already exist.
-pfamseq_dir is the directory where the uniprot data will be downloaded
-to.  The status directory is where a log of the progress of the script
-is recorded.
+pfamseq_dir is the directory where the pfamseq fasta file will be
+generated. The status directory is where a log of the progress of 
+the script is recorded.
 
 EOF
 }
