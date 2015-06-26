65e66417caad4dd325ba5d47654ce885d3a040c9
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-06-26 13:16:47 +0000

    Use FastTree for families where RAxML is too slow

--- PfamScripts/merops/makeTree.pl
+++ PfamScripts/merops/makeTree.pl
@@ -2,6 +2,12 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
+
+my $family;
+
+GetOptions("family=s" => \$family);
+die "No family" unless($family);
 
 my $raxml="raxmlHPC-PTHREADS-SSE3";
 my $hmmerBin="/nfs/production/xfam/users/jaina/merops/bin";
@@ -18,10 +24,26 @@ unless(-s "seed.afa") {
   die "Failed to reformat SEED into aligned fasta\n";
 }
 
-#Make initial tree
-my $raxmlCmd="$raxml -T 4 -p12345 -m PROTGAMMAAUTO -s seed.afa -n seed";
-system("$raxmlCmd") and die "Failed to run '$raxmlCmd', $!";
+my @fastTree= qw(A01A C01A C19 C26 M12B S01A S08A S09C S12 S33 S54); #These families take too long to run using RAxML
+my %fastTree = map {($_, 1)} @fastTree;
 
-#Insert homologues into tree
-my $raxmlInsertCmd="$raxml -T 4 -p12345 -m PROTGAMMAAUTO -n insert -s homologues.afa -f v -t RAxML_bestTree.seed";
+my $seedTree;
+if(exists($fastTree{$family})) {
+  #Make initial Fasttree tree
+  my $fastTreeCmd="FastTree seed.afa > FastTree.seed";
+  system("$fastTreeCmd") and die "Failed to run '$fastTreeCmd', $!";
+
+  $seedTree="FastTree.seed";
+}
+else {
+  #Make initial RAxML tree
+  my $raxmlCmd="$raxml -T 4 -p12345 -m PROTGAMMAAUTO -s seed.afa -n seed";
+  system("$raxmlCmd") and die "Failed to run '$raxmlCmd', $!";
+
+  $seedTree="RAxML_bestTree.seed";
+}
+
+#Insert homologues into tree using  RAxML
+my $raxmlInsertCmd="$raxml -T 4 -p12345 -m PROTGAMMAAUTO -n insert -s homologues.afa -f v -t $seedTree";
 system("$raxmlInsertCmd") and die "Failed to run '$raxmlInsertCmd', $!";
+
