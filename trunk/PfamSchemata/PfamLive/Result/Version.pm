95b258b4dbb2d470ecae760ce31de0d2e4f63179
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-07-16 08:21:35 +0000

    Removed this line from bottom of module which was generating a warning: __PACKAGE__->set_primary_key( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);

--- trunk/PfamSchemata/PfamLive/Result/Version.pm
+++ trunk/PfamSchemata/PfamLive/Result/Version.pm
@@ -97,7 +97,7 @@ __PACKAGE__->add_columns(
 
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-06-24 10:28:21
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:3eMlpUTBVCsFKENSXxdiig
-__PACKAGE__->set_primary_key( qw/pfam_release swiss_prot_version trembl_version hmmer_version/);
+
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
