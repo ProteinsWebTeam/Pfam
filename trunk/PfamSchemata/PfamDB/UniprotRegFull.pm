5677968443098f90f4756e1dea0a14881efe1a4e
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2015-09-04 14:56:07 +0000

    New table

---
+++ trunk/PfamSchemata/PfamDB/UniprotRegFull.pm
@@ -0,0 +1,207 @@
+use utf8;
+package PfamDB::UniprotRegFull;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+PfamDB::UniprotRegFull
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<uniprot_reg_full>
+
+=cut
+
+__PACKAGE__->table("uniprot_reg_full");
+
+=head1 ACCESSORS
+
+=head2 auto_uniprot_reg_full
+
+  data_type: 'integer'
+  extra: {unsigned => 1}
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 pfama_acc
+
+  data_type: 'varchar'
+  is_foreign_key: 1
+  is_nullable: 0
+  size: 7
+
+=head2 uniprot_acc
+
+  data_type: 'varchar'
+  is_foreign_key: 1
+  is_nullable: 0
+  size: 10
+
+=head2 seq_start
+
+  data_type: 'mediumint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 seq_end
+
+  data_type: 'mediumint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 ali_start
+
+  data_type: 'mediumint'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 ali_end
+
+  data_type: 'mediumint'
+  extra: {unsigned => 1}
+  is_nullable: 0
+
+=head2 model_start
+
+  data_type: 'mediumint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 model_end
+
+  data_type: 'mediumint'
+  default_value: 0
+  is_nullable: 0
+
+=head2 domain_bits_score
+
+  data_type: 'double precision'
+  default_value: 0.00
+  is_nullable: 0
+  size: [8,2]
+
+=head2 domain_evalue_score
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 15
+
+=head2 sequence_bits_score
+
+  data_type: 'double precision'
+  default_value: 0.00
+  is_nullable: 0
+  size: [8,2]
+
+=head2 sequence_evalue_score
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 15
+
+=head2 in_full
+
+  data_type: 'tinyint'
+  default_value: 0
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "auto_uniprot_reg_full",
+  {
+    data_type => "integer",
+    extra => { unsigned => 1 },
+    is_auto_increment => 1,
+    is_nullable => 0,
+  },
+  "pfama_acc",
+  { data_type => "varchar", is_foreign_key => 1, is_nullable => 0, size => 7 },
+  "uniprot_acc",
+  { data_type => "varchar", is_foreign_key => 1, is_nullable => 0, size => 10 },
+  "seq_start",
+  { data_type => "mediumint", default_value => 0, is_nullable => 0 },
+  "seq_end",
+  { data_type => "mediumint", default_value => 0, is_nullable => 0 },
+  "ali_start",
+  { data_type => "mediumint", extra => { unsigned => 1 }, is_nullable => 0 },
+  "ali_end",
+  { data_type => "mediumint", extra => { unsigned => 1 }, is_nullable => 0 },
+  "model_start",
+  { data_type => "mediumint", default_value => 0, is_nullable => 0 },
+  "model_end",
+  { data_type => "mediumint", default_value => 0, is_nullable => 0 },
+  "domain_bits_score",
+  {
+    data_type => "double precision",
+    default_value => "0.00",
+    is_nullable => 0,
+    size => [8, 2],
+  },
+  "domain_evalue_score",
+  { data_type => "varchar", is_nullable => 0, size => 15 },
+  "sequence_bits_score",
+  {
+    data_type => "double precision",
+    default_value => "0.00",
+    is_nullable => 0,
+    size => [8, 2],
+  },
+  "sequence_evalue_score",
+  { data_type => "varchar", is_nullable => 0, size => 15 },
+  "in_full",
+  { data_type => "tinyint", default_value => 0, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</auto_uniprot_reg_full>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("auto_uniprot_reg_full");
+
+=head1 RELATIONS
+
+=head2 pfama_acc
+
+Type: belongs_to
+
+Related object: L<PfamDB::Pfama>
+
+=cut
+
+__PACKAGE__->belongs_to("pfama_acc", "PfamDB::Pfama", { pfama_acc => "pfama_acc" });
+
+=head2 uniprot_acc
+
+Type: belongs_to
+
+Related object: L<PfamDB::Uniprot>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "uniprot_acc",
+  "PfamDB::Uniprot",
+  { uniprot_acc => "uniprot_acc" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-09-04 15:54:40
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:p5U9v0KHFZdrWnU8bdkDRQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
