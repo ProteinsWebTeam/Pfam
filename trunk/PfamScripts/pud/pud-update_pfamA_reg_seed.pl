839ad7bc39f4d29305814c483a670e0811e79a93
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2016-02-05 13:53:37 +0000

    Updated queries such that accs are removed if they are deleted from pfamseq or uniprot tables (previously they were only removed if they were deleted from pfamseq AND uniprot)

--- trunk/PfamScripts/pud/pud-update_pfamA_reg_seed.pl
+++ trunk/PfamScripts/pud/pud-update_pfamA_reg_seed.pl
@@ -35,37 +35,34 @@ close MD5;
 
 
 #See which sequences from pfamA_reg_seed are not in the pfamseq table
-$logger->info("Seeing which sequences from pfamA_reg_seed are in the pfamseq table");
-my $pfamseq_sth=$dbh->prepare("select s.pfamseq_acc from pfamA_reg_seed s left join pfamseq p on p.pfamseq_acc=s.pfamseq_acc and p.md5=s.md5 and p.seq_version=s.seq_version where p.pfamseq_acc is null");
+$logger->info("Seeing which sequences from pfamA_reg_seed have been deleted from the pfamseq table");
+my $pfamseq_sth=$dbh->prepare("select s.pfamseq_acc from pfamA_reg_seed s left join pfamseq p on p.pfamseq_acc=s.pfamseq_acc and p.md5=s.md5 and p.seq_version=s.seq_version where source='pfamseq' and p.pfamseq_acc is null");
 $pfamseq_sth->execute() or $logger->logdie("Couldn't execute statement ".$pfamseq_sth->errstr);
 my ($pfamseq_acc);
 $pfamseq_sth->bind_columns(\$pfamseq_acc);
 
-my %notInPfamseq;
+my %delete;
 while ($pfamseq_sth->fetch()) {
-  $notInPfamseq{$pfamseq_acc}=1;
+  $delete{$pfamseq_acc}=1;
 }
 
 
 #See which sequences from pfamA_reg_seed are not in the uniprot table
-$logger->info("Seeing which sequences from pfamA_reg_seed are in the uniprot table");
-my $uniprot_sth=$dbh->prepare("select s.pfamseq_acc from pfamA_reg_seed s left join uniprot u on u.uniprot_acc=s.pfamseq_acc and u.md5=s.md5 and u.seq_version=s.seq_version where u.uniprot_acc is null");
+$logger->info("Seeing which sequences from pfamA_reg_seed have been deleted from the uniprot table");
+my $uniprot_sth=$dbh->prepare("select s.pfamseq_acc from pfamA_reg_seed s left join uniprot u on u.uniprot_acc=s.pfamseq_acc and u.md5=s.md5 and u.seq_version=s.seq_version where source='uniprot' and u.uniprot_acc is null");
 $uniprot_sth->execute() or $logger->logdie("Couldn't execute statement ".$uniprot_sth->errstr);
 my ($uniprot_acc);
 $uniprot_sth->bind_columns(\$uniprot_acc);
 
-my %delete;
 while ($uniprot_sth->fetch()) {
-  if(exists($notInPfamseq{$uniprot_acc})) {
-    $delete{$uniprot_acc}=1; #It isn't in pfamseq or uniprot, so need to delete from pfamA_reg_seed
-  }
+  $delete{$uniprot_acc}=1; 
 }
 
 my $num_seq = keys %delete;
 $num_seq=0 unless($num_seq);
 
 #Delete sequences from pfamA_reg_seed that are not in the pfamseq or uniprot tables
-$logger->info("Deleting the $num_seq accessions from pfamA_reg_seed that are not in the pfamseq or uniprot tables");
+$logger->info("Deleting the $num_seq accessions from pfamA_reg_seed that have been deleted from the pfamseq or uniprot tables");
 my $delete_sth=$dbh->prepare("delete from pfamA_reg_seed where pfamseq_acc=?");
 foreach my $acc (keys %delete) {
   $delete_sth->execute($acc) or $logger->logdie("Couldn't execute statement ".$delete_sth->errstr);
