ae0f07a19e32d2d7d6d606b758c8db348332911e
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2016-07-01 12:02:58 +0000

    Updated clan query for database changes Tidied the code and added a header to the alldata file

--- trunk/PfamScripts/make/jackhmmer_summary.pl
+++ trunk/PfamScripts/make/jackhmmer_summary.pl
@@ -14,6 +14,13 @@ use Log::Log4perl qw(:easy);
 #
 #seq_id  totalseq:     <num seq>  overlaps:      <num overlaps>  Gain:      <gain>  Frac gained: <frac gained> <List of overlapping fam>
 #
+#totalseq=number of sequences (not domains) in the align file
+#overlaps=number of sequences (not domains) in the overlaps file
+#gain=total_seq-overlaps
+#frac gained=gain/totalseq*100
+#
+#The list of overlapping families/clans is given if overlaps are present
+#
 #The script concatenates all these files together in a file
 #called alldata in the cwd.  It is intended for use after running
 #the proteome_jackhmmer.pl script.
@@ -29,8 +36,8 @@ my $directory;
 &GetOptions ( "directory=s"  => \$directory);
 
 unless($directory) {
-    print STDERR "Need to specify directory (-directoy <dir>) to work from\n";
-    &help;
+  print STDERR "Need to specify directory (-directoy <dir>) to work from\n";
+  &help;
 }
 
 
@@ -46,91 +53,102 @@ closedir DIR;
 
 #Loopt through dir and write file
 foreach my $dir (@dirs) {
-    my $full_dir = "$directory/$dir";   
+  my $full_dir = "$directory/$dir";   
 
-    next unless(-d $full_dir);
-    next if(-s "$full_dir/summary.txt");
-    next if($full_dir =~ /Done$/);
-   
-    unless(-s "$full_dir/align") {
-	$logger->info("Skipping $full_dir (no align file)");
-	next;
+  next unless(-d $full_dir);
+  next if(-s "$full_dir/summary.txt");
+  next if($full_dir =~ /Done$/);
+
+  unless(-s "$full_dir/align") {
+    $logger->info("Skipping $full_dir (no align file)");
+    next;
+  }
+
+  $logger->info("Doing $full_dir");
+
+  open(ALN, "$full_dir/align") or $logger->logdie("Couldn't open $full_dir/align, $!");
+  my %align;
+
+  while(<ALN>) {
+    if(/^(\S+)\//) {
+      $align{$1}=1;
     }
+  }
+  close ALN;
 
-    $logger->info("Doing $full_dir");
+  my $align = keys %align;
 
-    open(ALN, "$full_dir/align") or $logger->logdie("Couldn't open $full_dir/align, $!");
-    my %align;
+  next if($align eq 0); #No homologues found
 
-    while(<ALN>) {
-	if(/^(\S+)\//) {
-	    $align{$1}=1;
-	}
+  open(OVERLAP, "$full_dir/overlap") or $logger->logdie("Couldn't open $full_dir/overlap, $!");
+  my %overlap;
+  my %clan;
+  my %fam;
+  my %tmp;
+  while(<OVERLAP>) {
+
+    if(/\S+\s+\[(\S+)\]\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/) {
+      my ($seq, $fam) = ($1, $2);
+
+      $overlap{$seq}=1;
+      next if(exists($tmp{"$seq:$fam"})); #Don't want to count same overlapping seq twice for a family
+      $fam{$fam}++;
+      $tmp{"$seq:$fam"}=1;
     }
-    close ALN;
+  }
+  close OVERLAP;
 
-    my $align = keys %align;
+  foreach my $f (keys %fam) {
+    my $c = $fam2clan{$f};
+    $c = "No_clan" unless($c);
+    $clan{$c} .= " $f($fam{$f})";
+  }
 
-    next if($align eq 0); #No homologues found
+  my $clan_fam;
+  foreach my $clan (keys %clan) {
+    $clan_fam .= $clan . ":" . $clan{$clan} . ", ";
+  }
 
-    open(OVERLAP, "$full_dir/overlap") or $logger->logdie("Couldn't open $full_dir/overlap, $!");
-    my %overlap;
-    my %clan;
-    my %fam;
-    my %tmp;
-    while(<OVERLAP>) {
+  if($clan_fam) {
+    chop $clan_fam;#Remove final ", "
+    chop $clan_fam;
+  }
 
-	if(/\S+\s+\[(\S+)\]\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+(\S+)/) {
-	    my ($seq, $fam) = ($1, $2);
+  my $overlap = keys %overlap;
 
-	    $overlap{$seq}=1;
-	    next if(exists($tmp{"$seq:$fam"})); #Don't want to count same overlapping seq twice for a family
-	    $fam{$fam}++;
-	    $tmp{"$seq:$fam"}=1;
-	}
-    }
-    close OVERLAP;
-    
-    foreach my $f (keys %fam) {
-	my $c = $fam2clan{$f};
-	$c = "No_clan" unless($c);
-	$clan{$c} .= " $f($fam{$f})";
-    }
+  my $gain = $align - $overlap;
 
-    my $clan_fam;
-    foreach my $clan (keys %clan) {
-	$clan_fam .= $clan . ":" . $clan{$clan} . ", ";
-    }
-
-    if($clan_fam) {
-	chop $clan_fam;#Remove final ", "
-	chop $clan_fam;
-    }
-	
-    my $overlap = keys %overlap;
-    
-    my $gain = $align - $overlap;
-
-    my $frac_gain = $gain/$align;
-    $frac_gain = sprintf("%.2f", $frac_gain);
+  my $frac_gain = $gain/$align;
+  $frac_gain = sprintf("%.2f", $frac_gain);
 
 
-    open(OUT, ">$full_dir/summary.txt") or $logger->logdie("Couldn't open fh to $full_dir/summary.txt, $!");
+  open(OUT, ">$full_dir/summary.txt") or $logger->logdie("Couldn't open fh to $full_dir/summary.txt, $!");
 
-    print OUT sprintf ("%6s totalseq:%7s  overlaps:%7s  Gain: %7s  Frac gained: %4s  ", $dir, $align, $overlap, $gain, $frac_gain);
+  print OUT sprintf ("%-10s totalseq:%7s  overlaps:%7s  Gain: %7s  Frac gained: %4s  ", $dir, $align, $overlap, $gain, $frac_gain);
+  
+  if($clan_fam) {
+    print OUT "$clan_fam\n";
+  }
+  else {
+    print OUT "\n";
+  }
 
-    if($clan_fam) {
-	print OUT "$clan_fam\n";
-    }
-    else {
-	print OUT "\n";
-    }
-
-    close OUT;
+  close OUT;
 }
 
+#Print header 
+open(ALL, ">alldata") or die "Coudln't open fh to alldata, $!";
+print ALL "#File is sorted by overlaps, then by number gained\n";
+print ALL "Only squences with at least one match in the sequence database are included in this file\n";
+print ALL "#totalseq=number of sequences (not domains) in the align file\n";
+print ALL "#overlaps=number of sequences (not domains) in the overlaps file\n";
+print ALL "#gain=total_seq-overlaps\n";
+print ALL "#frac gained=gain/totalseq\n";
+print ALL "#Overlapping clans and families are given at the end of the line (if any)\n";
+close ALL;
+
 #Sort by overlaps, then by number gained
-system("cat $directory/*/summary.txt | sort -k5n -k7nr > alldata") and die "Couldnt run |cat $directory/*/summary.txt | sort -k5n -k7nr > alldata| $!";
+system("cat $directory/*/summary.txt | sort -k5n -k7nr >> alldata") and die "Couldnt run |cat $directory/*/summary.txt | sort -k5n -k7nr >> alldata| $!";
 
 
 
@@ -138,27 +156,27 @@ system("cat $directory/*/summary.txt | sort -k5n -k7nr > alldata") and die "Coul
 
 sub clan_mapping {
 
-    my $hash = shift;
-    $logger->info("Getting clan membership from rdb");
-    my $config = Bio::Pfam::Config->new;
+  my $hash = shift;
+  $logger->info("Getting clan membership from rdb");
+  my $config = Bio::Pfam::Config->new;
 
-    my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamlive } );
+  my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamlive } );
 
-    my $array_ref = $pfamDB->getAllClanData;
-    
-    foreach my $clan (@$array_ref){
-        my $membership_ref = $pfamDB->getClanMembership($clan->clan_acc);
-        foreach my $pfamA (@$membership_ref){               
-            $$hash{$pfamA->auto_pfama->pfama_id}=$clan->clan_acc;
-        }
+  my $array_ref = $pfamDB->getAllClanData;
+
+  foreach my $clan (@$array_ref){
+    my $membership_ref = $pfamDB->getClanMembership($clan->clan_acc);
+    foreach my $pfamA (@$membership_ref){               
+      $hash->{$pfamA->pfama_acc->pfama_id}=$clan->clan_acc;
     }
-    $logger->info("Got clan membership");
+  }
+  $logger->info("Got clan membership");
 }
 
 
 sub help {
 
-print STDERR << "EOF";
+  print STDERR << "EOF";
 
 This program should be run after proteome_jackhmmer.pl.  It loops over
 each directory of Jackhmmer searches and creates a file called
@@ -174,5 +192,5 @@ Usage:
 
 EOF
 
-exit (0);
+  exit (0);
 }
