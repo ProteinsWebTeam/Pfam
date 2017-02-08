f5d78978662ab8ed7bd8401f92c5178c4885dada
apetrov <apetrov@98333f76-0502-11df-81af-936bbf3a1e26>
2017-02-08 12:22:02 +0000

    Use custom user-agent for Wiki requests

--- trunk/PfamLib/Bio/Pfam/Wiki/Updater.pm
+++ trunk/PfamLib/Bio/Pfam/Wiki/Updater.pm
@@ -201,7 +201,9 @@ has '_mw_bot' => (
   is      => 'ro',
   isa     => 'MediaWiki::Bot',
   lazy    => 1,
-  default => sub { return MediaWiki::Bot->new; }
+  default => sub { return MediaWiki::Bot->new({
+    agent => 'RfamPfamBot/1.1 (http://rfam.xfam.org; http://pfam.xfam.org)'
+  }); }
 );
 
 #-------------------------------------------------------------------------------
