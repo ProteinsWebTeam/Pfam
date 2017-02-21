9fd744cb9306189cb0a8e67aa0e444aecabff0f0
jaina <jaina@98333f76-0502-11df-81af-936bbf3a1e26>
2017-02-21 13:49:03 +0000

    New script

---
+++ PfamScripts/release/MD5checksum.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+#Script to generate md5 checksums for all files in a particular dir
+#md5 checksums are written to a file in the cwd called md5_checksums
+#Need to pass dir on the command line
+#Run this on the ftp dir, the database_files and the proteomes dir
+
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Slurp;
+
+
+
+my ($dir);
+GetOptions('dir=s'  => \$dir);
+
+unless(-d $dir) {
+  print STDERR "Need to pass the directory containing all the files on the command line\n";
+  print STDERR "E.g. $0 -dir <dir_location>\n";
+  exit;
+}
+
+my @files = read_dir($dir);
+my @sorted_files = sort @files;
+my $outfile = "md5_checksums";
+if(-s $outfile) {
+  unlink($outfile);
+}
+
+foreach my $file (@sorted_files) {
+  next if(-d $file);
+  print STDERR "Doing $file\n";
+  system("md5sum $file >> $outfile") and die "Problem running 'md5sum $file >> $outfile', $!";
+}
+
