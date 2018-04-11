8c928c1037a8bc654c8f949f5428afdcb2d4f5f8
jaina <jaina@ebi.ac.uk>
2018-04-11 13:27:36 +0100

    Added author and pfamA_author to list of tables to be copied

--- PfamScripts/pud/pud-cloneDB.pl
+++ PfamScripts/pud/pud-cloneDB.pl
@@ -68,7 +68,7 @@ if ($data) {
                     _lock clan_database_links clan_lit_ref clan_membership
                     dead_clan dead_family pfamA_database_links pfamA_literature_reference
                     pfamA_wiki current_pfam_version nested_domains version pfamA_reg_seed 
-                    released_pfam_version released_clan_version);
+                    released_pfam_version released_clan_version author pfamA_author);
 
     foreach my $table (@tables) {
         if (my @matched_cols = columns_matched($table)) {
@@ -89,7 +89,8 @@ if ($check) {
     my @tables = qw(pfamA evidence markup_key wikipedia pfamseq clan literature_reference
                     _lock clan_database_links clan_lit_ref clan_membership clan_wiki
                     dead_clan dead_family pfamA_database_links pfamA_literature_reference
-                    pfamA_wiki current_pfam_version nested_domains version pfamA_reg_seed);
+                    pfamA_wiki current_pfam_version nested_domains version pfamA_reg_seed
+                    author pfamA_author);
 
     foreach my $table (@tables) {
         print "Table $table: ";
