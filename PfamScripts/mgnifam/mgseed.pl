c584793a6630a77b6972ce7837412e26a7f6f4b8
jaina <jaina@ebi.ac.uk>
2019-04-11 12:31:56 +0100

    Script to make alignments from a mgnify cluster

---
+++ PfamScripts/mgnifam/mgseed.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+my $cluster_name;
+GetOptions( "cluster=s" => \$cluster_name);
+
+#Check cluster name seems sensible
+unless($cluster_name and $cluster_name =~ /^MGYP\d{12}$/) {
+  print STDERR "Need to specify a valid cluster name on the command line\n";
+  print STDERR "E.g. $0 -cluster MGYP000001829837\n";
+  exit;
+}
+
+#Create dir
+mkdir($cluster_name, 0775) or die "Couldn't mkdir $cluster_name, $!";
+chdir($cluster_name) or die "Couldn't chdir into $cluster_name, $!";
+
+#Set memory for farm
+my $memory_gb = 4;
+my $memory_mb = $memory_gb*1000;
+
+#Submit job to farm
+system("bsub -q production-rh7 -o seed.log -J$cluster_name -M $memory_mb -R \"rusage[mem=$memory_mb]\" /homes/jaina/Code/Pfam/PfamScripts/mgnifam/create_seed_from_cluster.pl -cluster $cluster_name");
+chdir("../") or die "Coudln't changde dir up from $cluster_name, $!";
