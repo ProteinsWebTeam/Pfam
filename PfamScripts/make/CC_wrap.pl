bfe70b158a7b3f4af222395c604381dfa9a46108
jaina <jaina@ebi.ac.uk>
2020-10-06 11:33:11 +0100

    Deal with spaces at the end of lines correctly

--- PfamScripts/make/CC_wrap.pl
+++ PfamScripts/make/CC_wrap.pl
@@ -12,7 +12,7 @@ my $desc_file = "DESC";
 my $tmp_desc = "DESC.b4.CCedit.$$";
 
 unless(-s $desc_file) {
-  print "Can't find DESC file in cwd\n";
+    print "Can't find DESC file in cwd\n";
 }
 
 rename($desc_file, $tmp_desc) or die "Couldn't rename $desc_file to $tmp_desc, $!";
@@ -22,26 +22,28 @@ my $found_CC;
 open(TMP, $tmp_desc) or die "Couldn't open $tmp_desc, $!";
 open(DESC, ">$desc_file") or die "Couldn't open $desc_file for writing, $!";
 while(<TMP>) {
-  if(/^CC\s+(.+)/) {
-    $CC_line .=$1;
-    $found_CC=1;
-  }
-  else {
-    if($found_CC) {
-      my $text = wrap( "CC   ", "CC   ", $CC_line . "\n" );
-      print DESC "$text";
-      $found_CC="";
-      print DESC $_;
+    if(/^CC\s+(.+)/) {
+        $CC_line .= $1." ";
+        $found_CC=1;
     }
     else {
-      print DESC $_;
+        if($found_CC) {
+            $CC_line =~ s/ +/ /g; #Remove any double spaces
+            my $text = wrap( "CC   ", "CC   ", $CC_line . "\n" );
+            print DESC "$text";
+            $found_CC="";
+            print DESC $_;
+        }
+        else {
+            print DESC $_;
+        }
     }
-  }
 }
 
 if($found_CC) {
-  my $text = wrap( "CC   ", "CC   ", $CC_line . "\n" );
-  print DESC "$text";
+    $CC_line =~ s/ +/ /g; #Remove any double spaces
+    my $text = wrap( "CC   ", "CC   ", $CC_line . "\n" );
+    print DESC "$text";
 }
 
 close TMP;
