5d6284b8b7bdfd0a87d0c37e549fef01e3560227
jaina <jaina@ebi.ac.uk>
2020-12-15 15:16:46 +0000

    Added --quick to the mysqldump commands so it doesn't need to read a whole table into memory

--- PfamScripts/pud/pud-pfam_release_copy.pl
+++ PfamScripts/pud/pud-pfam_release_copy.pl
@@ -76,7 +76,7 @@ else {
     }
 
     $logger->info("Doing $table");
-    my $mysqldump_command = "mysqldump --max_allowed_packet=1024M --routines -uadmin -P".$pfamDB_live->{port}." -h".$pfamDB_live->{host}." -p".$pfamDB_live->{password}." ".$pfamDB_live->{database}." $table > $pfam_live_dir/$table.txt";
+    my $mysqldump_command = "mysqldump --max_allowed_packet=1024M --routines --quick -uadmin -P".$pfamDB_live->{port}." -h".$pfamDB_live->{host}." -p".$pfamDB_live->{password}." ".$pfamDB_live->{database}." $table > $pfam_live_dir/$table.txt";
     system($mysqldump_command) and $logger->logdie("Couldn't run '$mysqldump_command, $!");
   }
   close TABLES; 
@@ -93,11 +93,11 @@ system($drop_db_command);
 
 #Copy data from pfam_release into empty pfam_live
 #Tables need to be imported in following order to satisfy FK relationships
-my @tables=qw(ncbi_taxonomy taxonomy pfamA evidence uniprot markup_key wikipedia pfamseq pfamseq_markup pfamseq_disulphide pfamseq_antifam secondary_pfamseq_acc pfamA_reg_seed clan literature_reference _lock clan_database_links clan_lit_ref clan_membership dead_clan dead_family pfamA_database_links pfamA_literature_reference pfamA_wiki current_pfam_version nested_domains version _active_site_hmm_positions  alignment_and_tree architecture clan_alignment_and_relationship clan_architecture complete_proteomes edits gene_ontology interpro nested_locations other_reg pdb pdb_image pdb_pfamA_reg pdb_residue_data pfamA2pfamA_hhsearch pfamA2pfamA_scoop pfamA_architecture pfamA_fasta pfamA_HMM pfamA_interactions _pfamA_internal pfamA_ncbi pfam_annseq pfamA_reg_full_insignificant pfamA_reg_full_significant uniprot_reg_full pfamA_species_tree pfamA_tax_depth proteome_architecture proteome_regions released_clan_version released_pfam_version pfamA_ncbi_uniprot author pfamA_author);
+my @tables=qw(ncbi_taxonomy taxonomy pfamA evidence uniprot markup_key wikipedia pfamseq pfamseq_markup pfamseq_disulphide pfamseq_antifam secondary_pfamseq_acc pfamA_reg_seed clan literature_reference _lock clan_database_links clan_lit_ref clan_membership dead_clan dead_family pfamA_database_links pfamA_literature_reference pfamA_wiki current_pfam_version nested_domains version _active_site_hmm_positions  alignment_and_tree architecture clan_alignment_and_relationship clan_architecture complete_proteomes edits gene_ontology interpro nested_locations other_reg pdb pdb_image pdb_pfamA_reg pdb_residue_data pfamA2pfamA_hhsearch pfamA2pfamA_scoop pfamA_architecture pfamA_fasta pfamA_HMM pfamA_interactions _pfamA_internal pfamA_ncbi pfam_annseq pfamA_reg_full_insignificant pfamA_reg_full_significant uniprot_reg_full pfamA_species_tree pfamA_tax_depth proteome_architecture proteome_regions released_clan_version released_pfam_version pfamA_ncbi_uniprot author sequence_ontology pfamA_author);
 
 foreach my $table (@tables) {
   $logger->info("Copying $table");
-  my $copy_command="mysqldump --max_allowed_packet=1024M --routines -h".$pfamDB_release->{host}." -u ".$pfamDB_release->{user}." -P".$pfamDB_release->{port}." -p".$pfamDB_release->{password}." ".$pfamDB_release->{database}." $table | ";
+  my $copy_command="mysqldump --max_allowed_packet=1024M --routines --quick -h".$pfamDB_release->{host}." -u ".$pfamDB_release->{user}." -P".$pfamDB_release->{port}." -p".$pfamDB_release->{password}." ".$pfamDB_release->{database}." $table | ";
   $copy_command.="mysql --max_allowed_packet=1024M -h ".$pfamDB_live->{host}." -u ".$pfamDB_live->{user}." -p". $pfamDB_live->{password}." -P ".$pfamDB_live->{port}." ".$pfamDB_live->{database};
 
   system($copy_command) and $logger->logdie("Couldn't run $copy_command, $!");
