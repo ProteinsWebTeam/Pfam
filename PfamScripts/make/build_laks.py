fa575ba8f14e7f517ae2e0d1aa165a37f8eb5eb6
jaina <jaina@ebi.ac.uk>
2021-11-16 13:13:50 +0000

    First commit

---
+++ PfamScripts/make/build_laks.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import os
+from shutil import copy2
+import sys
+
+
+alns_filepath = None
+
+#Check directory containging the alignments is supplied on the command line
+if len(sys.argv) == 1:
+    sys.exit(f"Need to pass directory containing alignments on the command line\nE.g. {sys.argv[0]} <dir>\n")
+else:
+    alns_filepath = sys.argv[1]
+
+if not os.path.isdir(alns_filepath):
+    sys.exit(f"[{alns_filepath}] does not exist")
+
+
+#Get a list of files in the directory
+files = os.listdir(alns_filepath)
+
+#Loop through each alignment, create a new dir and run liftover_alignment, create_alignment, pfbuild and pqc-overlap-rdb on each one
+for filename in files: #EAD9.fa
+  name, ext = os.path.splitext(filename) #EAD9, fa
+
+  fasta = os.path.join(alns_filepath, filename) #/hps/nobackup/production/xfam/jaina/Laks/alignments/EAD9.fa
+
+  #Make dir and cp fasta across
+  os.mkdir(name)
+  copy2(fasta, name)
+  os.chdir(name)
+
+  #Reformat afa to pfam format, then use grep to remove STOCKHOLM tag, blank lines and // lines
+  os.system(f"esl-reformat pfam {filename}  | grep -v '^#\|^\s*$\|\/\/' > seed");
+
+  #Submit liftover_alignment, create_alignment, pfbuild and pqc-overlap-rdb to farm
+  os.system(f"bsub -q production-rh74 -M 5000 -R \"rusage[mem=5000]\" -o farm.log -J{name} 'liftover_alignment.pl -local -align seed && create_alignment.pl -fasta seed.phmmer -mu > SEED && pfbuild -withpfmake -local && cd .. && pqc-overlap-rdb {name} '");
+
+  os.chdir('../')
+
