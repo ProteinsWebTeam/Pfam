9d524a634372396e7530ed4999660bcad73de522
Tiago Grego <tgrego@ebi.ac.uk>
2024-03-25 13:39:56 +0000

    make_pfamC svn-independent

--- PfamScripts/release/make_pfamC.pl
+++ PfamScripts/release/make_pfamC.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 
 use Bio::Pfam::Config;
-use Bio::Pfam::SVN::Client;
 use Bio::Pfam::ClanIO;
 use Bio::Pfam::PfamLiveDBManager;
 use Log::Log4perl qw(:easy);
@@ -17,35 +16,31 @@ my $config = Bio::Pfam::Config->new;
 
 
 #Can we connect to the PfamLive database?
-$logger->info("Going to connect to the live database\n");
+$logger->info("Going to connect to the " . $config->pfamlive->{database} . " database\n");
 
 my $pfamDB = Bio::Pfam::PfamLiveDBManager->new( %{ $config->pfamlive } );
 
 open(P, ">Pfam-C");
 
 my $clans = $pfamDB->getAllClanData;
-foreach my $clan (sort{$a->clan_id cmp $b->clan_id} @$clans){
-  #Check that family exists in svn
-  my $client = Bio::Pfam::SVN::Client->new;
-  $client->checkClanExists($clan->clan_acc);
-  my $T;
-  open($T, ">$$.tmp") or $logger->logdie("Could not open $$.tmp for writing");
-  $client->catFile( $clan->clan_acc, "CLANDESC", $T );
-  close($T);
-  
-  
+foreach my $clan (sort{$a->clan_id cmp $b->clan_id} @$clans) {
+  my $clanIO = Bio::Pfam::ClanIO->new;
+  my $clan_obj = $clanIO->loadClanFromRDB($clan->clan_acc, $pfamDB);
+
+  $clanIO->writeCLANDESC($clan_obj->DESC, ".", "$$.tmp");
+
   open(D, "$$.tmp")  or $logger->logdie("Could not open $$.tmp for writing");
   print P "# STOCKHOLM 1.0\n";
-  
+
   while(<D>){
     if(/^AC   CL\d+/){
-      print P "#=GF AC   ".$clan->clan_acc.".".$clan->version."\n";   
+      print P "#=GF AC   ".$clan->clan_acc.".".$clan->version."\n";
     }else{
-      print P "#=GF $_";  
-    }  
+      print P "#=GF $_";
+    }
   }
   close(D);
-  print P "//\n";  
+  print P "//\n";
   unlink("$$.tmp");
 }
 close(P);
